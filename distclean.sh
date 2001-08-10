8dc064d2296902f01afd9107e89a81146e3771fe
William Harold Newman <william.newman@airmail.net>
2001-08-10 14:43:17 +0000

    0.pre7.8: 	(The old CMU CL documentation is now available from 		<ftp://sbcl.sourceforge.net/pub/sbcl/cmucl-docs.tar.bz2>.) 	tweaked docs/.cvsignore so that if you like keeping the old 		CMU CL documentation in the old place, CVS doesn't 		pester you about it

--- distclean.sh
+++ distclean.sh
@@ -4,5 +4,13 @@
 # generated files but other things (e.g. customization files)
 # which shouldn't be in the distribution
 
-rm customize-target-features.lisp
+# It's not unheard of to copy the old CMU CL docs into their
+# traditional sbcl-0.6.x place. If so, it shouldn't end up in
+# the distribution.
+rm -rf doc/cmucl
+
+# miscellaneous other customizations which shouldn't be propagated
+rm -rf customize-target-features.lisp
+
+# Fall through to ordinary cleanup.
 sh clean.sh
