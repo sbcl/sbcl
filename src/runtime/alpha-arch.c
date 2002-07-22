92dcf81a15c2b2b021d66d6d3e5545b9c150f250
Christophe Rhodes <csr21@cam.ac.uk>
2002-07-22 17:17:48 +0000

    0.7.5.21: 	Complilation fix for Alpha/Linux (from DB, fixing CSR bogosity)

--- src/runtime/alpha-arch.c
+++ src/runtime/alpha-arch.c
@@ -279,14 +279,14 @@ static void
 sigtrap_handler(int signal, siginfo_t *siginfo, os_context_t *context)
 {
     unsigned int code;
-
+    sigset_t *mask;
 #ifdef LISP_FEATURE_LINUX
     os_restore_fp_control(context);
 #endif
 
     /* Don't disallow recursive breakpoint traps.  Otherwise, we can't */
     /* use debugger breakpoints anywhere in here. */
-    sigset_t *mask=(os_context_sigmask_addr(context));
+    mask=(os_context_sigmask_addr(context));
     sigsetmask(mask); 
 
     /* this is different from how CMUCL does it.  CMUCL used "call_pal
