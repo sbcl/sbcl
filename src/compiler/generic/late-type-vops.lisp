7ce4dc62a7b03590b127bb950738e771e33341a7
Christophe Rhodes <csr21@cam.ac.uk>
2002-09-12 15:11:33 +0000

    0.7.7.20-backend-cleanup-1.9: 	Really delete the PPC dead code (was commented out before) 	... deletia 	Add some boilerplate to various files.

--- src/compiler/generic/late-type-vops.lisp
+++ src/compiler/generic/late-type-vops.lisp
@@ -1,5 +1,15 @@
-(in-package "SB!VM")
+;;;; generic type testing and checking VOPs
 
+;;;; This software is part of the SBCL system. See the README file for
+;;;; more information.
+;;;;
+;;;; This software is derived from the CMU CL system, which was
+;;;; written at Carnegie Mellon University and released into the
+;;;; public domain. The software is in the public domain and is
+;;;; provided with absolutely no warranty. See the COPYING and CREDITS
+;;;; files for more information.
+(in-package "SB!VM")
+
 (!define-type-vops fixnump check-fixnum fixnum object-not-fixnum-error
   (even-fixnum-lowtag odd-fixnum-lowtag)
   ;; we can save a register on the x86.
