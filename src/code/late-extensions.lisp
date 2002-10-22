c41d75f1d2defd6234e644ef2b40440a5d1526c5
William Harold Newman <william.newman@airmail.net>
2002-10-22 13:26:10 +0000

    0.7.8.53: 	new BUGS entry 	added draft of LIST-LENGTH-OR-DIE (commented out) since I was 		at one point thinking of actually addressing CSR's 		point about sequence function arg checking

--- src/code/late-extensions.lisp
+++ src/code/late-extensions.lisp
@@ -19,3 +19,19 @@
 ;;; not improper and which is not circular?
 (defun list-with-length-p (x)
   (values (ignore-errors (list-length x))))
+
+;;; not used in 0.7.8, but possibly useful for defensive programming 
+;;; in e.g. (COERCE ... 'VECTOR)
+;;;(defun list-length-or-die (x)
+;;;  (or (list-length x)
+;;;      ;; not clear how to do this best:
+;;;      ;;   * Should this be a TYPE-ERROR? Colloquially that'd make 
+;;;      ;;     lots of sense, but since I'm not sure how to express
+;;;      ;;     "noncircular list" as a Lisp type expression, coding
+;;;      ;;     it seems awkward.
+;;;      ;;   * Should the ERROR object include the offending value?
+;;;      ;;     Ordinarily that's helpful, but if the user doesn't have
+;;;      ;;     his printer set up to deal with cyclicity, we might not
+;;;      ;;     be doing him a favor by printing the object here.
+;;;      ;; -- WHN 2002-10-19
+;;;      (error "can't calculate length of cyclic list")))
