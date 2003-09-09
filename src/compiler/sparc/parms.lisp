cc56ee05b7ef035da9116a2117280087f58c8e94
Christophe Rhodes <csr21@cam.ac.uk>
2003-09-09 16:57:46 +0000

    0.8.3.45.modular3: 	Implement (I think) modular functions on sparc 	... as usual in this kind of thing, an untested last-minute 		modification creeps in, because I broke LOGEQV in 		an obvious way.  Will test the modification before this 		branch gets near HEAD.

--- src/compiler/sparc/parms.lisp
+++ src/compiler/sparc/parms.lisp
@@ -228,7 +228,7 @@
   '(length
     two-arg-+ two-arg-- two-arg-* two-arg-/ two-arg-< two-arg-> two-arg-=
     two-arg-<= two-arg->= two-arg-/= eql %negate
-    two-arg-and two-arg-ior two-arg-xor
+    two-arg-and two-arg-ior two-arg-xor two-arg-eqv
     two-arg-gcd two-arg-lcm
     ))
 
