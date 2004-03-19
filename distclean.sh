97a61eb7026c6b2e988f7cc0346b007afcfedef8
William Harold Newman <william.newman@airmail.net>
2004-03-19 19:51:26 +0000

    0.8.8.32: 	Make distclean.sh delete CVS/ subdirectories.

--- distclean.sh
+++ distclean.sh
@@ -12,5 +12,15 @@ rm -rf doc/cmucl
 # miscellaneous other customizations which shouldn't be propagated
 rm -rf customize-target-features.lisp
 
+# Since the CVS/ subdirectories on my (WHN) machine have CVS/Root
+# containing
+#    :ext:wnewman@cvs.sbcl.sourceforge.net:/cvsroot/sbcl
+# they're not useful for anyone else, so blow them away. (And even 
+# if we could set them up to refer to CVS in a way that someone else 
+# could use, perhaps referring to SourceForge anoncvs, what'd be the 
+# point? I'd expect a comfortable majority of those who want to do 
+# CVS operations would be inclined to start with "cvs co" anyway.)
+find . \( -type d -a -name CVS \) -print | xargs rm -r
+
 # Fall through to ordinary cleanup.
 sh clean.sh
