c05927ab82e234da99f8409038e7e338e36ddf8e
Nikodemus Siivola <nikodemus@random-state.net>
2004-05-29 15:35:47 +0000

    0.8.10.61:          INSTALL MORE DOCUMENTATION          ... Clean up INSTALL          ... Add "SEE ALSO" section to sbcl.1, and fold the DOCUMENTATION                 section there. Also mention Slime in addition to ilisp,                 and drop the mention of shell mode and package confusion.          ... Make install.sh install the manual, BUGS, SUPPORT, CREDITS,                 COPYING and NEWS as well, and make it tell where it put                 everything. Don't install sbcl-asdf-install.1.

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ ensure_dirs ()
 
 INSTALL_ROOT=${INSTALL_ROOT-/usr/local}
 MAN_DIR=${MAN_DIR-$INSTALL_ROOT/share/man}
+INFO_DIR=${INFO_DIR-$INSTALL_ROOT/share/info}
+DOC_DIR=${DOC_DIR-$INSTALL_ROOT/share/doc/sbcl}
+
 SBCL_SOURCE=`pwd`
 if [ -n "$SBCL_HOME" -a "$INSTALL_ROOT/lib/sbcl" != "$SBCL_HOME" ];then
    echo SBCL_HOME environment variable is set, and conflicts with INSTALL_ROOT.
@@ -24,6 +27,7 @@ export SBCL_HOME INSTALL_ROOT
 ensure_dirs $BUILD_ROOT$INSTALL_ROOT $BUILD_ROOT$INSTALL_ROOT/bin \
     $BUILD_ROOT$INSTALL_ROOT/lib  \
     $BUILD_ROOT$MAN_DIR $BUILD_ROOT$MAN_DIR/man1 \
+    $BUILD_ROOT$INFO_DIR $BUILD_ROOT$DOC_DIR \
     $BUILD_ROOT$SBCL_HOME $BUILD_ROOT$SBCL_HOME/systems \
     $BUILD_ROOT$SBCL_HOME/site-systems
 
@@ -36,7 +40,6 @@ test -f $BUILD_ROOT$SBCL_HOME/sbcl.core && \
 
 cp src/runtime/sbcl $BUILD_ROOT$INSTALL_ROOT/bin/
 cp output/sbcl.core $BUILD_ROOT$SBCL_HOME/sbcl.core
-cp doc/sbcl.1 doc/sbcl-asdf-install.1 $BUILD_ROOT$MAN_DIR/man1/
 
 # installing contrib 
 
@@ -52,4 +55,48 @@ for i in contrib/*; do
     INSTALL_DIR=$SBCL_HOME/`basename $i `
     export INSTALL_DIR
     ensure_dirs $BUILD_ROOT$INSTALL_DIR && $GNUMAKE -C $i install
+done
+
+echo
+echo "SBCL has been installed:"
+echo " binary $BUILD_ROOT$INSTALL_ROOT/bin/sbcl"
+echo " core and contribs in $BUILD_ROOT$INSTALL_ROOT/lib/sbcl/"
+
+# Installing manual & misc bits of documentation
+#
+# Locations based on FHS 2.3.
+# See: <http://www.pathname.com/fhs/pub/fhs-2.3.html>
+#
+# share/       architecture independent read-only things
+# share/man/   manpages, should be the same as man/
+# share/info/  info files
+# share/doc/   misc documentation
+
+echo
+echo "Documentation:"
+
+# man
+cp doc/sbcl.1 $BUILD_ROOT$MAN_DIR/man1/ && echo " man $BUILD_ROOT$MAN_DIR/man1/sbcl.1"
+
+# info
+INFO_FILE=doc/manual/sbcl.info
+test -f $INFO_FILE && cp $INFO_FILE $BUILD_ROOT$INFO_DIR/ \
+    && echo -n " info $BUILD_ROOT$INFO_DIR/sbcl.info" \
+    && ( install-info $BUILD_ROOT$INFO_DIR/sbcl.info > /dev/null 2>&1 \
+         || echo -n " (could not add to system catalog)" ) \
+    && echo
+
+# pdf
+PDF_FILE=doc/manual/sbcl.pdf
+test -f $PDF_FILE && cp $PDF_FILE $BUILD_ROOT$DOC_DIR/ \
+    && echo " pdf $BUILD_ROOT$DOC_DIR/sbcl.pdf"
+
+# html
+HTMLS=doc/manual/sbcl
+test -d $HTMLS && cp -r $HTMLS $BUILD_ROOT$DOC_DIR/html \
+    && echo " html $BUILD_ROOT$DOC_DIR/html/index.html"
+
+for f in BUGS SUPPORT CREDITS COPYING NEWS
+do
+  cp $f $BUILD_ROOT$DOC_DIR/
 done
