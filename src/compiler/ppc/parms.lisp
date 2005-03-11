359a6c88dfc80fbae9bdae5251ce0976d003355c
Brian Downing <bdowning@lavos.net>
2005-03-11 07:00:51 +0000

    0.8.20.16: 	Fixed cross-compilation problems to and from Darwin/PPC.
    	* #+darwin changed to #!+darwin before linkage-table constants
    	  in src/compiler/ppc/parms.lisp

--- src/compiler/ppc/parms.lisp
+++ src/compiler/ppc/parms.lisp
@@ -102,7 +102,7 @@
 (def!constant dynamic-1-space-start #x48000000)
 (def!constant dynamic-1-space-end   #x4ffff000)
 
-#+darwin
+#!+darwin
 (progn
   (def!constant linkage-table-space-start #x50000000)
   (def!constant linkage-table-space-end   #x51000000)
