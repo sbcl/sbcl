59450fe1d0e10045a9e1a642ff5af59650176b71
Christophe Rhodes <csr21@cam.ac.uk>
2005-04-02 10:30:50 +0000

    0.8.21.14: 	By popular request (hi Zach), make the banner inert.

--- src/runtime/runtime.c
+++ src/runtime/runtime.c
@@ -166,13 +166,7 @@ void
 print_banner()
 {
     printf(
-"                                  Cl\n\
-                             Cl._ |\n\
-                                _>Sb---Cl\n\
-                             Cl#  |\n\
-                                  Cl\n\
-\n\
-This is SBCL %s, an implementation of ANSI Common Lisp.\n\
+"This is SBCL %s, an implementation of ANSI Common Lisp.\n\
 More information about SBCL is available at <http://www.sbcl.org/>.\n\
 \n\
 SBCL is free software, provided as is, with absolutely no warranty.\n\
