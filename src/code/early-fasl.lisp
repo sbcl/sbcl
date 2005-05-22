cb9c91b411837a85651c68302c43c71fa8d3093c
Juho Snellman <jsnell@iki.fi>
2005-05-22 00:07:45 +0000

    0.9.0.41: 	Increment +FASL-FILE-VERSION+.

--- src/code/early-fasl.lisp
+++ src/code/early-fasl.lisp
@@ -76,7 +76,7 @@
 ;;; versions which break binary compatibility. But it certainly should
 ;;; be incremented for release versions which break binary
 ;;; compatibility.
-(def!constant +fasl-file-version+ 55)
+(def!constant +fasl-file-version+ 56)
 ;;; (record of versions before 2003 deleted in 2003-04-26/0.pre8.107 or so)
 ;;; 38: (2003-01-05) changed names of internal SORT machinery
 ;;; 39: (2003-02-20) in 0.7.12.1 a slot was added to
@@ -119,6 +119,8 @@
 ;;;     on x86-64."
 ;;; 55: (2005-04-06) EXTERN-ALIEN-NAME logic moved from fixups to
 ;;;     FIND-FOREIGN-SYMBOL-IN-TABLE &co.
+;;; 56: (2005-05-22) Something between 0.9.0.1 and 0.9.0.14. My money is
+;;;     on 0.9.0.6 (MORE CASE CONSISTENCY).
 
 ;;; the conventional file extension for our fasl files
 (declaim (type simple-string *fasl-file-type*))
