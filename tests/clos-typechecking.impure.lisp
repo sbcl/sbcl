7c850cd952cff85a3b57371af212f00ad35de742
Christophe Rhodes <csr21@cam.ac.uk>
2005-11-24 17:09:40 +0000

    0.9.6.56.clos-typechecking2.2: 	Propagate type information for funcallable-standard-class 	instances in instance-writer too. 	... fixes the fail/sv/gf case (compiled slot-value inside 		a generic function) 	... probably doesn't fix subclasses with hairy intersection 		types... 	... doesn't fix the compiled use of slot-value outside 		methods, which uses the (SLOT-ACCESSOR :GLOBAL ... 		WRITER) generic function.

--- tests/clos-typechecking.impure.lisp
+++ tests/clos-typechecking.impure.lisp
@@ -31,114 +31,110 @@
 ;; evaluator
 (eval '(setf (slot-value (make-instance 'foo) 'slot) 1))
 (assert (raises-error? (eval '(setf (slot-value (make-instance 'foo) 'slot) t))
-		       type-error))
+                       type-error))
 (eval '(setf (slot (make-instance 'foo)) 1))
 (assert (raises-error? (eval '(setf (slot (make-instance 'foo)) t))
-		       type-error))
+                       type-error))
 (eval '(succeed/sv (make-instance 'foo)))
 (assert (raises-error? (eval '(fail/sv (make-instance 'foo)))
-		       type-error))
+                       type-error))
 (eval '(succeed/acc (make-instance 'foo)))
 (assert (raises-error? (eval '(fail/acc (make-instance 'foo)))
-		       type-error))
+                       type-error))
 (eval '(make-instance 'foo :slot 1))
 (assert (raises-error? (eval '(make-instance 'foo :slot t))
-		       type-error))
+                       type-error))
 (eval '(make-instance 'foo :slot *one*))
 (assert (raises-error? (eval '(make-instance 'foo :slot *t*))
-		       type-error))
+                       type-error))
 ;; evaluator/gf
 (eval '(setf (slot-value (make-instance 'foo/gf) 'slot/gf) 1))
 (assert (raises-error?
-	 (eval '(setf (slot-value (make-instance 'foo/gf) 'slot/gf) t))
-	 type-error))
+         (eval '(setf (slot-value (make-instance 'foo/gf) 'slot/gf) t))
+         type-error))
 (eval '(setf (slot/gf (make-instance 'foo/gf)) 1))
 (assert (raises-error? (eval '(setf (slot/gf (make-instance 'foo/gf)) t))
-		       type-error))
+                       type-error))
 (eval '(succeed/sv/gf (make-instance 'foo/gf)))
-#+nil ; funcallable standard instance slot-value access go through
-      ; ACCESSOR-SLOT-VALUE because their classes are not
-      ; STANDARD-CLASS-P.
 (assert (raises-error? (eval '(fail/sv/gf (make-instance 'foo/gf)))
-		       type-error))
+                       type-error))
 (eval '(succeed/acc/gf (make-instance 'foo/gf)))
 (assert (raises-error? (eval '(fail/acc/gf (make-instance 'foo/gf)))
-		       type-error))
+                       type-error))
 (eval '(make-instance 'foo/gf :slot/gf 1))
 (assert (raises-error? (eval '(make-instance 'foo/gf :slot/gf t))
-		       type-error))
+                       type-error))
 (eval '(make-instance 'foo/gf :slot/gf *one*))
 (assert (raises-error? (eval '(make-instance 'foo/gf :slot/gf *t*))
-		       type-error))
+                       type-error))
 
 ;; compiler
 (funcall (compile nil '(lambda ()
-			(setf (slot-value (make-instance 'foo) 'slot) 1))))
+                        (setf (slot-value (make-instance 'foo) 'slot) 1))))
 #+nil ; this one still fails goddamit.
 (assert (raises-error?
-	 (funcall
-	  (compile nil '(lambda ()
-			 (setf (slot-value (make-instance 'foo) 'slot) t))))
-	 type-error))
+         (funcall
+          (compile nil '(lambda ()
+                         (setf (slot-value (make-instance 'foo) 'slot) t))))
+         type-error))
 (funcall (compile nil '(lambda () (setf (slot (make-instance 'foo)) 1))))
 (assert (raises-error?
-	 (funcall
-	  (compile nil '(lambda () (setf (slot (make-instance 'foo)) t))))
-	 type-error))
+         (funcall
+          (compile nil '(lambda () (setf (slot (make-instance 'foo)) t))))
+         type-error))
 (funcall (compile nil '(lambda () (succeed/sv (make-instance 'foo)))))
 (assert (raises-error?
-	 (funcall (compile nil '(lambda () (fail/sv (make-instance 'foo)))))
-	 type-error))
+         (funcall (compile nil '(lambda () (fail/sv (make-instance 'foo)))))
+         type-error))
 (funcall (compile nil '(lambda () (succeed/acc (make-instance 'foo)))))
 (assert (raises-error?
-	 (funcall (compile nil '(lambda () (fail/acc (make-instance 'foo)))))
-	 type-error))
+         (funcall (compile nil '(lambda () (fail/acc (make-instance 'foo)))))
+         type-error))
 (funcall (compile nil '(lambda () (make-instance 'foo :slot 1))))
 (assert (raises-error?
-	 (funcall (compile nil '(lambda () (make-instance 'foo :slot t))))
-	 type-error))
+         (funcall (compile nil '(lambda () (make-instance 'foo :slot t))))
+         type-error))
 (funcall (compile nil '(lambda () (make-instance 'foo :slot *one*))))
 (assert (raises-error?
-	 (funcall (compile nil '(lambda () (make-instance 'foo :slot *t*))))
-	 type-error))
+         (funcall (compile nil '(lambda () (make-instance 'foo :slot *t*))))
+         type-error))
 ;; compiler/gf
 (funcall (compile nil
-		  '(lambda ()
-		    (setf (slot-value (make-instance 'foo/gf) 'slot/gf) 1))))
+                  '(lambda ()
+                    (setf (slot-value (make-instance 'foo/gf) 'slot/gf) 1))))
 #+nil ; this one too
 (assert (raises-error?
-	 (funcall
-	  (compile nil
-		   '(lambda ()
-		     (setf (slot-value (make-instance 'foo/gf) 'slot/gf) t))))
-	 type-error))
+         (funcall
+          (compile nil
+                   '(lambda ()
+                     (setf (slot-value (make-instance 'foo/gf) 'slot/gf) t))))
+         type-error))
 (funcall (compile nil '(lambda () (setf (slot/gf (make-instance 'foo/gf)) 1))))
 (assert (raises-error?
-	 (funcall
-	  (compile nil
-		   '(lambda () (setf (slot/gf (make-instance 'foo/gf)) t))))
-	 type-error))
+         (funcall
+          (compile nil
+                   '(lambda () (setf (slot/gf (make-instance 'foo/gf)) t))))
+         type-error))
 (funcall (compile nil '(lambda () (succeed/sv/gf (make-instance 'foo/gf)))))
-#+nil ; see above
 (assert (raises-error?
-	 (funcall (compile nil '(lambda ()
-				 (fail/sv/gf (make-instance 'foo/gf)))))
-	 type-error))
+         (funcall (compile nil '(lambda ()
+                                 (fail/sv/gf (make-instance 'foo/gf)))))
+         type-error))
 (funcall (compile nil '(lambda () (succeed/acc/gf (make-instance 'foo/gf)))))
 (assert (raises-error?
-	 (funcall (compile nil '(lambda ()
-				 (fail/acc/gf (make-instance 'foo/gf)))))
-	 type-error))
+         (funcall (compile nil '(lambda ()
+                                 (fail/acc/gf (make-instance 'foo/gf)))))
+         type-error))
 (funcall (compile nil '(lambda () (make-instance 'foo/gf :slot/gf 1))))
 (assert (raises-error?
-	 (funcall (compile nil '(lambda ()
-				 (make-instance 'foo/gf :slot/gf t))))
-	 type-error))
+         (funcall (compile nil '(lambda ()
+                                 (make-instance 'foo/gf :slot/gf t))))
+         type-error))
 (funcall (compile nil '(lambda () (make-instance 'foo/gf :slot/gf *one*))))
 (assert (raises-error?
-	 (funcall (compile nil '(lambda ()
-				 (make-instance 'foo/gf :slot/gf *t*))))
-	 type-error))
+         (funcall (compile nil '(lambda ()
+                                 (make-instance 'foo/gf :slot/gf *t*))))
+         type-error))
 
 ;;;; success
 (sb-ext:quit :unix-status 104)
