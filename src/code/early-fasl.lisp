b16ab6d8df8b236728b4097a989eb626ad278eff
Juho Snellman <jsnell@iki.fi>
2006-07-24 22:50:59 +0000

    0.9.14.31: 	Bump +fasl-file-version+ for 0.9.15.

--- src/code/early-fasl.lisp
+++ src/code/early-fasl.lisp
@@ -76,7 +76,7 @@
 ;;; versions which break binary compatibility. But it certainly should
 ;;; be incremented for release versions which break binary
 ;;; compatibility.
-(def!constant +fasl-file-version+ 66)
+(def!constant +fasl-file-version+ 67)
 ;;; (record of versions before 2003 deleted in 2003-04-26/0.pre8.107 or so)
 ;;; 38: (2003-01-05) changed names of internal SORT machinery
 ;;; 39: (2003-02-20) in 0.7.12.1 a slot was added to
@@ -136,6 +136,8 @@
 ;;;     x86-64.  Also (belatedly) PPC/gencgc, including :gencgc on FPAFF.
 ;;; 65: (2006-04-11) Package locking interface changed.
 ;;; 66: (2006-05-13) Fopcompiler
+;;; 67: (2006-07-25) Reports on #lisp about 0.9.13 fasls being invalid on
+;;;     0.9.14.something
 
 ;;; the conventional file extension for our fasl files
 (declaim (type simple-string *fasl-file-type*))
