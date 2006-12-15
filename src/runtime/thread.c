ce798749f957d005670b71edf38b893264e4f7ba
sa2c <sa2c>
2006-12-15 01:16:36 +0000

    1.0.0.30: Stability fix of threading on FreeBSD
    Yield current thread on thread creation so that clean-up threads can
    run.  This fixes some test failures on FreeBSD 7.

--- src/runtime/thread.c
+++ src/runtime/thread.c
@@ -350,6 +350,10 @@ create_thread_struct(lispobj initial_function) {
     int i;
 #endif
 
+#ifdef CREATE_CLEANUP_THREAD
+    /* Give a chance for cleanup threads to run. */
+    sched_yield();
+#endif
     /* may as well allocate all the spaces at once: it saves us from
      * having to decide what to do if only some of the allocations
      * succeed */
