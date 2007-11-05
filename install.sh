df71de136d8a4a5165ca47379a43e71525e912ae
Rudi Schlatte <rudi@constantly.at>
2007-11-05 04:14:41 +0000

    1.0.11.7: Don't try to register info-* files using install-info   Reported by Eugene Ossintsev

--- install.sh
+++ install.sh
@@ -108,13 +108,19 @@ echo "Documentation:"
 cp doc/sbcl.1 "$BUILD_ROOT$MAN_DIR"/man1/ && echo " man $BUILD_ROOT$MAN_DIR/man1/sbcl.1"
 
 # info
-for info in doc/manual/*.info doc/manual/*.info-*
+for info in doc/manual/*.info
 do
   cp $info "$BUILD_ROOT$INFO_DIR"/ \
       && echo -n " info $BUILD_ROOT$INFO_DIR/`basename $info` $BUILD_ROOT$INFO_DIR/dir" \
       && ( install-info "$BUILD_ROOT$INFO_DIR"/`basename $info` "$BUILD_ROOT$INFO_DIR"/dir > /dev/null 2>&1 \
            || echo -n " (could not add to system catalog)" ) \
       && echo
+done
+
+for info in doc/manual/*.info-*
+do
+  cp $info "$BUILD_ROOT$INFO_DIR"/ \
+      && echo " info $BUILD_ROOT$INFO_DIR/`basename $info`"
 done
 
 # pdf
