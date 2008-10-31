f86c1ec4e8782fd24c758c9775c8592a16768215
Gabor Melis <mega@hotpop.com>
2008-10-31 09:56:15 +0000

    1.0.22.1: run-sbcl.sh improvements
    - make run-sbcl.sh executable
    - don't require it to be started from the top of the source dir
    
    ... so that "~/sbcl/run-sbcl.sh" is a drop in replacement for "sbcl".

--- run-sbcl.sh
+++ run-sbcl.sh
@@ -1,6 +1,6 @@
+#!/bin/sh
 # A simple shell-script to run the freshly build SBCL without
-# installing it. Expects to be run from the top of the SBCL source
-# tree.
+# installing it.
 
 # This software is part of the SBCL system. See the README file for
 # more information.
@@ -20,24 +20,16 @@ if [ "$1" = "--help" ]; then
     echo "installation. Except for --help and --core, accepts all the same"
     echo "command-line options as SBCL does."
     echo
-    echo "Expects the current directory to be the topmost directory of the SBCL"
-    echo "source tree or binary tarball."
-    echo
     exit 1
 fi
 
-if [ -f sbcl-pwd.sh -a -x src/runtime/sbcl -a -f output/sbcl.core ]; then
-    . ./sbcl-pwd.sh
-    sbcl_pwd
-    echo "(running SBCL from: $SBCL_PWD)"
-    SBCL_HOME=$SBCL_PWD/contrib src/runtime/sbcl --core output/sbcl.core "$@"
-elif [ -f run-sbcl.sh -a -f version.lisp-expr ]; then
-    echo "No built SBCL here ($(pwd)): run 'sh make.sh' first!"
-    exit 1
+BASE=`dirname "$0"`
+
+if [ -x "$BASE"/src/runtime/sbcl -a -f "$BASE"/output/sbcl.core ]; then
+    echo "(running SBCL from: $BASE)"
+    SBCL_HOME="$BASE"/contrib \
+        "$BASE"/src/runtime/sbcl --core "$BASE"/output/sbcl.core "$@"
 else
-    echo "No SBCL here ($(pwd))!"
-    echo
-    echo "run-sbcl.sh needs to be run from the top of the SBCL source tree or"
-    echo "binary tarball."
+    echo "No built SBCL here ($BASE): run 'sh make.sh' first!"
     exit 1
 fi
