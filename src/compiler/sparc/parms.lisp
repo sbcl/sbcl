f21346ee001dcff7fed0a62da80a9e002d626b07
Nikodemus Siivola <nikodemus@random-state.net>
2009-01-03 15:17:44 +0000

    1.0.24.9: fix overlapping address spaces on sparc
     * Patch by Bruce O'Neel.

--- src/compiler/sparc/parms.lisp
+++ src/compiler/sparc/parms.lisp
@@ -95,7 +95,7 @@
   (def!constant linkage-table-space-start #x0f800000)
   (def!constant linkage-table-space-end   #x10000000)
 
-  (def!constant read-only-space-start     #x10000000)
+  (def!constant read-only-space-start     #x11000000)
   (def!constant read-only-space-end       #x15000000)
 
   (def!constant static-space-start        #x28000000)
@@ -112,7 +112,7 @@
   (def!constant linkage-table-space-start #x0f800000)
   (def!constant linkage-table-space-end   #x10000000)
 
-  (def!constant read-only-space-start     #x10000000)
+  (def!constant read-only-space-start     #x11000000)
   (def!constant read-only-space-end       #x15000000)
 
   (def!constant static-space-start        #x28000000)
@@ -129,17 +129,17 @@
   (def!constant linkage-table-space-start #x0f800000)
   (def!constant linkage-table-space-end   #x10000000)
 
-  (def!constant read-only-space-start     #x10000000)
+  (def!constant read-only-space-start     #x11000000)
   (def!constant read-only-space-end       #x15000000)
 
   (def!constant static-space-start        #x18000000)
   (def!constant static-space-end          #x1c000000)
 
-  (def!constant dynamic-0-space-start     #x20000000)
-  (def!constant dynamic-0-space-end       #x38000000)
+  (def!constant dynamic-0-space-start     #x48000000)
+  (def!constant dynamic-0-space-end       #x5ffff000)
 
-  (def!constant dynamic-1-space-start     #x48000000)
-  (def!constant dynamic-1-space-end       #x60000000))
+  (def!constant dynamic-1-space-start     #x60000000)
+  (def!constant dynamic-1-space-end       #x77fff000))
 
 ;; Size of one linkage-table entry in bytes. See comment in
 ;; src/runtime/sparc-arch.c
