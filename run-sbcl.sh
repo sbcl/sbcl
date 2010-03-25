0cfd289365b37a66fd4108054f0d99e95d396a8a
Nikodemus Siivola <nikodemus@random-state.net>
2010-03-25 12:24:39 +0000

    1.0.36.39: report location correctly in run-sbcl.sh
     * Thanks to Attila Lendvai for noticing. In despite the freeze.

--- run-sbcl.sh
+++ run-sbcl.sh
@@ -40,7 +40,7 @@ if [ "$CORE_DEFINED" = "no" ]; then
 fi
 
 if [ -x "$BASE"/src/runtime/sbcl -a -f "$BASE"/output/sbcl.core ]; then
-    echo "(running SBCL from: `pwd`)"
+    echo "(running SBCL from: $BASE)"
     SBCL_HOME="$BASE"/contrib "$BASE"/src/runtime/sbcl $ARGUMENTS "$@"
 else
     echo "No built SBCL here ($BASE): run 'sh make.sh' first!"
