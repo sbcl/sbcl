98b538e06db617049c57fbe69e9ddefa66aee985
Tobias C. Rittweiler <trittweiler@users.sourceforge.net>
2010-03-30 21:40:39 +0000

    1.0.37.19: Forgot to add sb-queue/package.lisp in last commit.

---
+++ contrib/sb-queue/package.lisp
@@ -0,0 +1,25 @@
+;;;; This software is part of the SBCL system. See the README file for
+;;;; more information.
+;;;;
+;;;; This software is derived from the CMU CL system, which was written at
+;;;; Carnegie Mellon University and released into the public domain. The
+;;;; software is in the public domain and is provided with absolutely no
+;;;; warranty. See the COPYING and CREDITS files for more information.
+
+;;; SB-QUEUE has been slurped in SB-CONCURRENCY.
+
+;;; Here we just provide a reexporting stub for backwards
+;;; compatibility.
+
+(defpackage :sb-queue
+  (:use :cl :sb-concurrency)
+  (:export
+   "DEQUEUE"
+   "ENQUEUE"
+   "LIST-QUEUE-CONTENTS"
+   "MAKE-QUEUE"
+   "QUEUE"
+   "QUEUE-COUNT"
+   "QUEUE-EMPTY-P"
+   "QUEUE-NAME"
+   "QUEUEP"))
