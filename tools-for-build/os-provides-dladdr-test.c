97d73de3875a70f0686b0b692509c1a30e3b44a4
Nikodemus Siivola <nikodemus@random-state.net>
2010-08-31 12:26:44 +0000

    1.0.42.6: fix os-provides-dladdr-test.c
      For dladdr to be present in dlfcn.h, #define _GNU_SOURCE should be
      before any includes.
    
      Patch by Stas Boukarev.

--- tools-for-build/os-provides-dladdr-test.c
+++ tools-for-build/os-provides-dladdr-test.c
@@ -1,11 +1,10 @@
 /* test to build and run so that we know if we have dladdr
  */
 
-#include <stdlib.h>
-
 /* bloody FSF dlcfn.h won't give us dladdr without this */
 #define _GNU_SOURCE
 
+#include <stdlib.h>
 #include <dlfcn.h>
 
 int main ()
