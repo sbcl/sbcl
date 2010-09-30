6e3786cb5116f1099542212bc9957bc30def64f3
Nikodemus Siivola <nikodemus@random-state.net>
2010-09-30 07:12:47 +0000

    1.0.43.2: remove bashishm from source-distribution.sh
     Thanks to Jim Wise, lp#644702.

--- source-distribution.sh
+++ source-distribution.sh
@@ -3,5 +3,5 @@ set -e
 
 # Create a source distribution. (You should run clean.sh first.)
 
-b=${1:?missing base directory name argument}
+b=${1:?"missing base directory name argument"}
 tar cf $b-source.tar $b
