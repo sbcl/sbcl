2a9b33fc45842ec0335fef2988e07337ae8f1fc7
Joshua Elsasser <joshua@elsasser.org>
2011-10-16 18:12:53 -0700

    Fix errno value for sb-posix rmdir.error.3 test on OpenBSD.
    Use #+bsd instead of #+(or darwin openbsd) on the assumption that all
    the other BSD-derived systems use the same errno value.

--- contrib/sb-posix/posix-tests.lisp
+++ contrib/sb-posix/posix-tests.lisp
@@ -174,7 +174,7 @@
       (typep
        (sb-posix:syscall-errno c)
        '(member
-         #+darwin
+         #+bsd
          #.sb-posix:eisdir
          #+win32
          #.sb-posix::eacces
@@ -182,7 +182,7 @@
          #.sb-posix::enotempty
          #+sunos
          #.sb-posix::einval
-         #-(or darwin win32 sunos)
+         #-(or bsd win32 sunos)
          #.sb-posix::ebusy)))) t)
 
 (deftest rmdir.error.4
