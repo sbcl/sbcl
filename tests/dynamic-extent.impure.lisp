9aa842e2eddd107b697c9bdf1c29b380f4b80853
Juho Snellman <jsnell@iki.fi>
2012-01-09 03:05:53 +0100

    Mark :no-consing :specialized-dx-vectors as failing on x86.

--- tests/dynamic-extent.impure.lisp
+++ tests/dynamic-extent.impure.lisp
@@ -584,7 +584,7 @@
   (assert-no-consing (vector-on-stack :x :y)))
 
 (with-test (:name (:no-consing :specialized-dx-vectors)
-            :fails-on '(and :sunos :x86)
+            :fails-on :x86
             :skipped-on `(not (and :stack-allocatable-vectors
                                    :c-stack-is-control-stack)))
   (assert-no-consing (make-array-on-stack-6))
