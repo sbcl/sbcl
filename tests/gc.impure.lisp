4ce962e92568e52624959d8abc83f609abc3605c
Nikodemus Siivola <nikodemus@sb-studio.net>
2012-04-29 20:55:35 +0300

    tune bug-981106 test based on the heap size
      Fixes lp#983807.

--- tests/gc.impure.lisp
+++ tests/gc.impure.lisp
@@ -85,10 +85,11 @@
    (assert (eq :ok
                (handler-case
                    (dotimes (runs 100 :ok)
-                     (let ((len (length
-                                 (with-output-to-string (string)
-                                   (dotimes (i 1000000)
-                                     (write-sequence "hi there!" string))))))
-                       (assert (eql len (* 1000000 (length "hi there!"))))))
+                     (let* ((n (truncate (dynamic-space-size) 1200))
+                            (len (length
+                                  (with-output-to-string (string)
+                                    (dotimes (i n)
+                                      (write-sequence "hi there!" string))))))
+                       (assert (eql len (* n (length "hi there!"))))))
                  (storage-condition ()
                    :oom))))))
