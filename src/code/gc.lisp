sbcl/sbcl

commit 6848a92fd4af23d0695eeaaed9efcbdfd9e1d9e5

default gen.bytes_consed_between_gc to same 5% of dynamic space size
  ...the previous default was 2,000,000 bytes, not 20Mb as the
  documentation claimed. Oops.

src/code/gc.lisp
@@ -419,7 +419,8 @@ Note: currently changes to this value are lost when saving core."
       "Number of bytes that can be allocated to GENERATION before that
 generation is considered for garbage collection. This value is meaningless for
 generation 0 (the nursery): see BYTES-CONSED-BETWEEN-GCS instead. Default is
-20Mb. Can be assigned to using SETF. Available on GENCGC platforms only.
+5% of the dynamic space size. Can be assigned to using SETF. Available on
+GENCGC platforms only.
 
 Experimental: interface subject to change."
     t)

src/runtime/gencgc.c
@@ -4136,7 +4136,7 @@ gc_init(void)
         generations[i].num_gc = 0;
         generations[i].cum_sum_bytes_allocated = 0;
         /* the tune-able parameters */
-        generations[i].bytes_consed_between_gc = 2000000;
+        generations[i].bytes_consed_between_gc = bytes_consed_between_gcs;
         generations[i].number_of_gcs_before_promotion = 1;
         generations[i].minimum_age_before_gc = 0.75;
     }

tests/gc.impure.lisp
@@ -54,10 +54,11 @@
 
 ;;; SB-EXT:GENERATION-* accessors returned bogus values for generation > 0
 (with-test (:name :bug-529014 :skipped-on '(not :gencgc))
-  ;; FIXME: These parameters are a) tunable in the source and b)
-  ;; duplicated multiple times there and now here.  It would be good to
-  ;; OAOO-ify them (probably to src/compiler/generic/params.lisp).
   (loop for i from 0 to sb-vm:+pseudo-static-generation+
-     do (assert (= (sb-ext:generation-bytes-consed-between-gcs i) 2000000))
-       (assert (= (sb-ext:generation-minimum-age-before-gc i) 0.75))
-       (assert (= (sb-ext:generation-number-of-gcs-before-promotion i) 1))))
+     do (assert (= (sb-ext:generation-bytes-consed-between-gcs i)
+                   (sb-ext:bytes-consed-between-gcs)))
+        ;; FIXME: These parameters are a) tunable in the source and b)
+        ;; duplicated multiple times there and now here.  It would be good to
+        ;; OAOO-ify them (probably to src/compiler/generic/params.lisp).
+        (assert (= (sb-ext:generation-minimum-age-before-gc i) 0.75))
+        (assert (= (sb-ext:generation-number-of-gcs-before-promotion i) 1))))

commit 661bcd7d3e0bdc1966f3878fa71d322ffd5927a4

gencgc: scale generation.bytes_consed_between_gc to number of gens
  This means we have by default 5% of total heap usable in the nursery
  between GCs, and another 5% of the total heap split between all the
  generations -- and roughly matches the nursery/old generation sizes
  relations from before the dynamic-space-size based scaling.

  Fixes lp#991293, regression since
  6848a92fd4af23d0695eeaaed9efcbdfd9e1d9e5.

src/code/gc.lisp
@@ -435,8 +435,8 @@ Note: currently changes to this value are lost when saving core."
       "Number of bytes that can be allocated to GENERATION before that
 generation is considered for garbage collection. This value is meaningless for
 generation 0 (the nursery): see BYTES-CONSED-BETWEEN-GCS instead. Default is
-5% of the dynamic space size. Can be assigned to using SETF. Available on
-GENCGC platforms only.
+5% of the dynamic space size divided by the number of non-nursery generations.
+Can be assigned to using SETF. Available on GENCGC platforms only.
 
 Experimental: interface subject to change."
     t)

src/runtime/gencgc.c
@@ -4038,7 +4038,8 @@ gc_init(void)
         generations[i].num_gc = 0;
         generations[i].cum_sum_bytes_allocated = 0;
         /* the tune-able parameters */
-        generations[i].bytes_consed_between_gc = bytes_consed_between_gcs;
+        generations[i].bytes_consed_between_gc
+            = bytes_consed_between_gcs/(os_vm_size_t)HIGHEST_NORMAL_GENERATION;
         generations[i].number_of_gcs_before_promotion = 1;
         generations[i].minimum_age_before_gc = 0.75;
     }

tests/gc.impure.lisp
@@ -56,7 +56,8 @@
 (with-test (:name :bug-529014 :skipped-on '(not :gencgc))
   (loop for i from 0 to sb-vm:+pseudo-static-generation+
      do (assert (= (sb-ext:generation-bytes-consed-between-gcs i)
-                   (sb-ext:bytes-consed-between-gcs)))
+                   (truncate (sb-ext:bytes-consed-between-gcs)
+                             sb-vm:+highest-normal-generation+)))
         ;; FIXME: These parameters are a) tunable in the source and b)
         ;; duplicated multiple times there and now here.  It would be good to
         ;; OAOO-ify them (probably to src/compiler/generic/params.lisp).