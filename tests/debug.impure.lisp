b73291745b3c5924276230eba6768ad5e333d570
Nikodemus Siivola <nikodemus@random-state.net>
2012-06-09 20:39:47 +0300

    test backtrace-interrupted-condition-wait works on Darwin/x86 as well

--- tests/debug.impure.lisp
+++ tests/debug.impure.lisp
@@ -177,7 +177,7 @@
                   ;; stunted, ending at _sigtramp, when we add :TIMEOUT NIL to
                   ;; the frame we expect. If we leave it out, the backtrace is
                   ;; fine -- but the test fails. I can only boggle right now.
-            :fails-on '(and :x86 (or :linux :darwin)))
+            :fails-on '(and :x86 :linux))
   (let ((m (sb-thread:make-mutex))
         (q (sb-thread:make-waitqueue)))
     (assert (verify-backtrace
