5c6755ecd95f037009d394665ae3d964bac088a7
Paul Khuong <pvk@pvk.ca>
2012-08-17 18:11:47 -0400

    Fix "exit from normal thread" test
     The test did not wait for the child to exit, resulting in spurious
     failures on loaded or single-core machines.

--- tests/threads.test.sh
+++ tests/threads.test.sh
@@ -20,7 +20,7 @@ check_status_maybe_lose "return from main thread" $? 0 "ok"
 run_sbcl --eval '(sb-thread:abort-thread :allow-exit t)'
 check_status_maybe_lose "abort main thread" $? 1 "ok"
 
-run_sbcl --eval '#+sb-thread (sb-thread:make-thread (lambda () (sb-ext:exit :code 77))) #-sb-thread (sb-ext:exit :code 77)'
+run_sbcl --eval '#+sb-thread (sb-thread:join-thread (sb-thread:make-thread (lambda () (sb-ext:exit :code 77)))) #-sb-thread (sb-ext:exit :code 77)'
 check_status_maybe_lose "exit from normal thread" $? 77 "ok"
 
 flag="condition-wait-sigcont.tmp"
