28346e0e47ab48ff7f8ba3de73840a4992bab28d
Stas Boukarev <stassats@gmail.com>
2012-08-25 03:18:17 +0400

    sb-bsd-sockets: Package clean up.
    sb-bsd-sockets-internal: Don't use sb-c-call package, it's a
    deprecated nickname for sb-alien, and sb-alien is already
    used.
    sb-win32-sockets-internal: Remove, not used anywhere.

--- contrib/sb-bsd-sockets/defpackage.lisp
+++ contrib/sb-bsd-sockets/defpackage.lisp
@@ -1,14 +1,8 @@
-#+(and sbcl win32)
-(defpackage "SB-WIN32-SOCKETS-INTERNAL"
-  (:nicknames "WIN32SOCKINT")
-  (:shadow close listen)
-  (:use "COMMON-LISP" "SB-ALIEN" "SB-EXT" "SB-C-CALL"))
-
 (defpackage "SB-BSD-SOCKETS-INTERNAL"
   (:nicknames "SOCKINT")
   (:shadow close listen)
   (:shadowing-import-from "SB-KERNEL" with-array-data)
-  (:use "COMMON-LISP" "SB-ALIEN" "SB-EXT" "SB-C-CALL"))
+  (:use "COMMON-LISP" "SB-ALIEN" "SB-EXT"))
 
 (defpackage "SB-BSD-SOCKETS"
   (:export socket local-socket local-abstract-socket inet-socket
@@ -45,21 +39,15 @@
 
            make-inet-address
 
-           non-blocking-mode
-           )
+           non-blocking-mode)
   (:use "COMMON-LISP" "SB-BSD-SOCKETS-INTERNAL")
   (:import-from "SB-INT" "UNSUPPORTED-OPERATOR" "FEATUREP")
   (:documentation
-   "
-
-A thinly-disguised BSD socket API for SBCL.  Ideas stolen from the BSD
+   "A thinly-disguised BSD socket API for SBCL.  Ideas stolen from the BSD
 socket API for C and Graham Barr's IO::Socket classes for Perl.
 
 We represent sockets as CLOS objects, and rename a lot of methods and
-arguments to fit Lisp style more closely.
-
-"
-   ))
+arguments to fit Lisp style more closely."))
 
 ;;; gethostbyname/gethostbyaddr are generally not thread safe. POSIX
 ;;; 1003.1-2003 defines an alternative API, which is specified in the
