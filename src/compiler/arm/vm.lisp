e6a55eaf6de89501d2bcb0a6f5d6bb9d0b24236e
Alastair Bridgewater <nyef@kana.lisphacker.com>
2012-10-02 10:11:55 -0400

    compiler/arm/vm: Initial register definitions.
      * These are only the integer registers, with partitioning into
    system registers, descriptor registers, and non-descriptor
    registers, and definitions for arg-passing registers.  No
    floating-point yet.

--- src/compiler/arm/vm.lisp
+++ src/compiler/arm/vm.lisp
@@ -11,4 +11,54 @@
 
 (in-package "SB!VM")
 
-;;; Dummy placeholder file.
+
+;;;; register specs
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *register-names* (make-array 16 :initial-element nil)))
+
+(macrolet ((defreg (name offset)
+               (let ((offset-sym (symbolicate name "-OFFSET")))
+                 `(eval-when (:compile-toplevel :load-toplevel :execute)
+                   (def!constant ,offset-sym ,offset)
+                   (setf (svref *register-names* ,offset-sym) ,(symbol-name name)))))
+
+           (defregset (name &rest regs)
+               `(eval-when (:compile-toplevel :load-toplevel :execute)
+                 (defparameter ,name
+                   (list ,@(mapcar #'(lambda (name)
+                                       (symbolicate name "-OFFSET")) regs))))))
+
+  (defreg r0 0)
+  (defreg r1 1)
+  (defreg r2 2)
+  (defreg r3 3)
+  (defreg r4 4)
+  (defreg r5 5)
+  (defreg r6 6)
+  (defreg r7 7)
+  (defreg r8 8)
+  (defreg r9 9)
+  (defreg null 10)
+  (defreg fp 11)
+  (defreg r12 12)
+  (defreg sp 13)
+  (defreg lr 14)
+  (defreg pc 15) ;; Yes, the program counter.
+
+  (defregset system-regs
+      null fp sp lr pc)
+
+  (defregset descriptor-regs
+      r0 r1 r2 r3 r4 r5 r6)
+
+  (defregset non-descriptor-regs
+      r7 r8 r9 r12)
+
+  ;; registers used to pass arguments
+  ;;
+  ;; the number of arguments/return values passed in registers
+  (def!constant  register-arg-count 4)
+  ;; names and offsets for registers used to pass arguments
+  (defregset *register-arg-offsets*  r0 r1 r2 r3)
+  (defparameter *register-arg-names* '(r0 r1 r2 r3)))
