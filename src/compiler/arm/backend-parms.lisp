dc06a9b83475aa04cce7cd8b5d5a586e36e98fc0
Alastair Bridgewater <nyef@kana.lisphacker.com>
2012-10-02 22:27:02 -0400

    compiler/arm/backend-params: Define *BACKEND-BYTE-ORDER*.
      * Crib this from the MIPS backend, which is another architecture
    that can be run (and is seen in the wild to be run) in both little
    and big endian modes.

--- src/compiler/arm/backend-parms.lisp
+++ src/compiler/arm/backend-parms.lisp
@@ -1,3 +1,6 @@
 (in-package "SB!VM")
 
 (def!constant +backend-fasl-file-implementation+ :arm)
+(setf *backend-byte-order*
+      #!+little-endian :little-endian
+      #!-little-endian :big-endian)
