4eb88932654271ea6571c7e414a09e813dcbfb72
Alastair Bridgewater <nyef@kana.lisphacker.com>
2012-10-16 22:14:16 -0400

    compiler/arm/backend-parms: Define *BACKEND-PAGE-BYTES*.
      * This defaults to zero for some reason, but it's used as a
    divisor in genesis.
    
      * Define it as 4096, which is the minimum page size observed on
    my target system in the output from "cat /proc/self/maps".  Not a
    particularly authoritative value, but one that works for me for
    now.

--- src/compiler/arm/backend-parms.lisp
+++ src/compiler/arm/backend-parms.lisp
@@ -4,3 +4,7 @@
 (setf *backend-byte-order*
       #!+little-endian :little-endian
       #!-little-endian :big-endian)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  ;; Minumum observed value, not authoritative.
+  (setf *backend-page-bytes* 4096))
