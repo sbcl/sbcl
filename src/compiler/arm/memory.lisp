deba59b8c84c71c899420d78bc77de35462a6e8c
Alastair Bridgewater <nyef@kana.lisphacker.com>
2012-10-22 18:24:38 -0400

    compiler/arm/memory: VOPs CELL-REF and CELL-SET.
      * These are used for a whole host of things, mostly involving
    fixed object layouts known to the compiler, such as the slots in a
    CONS cell, a VALUE-CELL, a SYMBOL, and so on.

--- src/compiler/arm/memory.lisp
+++ src/compiler/arm/memory.lisp
@@ -12,4 +12,21 @@
 
 (in-package "SB!VM")
 
-;;; Dummy placeholder file.
+;;; Cell-Ref and Cell-Set are used to define VOPs like CAR, where the
+;;; offset to be read or written is a property of the VOP used.
+
+(define-vop (cell-ref)
+  (:args (object :scs (descriptor-reg)))
+  (:results (value :scs (descriptor-reg any-reg)))
+  (:variant-vars offset lowtag)
+  (:policy :fast-safe)
+  (:generator 4
+    (loadw value object offset lowtag)))
+
+(define-vop (cell-set)
+  (:args (object :scs (descriptor-reg))
+         (value :scs (descriptor-reg any-reg null)))
+  (:variant-vars offset lowtag)
+  (:policy :fast-safe)
+  (:generator 4
+    (storew value object offset lowtag)))
