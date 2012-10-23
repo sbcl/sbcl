48546b0b0bfe84101c2b9a0851f7c2536d41e134
Alastair Bridgewater <nyef@kana.lisphacker.com>
2012-10-22 22:55:13 -0400

    arm-bootstrap: Updates for IN-PACKAGE and more testing of funcall/return.
      * Testing more RETURN VOPs and some of the CALL VOPs.
    
      * Also using IN-PACKAGE, now that it works, in order to reduce
    the number of package prefixes (one for each non-CL: symbol).

--- src/arm-bootstrap.lisp
+++ src/arm-bootstrap.lisp
@@ -1,4 +1,14 @@
+(in-package "SB!IMPL")
+
 (locally
     (declare (optimize (speed 3)
                        (safety 0)))
-  (defun sb!impl::!cold-init ()))
+  (defun mv-test-callee ()
+    (values 0 1 2 3 4 5))
+  (defun mv-test-caller ()
+    (multiple-value-bind (v0 v1 v2 v3 v4 v5)
+        (mv-test-callee)
+      (values v5 v4 v3 v2 v1 v0)))
+  (defun !cold-init ()
+    (mv-test-caller)
+    nil))
