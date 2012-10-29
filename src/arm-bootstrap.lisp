19f664b7e3aefab12d99bc3c6ed0946f4bf1e522
Alastair Bridgewater <nyef@kana.lisphacker.com>
2012-10-29 11:20:19 -0400

    arm-bootstrap: Test case for lexical non-local exit.
      * Not much to see here, just recording the change in test cases.

--- src/arm-bootstrap.lisp
+++ src/arm-bootstrap.lisp
@@ -5,9 +5,13 @@
   (funcall fun))
 
 (defun test-closures (value)
-  (test-closures-receiver
-   (lambda ()
-     value)))
+  (flet ((closure-test ()
+           (setf value 27)
+           (return-from test-closures
+             13)))
+    (declare (dynamic-extent #'closure-test))
+    (test-closures-receiver #'closure-test)
+    value))
 
 (defun !cold-init ()
   (test-closures 14))
