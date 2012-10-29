d6468dd76fa674bcd6ad56defb4b0f4613ed4bfb
Alastair Bridgewater <nyef@kana.lisphacker.com>
2012-10-29 09:55:09 -0400

    assembly/arm/support: Partial assembly-routine call/return support.
      * Cribbed (quite obviously) from the PPC backend, VM support
    routines for assembly-routine call / return sequences, covering
    the :NONE and :RAW call options (we don't need :FULL-CALL yet, and
    it happens to be significantly more complex, so leave it for later).

--- src/assembly/arm/support.lisp
+++ src/assembly/arm/support.lisp
@@ -11,4 +11,66 @@
 
 (in-package "SB!VM")
 
-;;; Dummy placeholder file.
+(defun generate-call-sequence (name style vop)
+  (ecase style
+    ((:none :raw)
+     (let ((fixup-address (make-symbol "FIXUP-ADDRESS")))
+       (values
+        `((let ((,fixup-address (gen-label)))
+            ,@(if (eq style :none)
+                  `((inst ldr pc-tn (@ ,fixup-address)))
+                  `((inst ldr lr-tn (@ ,fixup-address))
+                    (inst bl lr-tn)))
+            (assemble (*elsewhere* ,vop)
+              (emit-label ,fixup-address)
+              (inst word (make-fixup ',name :assembly-routine)))))
+        nil)))
+    #+(or)
+    (:full-call
+     (let ((temp (make-symbol "TEMP"))
+           (jump (make-symbol "JUMP"))
+           (nfp-save (make-symbol "NFP-SAVE"))
+           (lra (make-symbol "LRA")))
+       (values
+        `((let ((lra-label (gen-label))
+                (cur-nfp (current-nfp-tn ,vop)))
+            (when cur-nfp
+              (store-stack-tn ,nfp-save cur-nfp))
+            (inst compute-lra-from-code ,lra code-tn lra-label ,temp)
+            (note-next-instruction ,vop :call-site)
+            (inst lr ,jump (make-fixup ',name :assembly-routine))
+            (inst mtlr ,jump)
+            (inst blr)
+            (emit-return-pc lra-label)
+            (note-this-location ,vop :single-value-return)
+            (without-scheduling ()
+              (move csp-tn ocfp-tn)
+              (inst nop))
+            (inst compute-code-from-lra code-tn lra-tn
+                  lra-label ,temp)
+            (when cur-nfp
+              (load-stack-tn cur-nfp ,nfp-save))))
+        `((:temporary (:scs (non-descriptor-reg) :from (:eval 0) :to (:eval 1))
+                      ,temp)
+          (:temporary (:sc descriptor-reg :offset lra-offset
+                           :from (:eval 0) :to (:eval 1))
+                      ,lra)
+          (:temporary (:scs (control-stack) :offset nfp-save-offset)
+                      ,nfp-save)
+          (:temporary (:sc any-reg) ,jump)
+          (:save-p :compute-only)))))))
+
+(defun generate-return-sequence (style)
+  (ecase style
+    (:raw
+     `((inst b lr-tn)))
+    #+(or)
+    (:full-call
+     `((lisp-return (make-random-tn :kind :normal
+                                    :sc (sc-or-lose 'descriptor-reg )
+                                    :offset lra-offset)
+                    (make-random-tn :kind :normal
+                                    :sc (sc-or-lose 'interior-reg )
+                                    :offset lip-offset)
+                    :offset 2)))
+    (:none)))
