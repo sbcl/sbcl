e345436f0efaca2c0ba6be2c30ce6b5a3dae3836
Stas Boukarev <stassats@gmail.com>
2013-02-04 16:08:07 +0400

    Fix (documentation closure) test.
    Take into account the idea that
    (setf (documentation #'function t) "doc")
    (setf (documentation 'function 'function) "doc")
    
    should do the same.

--- tests/interface.impure.lisp
+++ tests/interface.impure.lisp
@@ -234,15 +234,14 @@
 
 (with-test (:name (documentation closure))
   (assert (string= (documentation 'docfoo 'function) "bar"))
-  (assert (string= (documentation #'docfoo t) "bar"))
   (assert (string= (setf (documentation 'docfoo 'function) "baz") "baz"))
   (assert (string= (documentation 'docfoo 'function) "baz"))
-  (assert (string= (documentation #'docfoo t) "bar"))
+  (assert (string= (documentation #'docfoo t) "baz"))
   (assert (string= (setf (documentation #'docfoo t) "zot") "zot"))
   (assert (string= (documentation #'docfoo t) "zot"))
-  (assert (string= (documentation 'docfoo 'function) "baz"))
+  (assert (string= (documentation 'docfoo 'function) "zot"))
   (assert (not (setf (documentation 'docfoo 'function) nil)))
-  (assert (string= (documentation 'docfoo 'function) "zot")))
+  (assert (not (documentation 'docfoo 'function))))
 
 (with-test (:name (documentation built-in-macro) :skipped-on '(not :sb-doc))
   (assert (documentation 'trace 'function)))
@@ -310,7 +309,6 @@
   (assert (not
            (equal (documentation 'test 'function)
                   (documentation 'test2 'function)))))
-
 
 
 ;;;; success
