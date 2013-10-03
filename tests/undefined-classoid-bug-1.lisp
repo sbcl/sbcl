a566e334e16d9cd0ff4f6858d796442305fd0f99
Stas Boukarev <stassats@gmail.com>
2013-10-03 22:55:18 +0400

    Add a test-case for the previous commit.

--- tests/undefined-classoid-bug-1.lisp
+++ tests/undefined-classoid-bug-1.lisp
@@ -3,3 +3,5 @@
 (defun a-struct-referencer-1 (struct)
   (a-struct-slot struct))
 
+(defun a-class-typep-2 (x)
+  (typep x 'a-class))
--- tests/undefined-classoid-bug-2.lisp
+++ tests/undefined-classoid-bug-2.lisp
@@ -3,4 +3,10 @@
 (defun a-struct-referencer-2 (struct)
   (a-struct-slot struct))
 
+(defun a-class-typep (x)
+  (typep x 'a-class))
+
 (defstruct a-struct slot)
+
+(defclass a-class ()
+  ())
