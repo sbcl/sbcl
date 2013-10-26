d0f65b07a30adc989e36a82ddc0ed54d135d638e
Stas Boukarev <stassats@gmail.com>
2013-10-26 18:42:22 +0400

    Reduce consing during SUBTYPEP on classes.
    sb-pcl::class-has-a-forward-referenced-superclass-p, used in the
    implementation of subtypep on classes, conses because SOME can't
    perform the required inlining when used on the sequences of unknown
    type. (See lp#1070635)

--- src/pcl/std-class.lisp
+++ src/pcl/std-class.lisp
@@ -852,7 +852,8 @@
   (or (when (forward-referenced-class-p class)
         class)
       (some #'class-has-a-forward-referenced-superclass-p
-            (class-direct-superclasses class))))
+            ;; KLUDGE: SOME conses without knowing the type
+            (the list (class-direct-superclasses class)))))
 
 ;;; This is called by :after shared-initialize whenever a class is initialized
 ;;; or reinitialized. The class may or may not be finalized.
