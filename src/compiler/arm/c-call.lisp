cbadf1b6c9a346170204661128c873140c988527
Alastair Bridgewater <nyef@kana.lisphacker.com>
2014-01-01 21:38:56 -0500

    compiler/arm/c-call: Define VOPs {,DE}ALLOC-NUMBER-STACK-SPACE.
      * These are roughed in based on a couple of different backends,
    plus the constraints involved in the number stack pointer being
    stored in a static symbol, plus ARM being a load/store
    architecture.  They are also untested in practice.

--- src/compiler/arm/c-call.lisp
+++ src/compiler/arm/c-call.lisp
@@ -11,4 +11,28 @@
 
 (in-package "SB!VM")
 
-;;; Dummy placeholder file.
+(defconstant +number-stack-allocation-granularity+ n-word-bytes)
+
+(define-vop (alloc-number-stack-space)
+  (:info amount)
+  (:result-types system-area-pointer)
+  (:results (result :scs (sap-reg any-reg)))
+  (:generator 0
+    (load-symbol-value result number-stack-pointer)
+    (unless (zerop amount)
+      (let ((delta (logandc2 (+ amount (1- +number-stack-allocation-granularity+))
+                             (1- +number-stack-allocation-granularity+))))
+        (inst sub result result delta)
+        (store-symbol-value result number-stack-pointer)))))
+
+(define-vop (dealloc-number-stack-space)
+  (:info amount)
+  (:policy :fast-safe)
+  (:temporary (:scs (unsigned-reg) :to (:result 0)) temp)
+  (:generator 0
+    (unless (zerop amount)
+      (let ((delta (logandc2 (+ amount (1- +number-stack-allocation-granularity+))
+                             (1- +number-stack-allocation-granularity+))))
+        (load-symbol-value temp number-stack-pointer)
+        (inst add temp temp delta)
+        (store-symbol-value temp number-stack-pointer)))))
