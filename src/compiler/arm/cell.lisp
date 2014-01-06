314049808e3d9d5f49bc44e380c1b324ff2d837b
Alastair Bridgewater <nyef@kana.lisphacker.com>
2014-01-06 18:10:07 -0500

    compiler/arm/cell: Implement remaining "closure indexing" VOPs.
      * Cribbed from the MIPS backend.

--- src/compiler/arm/cell.lisp
+++ src/compiler/arm/cell.lisp
@@ -162,6 +162,18 @@
 
 ;;;; Closure indexing.
 
+(define-full-reffer closure-index-ref *
+  closure-info-offset fun-pointer-lowtag
+  (descriptor-reg any-reg) * %closure-index-ref)
+
+(define-full-setter set-funcallable-instance-info *
+  funcallable-instance-info-offset fun-pointer-lowtag
+  (descriptor-reg any-reg null) * %set-funcallable-instance-info)
+
+(define-full-reffer funcallable-instance-info *
+  funcallable-instance-info-offset fun-pointer-lowtag
+  (descriptor-reg any-reg) * %funcallable-instance-info)
+
 (define-vop (closure-ref slot-ref)
   (:variant closure-info-offset fun-pointer-lowtag))
 
