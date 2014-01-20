01b6d4d11c760f9cbb6b5960d2dcb8683970804c
Philipp Matthias Schäfer <philipp.schaefer@gmail.com>
2014-01-20 21:54:07 +0100

    compiler/arm/alloc: added translation VOP LENGTH/LIST, adjusted from PPC version

--- src/compiler/arm/subprim.lisp
+++ src/compiler/arm/subprim.lisp
@@ -12,4 +12,40 @@
 
 (in-package "SB!VM")
 
-;;; Dummy placeholder file.
+(define-vop (length/list)
+  (:translate length)
+  (:args (object :scs (descriptor-reg) :target ptr))
+  (:arg-types list)
+  (:temporary (:scs (descriptor-reg) :from (:argument 0)) ptr)
+  (:temporary (:scs (non-descriptor-reg)) temp)
+  (:temporary (:scs (non-descriptor-reg) :offset ocfp-offset) err-temp)
+  (:temporary (:scs (any-reg) :type fixnum :to (:result 0) :target result)
+              count)
+  (:results (result :scs (any-reg descriptor-reg)))
+  (:policy :fast-safe)
+  (:vop-var vop)
+  (:save-p :compute-only)
+  (:generator 50
+    (let ((done (gen-label))
+          (loop (gen-label))
+          (not-list (gen-label)))
+      (move ptr object)
+      (inst eor count count count)
+
+      (emit-label loop)
+
+      (inst cmp ptr null-tn)
+      (inst b :eq done)
+
+      (test-type ptr not-list t (list-pointer-lowtag) :temp temp)
+
+      (loadw ptr ptr cons-cdr-slot list-pointer-lowtag)
+      (inst add count count (fixnumize 1))
+      (test-type ptr loop nil (list-pointer-lowtag) :temp temp)
+
+      (emit-label not-list)
+
+      (error-call vop err-temp 'object-not-list-error ptr)
+
+      (emit-label done)
+      (move result count))))
