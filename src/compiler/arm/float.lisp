3496b05d118e89cd7eb07f2c681411ceb94b6ff3
Alastair Bridgewater <nyef@kana.lisphacker.com>
2014-04-16 18:33:06 -0400

    compiler/arm/float: A whole lot of conversion VOPs.
      * All the basics, whatever-float to integer.  Whatever-float to
    whatever-other-float.  Integer to whatever-float.  Whatever-float
    to bits.  Bits to whatever-float.
    
      * Cribbed from the PPC backend, then given a good going over to
    account for differences between PPC and ARM VFP architectures.

--- src/compiler/arm/float.lisp
+++ src/compiler/arm/float.lisp
@@ -225,3 +225,202 @@
   (frob < :mi :pl </single-float </double-float nil)
   (frob > :gt :le >/single-float >/double-float nil)
   (frob = :eq :ne eql/single-float eql/double-float t))
+
+;;;; Conversion:
+
+(macrolet ((frob (name translate inst from-sc from-type to-sc to-type)
+            `(define-vop (,name)
+               (:args (x :scs (,from-sc)))
+               (:temporary (:scs (signed-reg)) rtemp)
+               (:results (y :scs (,to-sc)))
+               (:arg-types ,from-type)
+               (:result-types ,to-type)
+               (:policy :fast-safe)
+               (:note "inline float coercion")
+               (:translate ,translate)
+               (:vop-var vop)
+               (:save-p :compute-only)
+               (:generator 5
+                 (inst fmsr rtemp x)
+                 (inst ,inst y rtemp)))))
+  (frob %single-float/signed %single-float fsitos
+        signed-reg signed-num single-reg single-float)
+  (frob %double-float/signed %double-float fsitod
+        signed-reg signed-num double-reg double-float)
+  (frob %single-float/unsigned %single-float fuitos
+        unsigned-reg unsigned-num single-reg single-float)
+  (frob %double-float/unsigned %double-float fuitod
+        unsigned-reg unsigned-num double-reg double-float))
+
+(macrolet ((frob (name translate inst from-sc from-type to-sc to-type)
+             `(define-vop (,name)
+                (:args (x :scs (,from-sc)))
+                (:results (y :scs (,to-sc)))
+                (:arg-types ,from-type)
+                (:result-types ,to-type)
+                (:policy :fast-safe)
+                (:note "inline float coercion")
+                (:translate ,translate)
+                (:vop-var vop)
+                (:save-p :compute-only)
+                (:generator 2
+                  (note-this-location vop :internal-error)
+                  (inst ,inst y x)))))
+  (frob %single-float/double-float %single-float fcvtsd
+    double-reg double-float single-reg single-float)
+  (frob %double-float/single-float %double-float fcvtds
+    single-reg single-float double-reg double-float))
+
+(macrolet ((frob (trans from-sc from-type inst)
+             `(define-vop (,(symbolicate trans "/" from-type))
+                (:args (x :scs (,from-sc) :target temp))
+                (:temporary (:from (:argument 0) :sc single-reg) temp)
+                (:results (y :scs (signed-reg)))
+                (:arg-types ,from-type)
+                (:result-types signed-num)
+                (:translate ,trans)
+                (:policy :fast-safe)
+                (:note "inline float truncate")
+                (:vop-var vop)
+                (:save-p :compute-only)
+                (:generator 5
+                  (note-this-location vop :internal-error)
+                  (inst ,inst temp x)
+                  (inst fmrs y temp)))))
+  (frob %unary-truncate/single-float single-reg single-float ftosizs)
+  (frob %unary-truncate/double-float double-reg double-float ftosizd)
+  (frob %unary-round single-reg single-float ftosis)
+  (frob %unary-round double-reg double-float ftosid))
+
+(define-vop (make-single-float)
+  (:args (bits :scs (signed-reg) :target res
+               :load-if (not (sc-is bits signed-stack))))
+  (:results (res :scs (single-reg)
+                 :load-if (not (sc-is res single-stack))))
+  (:temporary (:scs (signed-reg) :from (:argument 0) :to (:result 0)) temp)
+  (:arg-types signed-num)
+  (:result-types single-float)
+  (:translate make-single-float)
+  (:policy :fast-safe)
+  (:vop-var vop)
+  (:generator 4
+    (sc-case bits
+      (signed-reg
+       (sc-case res
+         (single-reg
+          (inst fmsr res bits))
+         (single-stack
+          (storew bits (current-nfp-tn vop) (tn-offset res)))))
+      (signed-stack
+       (sc-case res
+         (single-reg
+          (inst flds res
+                (@ (current-nfp-tn vop)
+                   (* (tn-offset bits) n-word-bytes))))
+         (single-stack
+          (unless (location= bits res)
+            (loadw temp (current-nfp-tn vop) (tn-offset bits))
+            (storew temp (current-nfp-tn vop) (tn-offset res)))))))))
+
+(define-vop (make-double-float)
+  (:args (hi-bits :scs (signed-reg))
+         (lo-bits :scs (unsigned-reg)))
+  (:results (res :scs (double-reg)
+                 :load-if (not (sc-is res double-stack))))
+  (:arg-types signed-num unsigned-num)
+  (:result-types double-float)
+  (:translate make-double-float)
+  (:policy :fast-safe)
+  (:vop-var vop)
+  (:generator 2
+    (sc-case res
+      (double-reg
+       (inst fmdrr res lo-bits hi-bits))
+      (double-stack
+       (cond
+         ((eq *backend-byte-order* :big-endian)
+          (storew hi-bits (current-nfp-tn vop) (tn-offset res))
+          (storew lo-bits (current-nfp-tn vop) (1+ (tn-offset res))))
+         (t
+          (storew lo-bits (current-nfp-tn vop) (tn-offset res))
+          (storew hi-bits (current-nfp-tn vop) (1+ (tn-offset res)))))))))
+
+(define-vop (single-float-bits)
+  (:args (float :scs (single-reg descriptor-reg)
+                :load-if (not (sc-is float single-stack))))
+  (:results (bits :scs (signed-reg)
+                  :load-if (or (sc-is float descriptor-reg single-stack)
+                               (not (sc-is bits signed-stack)))))
+  (:arg-types single-float)
+  (:result-types signed-num)
+  (:translate single-float-bits)
+  (:policy :fast-safe)
+  (:vop-var vop)
+  (:generator 4
+    (sc-case bits
+      (signed-reg
+       (sc-case float
+         (single-reg
+          (inst fmrs bits float))
+         (single-stack
+          (loadw bits (current-nfp-tn vop) (tn-offset float)))
+         (descriptor-reg
+          (loadw bits float single-float-value-slot other-pointer-lowtag))))
+      (signed-stack
+       (sc-case float
+         (single-reg
+          (inst fsts float (@ (current-nfp-tn vop)
+                              (* (tn-offset bits) n-word-bytes))))
+         ((single-stack descriptor-reg)
+          ;; Fun and games: This also affects PPC, silently.
+          ;; Hopefully it's a non-issue, but I'd rather have the
+          ;; explicit error than a silent miscompilation.
+          (bug "Unable to extract single-float bits from ~S to ~S" float bits)))))))
+
+(define-vop (double-float-high-bits)
+  (:args (float :scs (double-reg descriptor-reg)
+                :load-if (not (sc-is float double-stack))))
+  (:results (hi-bits :scs (signed-reg)))
+  (:arg-types double-float)
+  (:result-types signed-num)
+  (:translate double-float-high-bits)
+  (:policy :fast-safe)
+  (:vop-var vop)
+  (:generator 5
+    (sc-case float
+      (double-reg
+        (inst fmrdh hi-bits float))
+      (double-stack
+        (loadw hi-bits (current-nfp-tn vop)
+               (+ (tn-offset float)
+                  (if (eq *backend-byte-order* :big-endian)
+                      0 1))))
+      (descriptor-reg
+        (loadw hi-bits float (+ double-float-value-slot
+                                (if (eq *backend-byte-order* :big-endian)
+                                    0 1))
+               other-pointer-lowtag)))))
+
+(define-vop (double-float-low-bits)
+  (:args (float :scs (double-reg descriptor-reg)
+                :load-if (not (sc-is float double-stack))))
+  (:results (lo-bits :scs (unsigned-reg)))
+  (:arg-types double-float)
+  (:result-types unsigned-num)
+  (:translate double-float-low-bits)
+  (:policy :fast-safe)
+  (:vop-var vop)
+  (:generator 5
+    (sc-case float
+      (double-reg
+        (inst fmrdl lo-bits float))
+      (double-stack
+        (loadw lo-bits (current-nfp-tn vop)
+               (+ (tn-offset float)
+                  (if (eq *backend-byte-order* :big-endian)
+                      1 0))))
+      (descriptor-reg
+        (loadw lo-bits float (+ double-float-value-slot
+                                (if (eq *backend-byte-order* :big-endian)
+                                    1 0))
+               other-pointer-lowtag)))))
