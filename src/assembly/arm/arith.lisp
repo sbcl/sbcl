25a88c85a3933b550fabd852e0b3965f298dd5c4
Alastair Bridgewater <nyef@kana.lisphacker.com>
2014-04-21 21:50:55 -0400

    assembly/arm/arith: Implement SIGNED/SIGNED=>SIGNED/SIGNED TRUNCATE.
      * Based on the ALPHA version for the basic idea, our existing
    ARM %BIGFLOOR setup for the details of actually doing the division,
    and behavior-checked against a chart of results in Hacker's
    Delight (Henry S. Warren, Jr., 2003) to make sure that the sign
    handling was correct for all combination of input signs.

--- src/assembly/arm/arith.lisp
+++ src/assembly/arm/arith.lisp
@@ -1,3 +1,61 @@
-(in-package "SB!VM")
+;;;; stuff to handle simple cases for generic arithmetic
 
-;;; Dummy placeholder file.
+;;;; This software is part of the SBCL system. See the README file for
+;;;; more information.
+;;;;
+;;;; This software is derived from the CMU CL system, which was
+;;;; written at Carnegie Mellon University and released into the
+;;;; public domain. The software is in the public domain and is
+;;;; provided with absolutely no warranty. See the COPYING and CREDITS
+;;;; files for more information.
+
+(in-package "SB!VM")
+
+;;;; division
+
+(define-assembly-routine (signed-truncate
+                          (:note "(signed-byte 32) truncate")
+                          (:cost 60)
+                          (:policy :fast-safe)
+                          (:translate truncate)
+                          (:arg-types signed-num signed-num)
+                          (:result-types signed-num signed-num))
+
+                         ((:arg dividend signed-reg nl2-offset)
+                          (:arg divisor signed-reg nargs-offset)
+
+                          (:res quo signed-reg nl2-offset)
+                          (:res rem signed-reg nl3-offset)
+
+                          (:temp quo-sign non-descriptor-reg ocfp-offset)
+                          (:temp rem-sign descriptor-reg r8-offset))
+
+  (inst eor quo-sign dividend divisor)
+  (inst bic rem-sign dividend fixnum-tag-mask)
+  (inst mov rem 0)
+
+  (inst tst divisor divisor)
+  ;; KLUDGE: In the error case, we need OCFP as a temp register, but
+  ;; it holds QUO-SIGN right now...  But it's dead as soon as we enter
+  ;; the error case, so ot doesn't matter.
+  (let ((error (generate-error-code nil quo-sign 'division-by-zero-error
+                                    dividend divisor)))
+    (inst b :eq error))
+  (inst rsb :mi divisor divisor 0)
+  (inst tst dividend dividend)
+  (inst rsb :mi dividend dividend 0)
+
+  (dotimes (i 33)
+    (inst cmp rem divisor)
+    (inst sub :hs rem rem divisor)
+    (inst adcs quo quo quo)
+    (unless (= i 32)
+      (inst adc rem rem rem)))
+
+  ;; If the quo-sign is negative, we need to negate quo.
+  (inst tst quo-sign quo-sign)
+  (inst rsb :mi quo quo 0)
+
+  ;; If the rem-sign is negative, we need to negate rem.
+  (inst tst rem-sign rem-sign)
+  (inst rsb :mi rem rem 0))
