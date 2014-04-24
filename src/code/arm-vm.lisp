35f44ecd560027fb7872cb012ba3834515320262
Alastair Bridgewater <nyef@kana.lisphacker.com>
2014-04-24 08:08:47 -0400

    code/arm-vm: Implement FIXUP-CODE-OBJECT.
      * This is used when doing in-core compilation or fasloading to
    patch in things like alien pointers.
    
      * Original structure cribbed from the PPC version.

--- src/code/arm-vm.lisp
+++ src/code/arm-vm.lisp
@@ -5,5 +5,15 @@
 ;;; See x86-vm.lisp for a description of this.
 ;;; FIXME: Why is this present in every ARCH-vm.lisp with the the same definition. Is there something like common-vm?
 (define-alien-type os-context-t (struct os-context-t-struct))
+
+;;;; FIXUP-CODE-OBJECT
 
-;;; Dummy placeholder file.
+(defun fixup-code-object (code offset fixup kind)
+  (declare (type index offset))
+  (unless (zerop (rem offset n-word-bytes))
+    (error "Unaligned instruction?  offset=#x~X." offset))
+  (sb!sys:without-gcing
+   (let ((sap (%primitive sb!kernel::code-instructions code)))
+     (ecase kind
+       (:absolute
+        (setf (sap-ref-32 sap offset) fixup))))))
