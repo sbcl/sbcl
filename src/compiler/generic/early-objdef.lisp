98da9306e37075774be3bf40d131785b03e032d7
Douglas Katzman <dougk@google.com>
2014-05-05 16:40:09 -0400

    Comment all widetag values correctly for 32- and 64-bit
    ... rather than dismissively say that they get screwy.

--- src/compiler/generic/early-objdef.lisp
+++ src/compiler/generic/early-objdef.lisp
@@ -117,6 +117,8 @@
 ;;; * BIGNUM + RATIO (+ FIXNUM) = RATIONAL
 ;;;
 ;;; * SINGLE-FLOAT + DOUBLE-FLOAT + LONG-FLOAT = FLOAT
+;;;   But: There's not a snowball's chance that #!+long-float works.
+;;;        changeset 7646ae obliterated LONG-FLOAT-WIDETAG.
 ;;;
 ;;; * RATIONAL + FLOAT = REAL
 ;;;
@@ -145,111 +147,111 @@
           ;; a long, horrible death.  --njf, 2004-08-09
           :start (+ (ash 1 n-lowtag-bits) other-immediate-0-lowtag)
           :step 4)
-  ;; NOTE: the binary numbers off to the side are only valid for 32-bit
-  ;; ports; add #b1000 if you want to know the values for 64-bit ports.
-  ;; And note that the numbers get a little scrambled further down.
-  ;;   --njf, 2004-08-09
-  bignum-widetag                            ; 00001010
-  ratio-widetag                             ; 00001110
-  single-float-widetag                      ; 00010010
-  double-float-widetag                      ; 00010110
-  complex-widetag                           ; 00011010
-  complex-single-float-widetag              ; 00011110
-  complex-double-float-widetag              ; 00100010
+                                            ; +unicode -unicode
+;;                             Word bits    ;  32 | 64  32 | 64
+                                            ;------------------
+                                            ; [ all numers are hex ]
+  bignum-widetag                            ;  0A   11  0A   11
+  ratio-widetag                             ;  0E   15  0E   15
+  single-float-widetag                      ;  12   19  12   19
+  double-float-widetag                      ;  16   1D  16   1D
+  complex-widetag                           ;  1A   21  1A   21
+  complex-single-float-widetag              ;  1E   25  1E   25
+  complex-double-float-widetag              ;  22   29  22   29
 
-  code-header-widetag                       ; 00100110
+  code-header-widetag                       ;  26   2D  26   2D
 
-  simple-fun-header-widetag                 ; 00101010
-  closure-header-widetag                    ; 00101110
-  funcallable-instance-header-widetag       ; 00110010
+  simple-fun-header-widetag                 ;  2A   31  2A   31
+  closure-header-widetag                    ;  2E   35  2E   35
+  funcallable-instance-header-widetag       ;  32   39  32   39
 
-  return-pc-header-widetag                  ; 00110110
-  value-cell-header-widetag                 ; 00111010
-  symbol-header-widetag                     ; 00111110
-  character-widetag                         ; 01000010
-  sap-widetag                               ; 01000110
-  unbound-marker-widetag                    ; 01001010
-  weak-pointer-widetag                      ; 01001110
-  instance-header-widetag                   ; 01010010
-  fdefn-widetag                             ; 01010110
+  return-pc-header-widetag                  ;  36   3D  36   3D
+  value-cell-header-widetag                 ;  3A   41  3A   41
+  symbol-header-widetag                     ;  3E   45  3E   45
+  character-widetag                         ;  42   49  42   49
+  sap-widetag                               ;  46   4D  46   4D
+  unbound-marker-widetag                    ;  4A   51  4A   51
+  weak-pointer-widetag                      ;  4E   55  4E   55
+  instance-header-widetag                   ;  52   59  52   59
+  fdefn-widetag                             ;  56   5D  56   5D
 
-  no-tls-value-marker-widetag               ; 01011010
+  no-tls-value-marker-widetag               ;  5A   61  5A   61
   #!-sb-simd-pack
-  unused01-widetag
+  unused01-widetag                          ;  5E       5E
   #!+sb-simd-pack
-  simd-pack-widetag                         ; 01011110
-  unused02-widetag                          ; 01100010
-  unused03-widetag                          ; 01100110
-  unused04-widetag                          ; 01101010
-  unused05-widetag                          ; 01101110
-  unused06-widetag                          ; 01110010
-  unused07-widetag                          ; 01110110
+  simd-pack-widetag                         ;       65       65
+  unused02-widetag                          ;  62   69  62   69
+  unused03-widetag                          ;  66   6D  66   6D
+  unused04-widetag                          ;  6A   71  6A   71
+  unused05-widetag                          ;  6E   75  6E   75
+  unused06-widetag                          ;  72   79  72   79
+  unused07-widetag                          ;  76   7D  76   7D
   #!+#.(cl:if (cl:= 32 sb!vm:n-word-bits) '(and) '(or))
-  unused08-widetag                          ; 01111010
+  unused08-widetag                          ;  7A       7A
   #!+#.(cl:if (cl:= 32 sb!vm:n-word-bits) '(and) '(or))
-  unused09-widetag                          ; 01111110
+  unused09-widetag                          ;  7E       7E
 
   #!+#.(cl:if (cl:= 32 sb!vm:n-word-bits) '(and) '(or))
-  unused10-widetag                          ; 10000010
+  unused10-widetag                          ;  82       82
   #!+#.(cl:if (cl:= 32 sb!vm:n-word-bits) '(and) '(or))
-  unused11-widetag                          ; 10000110
+  unused11-widetag                          ;  86       86
 
-  simple-array-widetag                      ; 10001010
-  simple-array-unsigned-byte-2-widetag      ; 10001110
-  simple-array-unsigned-byte-4-widetag      ; 10010010
-  simple-array-unsigned-byte-7-widetag      ; 10010110
-  simple-array-unsigned-byte-8-widetag      ; 10011010
-  simple-array-unsigned-byte-15-widetag     ; 10011110
-  simple-array-unsigned-byte-16-widetag     ; 10100010
+  simple-array-widetag                      ;  8A   81  8A   81
+  simple-array-unsigned-byte-2-widetag      ;  8E   85  8E   85
+  simple-array-unsigned-byte-4-widetag      ;  92   89  92   89
+  simple-array-unsigned-byte-7-widetag      ;  96   8D  96   8D
+  simple-array-unsigned-byte-8-widetag      ;  9A   91  9A   91
+  simple-array-unsigned-byte-15-widetag     ;  9E   95  9E   95
+  simple-array-unsigned-byte-16-widetag     ;  A2   99  A2   99
 
   #!+#.(cl:if (cl:= 32 sb!vm:n-word-bits) '(and) '(or))
-  simple-array-unsigned-fixnum-widetag      ; 10100110
-  simple-array-unsigned-byte-31-widetag     ; 10101010
-  simple-array-unsigned-byte-32-widetag     ; 10101110
+  simple-array-unsigned-fixnum-widetag      ;  A6   A5  A6   A5
+  simple-array-unsigned-byte-31-widetag     ;  AA   9D  AA   9D
+  simple-array-unsigned-byte-32-widetag     ;  AE   A1  AE   A1
   #!+#.(cl:if (cl:= 64 sb!vm:n-word-bits) '(and) '(or))
-  simple-array-unsigned-fixnum-widetag
+  simple-array-unsigned-fixnum-widetag      ;  A6   A5  A6   A5
   #!+#.(cl:if (cl:= 64 sb!vm:n-word-bits) '(and) '(or))
-  simple-array-unsigned-byte-63-widetag
+  simple-array-unsigned-byte-63-widetag     ;       A9       A9
   #!+#.(cl:if (cl:= 64 sb!vm:n-word-bits) '(and) '(or))
-  simple-array-unsigned-byte-64-widetag
-  simple-array-signed-byte-8-widetag        ; 10110010
-  simple-array-signed-byte-16-widetag       ; 10110110
+  simple-array-unsigned-byte-64-widetag     ;       AD       AD
+  simple-array-signed-byte-8-widetag        ;  B2   B1  B2   B1
+  simple-array-signed-byte-16-widetag       ;  B6   B5  B6   B5
   #!+#.(cl:if (cl:= 32 sb!vm:n-word-bits) '(and) '(or))
-  simple-array-fixnum-widetag               ; 10111010
-  simple-array-signed-byte-32-widetag       ; 10111110
+  simple-array-fixnum-widetag               ;  BA   BD  BA   BD
+  simple-array-signed-byte-32-widetag       ;  BE   B9  BE   B9
   #!+#.(cl:if (cl:= 64 sb!vm:n-word-bits) '(and) '(or))
-  simple-array-fixnum-widetag
+  simple-array-fixnum-widetag               ;  BA   BD  BA   BD
   #!+#.(cl:if (cl:= 64 sb!vm:n-word-bits) '(and) '(or))
-  simple-array-signed-byte-64-widetag
-  simple-array-single-float-widetag         ; 11000010
-  simple-array-double-float-widetag         ; 11000110
-  simple-array-complex-single-float-widetag ; 11001010
-  simple-array-complex-double-float-widetag ; 11001110
-  simple-bit-vector-widetag                 ; 11010010
-  simple-vector-widetag                     ; 11010110
+  simple-array-signed-byte-64-widetag       ;       C1       C1
+  simple-array-single-float-widetag         ;  C2   C5  C2   C5
+  simple-array-double-float-widetag         ;  C6   C9  C6   C9
+  simple-array-complex-single-float-widetag ;  CA   CD  CA   CD
+  simple-array-complex-double-float-widetag ;  CE   D1  CE   D1
+  simple-bit-vector-widetag                 ;  D2   D5  D2   D5
+  simple-vector-widetag                     ;  D6   D9  D6   D9
 
   ;; Strings
-  simple-array-nil-widetag                  ; 11011010
-  simple-base-string-widetag                ; 11011110
+  simple-array-nil-widetag                  ;  DA   DD  DA   DD
+  simple-base-string-widetag                ;  DE   E1  DE   E1
   #!+sb-unicode
-  simple-character-string-widetag
+  simple-character-string-widetag           ;  E2   E5
   #!+sb-unicode
-  complex-character-string-widetag          ; 11100110
-  complex-base-string-widetag               ; 11101010
-  complex-vector-nil-widetag                ; 11101110
+  complex-character-string-widetag          ;  E6   E9
+  complex-base-string-widetag               ;  EA   ED  E2   E5
+  complex-vector-nil-widetag                ;  EE   F1  E6   E9
 
-  complex-bit-vector-widetag                ; 11110010
-  complex-vector-widetag                    ; 11110110
-  complex-array-widetag                     ; 11111010
+  complex-bit-vector-widetag                ;  F2   F5  EA   ED
+  complex-vector-widetag                    ;  F6   F9  EE   F1
+  complex-array-widetag                     ;  FA   FD  F2   F5
 
   #!+#.(cl:if (cl:= 32 sb!vm:n-word-bits) '(and) '(or))
-  unused12-widetag                          ; 11111110
+  unused12-widetag                          ;  FE       F6
   #!+(and #.(cl:if (cl:= 32 sb!vm:n-word-bits) '(and) '(or))
           (not sb-unicode))
-  unused13-widetag                          ; 11111010
+  unused13-widetag                          ;           FA
   #!+(and #.(cl:if (cl:= 32 sb!vm:n-word-bits) '(and) '(or))
           (not sb-unicode))
-  unused14-widetag                          ; 11111110
+  unused14-widetag                          ;           FE
 )
 
 ;;; the different vector subtypes
@@ -257,3 +259,43 @@
   vector-normal-subtype
   vector-unused-subtype
   vector-valid-hashing-subtype)
+
+#|
+;; Run this in the SB-VM or SB!VM package once for each target feature combo.
+(defun rewrite-widetag-comments ()
+  (rename-file "src/compiler/generic/early-objdef.lisp" "early-objdef.old")
+  (with-open-file (in "src/compiler/generic/early-objdef.old")
+    (with-open-file (out "src/compiler/generic/early-objdef.lisp"
+                         :direction :output :if-exists :supersede)
+      (let* ((target-features
+              (if (find-package "SB-COLD")
+                  (symbol-value (find-symbol "*SHEBANG-FEATURES*" "SB-COLD"))
+                  *features*))
+             (feature-bits
+              (+ (if (= n-word-bits 64) 1 0)
+                 (if (member :sb-unicode target-features) 0 2)))
+             (comment-col)
+             (comment-offset (aref #(3 8 12 17) feature-bits))
+             (state 0))
+        (loop
+         (let* ((line (read-line in nil))
+                (trimmed (and line (string-left-trim " " line)))
+                symbol)
+           (unless line
+             (return))
+           (when (and (zerop state) (eql 0 (search "bignum-widetag" trimmed)))
+             (setq state 1 comment-col (position #\; line)))
+           (if (and (= state 1) (plusp (length trimmed))
+                    (alpha-char-p (char trimmed 0))
+                    (boundp (setq symbol (read-from-string trimmed))))
+               (let ((new (make-string (+ comment-col 19)
+                                       :initial-element #\Space)))
+                 (replace new line)
+                 (replace new (format nil "~2,'0X" (symbol-value symbol))
+                          :start1 (+ comment-col comment-offset))
+                 (write-line new out))
+               (progn
+                 (write-line line out)
+                 (if (and (= state 1) (string= line ")"))
+                     (setq state 2))))))))))
+|#
