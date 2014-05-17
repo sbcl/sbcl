8dc4c5d58565bec05271730eaf50d1704281c8d3
Stas Boukarev <stassats@gmail.com>
2014-05-18 03:50:58 +0400

    Adjust control-stack size on ARM.
    Now that copy-more-arg is fixed, it can be reduced, set size to 2 and
    size-increment to 1.

--- src/compiler/arm/vm.lisp
+++ src/compiler/arm/vm.lisp
@@ -67,7 +67,7 @@
 ;;;; SB and SC definition:
 
 (define-storage-base registers :finite :size 16)
-(define-storage-base control-stack :unbounded :size 8)
+(define-storage-base control-stack :unbounded :size 2 :size-increment 1)
 (define-storage-base non-descriptor-stack :unbounded :size 0)
 (define-storage-base constant :non-packed)
 (define-storage-base immediate-constant :non-packed)
