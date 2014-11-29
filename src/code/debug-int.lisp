sbcl/sbcl

commit ea264887b5e3fc106a234c55867cbee4b10368b0

Fix trace :encapsulate nil on x86.
It never actually worked, it construct a bogus code object without
provision for the fixup vector.

src/code/debug-int.lisp
@@ -502,7 +502,10 @@
 ;;; debug-info slot is :BOGUS-LRA, then the REAL-LRA-SLOT contains the
 ;;; real component to continue executing, as opposed to the bogus
 ;;; component which appeared in some frame's LRA location.
-(defconstant real-lra-slot sb!vm:code-constants-offset)
+(defconstant real-lra-slot
+  ;; X86 stores a fixup vector at the first constant slot
+  #!-x86 sb!vm:code-constants-offset
+  #!+x86 (1+ sb!vm:code-constants-offset))
 
 ;;; These are magically converted by the compiler.
 (defun current-sp () (current-sp))
@@ -3200,9 +3203,12 @@ register."
 ;;;; MAKE-BOGUS-LRA (used for :FUN-END breakpoints)
 
 (defconstant bogus-lra-constants
-  #!-(or x86 x86-64) 2 #!+(or x86 x86-64) 3)
+  #!-(or x86 x86-64) 2 #!+x86-64 3
+  ;; One more for a fixup vector
+  #!+x86 4)
+
 (defconstant known-return-p-slot
-  (+ sb!vm:code-constants-offset #!-(or x86 x86-64) 1 #!+(or x86 x86-64) 2))
+  (+ sb!vm:code-constants-offset (1- bogus-lra-constants)))
 
 ;;; Make a bogus LRA object that signals a breakpoint trap when
 ;;; returned to. If the breakpoint trap handler returns, REAL-LRA is
@@ -3218,7 +3224,7 @@ register."
           (trap-loc (static-foreign-symbol-sap "fun_end_breakpoint_trap"))
           (length (sap- src-end src-start))
           (code-object
-           (sb!c:allocate-code-object (1+ bogus-lra-constants) length))
+            (sb!c:allocate-code-object bogus-lra-constants length))
           (dst-start (code-instructions code-object)))
      (declare (type system-area-pointer
                     src-start src-end dst-start trap-loc)

src/runtime/breakpoint.c
@@ -25,13 +25,18 @@
 #include "genesis/code.h"
 #include "genesis/fdefn.h"
 
+#ifdef LISP_FEATURE_X86_64
 #define REAL_LRA_SLOT 0
-#if !(defined(LISP_FEATURE_X86) || defined(LISP_FEATURE_X86_64))
-#define KNOWN_RETURN_P_SLOT 1
-#define BOGUS_LRA_CONSTANTS 2
-#else
 #define KNOWN_RETURN_P_SLOT 2
 #define BOGUS_LRA_CONSTANTS 3
+#elif defined(LISP_FEATURE_X86)
+#define REAL_LRA_SLOT 1
+#define KNOWN_RETURN_P_SLOT 3
+#define BOGUS_LRA_CONSTANTS 4
+#else
+#define REAL_LRA_SLOT 0
+#define KNOWN_RETURN_P_SLOT 1
+#define BOGUS_LRA_CONSTANTS 2
 #endif
 
 static void *compute_pc(lispobj code_obj, int pc_offset)

commit 761ebacccaee6c7b61a352b91a3f06111d8e19e7

Fix trace :encapsulate nil.
Non x86oids had a regression since ea26488.

And x86oids were always broken when it came to stacked return values,
using the wrong offsets.
And x86-64 was broken because of the N-FIXNUM-TAG-BITS change.

src/code/debug-int.lisp
@@ -3192,65 +3192,76 @@ register."
         (reg-arg-offsets '#.sb!vm::*register-arg-offsets*)
         (results nil))
     (without-gcing
-     (dotimes (arg-num nargs)
-       (push (if reg-arg-offsets
-                 (make-lisp-obj
-                  (sb!vm:context-register scp (pop reg-arg-offsets)))
-               (stack-ref ocfp arg-num))
-             results)))
+      (dotimes (arg-num nargs)
+        (push (if reg-arg-offsets
+                  (make-lisp-obj
+                   (sb!vm:context-register scp (pop reg-arg-offsets)))
+                  (stack-ref ocfp (+ arg-num
+                                     #!+(or x86 x86-64) sb!vm::sp->fp-offset)))
+              results)))
     (nreverse results)))
 
 ;;;; MAKE-BOGUS-LRA (used for :FUN-END breakpoints)
 
 (defconstant bogus-lra-constants
-  #!-(or x86 x86-64) 2 #!+x86-64 3
+  #!-(or x86-64 x86) 1
+  #!+x86-64 2
   ;; One more for a fixup vector
-  #!+x86 4)
-
-(defconstant known-return-p-slot
-  (+ sb!vm:code-constants-offset (1- bogus-lra-constants)))
+  #!+x86 3)
 
 ;;; Make a bogus LRA object that signals a breakpoint trap when
 ;;; returned to. If the breakpoint trap handler returns, REAL-LRA is
 ;;; returned to. Three values are returned: the bogus LRA object, the
 ;;; code component it is part of, and the PC offset for the trap
 ;;; instruction.
-(defun make-bogus-lra (real-lra &optional known-return-p)
+(defun make-bogus-lra (real-lra)
   (without-gcing
-   ;; These are really code labels, not variables: but this way we get
-   ;; their addresses.
-   (let* ((src-start (static-foreign-symbol-sap "fun_end_breakpoint_guts"))
-          (src-end (static-foreign-symbol-sap "fun_end_breakpoint_end"))
-          (trap-loc (static-foreign-symbol-sap "fun_end_breakpoint_trap"))
-          (length (sap- src-end src-start))
-          (code-object
-            (sb!c:allocate-code-object bogus-lra-constants length))
-          (dst-start (code-instructions code-object)))
-     (declare (type system-area-pointer
-                    src-start src-end dst-start trap-loc)
-              (type index length))
-     (setf (%code-debug-info code-object) :bogus-lra)
-     #!-(or x86 x86-64)
-     (setf (code-header-ref code-object real-lra-slot) real-lra)
-     #!+(or x86 x86-64)
-     (multiple-value-bind (offset code) (compute-lra-data-from-pc real-lra)
-       (setf (code-header-ref code-object real-lra-slot) code)
-       (setf (code-header-ref code-object (1+ real-lra-slot)) offset))
-     (setf (code-header-ref code-object known-return-p-slot)
-           known-return-p)
-     (system-area-ub8-copy src-start 0 dst-start 0 length)
-     #!-(or x86 x86-64)
-     (sb!vm:sanctify-for-execution code-object)
-     #!+(or x86 x86-64)
-     (values dst-start code-object (sap- trap-loc src-start))
-     #!-(or x86 x86-64)
-     (let ((new-lra (make-lisp-obj (+ (sap-int dst-start)
-                                      sb!vm:other-pointer-lowtag))))
-       ;; We used to set the header value of the LRA here to the
-       ;; offset from the enclosing component to the LRA header, but
-       ;; MAKE-LISP-OBJ actually checks the value before we get a
-       ;; chance to set it, so it's now done in arch-assem.S.
-       (values new-lra code-object (sap- trap-loc src-start))))))
+    ;; These are really code labels, not variables: but this way we get
+    ;; their addresses.
+    (let* ((src-start (static-foreign-symbol-sap "fun_end_breakpoint_guts"))
+           (src-end (static-foreign-symbol-sap "fun_end_breakpoint_end"))
+           (trap-loc (static-foreign-symbol-sap "fun_end_breakpoint_trap"))
+           (length (sap- src-end src-start))
+           (code-object
+             (sb!c:allocate-code-object bogus-lra-constants length))
+           (dst-start (code-instructions code-object)))
+      (declare (type system-area-pointer
+                     src-start src-end dst-start trap-loc)
+               (type index length))
+      (setf (%code-debug-info code-object) :bogus-lra)
+      #!-(or x86 x86-64)
+      (setf (code-header-ref code-object real-lra-slot) real-lra
+            ;; Set up the widetag and header of LRA
+            ;; The header contains the same thing as the code object header,
+            ;; the number of boxed words, which include slots and
+            ;; constants and it has to be double word aligned.
+            ;;
+            ;; It used to be a part of the fun_end_breakpoint_guts
+            ;; but its position and value depend on the offsets
+            ;; and alignment of code object slots.
+            (sap-ref-word dst-start (- sb!vm:n-word-bits))
+            (+ sb!vm:return-pc-header-widetag
+               (logandc2 (+ code-constants-offset
+                            bogus-lra-constants
+                            2)
+                         3)))
+      #!+(or x86 x86-64)
+      (multiple-value-bind (offset code) (compute-lra-data-from-pc real-lra)
+        (setf (code-header-ref code-object real-lra-slot) code)
+        (setf (code-header-ref code-object (1+ real-lra-slot)) offset))
+      (system-area-ub8-copy src-start 0 dst-start 0 length)
+      #!-(or x86 x86-64)
+      (sb!vm:sanctify-for-execution code-object)
+      #!+(or x86 x86-64)
+      (values dst-start code-object (sap- trap-loc src-start))
+      #!-(or x86 x86-64)
+      (let ((new-lra (make-lisp-obj (+ (sap-int dst-start)
+                                       sb!vm:other-pointer-lowtag))))
+        ;; We used to set the header value of the LRA here to the
+        ;; offset from the enclosing component to the LRA header, but
+        ;; MAKE-LISP-OBJ actually checks the value before we get a
+        ;; chance to set it, so it's now done in arch-assem.S.
+        (values new-lra code-object (sap- trap-loc src-start))))))
 
 ;;;; miscellaneous
 

src/runtime/alpha-assem.S
@@ -330,7 +330,7 @@ funcallable_instance_tramp = . + 1
 	.set	noreorder
 	.globl	fun_end_breakpoint_guts
 fun_end_breakpoint_guts:
-	.long	RETURN_PC_HEADER_WIDETAG + 0x800
+	.long	RETURN_PC_HEADER_WIDETAG + 0x600
 	br	zero, fun_end_breakpoint_trap
 	nop
 	mov	reg_CSP, reg_OCFP

src/runtime/breakpoint.c
@@ -186,12 +186,10 @@ void *handle_fun_end_breakpoint(os_context_t *context)
 #ifdef LISP_FEATURE_PPC
     /* PPC now passes LRA objects in reg_LRA during return.  Other
      * platforms should as well, but haven't been fixed yet. */
-    if (codeptr->constants[KNOWN_RETURN_P_SLOT] == NIL)
-        *os_context_register_addr(context, reg_LRA) = lra;
+    *os_context_register_addr(context, reg_LRA) = lra;
 #else
 #ifdef reg_CODE
-    if (codeptr->constants[KNOWN_RETURN_P_SLOT] == NIL)
-        *os_context_register_addr(context, reg_CODE) = lra;
+    *os_context_register_addr(context, reg_CODE) = lra;
 #endif
 #endif
 

src/runtime/hppa-assem.S
@@ -444,7 +444,7 @@ SingleStepTraps:
 	.align	8
 	.export fun_end_breakpoint_guts
 fun_end_breakpoint_guts:
-	.word	RETURN_PC_HEADER_WIDETAG + 0x800
+	.word	RETURN_PC_HEADER_WIDETAG + 0x600
 	/* multiple value return point -- just jump to trap. */
 	b,n	fun_end_breakpoint_trap
 	/* single value return point -- convert to multiple w/ n=1 */

src/runtime/mips-assem.S
@@ -455,7 +455,7 @@ funcallable_instance_tramp = . + 1
 	.align  3 /* minimum alignment for a lisp object */
 	LEAF(fun_end_breakpoint_guts)
 	.set    noreorder
-	.word   RETURN_PC_HEADER_WIDETAG + 0x800
+	.word   RETURN_PC_HEADER_WIDETAG + 0x600
 	b       multiple_value_return
 	 nop
 	.set    reorder

src/runtime/ppc-assem.S
@@ -707,12 +707,13 @@ CSYMBOL(funcallable_instance_tramp) = . + 1
 
 	/* Due to pointer verification in MAKE-LISP-OBJ, this must
 	include its header data (the offset from the start of the
-	code-object to the LRA).  The code-object header is five
-	words, there are two words of constants, and the instruction
-	space is doubleword-aligned, making an offset of eight.
+	code-object to the LRA).  The code-object header is 4
+	words, there are 1 word of constants, and the instruction
+	space is doubleword-aligned, making an offset of six.
 	This is header data for a widetag, so shift left eight bits
 	and add. */
-	.long RETURN_PC_HEADER_WIDETAG + 0x800
+        /* FIXME: the above is full of magic numbers. */
+	.long RETURN_PC_HEADER_WIDETAG + 0x600
 
 	/* We are receiving unknown multiple values, thus must deal
 	with the single-value and multiple-value cases separately. */
@@ -724,11 +725,11 @@ CSYMBOL(funcallable_instance_tramp) = . + 1
 	reg_LRA (where the LRA is stored by the return sequence) to
 	obtain a tagged pointer to the enclosing code component.  Both
 	values are tagged OTHER_POINTER_LOWTAG, so we just have to
-	account for the eight words (see calculation for
+	account for the six words (see calculation for
 	RETURN_PC_HEADER_WIDETAG, above) between the two addresses.
 	Restoring reg_CODE doesn't appear to be strictly necessary
 	here, but let's observe the niceties.*/
-	addi reg_CODE, reg_LRA, -32
+	addi reg_CODE, reg_LRA, -24
 
 	/* Multiple values are stored relative to reg_OCFP, which we
 	set to be the current top-of-stack. */
@@ -751,7 +752,7 @@ CSYMBOL(funcallable_instance_tramp) = . + 1
 fun_end_breakpoint_multiple_values:
 	/* Compute the correct value for reg_CODE.  See the
 	explanation for the single-value case, above. */
-	addi reg_CODE, reg_LRA, -32
+	addi reg_CODE, reg_LRA, -24
 
 	/* The actual magic trap. */
 CSYMBOL(fun_end_breakpoint_trap):

src/runtime/sparc-assem.S
@@ -274,7 +274,7 @@ funcallable_instance_tramp = . + 1
 	.align	8
 	.global	fun_end_breakpoint_guts
 fun_end_breakpoint_guts:
-	.word	RETURN_PC_HEADER_WIDETAG + 0x800
+	.word	RETURN_PC_HEADER_WIDETAG + 0x600
 	b	1f
 	nop
 	mov	reg_CSP, reg_OCFP

src/runtime/x86-64-assem.S
@@ -438,14 +438,9 @@ GNAME(fun_end_breakpoint_guts):
 	   count of one. */
 	mov	%rsp,%rbx	# Setup ebx - the ofp.
 	sub	$8,%rsp		# Allocate one stack slot for the return value
-	mov	$8,%rcx		# Setup ecx for one return value.
-#if defined(LISP_FEATURE_DARWIN)
+	mov	$(1 << N_FIXNUM_TAG_BITS),%rcx		# Setup ecx for one return value.
 	mov	GSYM(NIL),%rdi	# default second value
 	mov	GSYM(NIL),%rsi	# default third value
-#else
-	mov	$NIL,%rdi	# default second value
-	mov	$NIL,%rsi	# default third value
-#endif
 multiple_value_return:
 	
 	.globl  GNAME(fun_end_breakpoint_trap)

src/runtime/x86-assem.S
@@ -571,7 +571,7 @@ GNAME(fun_end_breakpoint_guts):
 	   count of one. */
 	movl	%esp,%ebx	# Setup ebx - the ofp.
 	subl	$4,%esp		# Allocate one stack slot for the return value
-	movl	$4,%ecx		# Setup ecx for one return value.
+	movl	$(1 << N_FIXNUM_TAG_BITS),%ecx		# Setup ecx for one return value.
 	movl	$(NIL),%edi	# default second value
 	movl	$(NIL),%esi	# default third value
 		