sbcl/sbcl

commit a4393e3ddd0e6e7ab814ed7b6f8481b9442a7f66

Improve efficiency of DO-REST-ARG and make some warnings disappear.
Warnings resulted from "certainty" that one branch of an IF led to
a type violation error despite being dead code.
pvk said this would be difficult to handle in constraint propagation,
so the current patch forces the issue.

src/code/numbers.lisp
@@ -384,8 +384,8 @@
   or with one argument, negate the first argument."
   (if more-numbers
       (let ((result number))
-        (dotimes (i (length more-numbers) result)
-          (setf result (- result (nth i more-numbers)))))
+        (do-rest-arg ((n) more-numbers 0 result)
+          (setf result (- result n))))
       (- number)))
 
 (defun / (number &rest more-numbers)
@@ -394,8 +394,8 @@
   With one argument, return reciprocal."
   (if more-numbers
       (let ((result number))
-        (dotimes (i (length more-numbers) result)
-          (setf result (/ result (nth i more-numbers)))))
+        (do-rest-arg ((n) more-numbers 0 result)
+          (setf result (/ result n))))
       (/ number)))
 
 (defun 1+ (number)

src/compiler/fndb.lisp
@@ -1526,7 +1526,7 @@
 ;;;; magical compiler frobs
 
 (defknown %rest-values (t t t) * (always-translatable))
-(defknown %rest-ref (t t t t) * (always-translatable))
+(defknown %rest-ref (t t t t &optional boolean) * (always-translatable))
 (defknown %rest-length (t t t) * (always-translatable))
 (defknown %rest-null (t t t t) * (always-translatable))
 (defknown %rest-true (t t t) * (always-translatable))

src/compiler/srctran.lisp
@@ -4389,6 +4389,11 @@
     (if context
         `(%rest-ref ,n ,list ,context ,count)
         `(car (nthcdr ,n ,list)))))
+(define-source-transform fast-&rest-nth (n list)
+  (multiple-value-bind (context count) (possible-rest-arg-context list)
+    (if context
+        `(%rest-ref ,n ,list ,context ,count t)
+        (bug "no &REST context for FAST-REST-NTH"))))
 
 (define-source-transform elt (seq n)
   (if (policy *lexenv* (= safety 3))
@@ -4450,10 +4455,12 @@
       `(%more-arg-values context 0 count)
       `(values-list list)))
 
-(deftransform %rest-ref ((n list context count))
+(deftransform %rest-ref ((n list context count &optional length-checked-p))
   (cond ((rest-var-more-context-ok list)
-         `(and (< (the index n) count)
-               (%more-arg context n)))
+         (if (and (constant-lvar-p length-checked-p)
+                  (lvar-value length-checked-p))
+             `(%more-arg context n)
+             `(and (< (the index n) count) (%more-arg context n))))
         ((and (constant-lvar-p n) (zerop (lvar-value n)))
          `(car list))
         (t

src/code/sysmacs.lisp
@@ -249,16 +249,18 @@ maintained."
               (locally ,@body)
            (setf (ansi-stream-in-index ,f-stream) ,f-index))))))
 
-(defmacro do-rest-arg (((var &optional index) rest-var
+;; This is an internal-use-only macro.
+(defmacro do-rest-arg (((var &optional index-var) rest-var
                         &optional (start 0) result)
                        &body body)
   ;; If the &REST arg never needs to be reified, this is slightly quicker
   ;; than using a DX list.
-  (let ((index (or index (sb!xc:gensym "INDEX"))))
+  (let ((index (sb!xc:gensym "INDEX")))
     `(let ((,index ,start))
        (loop
         (cond ((< (truly-the index ,index) (length ,rest-var))
-               (let ((,var (nth ,index ,rest-var)))
+               (let ((,var (sb!c::fast-&rest-nth ,index ,rest-var))
+                     ,@(if index-var `((,index-var ,index))))
                  ,@body)
                (incf ,index))
               (t

commit b2936b759580b9f3ceb5ff3501d8e70c98e7eb13

Fix regression in &REST -> &MORE conversion caused by change a4393e3d.
1) A REF node to &REST var with no lvar-dest made REST-VAR-MORE-CONTEXT-OK
   think that the &REST arg had to be listified. I couldn't figure out
   why this wasn't an issue prior to the aforementioned revision,
   but it seems not to have been, though maybe it was.

2) The + and - functions should have used DO-REST-ARG but didn't,
   only because I didn't get to them yet, which should not have
   been a problem, except that their code actually got worse without.

Note- The problem that DO-REST-ARG tries to deal with is that %REST-REF
has an internal check for whether the index arg exceeds the more-count
because NTH is perfectly valid for N greater than the length of its
input list, and in general the user might write (NTH huge-num rest-arg).
But DO-REST-ARG never does that. Unfortunately the equivalent expressions
  (IF REST-VAR (CAR REST-VAR))
  (IF (> (LENGTH REST-VAR) 0) (NTH 0 REST-VAR))
  (IF (> MORE-COUNT 0) (NTH 0 REST-VAR))
would all effectively become
  (IF (> MORE-COUNT 0) (IF (< 0 MORE-COUNT) (%MORE-ARG...) NIL))
which contains two consecutive tests of the same condition.
Not only is constraint propagation unable to elide one test,
the compiler was sure that the "else" case of the inner IF causes
a type error in the form (THE NUMBER (<mumble>)).

src/code/numbers.lisp
@@ -363,15 +363,9 @@
                 #!+sb-doc
                 ,doc
                 (if numbers
-                    ;; FIXME: using NTH here produces
-                    ;; "caught STYLE-WARNING:
-                    ;;  The binding of RESULT is not a NUMBER"
-                    ;; Same warning occurs in -, /, =, /=, etc
-                    (do ((result (nth 0 numbers) (,op result (nth i numbers)))
-                         (i 1 (1+ i)))
-                        ((>= i (length numbers))
-                         result)
-                      (declare (number result)))
+                    (let ((result (the number (sb!c::fast-&rest-nth 0 numbers))))
+                      (do-rest-arg ((n) numbers 1 result)
+                        (setq result (,op result n))))
                     ,init))))
   (define-arith + 0
     "Return the sum of its arguments. With no args, returns 0.")

src/compiler/fndb.lisp
@@ -750,7 +750,7 @@
   (foldable flushable call))
 (defknown endp (list) boolean (foldable flushable movable))
 (defknown list-length (list) (or index null) (foldable unsafely-flushable))
-(defknown nth (unsigned-byte list) t (foldable flushable))
+(defknown (nth fast-&rest-nth) (unsigned-byte list) t (foldable flushable))
 (defknown nthcdr (unsigned-byte list) t (foldable unsafely-flushable))
 
 (defknown last (list &optional unsigned-byte) t (foldable flushable))

src/compiler/srctran.lisp
@@ -4351,6 +4351,8 @@
          (info (when (lambda-var-p var) (lambda-var-arg-info var)))
          (restp (when info (eq :rest (arg-info-kind info)))))
     (flet ((ref-good-for-more-context-p (ref)
+             (when (not (node-lvar ref)) ; ref that goes nowhere is ok
+               (return-from ref-good-for-more-context-p t))
              (let ((dest (principal-lvar-end (node-lvar ref))))
                (and (combination-p dest)
                     ;; If the destination is to anything but these, we're going to