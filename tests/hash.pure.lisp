e4dd5c23ae34af183e80410b81231c0766fea69d
Lutz Euler <lutz.euler@freenet.de>
2015-03-07 16:53:00 +0100

    Use WITH-TEST in hash.pure.lisp.
    Also reflow some long comment lines.

--- tests/hash.pure.lisp
+++ tests/hash.pure.lisp
@@ -14,32 +14,36 @@
 ;;; +MAGIC-HASH-VECTOR-VALUE+ is used to mark empty entries in the slot
 ;;; HASH-VECTOR of hash tables. It must be a value outside of the range
 ;;; of SXHASH. The range of SXHASH is the non-negative fixnums.
-(assert (not (typep sb-impl::+magic-hash-vector-value+
-                    '(and fixnum unsigned-byte))))
+(with-test (:name :magic-hash-vector-value)
+  (assert (not (typep sb-impl::+magic-hash-vector-value+
+                      '(and fixnum unsigned-byte)))))
 
 ;;; The return value of SXHASH on non-string/bitvector arrays should not
 ;;; change when the contents of the array change.
-(let* ((a (make-array '(1) :initial-element 1))
-       (sxhash (sxhash a))
-       (hash (make-hash-table :test 'equal)))
-  (setf (gethash a hash) t)
-  (setf (aref a 0) 0)
-  (assert (= sxhash (sxhash a)))
-  ;; Need to make another access to the hash to disable the last-seen-element
-  ;; cache.
-  (setf (gethash 'y hash) t)
-  (assert (gethash a hash)))
+(with-test (:name (sxhash array :independent-of-contents))
+  (let* ((a (make-array '(1) :initial-element 1))
+         (sxhash (sxhash a))
+         (hash (make-hash-table :test 'equal)))
+    (setf (gethash a hash) t)
+    (setf (aref a 0) 0)
+    (assert (= sxhash (sxhash a)))
+    ;; Need to make another access to the hash to disable the
+    ;; last-seen-element cache.
+    (setf (gethash 'y hash) t)
+    (assert (gethash a hash))))
 
 ;;; Minimum quality checks
-(assert (/= (sxhash "foo") (sxhash "bar")))
-(assert (/= (sxhash (pathname "foo.txt")) (sxhash (pathname "bar.txt"))))
-(assert (/= (sxhash (list 1 2 3)) (sxhash (list 3 2 1))))
-(assert (/= (sxhash #*1010) (sxhash #*0101)))
+(with-test (:name (sxhash :quality :minimum))
+  (assert (/= (sxhash "foo") (sxhash "bar")))
+  (assert (/= (sxhash (pathname "foo.txt")) (sxhash (pathname "bar.txt"))))
+  (assert (/= (sxhash (list 1 2 3)) (sxhash (list 3 2 1))))
+  (assert (/= (sxhash #*1010) (sxhash #*0101))))
 
 (with-test (:name :address-based-hash-counter)
   ;; It doesn't particularly matter what ADDRESS-BASED-COUNTER-VAL returns,
-  ;; but it's best to verify the assumption that each cons bumps the count by 1,
-  ;; lest it be violated in a way that affects the quality of CTYPE hashes.
+  ;; but it's best to verify the assumption that each cons bumps the count
+  ;; by 1, lest it be violated in a way that affects the quality of CTYPE
+  ;; hashes.
   (let ((win 0) (n-trials 10) (prev (sb-impl::address-based-counter-val)))
     (dotimes (i n-trials)
       (declare (notinline cons)) ; it's flushable, but don't flush it
