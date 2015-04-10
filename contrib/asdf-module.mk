43e8303af45614c93d809f5a55632e539bc2e936
Stas Boukarev <stassats@gmail.com>
2015-04-10 21:07:30 +0300

    Prune old and used stuff from makefiles.

--- contrib/asdf-module.mk
+++ contrib/asdf-module.mk
@@ -15,9 +15,6 @@ ifeq (SunOS,$(UNAME))
   PATH:=/usr/xpg4/bin:${PATH}
 endif
 ifeq (CYGWIN,$(findstring CYGWIN,$(UNAME)))
-  EXTRA_CFLAGS=-mno-cygwin
-  # GCC 4.x doesn't accept -mno-cygwin.
-  CC:=gcc-3
   # SBCL can't read cygwin symlinks, and cygwin likes to symlink
   # gcc.  To further complicate things, SBCL can't handle cygwin
   # paths, either.
@@ -27,9 +24,9 @@ ifeq (Linux,$(UNAME))
   EXTRA_CFLAGS=-D_GNU_SOURCE
 endif
 
-export CC SBCL EXTRA_CFLAGS EXTRA_LDFLAGS
+export CC SBCL EXTRA_CFLAGS
 
-all: $(FASL) $(ASD) $(EXTRA_ALL_TARGETS)
+all: $(FASL) $(ASD)
 
 $(FASL)::
 	$(MAKE) -C ../asdf
@@ -45,5 +42,5 @@ test: $(FASL) $(ASD)
 
 # KLUDGE: There seems to be no portable way to tell tar to not to
 # preserve owner, so chown after installing for the current user.
-install: $(EXTRA_INSTALL_TARGETS)
+install:
 	cp $(FASL) $(ASD) "$(BUILD_ROOT)$(INSTALL_DIR)"
