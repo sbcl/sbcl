0c3a803d0cd49d1cfcefbdfc3f45dd6715755297
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2015-07-03 21:55:28 +0200

    New generic function MAKE-METHOD-LAMBDA-USING-SPECIALIZERS
    This generic function allows access to specializers when generating the
    method lambda enabling features such as additional variable bindings
    based on specializers.
    
    This change is part of an effort to integrate the necessary extensions
    to support generalizer metaobjects [1] into SBCL's PCL.
    
    [1] https://github.com/sbcl/specializable

--- src/pcl/boot.lisp
+++ src/pcl/boot.lisp
@@ -145,6 +145,14 @@ bootstrapping.
       ()
       real-make-method-lambda))
 
+    (make-method-lambda-using-specializers
+     standard
+     ((proto-generic-function proto-method qualifiers specializers
+                              lambda-expression environment)
+      (standard-generic-function standard-method t t t t)
+      ()
+      real-make-method-lambda-using-specializers))
+
     (make-method-specializers-form
      standard
      ((proto-generic-function proto-method specializer-names environment)
@@ -451,60 +459,54 @@ generic function lambda list ~S~:>"
 (defvar *method-name* nil)
 (defvar *method-lambda-list* nil)
 
-(defun expand-defmethod (name
-                         proto-gf
-                         proto-method
-                         qualifiers
-                         lambda-list
-                         body
-                         env
-                         ;; ENV could be of type SB!INTERPRETER:BASIC-ENV
-                         ;; but I don't care to figure out what parts of PCL
-                         ;; would have to change to accept that, so coerce.
-                         &aux (env (sb-kernel:coerce-to-lexenv env)))
-  (multiple-value-bind (parameters unspecialized-lambda-list specializers)
-      (parse-specialized-lambda-list lambda-list)
-    (declare (ignore parameters))
+(defun expand-defmethod (name proto-gf proto-method qualifiers lambda-list
+                         body env)
+  (binding* (;; ENV could be of type SB!INTERPRETER:BASIC-ENV but I
+             ;; don't care to figure out what parts of PCL would have
+             ;; to change to accept that, so coerce.
+             (env (sb-kernel:coerce-to-lexenv env))
+             ((nil unspecialized-lambda-list specializers)
+              (parse-specialized-lambda-list lambda-list))
+             (*method-name* `(,name ,@qualifiers ,specializers))
+             (method-lambda `(lambda ,unspecialized-lambda-list ,@body))
+             ((method-function-lambda initargs new-lambda-list)
+              (make-method-lambda-using-specializers
+               proto-gf proto-method qualifiers specializers method-lambda env))
+             (initargs-form
+              (make-method-initargs-form
+               proto-gf proto-method method-function-lambda initargs env))
+             (specializers-form
+              (make-method-specializers-form
+               proto-gf proto-method specializers env)))
     (mapc (lambda (specializer)
             (when (typep specializer 'type-specifier)
               (check-deprecated-type specializer)))
           specializers)
-    (let ((method-lambda `(lambda ,unspecialized-lambda-list ,@body))
-          (*method-name* `(,name ,@qualifiers ,specializers))
-          (*method-lambda-list* lambda-list))
-      (multiple-value-bind (method-function-lambda initargs)
-          (make-method-lambda proto-gf proto-method method-lambda env)
-        (let ((initargs-form (make-method-initargs-form
-                              proto-gf proto-method method-function-lambda
-                              initargs env))
-              (specializers-form (make-method-specializers-form
-                                  proto-gf proto-method specializers env)))
-          `(progn
-             ;; Note: We could DECLAIM the ftype of the generic function
-             ;; here, since ANSI specifies that we create it if it does
-             ;; not exist. However, I chose not to, because I think it's
-             ;; more useful to support a style of programming where every
-             ;; generic function has an explicit DEFGENERIC and any typos
-             ;; in DEFMETHODs are warned about. Otherwise
-             ;;
-             ;;   (DEFGENERIC FOO-BAR-BLETCH (X))
-             ;;   (DEFMETHOD FOO-BAR-BLETCH ((X HASH-TABLE)) ..)
-             ;;   (DEFMETHOD FOO-BRA-BLETCH ((X SIMPLE-VECTOR)) ..)
-             ;;   (DEFMETHOD FOO-BAR-BLETCH ((X VECTOR)) ..)
-             ;;   (DEFMETHOD FOO-BAR-BLETCH ((X ARRAY)) ..)
-             ;;   (DEFMETHOD FOO-BAR-BLETCH ((X LIST)) ..)
-             ;;
-             ;; compiles without raising an error and runs without
-             ;; raising an error (since SIMPLE-VECTOR cases fall through
-             ;; to VECTOR) but still doesn't do what was intended. I hate
-             ;; that kind of bug (code which silently gives the wrong
-             ;; answer), so we don't do a DECLAIM here. -- WHN 20000229
-             ,(make-defmethod-form name qualifiers specializers-form
-                                   unspecialized-lambda-list
-                                   (if proto-method
-                                       (class-name (class-of proto-method))
-                                       'standard-method)
-                                   initargs-form)))))))
+    ;; Note: We could DECLAIM the ftype of the generic function here,
+    ;; since ANSI specifies that we create it if it does not
+    ;; exist. However, I chose not to, because I think it's more
+    ;; useful to support a style of programming where every generic
+    ;; function has an explicit DEFGENERIC and any typos in DEFMETHODs
+    ;; are warned about. Otherwise
+    ;;
+    ;;   (DEFGENERIC FOO-BAR-BLETCH (X))
+    ;;   (DEFMETHOD FOO-BAR-BLETCH ((X HASH-TABLE)) ..)
+    ;;   (DEFMETHOD FOO-BRA-BLETCH ((X SIMPLE-VECTOR)) ..)
+    ;;   (DEFMETHOD FOO-BAR-BLETCH ((X VECTOR)) ..)
+    ;;   (DEFMETHOD FOO-BAR-BLETCH ((X ARRAY)) ..)
+    ;;   (DEFMETHOD FOO-BAR-BLETCH ((X LIST)) ..)
+    ;;
+    ;; compiles without raising an error and runs without raising an
+    ;; error (since SIMPLE-VECTOR cases fall through to VECTOR) but
+    ;; still doesn't do what was intended. I hate that kind of bug
+    ;; (code which silently gives the wrong answer), so we don't do a
+    ;; DECLAIM here. -- WHN 20000229
+    (make-defmethod-form name qualifiers specializers-form
+                         (or new-lambda-list unspecialized-lambda-list)
+                         (if proto-method
+                             (class-name (class-of proto-method))
+                             'standard-method)
+                         initargs-form)))
 
 (defun interned-symbol-p (x)
   (and (symbolp x) (symbol-package x)))
@@ -606,6 +608,22 @@ generic function lambda list ~S~:>"
 (unless (fboundp 'make-method-initargs-form)
   (setf (gdefinition 'make-method-initargs-form)
         (symbol-function 'real-make-method-initargs-form)))
+
+(defun real-make-method-lambda-using-specializers
+    (proto-gf proto-method qualifiers specializers method-lambda env)
+  (declare (ignore qualifiers))
+  (check-method-lambda method-lambda 'make-method-lambda) ; TODO remove check in make-method-lambda
+  ;; Default behavior: delegate to MAKE-METHOD-LAMBDA.
+  (let* ((lambda-list (second method-lambda))
+         (*method-lambda-list*
+          (append
+           (mapcar #'list (subseq lambda-list 0 (length specializers)) specializers)
+           (subseq lambda-list (length specializers)))))
+    (make-method-lambda proto-gf proto-method method-lambda env)))
+
+(unless (fboundp 'make-method-lambda-using-specializers)
+  (setf (gdefinition 'make-method-lambda-using-specializers)
+        (symbol-function 'real-make-method-lambda-using-specializers)))
 
 ;;; When bootstrapping PCL MAKE-METHOD-LAMBDA starts out as a regular
 ;;; function: REAL-MAKE-METHOD-LAMBDA set to the fdefinition of
--- src/pcl/generic-functions.lisp
+++ src/pcl/generic-functions.lisp
@@ -381,6 +381,32 @@
     (proto-generic-function proto-method lambda-expression lambda-list
      environment))
 
+;;;; 6 arguments
+
+(defgeneric make-method-lambda-using-specializers
+    (proto-generic-function proto-method qualifiers specializers
+     method-lambda environment)
+  #+sb-doc
+  (:documentation
+   "Compute a method lambda form based on METHOD-LAMBDA, possibly
+taking into account PROTO-GENERIC-FUNCTION, PROTO-METHOD, QUALIFIERS,
+SPECIALIZERS and ENVIRONMENT.
+
+Both PROTO-GENERIC-FUNCTION and PROTO-METHOD may be
+uninitialized. However, their classes and prototypes can be inspected.
+
+SPECIALIZERS is a list of specializer objects (i.e. parsed).
+
+Return three values:
+1. the created method lambda form
+2. initargs for the method instance
+3. a (possibly modified) unspecialized method lambda list or nil if
+   the unspecialized lambda list contained in METHOD-LAMBDA should be
+   used
+
+NOTE: This generic function is part of an SBCL-specific experimental
+protocol. Interface subject to change."))
+
 ;;;; optional arguments
 
 (defgeneric get-method (generic-function
