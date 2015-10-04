9d6335459a849a30563c8dffd33f4cbbe45d485d
Stas Boukarev <stassats@gmail.com>
2015-10-04 23:37:37 +0300

    Fix (type-of NIL).
    Return NULL, not (specifier-type 'NULL)

--- src/code/pred.lisp
+++ src/code/pred.lisp
@@ -241,7 +241,7 @@
     ;; do that, we also have to pick off NIL so that it doesn't say SYMBOL.
     (symbol
      (cond ((eq object t) 'boolean)
-           ((eq object nil) (specifier-type 'null))
+           ((eq object nil) 'null)
            ((eq (symbol-package object) *keyword-package*) 'keyword)
            (t 'symbol)))
     ((or array complex #!+sb-simd-pack simd-pack)
