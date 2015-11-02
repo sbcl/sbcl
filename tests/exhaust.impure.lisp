1908bb39f613481402c54b741c8aebe3c138b55a
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-11-02 14:19:12 +0000

    Mark exhaust.impure.lisp / (:EXHAUST :BINDING-STACK) as broken on MIPS
      * This probably should be made to work at some point, but for
    now let's just make it obvious that it's not expected to work.

--- tests/exhaust.impure.lisp
+++ tests/exhaust.impure.lisp
@@ -85,7 +85,8 @@
              (recurse)))))
     (assert (= exhaust-count recurse-count *count*))))
 
-(with-test (:name (:exhaust :binding-stack))
+(with-test (:name (:exhaust :binding-stack)
+            :broken-on :mips)
   (let ((ok nil)
         (symbols (loop repeat 1024 collect (gensym)))
         (values (loop repeat 1024 collect nil)))
