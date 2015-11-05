0674017394c928071b6fbe2b96d60f3e8cd91b45
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-11-04 20:19:29 -0500

    Fix compiler.impure.lisp / :BUG-903821 for 32-on-64 systems
      * Systems with n-word-bits = 32 but n-machine-word-bits = 64
    have 64-bit modular operations, not 32-bit modular operations.
    Pick the right function, so we don't get hit with undefined
    function errors or similar.

--- tests/compiler.impure.lisp
+++ tests/compiler.impure.lisp
@@ -2461,11 +2461,11 @@
 (test-util:with-test (:name :bug-903821)
   (let* ((fun (compile nil '(lambda (x n)
                              (declare (sb-ext:word x)
-                              (type (integer 0 #.(1- sb-vm:n-word-bits)) n)
+                              (type (integer 0 #.(1- sb-vm:n-machine-word-bits)) n)
                               (optimize speed))
                              (logandc2 x (ash -1 n)))))
          (thing-not-to-call
-          (intern (format nil "ASH-LEFT-MOD~D" sb-vm::n-word-bits) "SB-VM")))
+          (intern (format nil "ASH-LEFT-MOD~D" sb-vm::n-machine-word-bits) "SB-VM")))
     (assert (not (member (symbol-function thing-not-to-call)
                          (ctu:find-named-callees fun))))
     (assert (= 7 (funcall fun 15 3)))))
