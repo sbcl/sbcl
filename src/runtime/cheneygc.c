6c62a086377b66ff6c69cb347f646743c4f05a0b
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2015-11-22 16:13:02 -0500

    cheneygc: Fix heap overflow detection for unsigned os_vm_size_t
      * Instead of checking to see if the overall size of the trigger
    area (unused part of a dynamic space) is negative, check first to
    see if it would BE negative if the operation were to be performed,
    allowing the high-water detection to occur even if os_vm_size_t is
    unsigned (thus, not having any negative values).
    
      * As a practical upshot, this means that we get a sane error
    message when we run out of heap space, instead of either silently
    doing something stupidly wrong or having a rather opaque message
    about mprotect() failing and various advice that applies to gencgc
    but not cheneygc.

--- src/runtime/cheneygc.c
+++ src/runtime/cheneygc.c
@@ -431,10 +431,10 @@ void set_auto_gc_trigger(os_vm_size_t dynamic_usage)
              (unsigned long)((os_vm_address_t)dynamic_space_free_pointer
                              - (os_vm_address_t)current_dynamic_space));
 
-    length = os_trunc_size_to_page(dynamic_space_size - dynamic_usage);
-    if (length < 0)
+    if (dynamic_usage > dynamic_space_size)
         lose("set_auto_gc_trigger: tried to set gc trigger too high! (0x%08lx)\n",
              (unsigned long)dynamic_usage);
+    length = os_trunc_size_to_page(dynamic_space_size - dynamic_usage);
 
 #if defined(SUNOS) || defined(SOLARIS) || defined(LISP_FEATURE_HPUX)
     os_invalidate(addr, length);
