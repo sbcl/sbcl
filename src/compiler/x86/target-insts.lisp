ec6c37c967b60d27b40b35adf9752cde17db1f35
Douglas Katzman <dougk@google.com>
2016-01-11 23:06:35 -0500

    Be 100% warning-free in make-host-1 for x86/linux.

--- src/compiler/x86/target-insts.lisp
+++ src/compiler/x86/target-insts.lisp
@@ -15,6 +15,91 @@
 
 (in-package "SB!X86-ASM")
 
+(defun print-reg-with-width (value width stream dstate)
+  (declare (ignore dstate))
+  (princ (aref (ecase width
+                 (:byte *byte-reg-names*)
+                 (:word *word-reg-names*)
+                 (:dword *dword-reg-names*))
+               value)
+         stream)
+  ;; XXX plus should do some source-var notes
+  )
+
+(defun print-reg (value stream dstate)
+  (declare (type reg value)
+           (type stream stream)
+           (type disassem-state dstate))
+  (print-reg-with-width value
+                        (inst-operand-size dstate)
+                        stream
+                        dstate))
+
+(defun print-word-reg (value stream dstate)
+  (declare (type reg value)
+           (type stream stream)
+           (type disassem-state dstate))
+  (print-reg-with-width value
+                        (inst-word-operand-size dstate)
+                        stream
+                        dstate))
+
+(defun print-byte-reg (value stream dstate)
+  (declare (type reg value)
+           (type stream stream)
+           (type disassem-state dstate))
+  (print-reg-with-width value :byte stream dstate))
+
+(defun print-addr-reg (value stream dstate)
+  (declare (type reg value)
+           (type stream stream)
+           (type disassem-state dstate))
+  (print-reg-with-width value *default-address-size* stream dstate))
+
+(defun print-reg/mem (value stream dstate)
+  (declare (type (or list reg) value)
+           (type stream stream)
+           (type disassem-state dstate))
+  (if (typep value 'reg)
+      (print-reg value stream dstate)
+      (print-mem-access value stream nil dstate)))
+
+;; Same as print-reg/mem, but prints an explicit size indicator for
+;; memory references.
+(defun print-sized-reg/mem (value stream dstate)
+  (declare (type (or list reg) value)
+           (type stream stream)
+           (type disassem-state dstate))
+  (if (typep value 'reg)
+      (print-reg value stream dstate)
+      (print-mem-access value stream t dstate)))
+
+(defun print-byte-reg/mem (value stream dstate)
+  (declare (type (or list reg) value)
+           (type stream stream)
+           (type disassem-state dstate))
+  (if (typep value 'reg)
+      (print-byte-reg value stream dstate)
+      (print-mem-access value stream t dstate)))
+
+(defun print-word-reg/mem (value stream dstate)
+  (declare (type (or list reg) value)
+           (type stream stream)
+           (type disassem-state dstate))
+  (if (typep value 'reg)
+      (print-word-reg value stream dstate)
+      (print-mem-access value stream nil dstate)))
+
+(defun print-label (value stream dstate)
+  (declare (ignore dstate))
+  (princ16 value stream))
+
+(defun maybe-print-segment-override (stream dstate)
+  (cond ((dstate-get-inst-prop dstate 'fs-segment-prefix)
+         (princ "FS:" stream))
+        ((dstate-get-inst-prop dstate 'gs-segment-prefix)
+         (princ "GS:" stream))))
+
 (defun print-mem-access (value stream print-size-p dstate)
   (declare (type list value)
            (type stream stream)
@@ -55,3 +140,52 @@
                   (maybe-note-assembler-routine offset nil dstate)))
             (princ offset stream))))))
   (write-char #\] stream))
+
+;;;; interrupt instructions
+
+(defun snarf-error-junk (sap offset &optional length-only)
+  (let* ((length (sap-ref-8 sap offset))
+         (vector (make-array length :element-type '(unsigned-byte 8))))
+    (declare (type system-area-pointer sap)
+             (type (unsigned-byte 8) length)
+             (type (simple-array (unsigned-byte 8) (*)) vector))
+    (cond (length-only
+           (values 0 (1+ length) nil nil))
+          (t
+           (copy-ub8-from-system-area sap (1+ offset) vector 0 length)
+           (collect ((sc-offsets)
+                     (lengths))
+             (lengths 1)                ; the length byte
+             (let* ((index 0)
+                    (error-number (read-var-integer vector index)))
+               (lengths index)
+               (loop
+                 (when (>= index length)
+                   (return))
+                 (let ((old-index index))
+                   (sc-offsets (read-var-integer vector index))
+                   (lengths (- index old-index))))
+               (values error-number
+                       (1+ length)
+                       (sc-offsets)
+                       (lengths))))))))
+
+(defun break-control (chunk inst stream dstate)
+  (declare (ignore inst))
+  (flet ((nt (x) (if stream (note x dstate))))
+    (case #!-ud2-breakpoints (byte-imm-code chunk dstate)
+          #!+ud2-breakpoints (word-imm-code chunk dstate)
+      (#.error-trap
+       (nt "error trap")
+       (handle-break-args #'snarf-error-junk stream dstate))
+      (#.cerror-trap
+       (nt "cerror trap")
+       (handle-break-args #'snarf-error-junk stream dstate))
+      (#.breakpoint-trap
+       (nt "breakpoint trap"))
+      (#.pending-interrupt-trap
+       (nt "pending interrupt trap"))
+      (#.halt-trap
+       (nt "halt trap"))
+      (#.fun-end-breakpoint-trap
+       (nt "function end breakpoint trap")))))
