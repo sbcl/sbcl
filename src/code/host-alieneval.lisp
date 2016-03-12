50a728a7ffd10664170dc8682102bd23b6835ab3
Douglas Katzman <dougk@google.com>
2016-03-12 11:46:11 -0500

    Change some alien-related def!macros to defmacros
    and get rid of some EVAL-WHENs as well.

--- src/code/host-alieneval.lisp
+++ src/code/host-alieneval.lisp
@@ -30,70 +30,9 @@
 
 ;;;; ALIEN-TYPE-INFO stuff
 
-(eval-when (#-sb-xc :compile-toplevel :execute :load-toplevel)
-
-(defstruct (alien-type-class (:copier nil))
-  (name nil :type symbol)
-  (defstruct-name nil :type symbol)
-  (include nil :type (or null alien-type-class))
-  (unparse nil :type (or null function))
-  (type= nil :type (or null function))
-  (lisp-rep nil :type (or null function))
-  (alien-rep nil :type (or null function))
-  (extract-gen nil :type (or null function))
-  (deposit-gen nil :type (or null function))
-  (naturalize-gen nil :type (or null function))
-  (deport-gen nil :type (or null function))
-  (deport-alloc-gen nil :type (or null function))
-  (deport-pin-p nil :type (or null function))
-  ;; Cast?
-  (arg-tn nil :type (or null function))
-  (result-tn nil :type (or null function))
-  (subtypep nil :type (or null function)))
-(def!method print-object ((type-class alien-type-class) stream)
-  (print-unreadable-object (type-class stream :type t)
-    (prin1 (alien-type-class-name type-class) stream)))
-
-(defun alien-type-class-or-lose (name)
-  (or (gethash name *alien-type-classes*)
-      (error "no alien type class ~S" name)))
-
-(defun create-alien-type-class-if-necessary (name defstruct-name include)
-  (let ((old (gethash name *alien-type-classes*))
-        (include (and include (alien-type-class-or-lose include))))
-    (if old
-        (setf (alien-type-class-include old) include)
-        (setf (gethash name *alien-type-classes*)
-              (make-alien-type-class :name name
-                                     :defstruct-name defstruct-name
-                                     :include include)))))
-
-(defparameter *method-slot-alist*
-  '((:unparse . alien-type-class-unparse)
-    (:type= . alien-type-class-type=)
-    (:subtypep . alien-type-class-subtypep)
-    (:lisp-rep . alien-type-class-lisp-rep)
-    (:alien-rep . alien-type-class-alien-rep)
-    (:extract-gen . alien-type-class-extract-gen)
-    (:deposit-gen . alien-type-class-deposit-gen)
-    (:naturalize-gen . alien-type-class-naturalize-gen)
-    (:deport-gen . alien-type-class-deport-gen)
-    (:deport-alloc-gen . alien-type-class-deport-alloc-gen)
-    (:deport-pin-p . alien-type-class-deport-pin-p)
-    ;; cast?
-    (:arg-tn . alien-type-class-arg-tn)
-    (:result-tn . alien-type-class-result-tn)))
-
-(defun method-slot (method)
-  (cdr (or (assoc method *method-slot-alist*)
-           (error "no method ~S" method))))
-
-) ; EVAL-WHEN
-
 ;;; We define a keyword "BOA" constructor so that we can reference the
 ;;; slot names in init forms.
-(def!macro define-alien-type-class ((name &key include include-args)
-                                    &rest slots)
+(defmacro define-alien-type-class ((name &key include include-args) &rest slots)
   (let ((defstruct-name (symbolicate "ALIEN-" name "-TYPE")))
     (multiple-value-bind (include include-defstruct overrides)
         (etypecase include
@@ -130,26 +69,13 @@
                                &aux (alignment (or alignment (guess-alignment bits))))))
            ,@slots)))))
 
-(def!macro define-alien-type-method ((class method) lambda-list &rest body)
+(defmacro define-alien-type-method ((class method) lambda-list &rest body)
   (let ((defun-name (symbolicate class "-" method "-METHOD")))
     `(progn
        (defun ,defun-name ,lambda-list
          ,@body)
        (setf (,(method-slot method) (alien-type-class-or-lose ',class))
              #',defun-name))))
-
-(def!macro invoke-alien-type-method (method type &rest args)
-  (let ((slot (method-slot method)))
-    (once-only ((type type))
-      `(funcall (do ((class (alien-type-class-or-lose (alien-type-class ,type))
-                            (alien-type-class-include class)))
-                    ((null class)
-                     (error "method ~S not defined for ~S"
-                            ',method (alien-type-class ,type)))
-                  (let ((fn (,slot class)))
-                    (when fn
-                      (return fn))))
-                ,type ,@args))))
 
 ;;;; type parsing and unparsing
 
@@ -167,14 +93,6 @@
           ;; NIL, but global symbol-macros make me vaguely queasy, so
           ;; I do it this way instead.
           nil))))
-
-;;; Process stuff in a new scope.
-(def!macro with-auxiliary-alien-types (env &body body)
-  ``(symbol-macrolet ((&auxiliary-type-definitions&
-                       ,(append *new-auxiliary-types*
-                                (auxiliary-type-definitions ,env))))
-      ,(let ((*new-auxiliary-types* nil))
-         ,@body)))
 
 ;;; Parse TYPE as an alien type specifier and return the resultant
 ;;; ALIEN-TYPE structure.
@@ -242,36 +160,12 @@
 
 ;;;; alien type defining stuff
 
-(def!macro define-alien-type-translator (name lambda-list &body body)
-  (let ((defun-name (symbolicate "ALIEN-" name "-TYPE-TRANSLATOR")))
-    `(eval-when (:compile-toplevel :load-toplevel :execute)
-       (setf (symbol-function ',defun-name)
-             ,(make-macro-lambda defun-name lambda-list body
-                                 'define-alien-type-translator name))
-       (%define-alien-type-translator ',name #',defun-name))))
-
 (eval-when (#-sb-xc :compile-toplevel :load-toplevel :execute)
   (defun %define-alien-type-translator (name translator)
     (setf (info :alien-type :kind name) :primitive)
     (setf (info :alien-type :translator name) translator)
     (clear-info :alien-type :definition name)
     name))
-
-(def!macro define-alien-type (name type &environment env)
-  #!+sb-doc
-  "Define the alien type NAME to be equivalent to TYPE. Name may be NIL for
-   STRUCT and UNION types, in which case the name is taken from the type
-   specifier."
-  (with-auxiliary-alien-types env
-    (let ((alien-type (parse-alien-type type env)))
-      `(eval-when (:compile-toplevel :load-toplevel :execute)
-         ,@(when *new-auxiliary-types*
-             `((%def-auxiliary-alien-types ',*new-auxiliary-types*
-                                           (sb!c:source-location))))
-         ,@(when name
-             `((%define-alien-type ',name ',alien-type)
-               (setf (info :source-location :alien-type ',name)
-                     (sb!c:source-location))))))))
 
 (eval-when (#-sb-xc :compile-toplevel :load-toplevel :execute)
   (defun %def-auxiliary-alien-types (types source-location)
@@ -444,23 +338,6 @@
               (ignore ignore))
      (naturalize ,(invoke-alien-type-method :extract-gen type 'sap 'offset)
                  ',type)))
-
-(def!macro maybe-with-pinned-objects (variables types &body body)
-  (declare (ignorable variables types))
-  (let ((pin-variables
-         ;; Only pin things on GENCGC, since on CHENEYGC it'd imply
-         ;; disabling the GC.  Which is something we don't want to do
-         ;; every time we're calling to C.
-         #!+gencgc
-         (loop for variable in variables
-            for type in types
-            when (invoke-alien-type-method :deport-pin-p type)
-            collect variable)))
-    (if pin-variables
-        `(with-pinned-objects ,pin-variables
-           ,@body)
-        `(progn
-           ,@body))))
 
 (defun compute-deposit-lambda (type)
   (declare (type alien-type type))
