1cc5cf0186c9b281f0ce25366ee13bf3f67369f1
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-07-17 20:21:07 +0200

    tests: WITH-TEST and CHECKED-COMPILE in float.pure.lisp

--- tests/float.pure.lisp
+++ tests/float.pure.lisp
@@ -13,91 +13,100 @@
 
 (cl:in-package :cl-user)
 
-(dolist (ifnis (list (cons single-float-positive-infinity
-                           single-float-negative-infinity)
-                     (cons double-float-positive-infinity
-                           double-float-negative-infinity)))
-  (destructuring-bind (+ifni . -ifni) ifnis
-    (assert (= (* +ifni 1) +ifni))
-    (assert (= (* +ifni -0.1) -ifni))
-    (assert (= (+ +ifni -0.1) +ifni))
-    (assert (= (- +ifni -0.1) +ifni))
-    (assert (= (sqrt +ifni) +ifni))
-    (assert (= (* -ifni -14) +ifni))
-    (assert (= (/ -ifni 0.1) -ifni))
-    (assert (= (/ -ifni 100/3) -ifni))
-    (assert (not (= +ifni -ifni)))
-    (assert (= -ifni -ifni))
-    (assert (not (= +ifni 100/3)))
-    (assert (not (= -ifni -1.0 -ifni)))
-    (assert (not (= -ifni -17/02 -ifni)))
-    (assert (< -ifni +ifni))
-    (assert (not (< +ifni 100)))
-    (assert (not (< +ifni 100.0)))
-    (assert (not (< +ifni -ifni)))
-    (assert (< 100 +ifni))
-    (assert (< 100.0 +ifni))
-    (assert (>= 100 -ifni))
-    (assert (not (<= 6/7 (* 3 -ifni))))
-    (assert (not (> +ifni +ifni)))))
+(with-test (:name (:infinities :comparison))
+  (dolist (ifnis (list (cons single-float-positive-infinity
+                             single-float-negative-infinity)
+                       (cons double-float-positive-infinity
+                             double-float-negative-infinity)))
+    (destructuring-bind (+ifni . -ifni) ifnis
+      (assert (= (* +ifni 1) +ifni))
+      (assert (= (* +ifni -0.1) -ifni))
+      (assert (= (+ +ifni -0.1) +ifni))
+      (assert (= (- +ifni -0.1) +ifni))
+      (assert (= (sqrt +ifni) +ifni))
+      (assert (= (* -ifni -14) +ifni))
+      (assert (= (/ -ifni 0.1) -ifni))
+      (assert (= (/ -ifni 100/3) -ifni))
+      (assert (not (= +ifni -ifni)))
+      (assert (= -ifni -ifni))
+      (assert (not (= +ifni 100/3)))
+      (assert (not (= -ifni -1.0 -ifni)))
+      (assert (not (= -ifni -17/02 -ifni)))
+      (assert (< -ifni +ifni))
+      (assert (not (< +ifni 100)))
+      (assert (not (< +ifni 100.0)))
+      (assert (not (< +ifni -ifni)))
+      (assert (< 100 +ifni))
+      (assert (< 100.0 +ifni))
+      (assert (>= 100 -ifni))
+      (assert (not (<= 6/7 (* 3 -ifni))))
+      (assert (not (> +ifni +ifni))))))
 
 ;;; ANSI: FLOAT-RADIX should signal an error if its argument is not a
 ;;; float.
 ;;;
 ;;; (Peter Van Eynde's ansi-test suite caught this, and Eric Marsden
 ;;; reported a fix for CMU CL, which was ported to sbcl-0.6.12.35.)
-(assert (typep (nth-value 1 (ignore-errors (float-radix "notfloat")))
-               'type-error))
-
-(assert (typep (nth-value 1 (ignore-errors
-                              (funcall (fdefinition 'float-radix) "notfloat")))
-               'type-error))
+(with-test (:name (float-radix simple-type-error))
+  (multiple-value-bind  (fun failure-p warnings)
+      (checked-compile '(lambda () (float-radix "notfloat")) :allow-warnings t)
+    (assert failure-p)
+    (assert (= 1 (length warnings)))
+    (assert-error (funcall fun) type-error))
+  (assert-error (funcall (fdefinition 'float-radix) "notfloat") type-error))
 
 ;;; Before 0.8.2.14 the cross compiler failed to work with
 ;;; denormalized numbers
-(when (subtypep 'single-float 'short-float)
-  (assert (eql least-positive-single-float least-positive-short-float)))
+(with-test (:name (:denormalized float))
+  (when (subtypep 'single-float 'short-float)
+    (assert (eql least-positive-single-float least-positive-short-float))))
 
 ;;; bug found by Paul Dietz: FFLOOR and similar did not work for integers
-(let ((tests '(((ffloor -8 3) (-3.0 1))
-               ((fround -8 3) (-3.0 1))
-               ((ftruncate -8 3) (-2.0 -2))
-               ((fceiling -8 3) (-2.0 -2)))))
-  (loop for (exp res) in tests
-        for real-res = (multiple-value-list (eval exp))
-        do (assert (equal real-res res))))
+(with-test (:name (ffloor integer))
+  (let ((tests '(((ffloor -8 3) (-3.0 1))
+                 ((fround -8 3) (-3.0 1))
+                 ((ftruncate -8 3) (-2.0 -2))
+                 ((fceiling -8 3) (-2.0 -2)))))
+    (loop for (exp res) in tests
+       for real-res = (multiple-value-list (eval exp))
+       do (assert (equal real-res res)))))
 
 ;;; bug 45b reported by PVE
-(dolist (type '(short single double long))
-  (dolist (sign '(positive negative))
-    (let* ((name (find-symbol (format nil "LEAST-~A-~A-FLOAT"
-                                      sign type)
-                              :cl))
-           (value (symbol-value name)))
-      (assert (zerop (/ value 2))))))
+(with-test (:name (:least-*-*-float :bug-45b))
+  (dolist (type '(short single double long))
+    (dolist (sign '(positive negative))
+      (let* ((name (find-symbol (format nil "LEAST-~A-~A-FLOAT"
+                                        sign type)
+                                :cl))
+             (value (symbol-value name)))
+        (assert (zerop (/ value 2)))))))
 
 ;;; bug found by Paul Dietz: bad rounding on small floats
-(assert (= (fround least-positive-short-float least-positive-short-float) 1.0))
+(with-test (:name (fround least-positive-short-float))
+  (assert (= (fround least-positive-short-float least-positive-short-float) 1.0)))
 
 ;;; bug found by Peter Seibel: scale-float was only accepting float
 ;;; exponents, when it should accept all integers.  (also bug #269)
-(assert (= (multiple-value-bind (significand expt sign)
-               (integer-decode-float least-positive-double-float)
-             (* (scale-float (float significand 0.0d0) expt) sign))
-           least-positive-double-float))
-(assert (= (multiple-value-bind (significand expt sign)
-               (decode-float least-positive-double-float)
-             (* (scale-float significand expt) sign))
-           least-positive-double-float))
-(assert (= 0.0 (scale-float 1.0 most-negative-fixnum)))
-(assert (= 0.0d0 (scale-float 1.0d0 (1- most-negative-fixnum))))
+(with-test (:name (scale-float :bug-269))
+  (assert (= (multiple-value-bind (significand expt sign)
+                 (integer-decode-float least-positive-double-float)
+               (* (scale-float (float significand 0.0d0) expt) sign))
+             least-positive-double-float))
+  (assert (= (multiple-value-bind (significand expt sign)
+                 (decode-float least-positive-double-float)
+               (* (scale-float significand expt) sign))
+             least-positive-double-float))
+  (assert (= 0.0 (scale-float 1.0 most-negative-fixnum)))
+  (assert (= 0.0d0 (scale-float 1.0d0 (1- most-negative-fixnum)))))
 
 (with-test (:name (:scale-float-overflow :bug-372)
-            :fails-on '(and :darwin :ppc)) ;; bug 372
-  (assert-error (scale-float 1.0 most-positive-fixnum)
-                floating-point-overflow)
-  (assert-error (scale-float 1.0d0 (1+ most-positive-fixnum))
-                floating-point-overflow))
+                  :fails-on '(and :darwin :ppc)) ;; bug 372
+  (flet ((test (form)
+           (assert-error (funcall (checked-compile `(lambda () ,form)
+                                                   :allow-style-warnings t))
+                         floating-point-overflow)))
+    (test '(scale-float 1.0 most-positive-fixnum))
+    (test '(scale-float 1.0d0 (1+ most-positive-fixnum)))))
 
 ;;; bug found by jsnell when nfroyd tried to implement better LOGAND
 ;;; type derivation.
@@ -106,36 +115,36 @@
            12780299))
 
 ;;; MISC.564: no out-of-line %ATAN2 for constant folding
-(assert (typep
-  (funcall
-   (compile
-    nil
-    '(lambda (p1)
-      (declare (optimize (speed 3) (safety 2) (debug 3) (space 0))
-       (type complex p1))
-      (phase (the (eql #c(1.0d0 2.0d0)) p1))))
-   #c(1.0d0 2.0d0))
-    'double-float))
+(with-test (:name (:%atan2 :constant-folding))
+  (assert (typep
+           (funcall
+            (checked-compile
+             '(lambda (p1)
+               (declare (optimize (speed 3) (safety 2) (debug 3) (space 0))
+                        (type complex p1))
+               (phase (the (eql #c(1.0d0 2.0d0)) p1))))
+            #c(1.0d0 2.0d0))
+           'double-float)))
 
 ;;; More out of line functions (%COS, %SIN, %TAN) for constant folding,
 ;;; reported by Mika Pihlajamäki
-(funcall (compile nil '(lambda () (cos (tan (round 0))))))
-(funcall (compile nil '(lambda () (sin (tan (round 0))))))
-(funcall (compile nil '(lambda () (tan (tan (round 0))))))
+(with-test (:name (sin cos tan :constant-folding))
+  (flet ((test (function)
+           (funcall (checked-compile
+                     `(lambda () (,function (tan (round 0))))))))
+    (mapc #'test '(sin cos tan))))
 
 (with-test (:name (:addition-overflow :bug-372)
             :fails-on '(or (and :ppc :openbsd)
                            (and :ppc :darwin)
                            (and :x86 :netbsd)))
-  (assert (typep (nth-value
-                  1
-                  (ignore-errors
-                    (sb-sys:without-interrupts
-                     (sb-int:set-floating-point-modes :current-exceptions nil
-                                                      :accrued-exceptions nil)
-                     (loop repeat 2 summing most-positive-double-float)
-                     (sleep 2))))
-                 'floating-point-overflow)))
+  (assert-error
+   (sb-sys:without-interrupts
+     (sb-int:set-floating-point-modes :current-exceptions nil
+                                      :accrued-exceptions nil)
+     (loop repeat 2 summing most-positive-double-float)
+     (sleep 2))
+   floating-point-overflow))
 
 ;; This is the same test as above.  Even if the above copy passes,
 ;; this copy will fail if SIGFPE handling ends up clearing the FPU
@@ -149,15 +158,13 @@
             :fails-on '(or (and :ppc :openbsd)
                            (and :ppc :darwin)
                            (and :x86 :netbsd)))
-  (assert (typep (nth-value
-                  1
-                  (ignore-errors
-                    (sb-sys:without-interrupts
-                     (sb-int:set-floating-point-modes :current-exceptions nil
-                                                      :accrued-exceptions nil)
-                     (loop repeat 2 summing most-positive-double-float)
-                     (sleep 2))))
-                 'floating-point-overflow)))
+  (assert-error
+   (sb-sys:without-interrupts
+     (sb-int:set-floating-point-modes :current-exceptions nil
+                                      :accrued-exceptions nil)
+     (loop repeat 2 summing most-positive-double-float)
+     (sleep 2))
+   floating-point-overflow))
 
 ;;; On x86-64 generating complex floats on the stack failed an aver in
 ;;; the compiler if the stack slot was the same as the one containing
@@ -166,24 +173,22 @@
 (with-test (:name :complex-float-stack)
   (dolist (type '((complex double-float)
                   (complex single-float)))
-    (compile nil
-             `(lambda (x0 x1 x2 x3 x4 x5 x6 x7)
-                (declare (type ,type x0 x1 x2 x3 x4 x5 x6 x7))
-                (let ((x0 (+ x0 x0))
-                      (x1 (+ x1 x1))
-                      (x2 (+ x2 x2))
-                      (x3 (+ x3 x3))
-                      (x4 (+ x4 x4))
-                      (x5 (+ x5 x5))
-                      (x6 (+ x6 x6))
-                      (x7 (+ x7 x7)))
-                  (* (+ x0 x1 x2 x3) (+ x4 x5 x6 x7)
-                     (+ x0 x2 x4 x6) (+ x1 x3 x5 x7)
-                     (+ x0 x3 x4 x7) (+ x1 x2 x5 x6)
-                     (+ x0 x1 x6 x7) (+ x2 x3 x4 x5)))))))
+    (checked-compile `(lambda (x0 x1 x2 x3 x4 x5 x6 x7)
+                        (declare (type ,type x0 x1 x2 x3 x4 x5 x6 x7))
+                        (let ((x0 (+ x0 x0))
+                              (x1 (+ x1 x1))
+                              (x2 (+ x2 x2))
+                              (x3 (+ x3 x3))
+                              (x4 (+ x4 x4))
+                              (x5 (+ x5 x5))
+                              (x6 (+ x6 x6))
+                              (x7 (+ x7 x7)))
+                          (* (+ x0 x1 x2 x3) (+ x4 x5 x6 x7)
+                             (+ x0 x2 x4 x6) (+ x1 x3 x5 x7)
+                             (+ x0 x3 x4 x7) (+ x1 x2 x5 x6)
+                             (+ x0 x1 x6 x7) (+ x2 x3 x4 x5)))))))
 
-
-(with-test (:name :nan-comparisons
+(with-test (:name (:nan :comparison)
             :fails-on '(or :sparc))
   (sb-int:with-float-traps-masked (:invalid)
     (macrolet ((test (form)
@@ -193,10 +198,12 @@
                       (assert (eval `(let ((nan (/ 0.0 0.0)))
                                        ,',form)))
                       (assert (funcall
-                               (compile nil `(lambda () ,',nform))))
+                               (checked-compile `(lambda () ,',nform))))
                       (assert (funcall
-                               (compile nil `(lambda (nan) ,',form))
-                               (/ 0.0 0.0)))))))
+                               (checked-compile `(lambda (nan) ,',form))
+                               (locally
+                                   (declare (muffle-conditions style-warning))
+                                 (/ 0.0 0.0))))))))
       (test (/= nan nan))
       (test (/= nan nan nan))
       (test (/= 1.0 nan 2.0 nan))
@@ -241,17 +248,17 @@
   (assert (eql 0.0f0 (log 123 (eval 0))))
   (assert (eql 0.0d0 (log 123.0d0 (eval 0))))
   (assert (eql 0.0d0 (log 123 (eval 0.0d0))))
-  (let ((f (compile nil '(lambda (x y)
-                          (declare (optimize speed))
-                          (etypecase x
-                            (single-float
-                             (etypecase y
-                               (single-float (log x y))
-                               (double-float (log x y))))
-                            (double-float
-                             (etypecase y
-                               (single-float (log x y))
-                               (double-float (log x y)))))))))
+  (let ((f (checked-compile '(lambda (x y)
+                              (declare (optimize speed))
+                              (etypecase x
+                                (single-float
+                                 (etypecase y
+                                   (single-float (log x y))
+                                   (double-float (log x y))))
+                                (double-float
+                                 (etypecase y
+                                   (single-float (log x y))
+                                   (double-float (log x y)))))))))
     (assert (eql 0.0f0 (funcall f 123.0 0.0)))
     (assert (eql 0.0d0 (funcall f 123.0d0 0.0)))
     (assert (eql 0.0d0 (funcall f 123.0d0 0.0d0)))
