b797372a71f1ced3ab8d3ac6cd49bfde05e87ab9
Douglas Katzman <dougk@google.com>
2016-09-11 14:52:06 -0400

    Remove unused variables, fix return type mismatch

--- src/runtime/cheneygc.c
+++ src/runtime/cheneygc.c
@@ -94,8 +94,8 @@ collect_garbage(generation_index_t ignore)
     unsigned long size_retained;
     lispobj *current_static_space_free_pointer;
     unsigned long static_space_size;
-    unsigned long control_stack_size, binding_stack_size;
-    sigset_t tmp, old;
+    unsigned long binding_stack_size;
+    sigset_t old;
     struct thread *th=arch_os_get_current_thread();
 
 #ifdef PRINTNOISE
@@ -342,7 +342,7 @@ print_garbage(lispobj *from_space, lispobj *from_space_free_pointer)
 #define WEAK_POINTER_NWORDS \
         CEILING((sizeof(struct weak_pointer) / sizeof(lispobj)), 2)
 
-static long
+static sword_t
 scav_weak_pointer(lispobj *where, lispobj object)
 {
     /* Do not let GC scavenge the value slot of the weak pointer */
