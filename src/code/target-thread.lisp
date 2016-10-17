c750e26b4decf8c43f1f7c474c8ab07250820a8d
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-10-17 12:29:00 +0200

    Improve CONDITION-WAIT documentation string
    * Removed double negative.
    
    * Fixed typo (the the).
    
    * Made use metasyntactic variables consistent.

--- src/code/target-thread.lisp
+++ src/code/target-thread.lisp
@@ -946,22 +946,24 @@ IF-NOT-OWNER is :FORCE)."
 
 (defun condition-wait (queue mutex &key timeout)
   #!+sb-doc
-  "Atomically release MUTEX and start waiting on QUEUE for till another thread
-wakes us up using either CONDITION-NOTIFY or CONDITION-BROADCAST on that
-queue, at which point we re-acquire MUTEX and return T.
+  "Atomically release MUTEX and start waiting on QUEUE until another thread
+wakes us up using either CONDITION-NOTIFY or CONDITION-BROADCAST on
+QUEUE, at which point we re-acquire MUTEX and return T.
 
 Spurious wakeups are possible.
 
-If TIMEOUT is given, it is the maximum number of seconds to wait, including
-both waiting for the wakeup and the time to re-acquire MUTEX. Unless both
-wakeup and re-acquisition do not occur within the given time, returns NIL
-without re-acquiring the mutex.
+If TIMEOUT is given, it is the maximum number of seconds to wait,
+including both waiting for the wakeup and the time to re-acquire
+MUTEX. When neither a wakeup nor a re-acquisition occurs within the
+given time, returns NIL without re-acquiring MUTEX.
 
-If CONDITION-WAIT unwinds, it may do so with or without the mutex being held.
+If CONDITION-WAIT unwinds, it may do so with or without MUTEX being
+held.
 
-Important: Since CONDITION-WAIT may return without CONDITION-NOTIFY having
-occurred the correct way to write code that uses CONDITION-WAIT is to loop
-around the call, checking the the associated data:
+Important: Since CONDITION-WAIT may return without CONDITION-NOTIFY or
+CONDITION-BROADCAST having occurred, the correct way to write code
+that uses CONDITION-WAIT is to loop around the call, checking the
+associated data:
 
   (defvar *data* nil)
   (defvar *queue* (make-waitqueue))
