3f350f445c1101141b63d04c6f1dfed2cb17b875
Douglas Katzman <dougk@google.com>
2016-11-30 00:24:42 -0500

    Fix {SET,GET}-HEADER-DATA for 64-bit architectures

--- src/compiler/generic/vm-fndb.lisp
+++ src/compiler/generic/vm-fndb.lisp
@@ -125,11 +125,12 @@
   (unsigned-byte #.sb!vm:n-widetag-bits)
   (flushable movable))
 
-;; FIXME: Sure looks like 24 should be (- n-word-bytes n-widetag-bits).
-;;        If not, add a comment why not.
-(defknown (get-header-data get-closure-length) (t) (unsigned-byte 24)
+(defknown (get-header-data get-closure-length) (t)
+    (unsigned-byte #.(- sb!vm:n-word-bits sb!vm:n-widetag-bits))
   (flushable))
-(defknown set-header-data (t (unsigned-byte 24)) t
+;;; This unconventional setter returns its first arg, not the newval.
+(defknown set-header-data
+    (t (unsigned-byte #.(- sb!vm:n-word-bits sb!vm:n-widetag-bits))) t
   ())
 
 (defknown %array-dimension (t index) index
