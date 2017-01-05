00785b7a5618ea13b99dc471e61c073a9d6edbab
Douglas Katzman <dougk@google.com>
2017-01-05 14:36:32 -0500

    Do !package-cold-init a little differently

--- src/code/target-package.lisp
+++ src/code/target-package.lisp
@@ -1617,9 +1617,8 @@ PACKAGE."
               (package-%locally-nicknamed-by pkg) nil
               (package-source-location pkg) nil
               (gethash (package-%name pkg) names) pkg)
-        (let ((nicks (package-%nicknames pkg)))
-          (setf (package-%nicknames pkg) nil) ; each is pushed in again
-          (%enter-new-nicknames pkg nicks))
+        (dolist (nick (package-%nicknames pkg))
+          (setf (gethash nick *package-names*) pkg))
         #!+sb-package-locks
         (setf (package-lock pkg) nil
               (package-%implementation-packages pkg) nil))))
