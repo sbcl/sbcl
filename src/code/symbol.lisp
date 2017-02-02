ed121de1ddcf86cdabe189a82b453a4d7ff5f13e
Douglas Katzman <dougk@google.com>
2017-02-02 12:44:11 -0500

    Remove kludge, fixed a while ago.

--- src/code/symbol.lisp
+++ src/code/symbol.lisp
@@ -199,11 +199,7 @@ distinct from the global value. Can also be SETF."
     ;; Do not use SYMBOL-INFO-VECTOR - this must not perform a slot read again.
     (setq current-vect (if (listp info-holder) (cdr info-holder) info-holder))
    inner-restart
-    ;; KLUDGE: The "#." on +nil-packed-infos+ is due to slightly crippled
-    ;; fops in genesis's fasload. Anonymizing the constant works around the
-    ;; issue, at the expense of an extra copy of the empty info vector.
-    (let ((new-vect (funcall update-fn
-                             (or current-vect #.+nil-packed-infos+))))
+    (let ((new-vect (funcall update-fn (or current-vect +nil-packed-infos+))))
       (unless (simple-vector-p new-vect)
         (aver (null new-vect))
         (return)) ; nothing to do
