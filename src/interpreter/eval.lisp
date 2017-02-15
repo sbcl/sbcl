1fbd0ca6389a0fa643d5c98f885078c8f32280fd
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2017-02-15 16:42:34 +0100

    Fix !UNINTERN-SYMBOLS in src/interpreter/eval.lisp some more

--- src/interpreter/eval.lisp
+++ src/interpreter/eval.lisp
@@ -245,11 +245,13 @@
         (%eval form interpreter-env))))
 
 (defun !unintern-symbols ()
-  `("SB-INTERPRETER"
-    %%eval ; got inlined
-    ,@(let (macros)
-        (do-symbols (s "SB-INTERPRETER" macros)
-          (when (and (eq (symbol-package s) this-pkg)
-                     (macro-function s)
-                     (not (member s '(defspecial do-decl-spec)))) ; for SB-CLTL2
-            (push s macros))))))
+  (let ((this-pkg (find-package "SB-INTERPRETER")))
+    `("SB-INTERPRETER"
+      %%eval ; got inlined
+      ,@(let (macros)
+          (do-symbols (s "SB-INTERPRETER" macros)
+            (when (and (eq (symbol-package s) this-pkg)
+                       (macro-function s)
+                       (not (member s '(defspecial with-subforms do-decl-spec)))) ; for SB-CLTL2
+              (push s macros)))
+          macros))))
