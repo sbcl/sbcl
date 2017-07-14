sbcl/sbcl

commit 7714fd53e90be2aa20036c353f2574e0426dd22f

x86-64: get rid of redundant sets of register names
The names in SB!VM only had entries for registers that appeared
in a DEFREG, which not all did; move the all-inclusive lists
from the disassembler into SB!VM. Use them from the disassembler.
*FLOAT-REGISTER-NAMES* were not needed; delete them.

src/compiler/x86-64/vm.lisp
@@ -13,24 +13,30 @@
 
 ;;;; register specs
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defvar *byte-register-names* (make-array 32 :initial-element nil))
-  (defvar *word-register-names* (make-array 32 :initial-element nil))
-  (defvar *dword-register-names* (make-array 32 :initial-element nil))
-  (defvar *qword-register-names* (make-array 32 :initial-element nil))
-  (defvar *float-register-names* (make-array 16 :initial-element nil)))
+(defconstant-eqx +byte-register-names+
+    #("AL"  "CL"  "DL"   "BL"   "SPL"  "BPL"  "SIL"  "DIL"
+      "R8B" "R9B" "R10B" "R11B" "R12B" "R13B" "R14B" "R15B")
+  #'equalp)
+(defconstant-eqx +word-register-names+
+    #("AX"  "CX"  "DX"   "BX"   "SP"   "BP"   "SI"   "DI"
+      "R8W" "R9W" "R10W" "R11W" "R12W" "R13W" "R14W" "R15W")
+  #'equalp)
+(defconstant-eqx +dword-register-names+
+    #("EAX" "ECX" "EDX"  "EBX"  "ESP"  "EBP"  "ESI"  "EDI"
+      "R8D" "R9D" "R10D" "R11D" "R12D" "R13D" "R14D" "R15D")
+  #'equalp)
+(defconstant-eqx +qword-register-names+
+    #("RAX" "RCX" "RDX" "RBX" "RSP" "RBP" "RSI" "RDI"
+      "R8"  "R9"  "R10" "R11" "R12" "R13" "R14" "R15")
+  #'equalp)
 
 (macrolet ((defreg (name offset size)
-             (let ((offset-sym (symbolicate name "-OFFSET"))
-                   (names-vector (symbolicate "*" size "-REGISTER-NAMES*")))
-               `(progn
-                  (eval-when (:compile-toplevel :load-toplevel :execute)
+             (declare (ignore size))
+             `(eval-when (:compile-toplevel :load-toplevel :execute)
                     ;; EVAL-WHEN is necessary because stuff like #.EAX-OFFSET
                     ;; (in the same file) depends on compile-time evaluation
                     ;; of the DEFCONSTANT. -- AL 20010224
-                    (defconstant ,offset-sym ,offset))
-                  (setf (svref ,names-vector ,offset-sym)
-                        ,(symbol-name name)))))
+                (defconstant ,(symbolicate name "-OFFSET") ,offset)))
            ;; FIXME: It looks to me as though DEFREGSET should also
            ;; define the related *FOO-REGISTER-NAMES* variable.
            (defregset (name &rest regs)
@@ -547,16 +553,17 @@
       (registers
        (let* ((sc-name (sc-name sc))
               (name-vec (cond ((member sc-name *byte-sc-names*)
-                               *byte-register-names*)
+                               +byte-register-names+)
                               ((member sc-name *word-sc-names*)
-                               *word-register-names*)
+                               +word-register-names+)
                               ((member sc-name *dword-sc-names*)
-                               *dword-register-names*)
+                               +dword-register-names+)
                               ((member sc-name *qword-sc-names*)
-                               *qword-register-names*))))
+                               +qword-register-names+))))
          (or (and name-vec
+                  (evenp offset)
                   (< -1 offset (length name-vec))
-                  (svref name-vec offset))
+                  (svref name-vec (ash offset -1)))
              ;; FIXME: Shouldn't this be an ERROR?
              (format nil "<unknown reg: off=~W, sc=~A>" offset sc-name))))
       (float-registers (format nil "FLOAT~D" offset))

src/compiler/x86-64/insts.lisp
@@ -44,17 +44,6 @@
 ;;; to :dword with a #x67 prefix, but this is never needed by SBCL
 ;;; and thus not supported by this assembler/disassembler.
 (defconstant +default-address-size+ :qword)
-
-(defparameter *byte-reg-names*
-  #(al cl dl bl spl bpl sil dil r8b r9b r10b r11b r12b r13b r14b r15b))
-(defparameter *high-byte-reg-names*
-  #(ah ch dh bh))
-(defparameter *word-reg-names*
-  #(ax cx dx bx sp bp si di r8w r9w r10w r11w r12w r13w r14w r15w))
-(defparameter *dword-reg-names*
-  #(eax ecx edx ebx esp ebp esi edi r8d r9d r10d r11d r12d r13d r14d r15d))
-(defparameter *qword-reg-names*
-  #(rax rcx rdx rbx rsp rbp rsi rdi r8 r9 r10 r11 r12 r13 r14 r15))
 
 ;;; The printers for registers, memory references and immediates need to
 ;;; take into account the width bit in the instruction, whether a #x66

src/compiler/x86-64/move.lisp
@@ -245,7 +245,7 @@
                     ;; but let's be future-proof and allow for it.
                     (unless (member x '(rsp rbp) :test 'string=)
                       (symbolicate "ALLOC-" signedp "-BIGNUM-IN-" x)))
-                  sb!x86-64-asm::*qword-reg-names*)
+                  +qword-register-names+)
            (ash (tn-offset ,tn) -1))))
 
 ;;; Convert an untagged signed word to a lispobj -- fixnum or bignum

src/compiler/x86-64/target-insts.lisp
@@ -32,12 +32,12 @@
   (princ (if (and (eq width :byte)
                   (<= 4 value 7)
                   (not (dstate-get-inst-prop dstate +rex+)))
-             (aref *high-byte-reg-names* (- value 4))
+             (aref #("AH" "CH" "DH" "BH") (- value 4))
              (aref (ecase width
-                     (:byte *byte-reg-names*)
-                     (:word *word-reg-names*)
-                     (:dword *dword-reg-names*)
-                     (:qword *qword-reg-names*))
+                     (:byte sb!vm::+byte-register-names+)
+                     (:word sb!vm::+word-register-names+)
+                     (:dword sb!vm::+dword-register-names+)
+                     (:qword sb!vm::+qword-register-names+))
                    value))
          stream)
   ;; XXX plus should do some source-var notes

commit 25523100925b6086c0560f73cee269de9706490d

Fix bug in location-print-name caused by rev 7714fd53e9

src/compiler/x86-64/vm.lisp
@@ -552,6 +552,7 @@
     (ecase sb
       (registers
        (let* ((sc-name (sc-name sc))
+              (index (ash offset -1))
               (name-vec (cond ((member sc-name *byte-sc-names*)
                                +byte-register-names+)
                               ((member sc-name *word-sc-names*)
@@ -562,8 +563,8 @@
                                +qword-register-names+))))
          (or (and name-vec
                   (evenp offset)
-                  (< -1 offset (length name-vec))
-                  (svref name-vec (ash offset -1)))
+                  (< -1 index (length name-vec))
+                  (svref name-vec index))
              ;; FIXME: Shouldn't this be an ERROR?
              (format nil "<unknown reg: off=~W, sc=~A>" offset sc-name))))
       (float-registers (format nil "FLOAT~D" offset))