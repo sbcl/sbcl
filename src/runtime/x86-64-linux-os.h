5137da6e743906b5c0250e01418cc84c09048f5e
Stas Boukarev <stassats@gmail.com>
2017-09-16 22:27:40 +0300

    Use ucontext_t instead of struct ucontext on linux.
    Fixes lp#1714549.

--- src/runtime/x86-64-linux-os.h
+++ src/runtime/x86-64-linux-os.h
@@ -1,7 +1,7 @@
 #ifndef _X86_64_LINUX_OS_H
 #define _X86_64_LINUX_OS_H
 
-typedef struct ucontext os_context_t;
+typedef ucontext_t os_context_t;
 typedef long os_context_register_t;
 
 static inline os_context_t *arch_os_get_context(void **void_context)
