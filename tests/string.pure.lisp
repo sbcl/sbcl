5770e0bbcb7aef293ca980f808d9ff8b13df0821
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2017-09-17 17:32:44 +0200

    tests: WITH-TEST and CHECKED-COMPILE in string.pure.lisp

--- tests/string.pure.lisp
+++ tests/string.pure.lisp
@@ -14,82 +14,93 @@
 (in-package "CL-USER")
 
 ;;; basic non-destructive case operations
-(assert (string= (string-upcase     "This is a test.") "THIS IS A TEST."))
-(assert (string= (string-downcase   "This is a test.") "this is a test."))
-(assert (string= (string-capitalize "This is a test.") "This Is A Test."))
-(assert (string= (string-upcase "Is this 900-Sex-hott, please?" :start 3)
-                 "Is THIS 900-SEX-HOTT, PLEASE?"))
-(assert (string= (string-downcase "Is this 900-Sex-hott, please?"
-                                  :start 10 :end 16)
-                 "Is this 900-sex-hott, please?"))
-(assert (string= (string-capitalize "Is this 900-Sex-hott, please?")
-                 "Is This 900-Sex-Hott, Please?"))
+(with-test (:name (string-upcase string-downcase string-capitalize :smoke))
+  (assert (string= (string-upcase     "This is a test.") "THIS IS A TEST."))
+  (assert (string= (string-downcase   "This is a test.") "this is a test."))
+  (assert (string= (string-capitalize "This is a test.") "This Is A Test."))
+  (assert (string= (string-upcase "Is this 900-Sex-hott, please?" :start 3)
+                   "Is THIS 900-SEX-HOTT, PLEASE?"))
+  (assert (string= (string-downcase "Is this 900-Sex-hott, please?"
+                                    :start 10 :end 16)
+                   "Is this 900-sex-hott, please?"))
+  (assert (string= (string-capitalize "Is this 900-Sex-hott, please?")
+                   "Is This 900-Sex-Hott, Please?")))
 
 ;;; The non-destructive case operations accept string designators, not
 ;;; just strings.
-(assert (string= (string-upcase '|String designator|) "STRING DESIGNATOR"))
-(assert (string= (string-downcase #\S) "s"))
-(assert (string= (string-downcase #\.) "."))
-(assert (string= (string-capitalize 'ya-str-desig :end 5) "Ya-StR-DESIG"))
+
+(with-test (:name (string-upcase string-downcase string-capitalize :designators))
+  (assert (string= (string-upcase '|String designator|) "STRING DESIGNATOR"))
+  (assert (string= (string-downcase #\S) "s"))
+  (assert (string= (string-downcase #\.) "."))
+  (assert (string= (string-capitalize 'ya-str-desig :end 5) "Ya-StR-DESIG")))
 
 ;;; basic destructive case operations
-(let ((nstring (make-array 5 :element-type 'character :fill-pointer 0)))
-  (vector-push-extend #\c nstring)
-  (vector-push-extend #\a nstring)
-  (vector-push-extend #\t nstring)
-  (nstring-upcase nstring)
-  (assert (string= nstring "CAT"))
-  (setf (fill-pointer nstring) 2)
-  (nstring-downcase nstring :start 1)
-  (setf (fill-pointer nstring) 3)
-  (assert (string= nstring "CaT"))
-  (nstring-capitalize nstring)
-  (assert (string= nstring "Cat")))
+(with-test (:name (nstring-upcase nstring-downcase nstring-capitalize :smoke))
+  (let ((nstring (make-array 5 :element-type 'character :fill-pointer 0)))
+    (vector-push-extend #\c nstring)
+    (vector-push-extend #\a nstring)
+    (vector-push-extend #\t nstring)
+    (nstring-upcase nstring)
+    (assert (string= nstring "CAT"))
+    (setf (fill-pointer nstring) 2)
+    (nstring-downcase nstring :start 1)
+    (setf (fill-pointer nstring) 3)
+    (assert (string= nstring "CaT"))
+    (nstring-capitalize nstring)
+    (assert (string= nstring "Cat"))))
 
 ;;; (VECTOR NIL)s are strings.  Tests for that and issues uncovered in
 ;;; the process.
-(assert (typep (make-array 1 :element-type nil) 'string))
-(assert (not (typep (make-array 2 :element-type nil) 'base-string)))
-(assert (typep (make-string 3 :element-type nil) 'simple-string))
-(assert (not (typep (make-string 4 :element-type nil) 'simple-base-string)))
+(with-test (:name (vector nil))
+  (assert (typep (make-array 1 :element-type nil) 'string))
+  (assert (not (typep (make-array 2 :element-type nil) 'base-string)))
+  (assert (typep (make-string 3 :element-type nil) 'simple-string))
+  (assert (not (typep (make-string 4 :element-type nil) 'simple-base-string)))
 
-(assert (subtypep (class-of (make-array 1 :element-type nil))
-                  (find-class 'string)))
-(assert (subtypep (class-of (make-array 2 :element-type nil :fill-pointer 1))
-                  (find-class 'string)))
+  (assert (subtypep (class-of (make-array 1 :element-type nil))
+                    (find-class 'string)))
+  (assert (subtypep (class-of (make-array 2 :element-type nil :fill-pointer 1))
+                    (find-class 'string)))
 
-(assert (string= "" (make-array 0 :element-type nil)))
-(assert (string/= "a" (make-array 0 :element-type nil)))
-(assert (string= "" (make-array 5 :element-type nil :fill-pointer 0)))
+  (assert (string= "" (make-array 0 :element-type nil)))
+  (assert (string/= "a" (make-array 0 :element-type nil)))
+  (assert (string= "" (make-array 5 :element-type nil :fill-pointer 0)))
 
-(assert (= (sxhash "")
-           (sxhash (make-array 0 :element-type nil))
-           (sxhash (make-array 5 :element-type nil :fill-pointer 0))
-           (sxhash (make-string 0 :element-type nil))))
-(assert (subtypep (type-of (make-array 2 :element-type nil)) 'simple-string))
-(assert (subtypep (type-of (make-array 4 :element-type nil :fill-pointer t))
-                  'string))
+  (assert (= (sxhash "")
+             (sxhash (make-array 0 :element-type nil))
+             (sxhash (make-array 5 :element-type nil :fill-pointer 0))
+             (sxhash (make-string 0 :element-type nil))))
+  (assert (subtypep (type-of (make-array 2 :element-type nil)) 'simple-string))
+  (assert (subtypep (type-of (make-array 4 :element-type nil :fill-pointer t))
+                    'string))
 
-(assert (eq (intern "") (intern (make-array 0 :element-type nil))))
-(assert (eq (intern "")
-            (intern (make-array 5 :element-type nil :fill-pointer 0))))
+  (assert (eq (intern "") (intern (make-array 0 :element-type nil))))
+  (assert (eq (intern "")
+              (intern (make-array 5 :element-type nil :fill-pointer 0)))))
 
-(assert-error (make-string 5 :element-type t))
-(assert-error (let () (make-string 5 :element-type t)))
+(with-test (:name (make-string :element-type t type-error))
+  (multiple-value-bind (fun failure-p warnings)
+      (checked-compile '(lambda () (make-string 5 :element-type t))
+                       :allow-failure t :allow-warnings t)
+    (assert failure-p)
+    (assert (= 1 (length warnings)))
+    (assert-error (funcall fun) type-error)))
 
 ;; MISC.574
-(assert (= (funcall (lambda (a)
-                      (declare (optimize (speed 3) (safety 1)
-                                         (debug 1) (space 2))
-                               (fixnum a))
-                      (string<= (coerce "e99mo7yAJ6oU4" 'base-string)
-                                (coerce "aaABAAbaa" 'base-string)
-                                :start1 a))
-                    9)
-           9))
+(with-test (:name (string<= base-string :optimized))
+  (assert (= (funcall (lambda (a)
+                        (declare (optimize (speed 3) (safety 1)
+                                           (debug 1) (space 2))
+                                 (fixnum a))
+                        (string<= (coerce "e99mo7yAJ6oU4" 'base-string)
+                                  (coerce "aaABAAbaa" 'base-string)
+                                  :start1 a))
+                      9)
+             9)))
 
 ;; String trimming.
-(with-test (:name :string-trim.1)
+(with-test (:name (string-trim 1))
   (flet ((make-test (string left right both)
            (macrolet ((check (fun wanted)
                         `(let ((result (,fun " " string)))
@@ -120,7 +131,7 @@
 
 
 ;;; Trimming should respect fill-pointers
-(with-test (:name :string-trim.fill-pointer)
+(with-test (:name (string-trim :fill-pointer))
   (let* ((s (make-array 9 :initial-contents "abcdabadd" :element-type
                         'character :fill-pointer 7))
          (s2 (string-left-trim "ab" s))
@@ -130,7 +141,7 @@
     (assert (equal "abcd" s3))))
 
 ;;; Trimming should replace displacement offsets
-(with-test (:name :string-trim.displaced)
+(with-test (:name (string-trim :displaced))
  (let* ((etype 'base-char)
         (s0
           (make-array '(6) :initial-contents "abcaeb" :element-type etype))
@@ -140,7 +151,7 @@
    (assert (equal "bca" s))
    (assert (equal "abcaeb" s0))))
 
-(with-test (:name :string-trimg.nothing-to-do)
+(with-test (:name (string-trim :nothing-to-do))
   ;; Trimming non-simple-strings when there is nothing to do
   (let ((a (make-array 10 :element-type 'character :initial-contents "abcde00000" :fill-pointer 5)))
     (assert (equal "abcde" (string-right-trim "Z" a))))
@@ -154,12 +165,12 @@
                   sb-kernel:nil-array-accessed-error)
     (flet ((test (accessor)
              (assert-error
-              (funcall (compile nil
-                                `(lambda ()
-                                   (,accessor (make-array 10 :element-type nil) 0))))
+              (funcall (checked-compile
+                        `(lambda ()
+                           (,accessor (make-array 10 :element-type nil) 0))))
               sb-kernel:nil-array-accessed-error)
              (assert-error
-              (funcall (compile nil `(lambda (x) (,accessor x 0)))
+              (funcall (checked-compile `(lambda (x) (,accessor x 0)))
                        nil-vector)
               sb-kernel:nil-array-accessed-error)))
       (test 'aref)
@@ -173,13 +184,15 @@
                   sb-kernel:nil-array-accessed-error)
     (flet ((test (accessor args)
              (assert-error
-              (funcall (compile nil
-                                `(lambda ()
-                                   (,accessor (make-array '(10 10) :element-type nil)
-                                              ,@(make-list args :initial-element 0)))))
+              (funcall (checked-compile
+                        `(lambda ()
+                           (,accessor (make-array '(10 10) :element-type nil)
+                                      ,@(make-list args :initial-element 0)))))
               sb-kernel:nil-array-accessed-error)
              (assert-error
-              (funcall (compile nil `(lambda (x) (,accessor x ,@(make-list args :initial-element 0))))
+              (funcall (checked-compile
+                        `(lambda (x)
+                           (,accessor x ,@(make-list args :initial-element 0))))
                        nil-array)
               sb-kernel:nil-array-accessed-error)))
       (test 'aref 2)
