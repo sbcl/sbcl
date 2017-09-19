f9dcc594b31b6013e6d3c6340b29dec3d8a968ba
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2017-09-19 12:06:11 +0200

    sb-bsd-sockets: {SYSCALL,SOCKET}-ERROR-CASE bind multiple return values

--- contrib/sb-bsd-sockets/util.lisp
+++ contrib/sb-bsd-sockets/util.lisp
@@ -6,7 +6,7 @@
 (defun interrupted-p (errno)
   (or (= errno sockint::EAGAIN) (= errno sockint::EINTR)))
 
-(defmacro syscall-error-case ((form result-var test-form errno-form)
+(defmacro syscall-error-case ((form result-var-or-vars test-form errno-form)
                               &optional no-error-form
                               &body clauses)
   (let ((interrupted-case nil)
@@ -18,7 +18,7 @@
          (setf interrupted-case (rest clause)))
         (:error
          (setf error-case (rest clause)))))
-    `(let ((,result-var ,form))
+    `(multiple-value-bind ,(sb-int:ensure-list result-var-or-vars) ,form
        (cond
          ,@(when interrupted-case
              `(((and ,test-form (interrupted-p ,errno-form))
@@ -31,12 +31,14 @@
 
 (defmacro socket-error-case ((context form
                               &optional
-                              (result-var (gensym "RESULT"))
-                              (test-form `(= ,result-var -1))
+                              (result-var-or-vars (gensym "RESULT"))
+                              (test-form `(= ,(first (sb-int:ensure-list
+                                                      result-var-or-vars))
+                                             -1))
                               (errno-form `(socket-errno)))
                              &optional no-error-form
                              &body clauses)
-  `(syscall-error-case (,form ,result-var ,test-form ,errno-form)
+  `(syscall-error-case (,form ,result-var-or-vars ,test-form ,errno-form)
        ,no-error-form
      ,@(unless (find :error clauses :key #'first)
          `((:error (socket-error ,context))))
