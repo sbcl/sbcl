ef7acd85a45f54054b2b3d8fb5ce2ba0887bc341
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2017-11-10 09:45:18 +0100

    sb-posix: Allow strtod tests to succeed on (some?) non-English locales

--- contrib/sb-posix/libc-tests.lisp
+++ contrib/sb-posix/libc-tests.lisp
@@ -5,18 +5,30 @@
   #("1.20203" "3.4400" "3240.2205" "10088.92" "12.3" "1000000000e-2"
     "-323.5233243" "10001.993344e-8" "3985.1"))
 
+(defparameter *decimal-point-character*
+  (loop for candidate in '(#\. #\,)
+     when (= (nth-value 1 (sb-posix:strtod "1.234")) 5)
+     return candidate))
+
+(defun substitute-decimal-point (string)
+  (substitute *decimal-point-character* #\. string))
+
 (deftest sb-libc.strtod.0
-  (loop with *read-default-float-format* = 'double-float
-        for string across *tests*
-        do (assert (= (sb-posix:strtod string) (read-from-string string)))
-        finally (return t))
+    (or (not *decimal-point-character*)
+        (loop with *read-default-float-format* = 'double-float
+           for string across *tests*
+           do (assert (= (sb-posix:strtod (substitute-decimal-point string))
+                         (read-from-string string)))
+           finally (return t)))
   t)
 
 (deftest sb-libc.strtod.1
-  (loop with *read-default-float-format* = 'double-float
-        for string across (map 'vector
-                               (lambda (x) (coerce x 'simple-base-string))
-                               *tests*)
-        do (assert (= (sb-posix:strtod string) (read-from-string string)))
-        finally (return t))
+    (or (not *decimal-point-character*)
+        (loop with *read-default-float-format* = 'double-float
+           for string across *tests*
+           for simple-base-string = (coerce (substitute-decimal-point string)
+                                            'simple-base-string)
+           do (assert (= (sb-posix:strtod simple-base-string)
+                         (read-from-string string)))
+           finally (return t)))
   t)
