4969ac0bf42d3c724a3af30c53750190c9eb5702
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-01-15 11:50:21 +0100

    tests: More WITH-TEST in clos.pure.lisp

--- tests/clos.pure.lisp
+++ tests/clos.pure.lisp
@@ -18,38 +18,40 @@
 ;;; system, because some functionality (e.g. &key argument checking)
 ;;; depends on it.  The basic functionality is tested elsewhere, but
 ;;; this is to investigate the internals for possible inconsistency.
-(assert (null
-         (let (collect)
-           (sb-pcl::map-all-generic-functions
-            (lambda (gf)
-              (let ((arg-info (sb-pcl::gf-arg-info gf)))
-                (when (eq (sb-pcl::arg-info-lambda-list arg-info)
-                          :no-lambda-list)
-                  (push gf collect)))))
-           (print (nreverse collect)))))
+(with-test (:name (:builtin-generic-functions :known :lambda-list))
+  (let ((collect '()))
+    (sb-pcl::map-all-generic-functions
+     (lambda (gf)
+       (let ((arg-info (sb-pcl::gf-arg-info gf)))
+         (when (eq (sb-pcl::arg-info-lambda-list arg-info)
+                   :no-lambda-list)
+           (push gf collect)))))
+    (assert (null collect))))
 
 ;;; Regressing test for invalid slot specification error printing
-(multiple-value-bind (value err)
-    (ignore-errors (macroexpand '(defclass foo () (frob (frob bar)))))
-  (declare (ignore value))
-  (assert (typep err 'simple-condition))
-  (multiple-value-bind (value format-err)
-      (ignore-errors (apply #'format nil
-                            (simple-condition-format-control err)
-                            (simple-condition-format-arguments err)))
+(with-test (:name (defclass :slot :syntax-error print))
+  (multiple-value-bind (value err)
+      (ignore-errors (macroexpand '(defclass foo () (frob (frob bar)))))
     (declare (ignore value))
-    (assert (not format-err))))
+    (assert (typep err 'simple-condition))
+    (multiple-value-bind (value format-err)
+        (ignore-errors (apply #'format nil
+                              (simple-condition-format-control err)
+                              (simple-condition-format-arguments err)))
+      (declare (ignore value))
+      (assert (not format-err)))))
 
 ;;; another not (user-)observable behaviour: make sure that
 ;;; sb-pcl::map-all-classes calls its function on each class once and
 ;;; exactly once.
-(let (result)
-  (sb-pcl::map-all-classes (lambda (c) (push c result)))
-  (assert (equal result (remove-duplicates result))))
+(with-test (:name (sb-pcl::map-all-classes :no-duplicates))
+  (let ((result '()))
+    (sb-pcl::map-all-classes (lambda (c) (push c result)))
+    (assert (equal result (remove-duplicates result)))))
 
 ;;; this one's user-observable
-(with-test (:name :type-of-setf-class-name)
- (assert (typep #'(setf class-name) 'generic-function)))
+(with-test (:name (type-of (setf class-name)))
+  (assert (typep #'(setf class-name) 'generic-function)))
 
 ;;; CLHS 1.4.4.5.  We could test for this by defining methods
 ;;; (i.e. portably) but it's much easier using the MOP and
