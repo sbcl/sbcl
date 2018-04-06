sbcl/sbcl

commit f2925d6ad4abaa3a47c1e09bab18c4d2f1f5bf0d

Fix storage of nested source forms from COMPILE (or LOAD)
This change adds the ability for each simple-fun in a code component
to store its own source form.

In the old way, a code component which contained multiple simple-funs would
store a single form in the code-debug-info of the code component.
A chosen representative function (the toplevel) from the component was stored
as DEBUG-SOURCE-FUNCTION so that we could remember whose source it was,
if more than one visible function. (Optional dispatches and XEP aren't visible).

So when more than one function existed, to inquire whether a function had a
source form, we checked the debug-source of the debug-info and asked whether
the function being queried was specifically the chosen function whose form
was stored. If not, we fell back to namestring. (Logic in GET-TOPLEVEL-FORM)

Now COMPUTE-ENTRY-INFO gets the correct source form for every simple-fun
so that we can attach to that simple-fun. The form goes in %SIMPLE-FUN-INFO
along with what was already there. The accessors for form/doc/xrefs figure
out which was saved. (Because 6 words is a nice even number of words per
function header, and 7 is not as nice, thus the design goal of no new slots)

We could eventually advertise in NEWS that FUNCTION-LAMBDA-EXPRESSION
works better, but not yet - the (DEBUGGER SOURCE 2) test in debug.impure.lisp
fails; and maybe some other undiscovered rough edges still exist.

src/code/debug-info.lisp
@@ -357,25 +357,12 @@
 ;;; toplevel or loaded from source.
 (def!struct (debug-source #-sb-xc-host (:pure t)
                           (:copier nil))
-  ;; (This is one of those structures where IWBNI we had multiple
-  ;; inheritance.  The first four slots describe compilation of a
-  ;; file, the fifth and sixth compilation of a form processed by
-  ;; EVAL, and the seventh and eigth all compilation units; and these
-  ;; are orthogonal concerns that can combine independently.)
-
   ;; When the DEBUG-SOURCE describes a file, the file's namestring.
   ;; Otherwise, NIL.
   (namestring nil :type (or null string) :read-only t)
   ;; the universal time that the source was written, or NIL if
   ;; unavailable
   (created nil :type (or unsigned-byte null))
-
-  ;; For functions processed by EVAL (including EVAL-WHEN and LOAD on
-  ;; a source file), the source form.
-  (form nil :type list :read-only nil)
-  ;; This is the function whose source is the form.
-  (function nil :read-only t)
-
   ;; the universal time that the source was compiled
   (compiled (missing-arg) :type unsigned-byte)
   ;; Additional information from (WITH-COMPILATION-UNIT (:SOURCE-PLIST ...))

src/code/debug-int.lisp
@@ -2663,12 +2663,26 @@ register."
 
 ;;; Given a code location, return the associated form-number
 ;;; translations and the actual top level form.
-(defun get-toplevel-form (location)
+;;; Note that functions compiled to memory (via COMPILE or implicitly
+;;; via LOAD if *EVALUATOR-MODE* = :COMPILE) do not save their source form
+;;; in the DEBUG-SOURCE corresponding to their code-component. Instead the
+;;; form hangs off the %SIMPLE-FUN-INFO slot, so that we can get an accurate
+;;; depiction of the source form for any lambda no matter where from.
+(defun get-toplevel-form (frame location)
   (let ((d-source (code-location-debug-source location)))
     (let* ((offset (code-location-toplevel-form-offset location))
            (res
-             (cond ((debug-source-form d-source)
-                    (debug-source-form d-source))
+             (cond ((let ((fun (debug-fun-fun (frame-debug-fun frame))))
+                      (and (simple-fun-p fun)
+                           ;; %SIMPLE-FUN-LEXPR's primary value is the right answer
+                           ;; provided that its secondary value is 0. But the general
+                           ;; case handles the special case just fine, so always call
+                           ;; it twice - once to extract the toplevel function index
+                           ;; and then again to extract that function's source form.
+                           (sb!impl::%simple-fun-lexpr
+                            (%code-entry-point (fun-code-header fun)
+                                               (nth-value 1 (sb!impl::%simple-fun-lexpr
+                                                             fun)))))))
                    ((debug-source-namestring d-source)
                     (get-file-toplevel-form location))
                    (t (bug "Don't know how to use a DEBUG-SOURCE without ~

src/code/debug.lisp
@@ -743,7 +743,7 @@ the current thread are replaced with dummy objects which can safely escape."
                 (not (eql (search "SYS:SRC;" path) 0)))
         (handler-case
             (let ((source (handler-case
-                              (code-location-source-form loc 0)
+                              (code-location-source-form frame loc 0)
                             (error (c)
                               (format stream "~&   error finding frame source: ~A" c)))))
               (format stream "~%   source: ~S" source))
@@ -1679,17 +1679,18 @@ forms that explicitly control this kind of evaluation.")
 (!def-debug-command-alias "L" "LIST-LOCALS")
 
 (!def-debug-command "SOURCE" ()
-  (print (code-location-source-form (sb!di:frame-code-location *current-frame*)
+  (print (code-location-source-form *current-frame*
+                                    (sb!di:frame-code-location *current-frame*)
                                     (read-if-available 0))
          *debug-io*))
 
 ;;;; source location printing
 
-(defun code-location-source-form (location context &optional (errorp t))
+(defun code-location-source-form (frame location context &optional (errorp t))
   (let* ((start-location (maybe-block-start-location location))
          (form-num (sb!di:code-location-form-number start-location)))
     (multiple-value-bind (translations form)
-        (sb!di:get-toplevel-form start-location)
+        (sb!di:get-toplevel-form frame start-location)
       (declare (notinline warn))
       (cond ((< form-num (length translations))
              (sb!di:source-path-context form

src/code/simple-fun.lisp
@@ -211,38 +211,83 @@
     (sb!interpreter:interpreted-function (sb!interpreter:%fun-type function))
     (t (%simple-fun-type (%fun-fun function)))))
 
-;;; Extract halves of SIMPLE-FUN-INFO, which is a string if it holds
-;;; documentation, a SIMPLE-VECTOR if XREFS,
-;;; or (CONS STRING SIMPLE-VECTOR) for both, or NIL if neither.
-(macrolet ((def (name info-part if-simple-vector if-string)
+;;; A FUN-SRC structure appears in %SIMPLE-FUN-INFO of any function for
+;;; which a source form is retained via COMPILE or LOAD and for which it was
+;;; required to store all three of these pieces of data.
+(defstruct (fun-src (:constructor make-fun-src (form doc xrefs))
+                    (:predicate nil)
+                    (:copier nil))
+  form
+  doc
+  xrefs)
+;;; Assign %SIMPLE-FUN-INFO given the three possible things that
+;;; we stash there.
+(defun set-simple-fun-info (fun form doc xrefs)
+  (setf (%simple-fun-info fun)
+        (if form
+            ;; If form starts with a string, we can't store it by itself
+            ;; because it's confusable with (CONS STRING *)
+            ;; Lambda expressions start with LAMBDA, obviously,
+            ;; so this really shouldn't happen. Just being defensive here.
+            (if (or doc xrefs (typep form '(cons string)))
+                (make-fun-src form doc xrefs)
+                form)
+            (if (and doc xrefs)
+                (cons doc xrefs)
+                (or doc xrefs)))))
+
+;;; Define readers for parts of SIMPLE-FUN-INFO, which holds:
+;;;  - a string if documentation only,
+;;;  - a SIMPLE-VECTOR if xrefs only
+;;;  - a (CONS STRING SIMPLE-VECTOR) if both
+;;;  - a CONS headed by LAMBDA or an integer if a source form only
+;;;  - a FUN-SRC if other combinations of the above
+;;;  - or NIL
+(macrolet ((def (name info-part if-simple-vector if-string if-struct)
              `(defun ,name (simple-fun)
                 (declare (simple-fun simple-fun))
                 (let ((info (%simple-fun-info simple-fun)))
                   (typecase info
-                    (list (,info-part info))
+                    ;; (CONS (NOT STRING)) implies neither doc nor xref present
+                    (list (if (stringp (car info)) (,info-part info)))
                     (simple-vector ,if-simple-vector)
                     (string ,if-string)
+                    (fun-src ,if-struct)
                     (t (bug "bogus INFO for ~S: ~S" simple-fun info)))))))
-  (def %simple-fun-doc   car nil info)
-  (def %simple-fun-xrefs cdr info nil))
+  (def %simple-fun-doc   car nil info (fun-src-doc info))
+  (def %simple-fun-xrefs cdr info nil (fun-src-xrefs info)))
+
+;;; Return two values - the lambda expression for SIMPLE-FUN if compiled
+;;; to memory (and retained), and secondarily the index of the toplevel
+;;; function that contained this function, the latter serving to extract
+;;; forms by traversal of stored source path indices.
+(defun %simple-fun-lexpr (simple-fun)
+  (declare (simple-fun simple-fun))
+  (let* ((info (%simple-fun-info simple-fun))
+         (expr (typecase info
+                 (fun-src (fun-src-form info))
+                 ((cons (not string)) info))))
+    ;; An integer is the index to the entry into SIMPLE-FUN's code component
+    ;; that contains the toplevel form containing SIMPLE-FUN.
+    (if (integerp (car expr))
+        (values (cdr expr) (car expr))
+        (values expr 0))))
 
 (defun (setf %simple-fun-doc) (doc simple-fun)
   (declare (type (or null string) doc)
            (simple-fun simple-fun))
   (let ((info (%simple-fun-info simple-fun)))
     (setf (%simple-fun-info simple-fun)
-          (cond ((typep info '(or null string))
-                 doc)
-                ((simple-vector-p info)
-                 (if doc
-                     (cons doc info)
-                     info))
-                ((consp info)
-                 (if doc
-                     (cons doc (cdr info))
-                     (cdr info)))
-                (t
-                 (bug "bogus INFO for ~S: ~S" simple-fun info))))))
+          (typecase info
+            ((or null string) doc)
+            (simple-vector
+             (if doc (cons doc info) info))
+            ((cons string)
+             (if doc (cons doc (cdr info)) (cdr info)))
+            (fun-src
+             (setf (fun-src-doc info) doc))
+            (t
+             (bug "bogus INFO for ~S: ~S" simple-fun info))))))
 
 (defun %fun-doc (function)
   (typecase function

src/compiler/debug-dump.lisp
@@ -285,7 +285,7 @@
 
 ;;; Return DEBUG-SOURCE structure containing information derived from
 ;;; INFO.
-(defun debug-source-for-info (info &key function)
+(defun debug-source-for-info (info)
   (declare (type source-info info))
   (let ((file-info (get-toplevelish-file-info info)))
     (make-debug-source
@@ -298,12 +298,7 @@
                                (truename (file-info-name file-info)))))
                         (if (pathnamep pathname) pathname))
                       file-info))
-     :created (file-info-write-date file-info)
-     :form (when function
-             (let ((direct-file-info (source-info-file-info info)))
-               (when (eq :lisp (file-info-name direct-file-info))
-                 (elt (file-info-forms direct-file-info) 0))))
-     :function function)))
+     :created (file-info-write-date file-info))))
 
 (defun smallest-element-type (integer negative)
   (let ((bits (max (+ (integer-length integer)

src/compiler/generic/target-core.lisp
@@ -236,6 +236,10 @@
             (setf (%simple-fun-name fun) (entry-info-name entry-info))
             (setf (%simple-fun-arglist fun) (entry-info-arguments entry-info))
             (setf (%simple-fun-type fun) (entry-info-type entry-info))
+            ;; We can't make a decision to record the source here
+            ;; because the policy is not available in a simple way.
+            ;; (It would demand groveling through the component to find
+            ;; the lambda-bind corresponding to this entry)
             (setf (%simple-fun-info fun) (entry-info-info entry-info))
             (note-fun entry-info fun object))))
 
@@ -265,11 +269,10 @@
 ;;; Backpatch all the DEBUG-INFOs dumped so far with the specified
 ;;; SOURCE-INFO list. We also check that there are no outstanding
 ;;; forward references to functions.
-(defun fix-core-source-info (info object &optional function)
-  (declare (type core-object object)
-           (type (or null function) function))
+(defun fix-core-source-info (info object)
+  (declare (type core-object object))
   (aver (zerop (hash-table-count (core-object-patch-table object))))
-  (let ((source (debug-source-for-info info :function function)))
+  (let ((source (debug-source-for-info info)))
     (dolist (info (core-object-debug-info object))
       (setf (debug-info-source info) source)))
   (setf (core-object-debug-info object) nil)

commit 0c7d134ae602cf5ea5ea079da3fab998a4c388b7

Restore DEBUG-SOURCE-FORM/FUNCTION, slightly renamed
As discussed w/stassats, the most expedient fix for the debugger problems
caused by change f2925d6ad4a was to undo them while preserving the better
access to source forms for DESCRIBE and FUNCTION-LAMBDA-EXPRESSION.
Redundancy of a pointer in two places for the toplevel function in a code
component is probably negligible compared to the size of source form anyway.

src/code/debug-info.lisp
@@ -367,6 +367,14 @@
   (compiled (missing-arg) :type unsigned-byte)
   ;; Additional information from (WITH-COMPILATION-UNIT (:SOURCE-PLIST ...))
   (plist *source-plist* :read-only t))
+(def!struct (core-debug-source #-sb-xc-host (:pure t)
+                               (:copier nil)
+                               (:include debug-source))
+  ;; Compilation to memory stores each toplevel form given to %COMPILE.
+  ;; That form can generate multiple functions, and those functions can
+  ;; be in one or more code components. They all point at the same form.
+  form
+  function)
 
 ;;;; DEBUG-INFO structures
 

src/code/debug-int.lisp
@@ -2668,21 +2668,12 @@ register."
 ;;; in the DEBUG-SOURCE corresponding to their code-component. Instead the
 ;;; form hangs off the %SIMPLE-FUN-INFO slot, so that we can get an accurate
 ;;; depiction of the source form for any lambda no matter where from.
-(defun get-toplevel-form (frame location)
+(defun get-toplevel-form (location)
   (let ((d-source (code-location-debug-source location)))
     (let* ((offset (code-location-toplevel-form-offset location))
            (res
-             (cond ((let ((fun (debug-fun-fun (frame-debug-fun frame))))
-                      (and (simple-fun-p fun)
-                           ;; %SIMPLE-FUN-LEXPR's primary value is the right answer
-                           ;; provided that its secondary value is 0. But the general
-                           ;; case handles the special case just fine, so always call
-                           ;; it twice - once to extract the toplevel function index
-                           ;; and then again to extract that function's source form.
-                           (sb!impl::%simple-fun-lexpr
-                            (%code-entry-point (fun-code-header fun)
-                                               (nth-value 1 (sb!impl::%simple-fun-lexpr
-                                                             fun)))))))
+             (cond ((and (typep d-source 'sb!c::core-debug-source)
+                         (sb!c::core-debug-source-form d-source)))
                    ((debug-source-namestring d-source)
                     (get-file-toplevel-form location))
                    (t (bug "Don't know how to use a DEBUG-SOURCE without ~

src/code/debug.lisp
@@ -743,7 +743,7 @@ the current thread are replaced with dummy objects which can safely escape."
                 (not (eql (search "SYS:SRC;" path) 0)))
         (handler-case
             (let ((source (handler-case
-                              (code-location-source-form frame loc 0)
+                              (code-location-source-form loc 0)
                             (error (c)
                               (format stream "~&   error finding frame source: ~A" c)))))
               (format stream "~%   source: ~S" source))
@@ -1679,18 +1679,17 @@ forms that explicitly control this kind of evaluation.")
 (!def-debug-command-alias "L" "LIST-LOCALS")
 
 (!def-debug-command "SOURCE" ()
-  (print (code-location-source-form *current-frame*
-                                    (sb!di:frame-code-location *current-frame*)
+  (print (code-location-source-form (sb!di:frame-code-location *current-frame*)
                                     (read-if-available 0))
          *debug-io*))
 
 ;;;; source location printing
 
-(defun code-location-source-form (frame location context &optional (errorp t))
+(defun code-location-source-form (location context &optional (errorp t))
   (let* ((start-location (maybe-block-start-location location))
          (form-num (sb!di:code-location-form-number start-location)))
     (multiple-value-bind (translations form)
-        (sb!di:get-toplevel-form frame start-location)
+        (sb!di:get-toplevel-form start-location)
       (declare (notinline warn))
       (cond ((< form-num (length translations))
              (sb!di:source-path-context form

src/code/simple-fun.lisp
@@ -257,21 +257,15 @@
   (def %simple-fun-doc   car nil info (fun-src-doc info))
   (def %simple-fun-xrefs cdr info nil (fun-src-xrefs info)))
 
-;;; Return two values - the lambda expression for SIMPLE-FUN if compiled
-;;; to memory (and retained), and secondarily the index of the toplevel
-;;; function that contained this function, the latter serving to extract
-;;; forms by traversal of stored source path indices.
+;;; Return the lambda expression for SIMPLE-FUN if compiled to memory
+;;; and rentention of forms was enabled via the EVAL-STORE-SOURCE-FORM policy
+;;; (as is the default).
 (defun %simple-fun-lexpr (simple-fun)
   (declare (simple-fun simple-fun))
-  (let* ((info (%simple-fun-info simple-fun))
-         (expr (typecase info
-                 (fun-src (fun-src-form info))
-                 ((cons (not string)) info))))
-    ;; An integer is the index to the entry into SIMPLE-FUN's code component
-    ;; that contains the toplevel form containing SIMPLE-FUN.
-    (if (integerp (car expr))
-        (values (cdr expr) (car expr))
-        (values expr 0))))
+  (let ((info (%simple-fun-info simple-fun)))
+    (typecase info
+      (fun-src (fun-src-form info))
+      ((cons (not string)) info))))
 
 (defun (setf %simple-fun-doc) (doc simple-fun)
   (declare (type (or null string) doc)

src/compiler/debug-dump.lisp
@@ -285,10 +285,11 @@
 
 ;;; Return DEBUG-SOURCE structure containing information derived from
 ;;; INFO.
-(defun debug-source-for-info (info)
+(defun debug-source-for-info (info &key function)
   (declare (type source-info info))
   (let ((file-info (get-toplevelish-file-info info)))
-    (make-debug-source
+    (multiple-value-call
+        (if function #'sb!c::make-core-debug-source #'make-debug-source)
      :compiled (source-info-start-time info)
      :namestring (or *source-namestring*
                      (make-file-info-namestring
@@ -298,7 +299,13 @@
                                (truename (file-info-name file-info)))))
                         (if (pathnamep pathname) pathname))
                       file-info))
-     :created (file-info-write-date file-info))))
+     :created (file-info-write-date file-info)
+     (if function
+         (values :form (let ((direct-file-info (source-info-file-info info)))
+                         (when (eq :lisp (file-info-name direct-file-info))
+                           (elt (file-info-forms direct-file-info) 0)))
+                 :function function)
+         (values)))))
 
 (defun smallest-element-type (integer negative)
   (let ((bits (max (+ (integer-length integer)

src/compiler/generic/target-core.lisp
@@ -269,10 +269,11 @@
 ;;; Backpatch all the DEBUG-INFOs dumped so far with the specified
 ;;; SOURCE-INFO list. We also check that there are no outstanding
 ;;; forward references to functions.
-(defun fix-core-source-info (info object)
+(defun fix-core-source-info (info object &optional function)
   (declare (type core-object object))
+  (declare (type (or null function) function))
   (aver (zerop (hash-table-count (core-object-patch-table object))))
-  (let ((source (debug-source-for-info info)))
+  (let ((source (debug-source-for-info info :function function)))
     (dolist (info (core-object-debug-info object))
       (setf (debug-info-source info) source)))
   (setf (core-object-debug-info object) nil)

src/compiler/main.lisp
@@ -1246,49 +1246,28 @@ necessary, since type inference may take arbitrarily long to converge.")
             (when (core-object-p object)
               #+sb-xc-host (error "Can't compile to core")
               #-sb-xc-host
-              (progn
-                (fix-core-source-info *source-info* object)
+              (let ((store-source
+                     (policy (lambda-bind fun)
+                             (> eval-store-source-form 0))))
+                (fix-core-source-info *source-info* object
+                                      (and store-source result))
                 ;; FIXME: here on down the logic probably belongs in
                 ;; MAKE-CORE-COMPONENT, but to do that we'd want to pass in
                 ;; the EVAL-STORE-SOURCE policy
-                (when (policy (lambda-bind fun)
-                              (> eval-store-source-form 0))
-                  (let* ((direct-file-info (source-info-file-info *source-info*))
-                         (code (fun-code-header result))
-                         ;; Find RESULT's index in the list of CODE entrypoints
-                         (toplevel-fun-index
-                          (dotimes (i (code-n-entries code))
-                            (when (eq result (%code-entry-point code i))
-                              (return i)))))
-                    (aver toplevel-fun-index)
-                    ;; Store each simple-fun's lambda expression
-                    (dohash ((entry simple-fun) entry-table)
-                      (let ((info (entry-info-info entry)))
-                        (sb!impl::set-simple-fun-info
+                (when store-source
+                  ;; Store each simple-fun's lambda expression
+                  (dohash ((entry simple-fun) entry-table)
+                    (let ((info (entry-info-info entry)))
+                      (sb!impl::set-simple-fun-info
                          simple-fun
-                         (if (and (eq simple-fun result)
-                                  (eq :lisp (file-info-name direct-file-info)))
-                             ;; The stored source form is a little different for the toplevel
-                             ;; to be debugger-compatible. All source form paths are
-                             ;; expressed in terms of a directed walk from the top of the
-                             ;; outermost form. See FORM-NUMBER-TRANSLATIONS, e.g.
-                             (elt (file-info-forms direct-file-info) 0)
-                             ;; Otherwise, store the LEXPR as given, and a backreference
-                             ;; to the index of the toplevel-est functional.
-                             ;; If the backreference is to entry index 0, it's not
-                             ;; necessary to store the index number.
-                             (let ((lexpr (entry-info-lexpr entry)))
-                               (if (eql toplevel-fun-index 0) ; almost always true
-                                   lexpr
-                                   ;; otherwise, remember the nonzero index
-                                   (cons toplevel-fun-index lexpr))))
+                         (entry-info-lexpr entry)
                          ;; SET-SIMPLE-FUN-INFO expects all 3 thingies that we might
                          ;; save (any can be NIL) so extract the existing docstring
                          (cond ((listp info) (car info))
                                ((stringp info) info))
                          ;; ... and the existing xrefs
                          (cond ((listp info) (cdr info))
-                               ((simple-vector-p info) info)))))))))
+                               ((simple-vector-p info) info))))))))
 
             (mapc #'clear-ir1-info components-from-dfo)
             result))))))

src/compiler/target-disassem.lisp
@@ -1222,7 +1222,7 @@
                (awhen (sfcache-last-location-retrieved cache)
                  (sb!di:code-location= loc it))))
       (values nil nil)
-      (let ((form (sb!debug::code-location-source-form nil loc context nil)))
+      (let ((form (sb!debug::code-location-source-form loc context nil)))
         (when cache
           (setf (sfcache-debug-source cache)
                 (sb!di:code-location-debug-source loc))