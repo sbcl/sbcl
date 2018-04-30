2bf667cacfee779ab3301e2cf97687077e2f3e48
Douglas Katzman <dougk@google.com>
2018-04-30 09:25:09 -0400

    Move 'src/code/module' to warm build phase

--- src/code/module.lisp
+++ src/code/module.lisp
@@ -13,7 +13,7 @@
 ;;;; provided with absolutely no warranty. See the COPYING and CREDITS
 ;;;; files for more information.
 
-(in-package "SB!IMPL")
+(in-package "SB-IMPL")
 
 ;;;; exported specials
 
