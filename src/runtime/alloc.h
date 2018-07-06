b970f914b1a8bc951023e7dd9edde9e1f802fe6c
Douglas Katzman <dougk@google.com>
2018-07-06 14:34:06 -0400

    Fix parameter name inconsistency noted by 'clang tidy'

--- src/runtime/alloc.h
+++ src/runtime/alloc.h
@@ -20,7 +20,8 @@
 #ifdef LISP_FEATURE_GENCGC
 extern lispobj *lisp_alloc(struct alloc_region *region, sword_t nbytes,
                            int page_type_flag, struct thread *thread);
-extern lispobj alloc_code_object(unsigned boxed, unsigned unboxed,
+extern lispobj alloc_code_object(unsigned boxed_nwords,
+                                 unsigned unboxed_nbytes,
                                  unsigned serialno);
 #endif
 
