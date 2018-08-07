ec19d43987f961841acfaf0cb58db812b3129a3a
Douglas Katzman <dougk@google.com>
2018-08-06 22:07:42 -0400

    Improve %SIMD-PACK-HIGH

--- src/compiler/x86-64/simd-pack.lisp
+++ src/compiler/x86-64/simd-pack.lisp
@@ -156,17 +156,13 @@
 
 (define-vop (%simd-pack-high)
   (:translate %simd-pack-high)
-  (:args (x :scs (int-sse-reg double-sse-reg single-sse-reg)
-            :target tmp))
+  (:args (x :scs (int-sse-reg double-sse-reg single-sse-reg)))
   (:arg-types simd-pack)
-  (:temporary (:sc sse-reg :from (:argument 0)) tmp)
   (:results (dst :scs (unsigned-reg)))
   (:result-types unsigned-num)
   (:policy :fast-safe)
   (:generator 3
-    (move tmp x)
-    (inst psrldq tmp 8)
-    (inst movd dst tmp)))
+    (inst pextrq dst x 1)))
 
 (define-vop (%make-simd-pack)
   (:translate %make-simd-pack)
