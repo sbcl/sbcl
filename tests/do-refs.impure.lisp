0bc1cb04e16bcaff01dd66f69e4d096c20505b51
Stas Boukarev <stassats@gmail.com>
2018-10-04 00:59:39 +0300

    Mark a test as no longer failing.

--- tests/do-refs.impure.lisp
+++ tests/do-refs.impure.lisp
@@ -116,8 +116,7 @@
     (walk-slots-test f `(,(find-layout 'sb-pcl::ctor) ,#'error ,@slot-vals))))
 
 #+sb-fasteval
-(test-util:with-test (:name :walk-slots-interpreted-fun
-                      :fails-on :interpreter)
+(test-util:with-test (:name :walk-slots-interpreted-fun)
   (let ((f (let ((sb-ext:*evaluator-mode* :interpret))
              (eval '(lambda (x y z))))))
     (funcall f 1 2 3) ; compute the digested slots
