e7485a9ba7d937f56efe9664789b6a97cbcb728d
Stas Boukarev <stassats@gmail.com>
2018-12-23 02:22:21 +0300

    ir1-to-dot: add NLX edges.

--- src/compiler/debug.lisp
+++ src/compiler/debug.lisp
@@ -1237,21 +1237,25 @@
                                       :direction :output)
     (write-line "digraph G {" stream)
     (do-blocks (block component)
+      (when (typep (block-out block) '(cons (eql :graph) cons))
+        (format stream "~a[style=filled color=~a];~%"
+                (block-number block)
+                (case (cadr (block-out block))
+                  (:marked "green")
+                  (:remarked "aquamarine")
+                  (:start "lightblue")
+                  (:end "red"))))
       (let ((succ (block-pred block)))
         (when succ
-          (when (typep (block-out block) '(cons (eql :graph) cons))
-            (format stream "~a[style=filled color=~a];~%"
-                    (block-number block)
-                    (case (cadr (block-out block))
-                      (:marked "green")
-                      (:remarked "aquamarine")
-                      (:start "lightblue")
-                      (:end "red"))))
           (loop for succ in succ
                 for attr = "[style=bold]" then ""
                 do
                 (format stream "~a -> ~a~a;~%"
                         (block-number block)
                         (block-number succ)
-                        attr)))))
+                        attr)))
+        (when (nle-block-p block)
+          (format stream "~a -> ~a [style=dotted];~%"
+                  (block-number block)
+                  (block-number (nle-block-entry-block block))))))
     (write-line "}" stream)))
