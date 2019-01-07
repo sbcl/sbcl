sbcl/sbcl

commit 1e72a4ce7be30b78223249d7f1a6bf22f5e02eaf

Futz with the first two words in code objects
With this change, the code header word contains the complete object size.
The boxed portion size (measured in bytes) is in the following word.

As evident from the number of deleted lines, it is easier to do pointer
arithmetic this way because all of the COMPUTE-FUN vops want bytes, not words.
For cheneygc, all of the backend-specific allocate-code-object vops are gone,
and the allocator essentially reduces to VAR-ALLOC.
For gencgc the change is similar but we still go through C code for reasons.

In either case, object sizing is trivial, involving no adding or rounding.

tools-for-build/editcore.lisp
@@ -28,7 +28,7 @@
                 #:alien-value-sap #:alien-value-type)
   (:import-from "SB-C" #:+backend-page-bytes+)
   (:import-from "SB-VM" #:map-objects-in-range #:reconstitute-object
-                #:%closure-callee #:code-component-size)
+                #:%closure-callee #:code-object-size)
   (:import-from "SB-DISASSEM" #:get-inst-space #:find-inst
                 #:make-dstate #:%make-segment
                 #:seg-virtual-location #:seg-length #:seg-sap-maker
@@ -333,7 +333,7 @@
                           (+ header-bytes (aref fun-map (1- i)))))
             (end-pc (if (< (1+ i) (length fun-map))
                         (+ header-bytes (aref fun-map (1+ i)))
-                        (sb-vm::code-component-size code))))
+                        (code-object-size code))))
         (unless (= end-pc start-pc)
           ;; Collapse adjacent address ranges named the same.
           ;; Use EQUALP instead of EQUAL to compare names
@@ -800,7 +800,7 @@
     ;; the unboxed bytes have an odd size in words making the total even.
     (format output " .byte ~{0x~x~^,~}~%"
             (loop for i from max-end
-                  below (- (code-component-size code)
+                  below (- (code-object-size code)
                            (* (code-header-words code) n-word-bytes))
                   collect (sap-ref-8 text-sap i)))))
 
@@ -918,7 +918,7 @@
                                      start-offs)
                                   nbytes output nil core)))))
       (format output " .quad 0, 0~%") ; trailer with SIMPLE-FUN count of 0
-      (let ((size (code-component-size code-component))) ; No need to pin
+      (let ((size (code-object-size code-component))) ; No need to pin
         (incf code-addr size)
         (setf total-code-size size)))
     (format output "~%# end of lisp asm routines~2%")
@@ -930,15 +930,16 @@
       (ecase (%widetag-of (sap-ref-word (int-sap (translate-ptr code-addr spaces)) 0))
         (#.code-header-widetag
          (let* ((code (make-code-obj code-addr))
-                (objsize (code-component-size code)))
+                (objsize (code-object-size code)))
            (incf total-code-size objsize)
            (cond
              ((< (code-header-words code) 4) ; filler object
-              (let ((nbytes (code-component-size code)))
+              (let ((sap (int-sap (- (get-lisp-obj-address code) other-pointer-lowtag))))
                 (format output " .quad 0x~x, 0x~x~% .fill 0x~x~%# ~x:~%"
-                        code-header-widetag (ash nbytes n-fixnum-tag-bits)
-                        (- nbytes (* 2 n-word-bytes))
-                        (+ code-addr nbytes))))
+                        (sap-ref-word sap 0)
+                        (sap-ref-word sap n-word-bytes)
+                        (- objsize (* 2 n-word-bytes))
+                        (+ code-addr objsize))))
              ((%instancep (%code-debug-info code)) ; assume it's a COMPILED-DEBUG-INFO
               (aver (plusp (code-n-entries code)))
               (let* ((source

package-data-list.lisp-expr
@@ -1729,6 +1729,7 @@ is a good idea, but see SB-SYS re. blurring of boundaries."
                "CODE-COMPONENT" "CODE-COMPONENT-P"
                "CODE-HEADER-REF" "CODE-HEADER-SET" "CODE-HEADER-WORDS"
                "CODE-INSTRUCTIONS" "CODE-N-UNBOXED-DATA-BYTES"
+               "CODE-OBJECT-SIZE"
                "COERCE-SYMBOL-TO-FUN"
                "COERCE-TO-FUN" "COERCE-TO-LEXENV" "COERCE-TO-LIST"
                "COERCE-TO-VALUES" "COERCE-TO-VECTOR"
@@ -3096,9 +3097,10 @@ structure representations"
                "CLOSURE-FUN-SLOT"
                "CLOSURE-WIDETAG"
                "CLOSURE-INFO-OFFSET"
-               "CODE-COMPONENT-SIZE"
-               "CODE-CODE-SIZE-SLOT" "CODE-CONSTANTS-OFFSET"
+               "CODE-BOXED-SIZE-SLOT"
+               "CODE-CONSTANTS-OFFSET"
                "CODE-DEBUG-INFO-SLOT"
+               "CODE-HEADER-SIZE-SHIFT"
                "CODE-HEADER-WIDETAG" "COMPLEX-ARRAY-WIDETAG"
                "COMPLEX-BIT-VECTOR-WIDETAG" "COMPLEX-DOUBLE-FLOAT-FILLER-SLOT"
                "COMPLEX-DOUBLE-FLOAT-IMAG-SLOT" "COMPLEX-DOUBLE-FLOAT-REAL-SLOT"

src/code/alloc.lisp
@@ -56,7 +56,7 @@
     (sb-thread:make-mutex :name "Immobile space"))
 
 (eval-when (:compile-toplevel)
-  (assert (eql code-code-size-slot 1))
+  (assert (eql code-boxed-size-slot 1))
   (assert (eql code-debug-info-slot 2)))
 
 (define-alien-variable "varyobj_holes" long)
@@ -104,16 +104,12 @@
 
 (declaim (inline hole-size))
 (defun hole-size (hole-address) ; in bytes
-  (the (and fixnum unsigned-byte)
-    (sap-ref-lispobj (int-sap hole-address) (ash code-code-size-slot word-shift))))
+  (ash (sap-ref-32 (int-sap hole-address) 4) word-shift))
 
 (declaim (inline (setf hole-size)))
 (defun (setf hole-size) (new-size hole) ; NEW-SIZE is in bytes
-  ;; FIXME: (SETF SET-REF-LISPOBJ) does not accept a fixnum
-  ;; because 'any-reg' is not an allowed storage class.
-  (setf (sap-ref-word (int-sap hole) (ash code-code-size-slot word-shift))
-        ;; 0 boxed words, so %CODE-CODE-SIZE covers everything.
-        (ash new-size n-fixnum-tag-bits)))
+  (setf (sap-ref-32 (int-sap hole) 4) (ash new-size (- word-shift)))
+  new-size)
 
 (declaim (inline hole-end-address))
 (defun hole-end-address (hole-address)
@@ -423,8 +419,9 @@
 
 (defun alloc-immobile-trampoline ()
   (values (%primitive alloc-immobile-fixedobj other-pointer-lowtag 6
-                      #.(make-code-header-word 4) ; boxed word count
-                      (ash (* 2 n-word-bytes) n-fixnum-tag-bits))))
+                      ;; total word count
+                      (logior (ash 6 code-header-size-shift) code-header-widetag)
+                      (* 4 n-word-bytes)))) ; boxed size in bytes
 
 ;;; Test whether there is room to allocate NBYTES.
 ;;; This only looks at the space in the frontier, not the free space list.
@@ -463,18 +460,20 @@
   ;; This uses ATOMIC-INCF to get automatic wraparound, not because atomicity
   ;; makes things deterministic per se. If multiple threads are allocating
   ;; code objects, the order is unpredictable.
-  (let ((serialno (atomic-incf sb-fasl::*code-serialno*)))
-    ;; FIXME: remove this use of WITHOUT-GCing and use pseudo-atomic.
+  (let ((total-words (align-up (+ boxed (ceiling unboxed n-word-bytes)) 2))
+        (serialno (atomic-incf sb-fasl::*code-serialno*)))
+    ;; FIXME: it seems possible now to use pseudo-atomic instead of
+    ;; without-gcing, except in the immobile code case.
     (without-gcing
       (block nil
         #!+immobile-code
         (when (member space '(:immobile :auto))
           (let ((code (allocate-immobile-bytes
-                          (align-up (+ (* boxed n-word-bytes) unboxed)
-                                    (* 2 n-word-bytes))
-                          (make-code-header-word boxed)
+                          (ash total-words word-shift)
+                          (logior (ash total-words code-header-size-shift)
+                                  code-header-widetag)
                           (logior (logand (ash serialno 32) most-positive-word)
-                                  (ash unboxed n-fixnum-tag-bits))
+                                  (* boxed n-word-bytes))
                           other-pointer-lowtag
                           (eq space :immobile))))
             (unless (eql code 0)
@@ -485,20 +484,22 @@
          (alien-funcall (extern-alien "alloc_code_object"
                                       (function unsigned (unsigned 32)
                                                 (unsigned 32) (unsigned 32)))
-                        boxed unboxed serialno)))))
-  #!-gencgc
-  (%primitive allocate-code-object boxed unboxed))
-
-#!+64-bit
-(progn
-  (declaim (inline %code-code-size))
-  (defun %code-serialno (code) ; return high 4 bytes
-    (ash (%%code-code-size code) (- n-fixnum-tag-bits 32)))
-  (defun %code-code-size (code) ; clip to max imposed by allocate-code-object
-    (ldb (byte 22 0) (%%code-code-size code))))
-
-;; Placeholder function so that the PRINT-OBJECT method doesn't
-;; have to worry about whether this accessor exists.
-#!-64-bit
-(defun %code-serialno (code)
-  (declare (ignore code)))
+                        total-words boxed serialno)))))
+  #!+cheneygc
+  ;; Something like this could work for gencgc, except there we have a different
+  ;; problem - that of segregating code from data.
+  (let* ((total-words
+          (the index (align-up (+ boxed (ceiling unboxed n-word-bytes)) 2)))
+         (code
+          (%primitive var-alloc total-words 'alloc-code
+                      ;; subtract 1 because var-alloc always adds 1 word
+                      ;; for the header, which is not right for code objects.
+                      -1 code-header-widetag other-pointer-lowtag)))
+    ;; The 1st slot beyond the header stores the boxed header size in bytes
+    ;; as an untagged number, which on 32-bit architectures has the same
+    ;; representation as a tagged value denoting a count in words.
+    ;; 64-bit architectures do not use this code path.
+    #!+64-bit (bug "No can do")
+    (setf (code-header-ref code code-boxed-size-slot) boxed
+          (code-header-ref code code-debug-info-slot) nil)
+    code))

src/code/immobile-space.lisp
@@ -136,7 +136,7 @@
           ;; Compute code bounds
           (let* ((code-begin (- (get-lisp-obj-address code)
                                 sb-vm:other-pointer-lowtag))
-                 (code-end (+ code-begin (sb-vm::code-component-size code))))
+                 (code-end (+ code-begin (sb-vm::code-object-size code))))
             ;; Loop over function's assembly code
             (dx-flet ((process-inst (chunk inst)
                (when (or (eq inst jmp) (eq inst call))

src/code/room.lisp
@@ -194,10 +194,6 @@
             (round-to-dualword (+ (* vector-data-offset n-word-bytes)
                                   n-data-octets)))))
 
-(defun code-component-size (x) ; in bytes
-  (declare (code-component x))
-  (round-to-dualword (+ (* (code-header-words x) n-word-bytes) (%code-code-size x))))
-
 (defun primitive-object-size (object)
   "Return number of bytes of heap or stack directly consumed by OBJECT"
   (if (is-lisp-pointer (get-lisp-obj-address object))
@@ -230,7 +226,7 @@
                                (nth-value 2 (reconstitute-vector
                                              object room-info))))))
                      (code-component
-                      (return-from primitive-object-size (code-component-size object)))
+                      (return-from primitive-object-size (code-object-size object)))
                      (t
                       ;; Other things (symbol, fdefn, value-cell, etc)
                       ;; don't have a sizer, so use GET-HEADER-DATA
@@ -290,7 +286,7 @@
            (let ((c (tagged-object other-pointer-lowtag)))
              (values c
                      code-header-widetag
-                     (code-component-size c))))
+                     (code-object-size c))))
 
           (:other
            (values (tagged-object other-pointer-lowtag)

src/code/simple-fun.lisp
@@ -350,11 +350,6 @@
 
 ;;;; CODE-COMPONENT
 
-(declaim (inline code-header-words))
-(defun code-header-words (code)
-  #!+64-bit (ash (get-header-data code) -24)
-  #!-64-bit (ldb (byte 22 0) (get-header-data code)))
-
 (defun %code-entry-points (code-obj) ; DO NOT USE IN NEW CODE
   (%code-entry-point code-obj 0))
 
@@ -372,7 +367,8 @@
 (declaim (inline code-fun-table-trailer-word))
 (defun code-fun-table-trailer-word (code-obj)
   (with-pinned-objects (code-obj)
-    (sap-ref-16 (code-instructions code-obj) (- (%code-code-size code-obj) 2))))
+    (sap-ref-16 (int-sap (get-lisp-obj-address code-obj))
+                (- (code-object-size code-obj) 2 sb-vm:other-pointer-lowtag))))
 
 ;;; Return the number of simple-funs in CODE-OBJ
 ;;; Keep in sync with C function code_n_funs()
@@ -390,8 +386,9 @@
   (declare ((unsigned-byte 16) fun-index))
   ;; subtracting the size of trailing uint16_t puts us one byte beyond
   ;; the last fun-index, so subtract an extra uint32_t as well.
-  (sap-ref-32 (code-instructions code-obj)
-              (- (%code-code-size code-obj) (* 4 fun-index) 8)))
+  (sap-ref-32 (int-sap (get-lisp-obj-address code-obj))
+              (- (code-object-size code-obj) (* 4 fun-index) 8
+                 sb-vm:other-pointer-lowtag)))
 
 ;;; Subtract from %CODE-CODE-SIZE the number of trailing data bytes which aren't
 ;;; machine instructions. It's generally ok to use either accessor if searching

src/code/stubs.lisp
@@ -86,8 +86,6 @@
   (def value-cell-ref)
   (def %caller-frame ())
   (def %caller-pc ())
-  ;; %code-code-size is an inline fun on 64-bit
-  #-64-bit (def %code-code-size)
   (def %code-debug-info)
   #+(or x86 immobile-space) (def sb-vm::%code-fixups)
 

src/compiler/alpha/alloc.lisp
@@ -76,31 +76,6 @@
 
 ;;;; special purpose inline allocators
 
-(define-vop (allocate-code-object)
-  (:args (boxed-arg :scs (any-reg) :to :save)
-         (unboxed-arg :scs (any-reg)))
-  (:results (result :scs (descriptor-reg)))
-  (:temporary (:scs (non-descriptor-reg)) ndescr)
-  (:temporary (:scs (any-reg) :from (:argument 0)) boxed)
-  (:temporary (:scs (non-descriptor-reg)) unboxed)
-  (:generator 100
-    (inst li (lognot lowtag-mask) ndescr)
-    (inst lda boxed 0 boxed-arg)
-    (inst srl unboxed-arg word-shift unboxed)
-    (inst lda unboxed lowtag-mask unboxed)
-    (inst and unboxed ndescr unboxed)
-    (inst sll boxed (- n-widetag-bits word-shift) ndescr)
-    (inst bis ndescr code-header-widetag ndescr)
-
-    (pseudo-atomic ()
-      (inst bis alloc-tn other-pointer-lowtag result)
-      (storew ndescr result 0 other-pointer-lowtag)
-      (storew unboxed-arg result code-code-size-slot other-pointer-lowtag)
-      (inst addq alloc-tn boxed alloc-tn)
-      (inst addq alloc-tn unboxed alloc-tn))
-
-    (storew null-tn result code-debug-info-slot other-pointer-lowtag)))
-
 (define-vop (make-fdefn)
   (:policy :fast-safe)
   (:translate make-fdefn)
@@ -193,9 +168,14 @@
   (:generator 6
     (inst lda bytes (* (1+ words) n-word-bytes) extra)
     (inst sll bytes (- n-widetag-bits 2) header)
-    (inst lda header (+ (ash -2 n-widetag-bits) type) header)
-    (inst srl bytes n-lowtag-bits bytes)
-    (inst sll bytes n-lowtag-bits bytes)
+    ;; The specified EXTRA value is the exact value placed in the header
+    ;; as the word count when allocating code.
+    (cond ((= type code-header-widetag)
+           (inst lda header type header))
+          (t
+           (inst lda header (+ (ash -2 n-widetag-bits) type) header)
+           (inst srl bytes n-lowtag-bits bytes)
+           (inst sll bytes n-lowtag-bits bytes)))
     (pseudo-atomic ()
       (inst bis alloc-tn lowtag result)
       (storew header result 0 lowtag)

commit 9144c66dc91d2aefe90100e5f7c8d71939af1ee1

Fix editcore, broken by 1e72a4ce7be3 or thereabouts

src/code/float.lisp
@@ -174,7 +174,7 @@
 
 (defconstant-eqx float-decoding-error "Can't decode NaN or infinity: ~S."
   #'string=)
-    
+
 #!-sb-fluid
 (declaim (maybe-inline integer-decode-single-float
                        integer-decode-double-float))

tools-for-build/editcore.lisp
@@ -907,20 +907,24 @@
                          (cadr (nth i name->addr)))))))
         (terpri output)
         ;; Loop over the embedded routines
-        (dolist (entry name->addr)
-          (destructuring-bind (name start-offs . end-offs) entry
-            (let ((nbytes (- (1+ end-offs) start-offs)))
+        (let ((list name->addr)
+              (obj-size (code-object-size code-component)))
+          (loop
+            (destructuring-bind (name start-offs . end-offs) (pop list)
+              (let ((nbytes (- (if (endp list)
+                                   (- obj-size (* header-len n-word-bytes))
+                                   (1+ end-offs))
+                               start-offs)))
               (format output " lasmsym ~(\"~a\"~), ~d~%" name nbytes)
               (emit-lisp-function (+ (sap-int (code-instructions code-component))
                                      start-offs)
                                   (+ code-addr
                                      (ash (code-header-words code-component) word-shift)
                                      start-offs)
-                                  nbytes output nil core)))))
-      (format output " .quad 0, 0~%") ; trailer with SIMPLE-FUN count of 0
-      (let ((size (code-object-size code-component))) ; No need to pin
-        (incf code-addr size)
-        (setf total-code-size size)))
+                                  nbytes output nil core)))
+            (when (endp list) (return)))
+          (incf code-addr obj-size)
+          (setf total-code-size obj-size))))
     (format output "~%# end of lisp asm routines~2%")
 
     (loop
@@ -933,7 +937,7 @@
                 (objsize (code-object-size code)))
            (incf total-code-size objsize)
            (cond
-             ((< (code-header-words code) 4) ; filler object
+             ((< (code-header-words code) 3) ; filler object
               (let ((sap (int-sap (- (get-lisp-obj-address code) other-pointer-lowtag))))
                 (format output " .quad 0x~x, 0x~x~% .fill 0x~x~%# ~x:~%"
                         (sap-ref-word sap 0)