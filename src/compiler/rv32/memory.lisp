5cf64c2efe4bcfc4512c189015de0f1bd67a207b
Charles Zhang <charleszhang99@yahoo.com>
2019-01-06 23:25:54 -0500

    Implement memory.lisp

--- src/compiler/rv32/memory.lisp
+++ src/compiler/rv32/memory.lisp
@@ -12,13 +12,18 @@
 
 (in-package "SB-VM")
 
+;;; Cell-Ref and Cell-Set are used to define VOPs like CAR, where the offset to
+;;; be read or written is a property of the VOP used.
+
 (define-vop (cell-ref)
   (:args (object :scs (descriptor-reg)))
   (:results (value :scs (descriptor-reg any-reg)))
   (:variant-vars offset lowtag)
-  (:generator 4))
+  (:generator 4
+    (loadw value object offset lowtag)))
 
 (define-vop (cell-set)
   (:args (object :scs (descriptor-reg)) (value :scs (descriptor-reg any-reg)))
   (:variant-vars offset lowtag)
-  (:generator 4))
+  (:generator 4
+    (storew value object offset lowtag)))
