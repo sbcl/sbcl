d48ae0584c7deea91539ed51ca34df52c880f9a1
Charles Zhang <charleszhang99@yahoo.com>
2019-01-19 13:14:21 -0800

    Define subprim.lisp

--- src/compiler/rv32/subprim.lisp
+++ src/compiler/rv32/subprim.lisp
@@ -11,3 +11,41 @@
 ;;;; files for more information.
 
 (in-package "SB-VM")
+
+;;;; Length
+
+(define-vop (length/list)
+  (:translate length)
+  (:args (object :scs (descriptor-reg) :target ptr))
+  (:arg-types list)
+  (:temporary (:scs (descriptor-reg) :from (:argument 0)) ptr)
+  (:temporary (:scs (non-descriptor-reg)) temp)
+  (:temporary (:scs (any-reg) :to (:result 0) :target result)
+              count)
+  (:results (result :scs (any-reg descriptor-reg)))
+  (:policy :fast-safe)
+  (:vop-var vop)
+  (:save-p :compute-only)
+  (:generator 50
+    (let ((done (gen-label))
+          (loop (gen-label))
+          (not-list (gen-label)))
+      (move ptr object)
+      (move count zero-tn)
+
+      (emit-label loop)
+
+      (inst beq ptr null-tn done)
+
+      (test-type ptr temp not-list t (list-pointer-lowtag))
+
+      (loadw ptr ptr cons-cdr-slot list-pointer-lowtag)
+      (inst addi count count (fixnumize 1))
+      (test-type ptr temp loop nil (list-pointer-lowtag))
+
+      (emit-label not-list)
+      
+      (cerror-call vop 'object-not-list-error ptr)
+
+      (emit-label done)
+      (move result count))))
