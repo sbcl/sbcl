911197a5db51237559837860645fe0cdfa56a4e8
Charles Zhang <charleszhang99@yahoo.com>
2019-01-19 13:15:04 -0800

    Fill in array stubs

--- src/compiler/rv32/array.lisp
+++ src/compiler/rv32/array.lisp
@@ -10,7 +10,58 @@
 ;;;; files for more information.
 
 (in-package "SB-VM")
+
+;;;; Allocator for the array header.
+(define-vop (make-array-header)
+  (:policy :fast-safe)
+  (:translate make-array-header)
+  (:args (type :scs (any-reg))
+         (rank :scs (any-reg)))
+  (:arg-types tagged-num tagged-num)
+  (:temporary (:scs (non-descriptor-reg)) bytes header)
+  (:temporary (:sc non-descriptor-reg :offset pa-flag-offset) pa-flag)
+  (:results (result :scs (descriptor-reg)))
+  (:generator 13
+    (style-warn "unimplemented make-array-header")))
 
+
+;;;; Additional accessors and setters for the array header.
+(define-full-reffer %array-dimension *
+  array-dimensions-offset other-pointer-lowtag
+  (any-reg) positive-fixnum sb-kernel:%array-dimension)
+
+(define-full-setter %set-array-dimension *
+  array-dimensions-offset other-pointer-lowtag
+  (any-reg) positive-fixnum sb-kernel:%set-array-dimension)
+
+(define-vop (array-rank-vop)
+  (:translate sb-kernel:%array-rank)
+  (:policy :fast-safe)
+  (:args (x :scs (descriptor-reg)))
+  (:temporary (:scs (non-descriptor-reg)) temp)
+  (:results (res :scs (any-reg descriptor-reg)))
+  (:generator 6
+    (style-warn "unimmplemented array-rank-vop")))
+
+;;;; Bounds checking routine.
+(define-vop (check-bound)
+  (:translate %check-bound)
+  (:policy :fast-safe)
+  (:args (array :scs (descriptor-reg))
+         (bound :scs (any-reg descriptor-reg))
+         (index :scs (any-reg descriptor-reg)))
+  (:temporary (:scs (non-descriptor-reg)) temp)
+  (:vop-var vop)
+  (:save-p :compute-only)
+  (:generator 5
+    (let ((error (generate-error-code vop 'invalid-array-index-error array bound index)))
+      (%test-fixnum index temp error t)
+      (inst bgeu index bound error))))
+
+;;;; Accessors/Setters
+
+;;; Variants built on top of word-index-ref, etc.  I.e. those vectors whos
+;;; elements are represented in integer registers.
 (macrolet
     ((def-full-data-vector-frobs (type element-type &rest scs)
        (let ((refname (symbolicate "DATA-VECTOR-REF/" type))
@@ -45,11 +96,25 @@
   (def-full-data-vector-frobs simple-array-signed-byte-32 signed-num signed-reg)
   (def-full-data-vector-frobs simple-array-unsigned-byte-32 unsigned-num unsigned-reg))
 
+;;; Integer vectors whose elements are smaller than a byte.  I.e. bit, 2-bit,
+;;; and 4-bit vectors.
 (macrolet
     ((def-small-data-vector-frobs (type bits)
-       (let ((setname (symbolicate "DATA-VECTOR-SET/" type)))
+       (let* ((refname (symbolicate "DATA-VECTOR-REF/" type))
+              (setname (symbolicate "DATA-VECTOR-SET/" type)))
          `(progn
+            (define-vop (,refname)
+              (:note "inline array access")
+              (:translate data-vector-ref)
+              (:policy :fast-safe)
+              (:args (object :scs (descriptor-reg))
+                     (index :scs (unsigned-reg)))
+              (:arg-types ,type positive-fixnum)
+              (:results (value :scs (any-reg)))
+              (:result-types positive-fixnum)
+              (:generator 20))
             (define-vop (,setname)
+              (:note "inline array store")
               (:translate data-vector-set)
               (:policy :fast-safe)
               (:args (object :scs (descriptor-reg))
@@ -63,10 +128,23 @@
   (def-small-data-vector-frobs simple-array-unsigned-byte-2 2)
   (def-small-data-vector-frobs simple-array-unsigned-byte-4 4))
 
+;;; And the float variants.
 (macrolet ((def-float-vector-frobs (type eltype sc)
-             (let ((setname (symbolicate "DATA-VECTOR-SET/" type)))
+             (let ((refname (symbolicate "DATA-VECTOR-REF/" type))
+                   (setname (symbolicate "DATA-VECTOR-SET/" type)))
                `(progn
+                  (define-vop (,refname)
+                    (:note "inline array access")
+                    (:translate data-vector-ref)
+                    (:policy :fast-safe)
+                    (:args (object :scs (descriptor-reg))
+                           (index :scs (any-reg)))
+                    (:arg-types ,type positive-fixnum)
+                    (:results (value :scs (,sc)))
+                    (:result-types ,eltype)
+                    (:generator 20))
                   (define-vop (,setname)
+                    (:note "inline array store")
                     (:translate data-vector-set)
                     (:policy :fast-safe)
                     (:args (object :scs (descriptor-reg))
@@ -80,3 +158,11 @@
   (def-float-vector-frobs simple-array-double-float double-float double-reg)
   (def-float-vector-frobs simple-array-complex-single-float complex-single-float complex-single-reg)
   (def-float-vector-frobs simple-array-complex-double-float complex-double-float complex-double-reg))
+
+
+;;; These vops are useful for accessing the bits of a vector irrespective of
+;;; what type of vector it is.
+(define-full-reffer vector-raw-bits * vector-data-offset other-pointer-lowtag
+  (unsigned-reg) unsigned-num %vector-raw-bits)
+(define-full-setter set-vector-raw-bits * vector-data-offset other-pointer-lowtag
+  (unsigned-reg) unsigned-num %set-vector-raw-bits)
