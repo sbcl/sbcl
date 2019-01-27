273da14696e8124dd454471df6b7af2a61264344
Douglas Katzman <dougk@google.com>
2019-01-27 12:37:37 -0500

    Remove needless reader conditional

--- src/code/early-thread.lisp
+++ src/code/early-thread.lisp
@@ -16,10 +16,8 @@ in future versions."
   (name          nil :type (or null simple-string)) ; some C code reads this
   (%alive-p      nil :type boolean)
   (%ephemeral-p  nil :type boolean)
-  #-sb-xc-host
   ;; 0 is used on thread-less builds
   (os-thread  (ldb (byte sb-vm:n-word-bits 0) -1) :type sb-vm:word)
-  #-sb-xc-host
   ;; Points to the SB-VM::THREAD primitive object.
   ;; Yes, there are three different thread structures.
   (primitive-thread 0 :type sb-vm:word)
