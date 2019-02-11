772504a55c5045c70ef47134c520bc0ae87a2082
Stas Boukarev <stassats@gmail.com>
2019-02-11 13:04:21 +0300

    Fix the coerce transform on FLOAT.
    It shouldn't coerce a float into a single-float.
    
    Reported by J. Gareth Williams.

--- tests/coerce.pure.lisp
+++ tests/coerce.pure.lisp
@@ -128,3 +128,12 @@
     (declare (ignore failure-p))
     (assert (= 1 (length warnings)))
     (assert-error (funcall fun))))
+
+(with-test (:name :coerce-float-to-float)
+  (checked-compile-and-assert
+      ()
+      '(lambda (f)
+        (coerce f 'float))
+    ((10) 10.0)
+    ((1/2) 0.5)
+    ((30d0) 30d0)))
