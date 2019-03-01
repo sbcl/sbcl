314aa0ed014d38642800f3484dbbe407d099db8b
Douglas Katzman <dougk@google.com>
2019-03-01 14:24:44 -0500

    Define thread-header-slots only if needed

--- src/compiler/generic/objdef.lisp
+++ src/compiler/generic/objdef.lisp
@@ -431,10 +431,11 @@ during backtrace.
 ;;; If we can't do that for some reason - like, say, the safepoint page
 ;;; is located prior to 'struct thread', then these just become ordinary slots.
 (defglobal *thread-header-slot-names*
-    '(function-layout
-      varyobj-space-addr
-      varyobj-card-count
-      varyobj-card-marks))
+  (append #+immobile-space
+          '(function-layout
+            varyobj-space-addr
+            varyobj-card-count
+            varyobj-card-marks)))
 
 #+(and immobile-space (not sb-safepoint))
 (macrolet ((define-thread-header-slots ()
