6d03ccb5e248c9dac5dc5b85a051cd15eb01fa7a
Charles Zhang <charleszhang99@yahoo.com>
2019-06-04 23:59:26 -0700

    sb-rotate-byte: Fix vops for 32-bit risc-v.

--- contrib/sb-rotate-byte/riscv-vm.lisp
+++ contrib/sb-rotate-byte/riscv-vm.lisp
@@ -12,15 +12,17 @@
   (:results (result :scs (sb-vm::unsigned-reg)))
   (:result-types sb-vm::unsigned-num)
   (:generator 5
-    (aver (not (= count 0)))
+    (aver (/= count 0))
     (let ((count (if (plusp count)
                      count
                      (+ 32 count))))
-      (inst slliw temp integer count)
-      (inst srliw result integer (- 32 count))
+      (inst slli temp integer count)
+      (inst #-64-bit srli #+64-bit srliw result integer (- 32 count))
       (inst or result result temp)
-      (inst slli result result 32)
-      (inst srli result result 32))))
+      #+64-bit
+      (progn
+        (inst slli result result 32)
+        (inst srli result result 32)))))
 
 (define-vop (%32bit-rotate-byte)
   (:policy :fast-safe)
@@ -37,13 +39,15 @@
     (inst slti count-temp count 0)
     (inst slli count-temp count-temp 5)
     (inst add count-temp count-temp count)
-    (inst sllw temp integer count-temp)
+    (inst sll temp integer count-temp)
     (inst subi count-temp count-temp 32)
     (inst sub count-temp sb-vm::zero-tn count-temp)
-    (inst srlw result integer count-temp)
+    (inst #-64-bit srl #+64-bit srlw result integer count-temp)
     (inst or result result temp)
-    (inst slli result result 32)
-    (inst srli result result 32)))
+    #+64-bit
+    (progn
+      (inst slli result result 32)
+      (inst srli result result 32))))
 
 ;;; 64-bit
 (define-vop (%64bit-rotate-byte/c)
