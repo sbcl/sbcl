a9881aa63440ad095d6fe283c1067b6882f1cc3c
Stas Boukarev <stassats@gmail.com>
2019-06-12 22:34:04 +0300

    Add sb-kernel:run-pending-finalizers to finalize.test.sh
    It's needed despite what the comment says.

--- tests/finalize.test.sh
+++ tests/finalize.test.sh
@@ -38,6 +38,8 @@ echo //entering finalize.test.sh
 ;; but that isn't part of the contract with stopping. If this test fails,
 ;; try inserting a call to RUN-PENDING-FINALIZERS after this line]
 #+sb-thread (sb-impl::finalizer-thread-stop)
+;; FIXME: turns out, it does fail:
+(sb-kernel:run-pending-finalizers)
 
 (if (= *count* 10000)
     (with-open-file (f "finalize-test-passed" :direction :output)
