f684a86bfc2595247720b97927f6942387421f04
Stas Boukarev <stassats@gmail.com>
2019-08-27 00:35:49 +0300

    ppc64: Fix layout-depthoid for #+little-endian

--- src/compiler/ppc64/system.lisp
+++ src/compiler/ppc64/system.lisp
@@ -56,9 +56,11 @@
   (:result-types fixnum)
   (:temporary (:scs (non-descriptor-reg)) temp)
   (:generator 1
-    (inst lr temp (- (ash (+ instance-slots-offset
-                             (get-dsd-index layout sb-kernel::%bits))
-                          word-shift) instance-pointer-lowtag))
+    (inst lr temp (- (+ #+little-endian 4
+                        (ash (+ instance-slots-offset
+                                (get-dsd-index layout sb-kernel::%bits))
+                             word-shift))
+                     instance-pointer-lowtag))
     (inst lwax res object temp)))
 
 (define-vop (%other-pointer-widetag)
