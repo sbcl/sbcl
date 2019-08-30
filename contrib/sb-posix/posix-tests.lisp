bebec0fb89ceefde0f1d584da439989bc12ad17d
Douglas Katzman <dougk@google.com>
2019-08-30 14:25:28 -0400

    Mask out O_LARGEFILE in fcntl.1 test

--- contrib/sb-posix/posix-tests.lisp
+++ contrib/sb-posix/posix-tests.lisp
@@ -432,14 +432,14 @@
   #+win32
   #.sb-posix:eacces)
 
-#-(or (and (or x86-64 arm64 alpha) (or linux sunos)) win32)
+#-(or (and (or x86-64 arm64 ppc64 alpha) (or linux sunos)) win32)
 (deftest fcntl.1
   (let ((fd (sb-posix:open "/dev/null" sb-posix::o-nonblock)))
     (= (sb-posix:fcntl fd sb-posix::f-getfl) sb-posix::o-nonblock))
   t)
 ;; On AMD64/Linux O_LARGEFILE is always set, even though the whole
 ;; flag makes no sense.
-#+(and (or x86-64 arm64 alpha) (or linux sunos))
+#+(and (or x86-64 arm64 ppc64 alpha) (or linux sunos))
 (deftest fcntl.1
   (let ((fd (sb-posix:open "/dev/null" sb-posix::o-nonblock)))
     (/= 0 (logand (sb-posix:fcntl fd sb-posix::f-getfl)
