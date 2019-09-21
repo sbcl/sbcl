6b12fc4091dd19c7edaead6ab7c60f9e2a03d018
Christophe Rhodes <csr21@cantab.net>
2019-09-21 17:01:08 +0100

    rename a previously-renamed Unicode data file
    It's a bit painful to have a file which embeds the Unicode version in
    its name: each time it is update, the SBCL sources have to change.
    Name allkeys.txt generically (aiming in the end for an unmodified data
    file, though we're not there yet).

--- tools-for-build/ucd.lisp
+++ tools-for-build/ucd.lisp
@@ -739,7 +739,7 @@ Length should be adjusted when the standard changes.")
     (values code-points ret))))
 
 (defparameter *collation-table*
-  (with-input-txt-file (stream "Allkeys80")
+  (with-input-txt-file (stream "allkeys")
     (loop with hash = (make-hash-table :test #'equal)
        for line = (read-line stream nil nil) while line
        unless (eql 0 (position #\# line))
