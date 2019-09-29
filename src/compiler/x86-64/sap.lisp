3e0f81728103cb9802a5e415a188abe0e0b92feb
Douglas Katzman <dougk@google.com>
2019-09-29 16:16:22 -0400

    Macroize a few definitions

--- src/compiler/x86-64/sap.lisp
+++ src/compiler/x86-64/sap.lisp
@@ -245,106 +245,51 @@
   (def-system-ref-and-set sap-ref-lispobj %set-sap-ref-lispobj mov
     descriptor-reg * :qword))
 
-;;;; SAP-REF-DOUBLE
+;;;; SAP-REF-SINGLE and SAP-REF-DOUBLE
 
-(define-vop (sap-ref-double)
-  (:translate sap-ref-double)
-  (:policy :fast-safe)
-  (:args (sap :scs (sap-reg))
-         (offset :scs (signed-reg)))
-  (:arg-types system-area-pointer signed-num)
-  (:results (result :scs (double-reg)))
-  (:result-types double-float)
-  (:generator 5
-     (inst movsd result (ea sap offset))))
-
-(define-vop (sap-ref-double-c)
-  (:translate sap-ref-double)
-  (:policy :fast-safe)
-  (:args (sap :scs (sap-reg)))
-  (:arg-types system-area-pointer (:constant (signed-byte 32)))
-  (:info offset)
-  (:results (result :scs (double-reg)))
-  (:result-types double-float)
-  (:generator 4
-     (inst movsd result (ea offset sap))))
-
-(define-vop (%set-sap-ref-double)
-  (:translate %set-sap-ref-double)
-  (:policy :fast-safe)
-  (:args (sap :scs (sap-reg) :to (:eval 0))
-         (offset :scs (signed-reg) :to (:eval 0))
-         (value :scs (double-reg)))
-  (:arg-types system-area-pointer signed-num double-float)
-  (:results (result :scs (double-reg)))
-  (:result-types double-float)
-  (:generator 5
-    (inst movsd (ea sap offset) value)
-    (move result value)))
-
-(define-vop (%set-sap-ref-double-c)
-  (:translate %set-sap-ref-double)
-  (:policy :fast-safe)
-  (:args (sap :scs (sap-reg) :to (:eval 0))
-         (value :scs (double-reg)))
-  (:arg-types system-area-pointer (:constant (signed-byte 32)) double-float)
-  (:info offset)
-  (:results (result :scs (double-reg)))
-  (:result-types double-float)
-  (:generator 4
-    (inst movsd (ea offset sap) value)
-    (move result value)))
-
-;;;; SAP-REF-SINGLE
-
-(define-vop (sap-ref-single)
-  (:translate sap-ref-single)
-  (:policy :fast-safe)
-  (:args (sap :scs (sap-reg))
-         (offset :scs (signed-reg)))
-  (:arg-types system-area-pointer signed-num)
-  (:results (result :scs (single-reg)))
-  (:result-types single-float)
-  (:generator 5
-     (inst movss result (ea sap offset))))
-
-(define-vop (sap-ref-single-c)
-  (:translate sap-ref-single)
-  (:policy :fast-safe)
-  (:args (sap :scs (sap-reg)))
-  (:arg-types system-area-pointer (:constant (signed-byte 32)))
-  (:info offset)
-  (:results (result :scs (single-reg)))
-  (:result-types single-float)
-  (:generator 4
-     (inst movss result (ea offset sap))))
-
-(define-vop (%set-sap-ref-single)
-  (:translate %set-sap-ref-single)
-  (:policy :fast-safe)
-  (:args (sap :scs (sap-reg) :to (:eval 0))
-         (offset :scs (signed-reg) :to (:eval 0))
-         (value :scs (single-reg)))
-  (:arg-types system-area-pointer signed-num single-float)
-  (:results (result :scs (single-reg)))
-  (:result-types single-float)
-  (:generator 5
-    (inst movss (ea sap offset) value)
-    (move result value)))
-
-(define-vop (%set-sap-ref-single-c)
-  (:translate %set-sap-ref-single)
-  (:policy :fast-safe)
-  (:args (sap :scs (sap-reg) :to (:eval 0))
-         (value :scs (single-reg)))
-  (:arg-types system-area-pointer (:constant (signed-byte 32)) single-float)
-  (:info offset)
-  (:results (result :scs (single-reg)))
-  (:result-types single-float)
-  (:generator 4
-    (inst movss (ea offset sap) value)
-    (move result value)))
-
+(macrolet ((def-system-ref-and-set (ref-fun res-sc res-type insn
+                                            &aux (set-fun (symbolicate "%SET-" ref-fun)))
+             `(progn
+                (define-vop (,ref-fun)
+                  (:translate ,ref-fun)
+                  (:policy :fast-safe)
+                  (:args (sap :scs (sap-reg))
+                         (offset :scs (signed-reg)))
+                  (:arg-types system-area-pointer signed-num)
+                  (:results (result :scs (,res-sc)))
+                  (:result-types ,res-type)
+                  (:generator 5 (inst ,insn result (ea sap offset))))
+                (define-vop (,(symbolicate ref-fun "-C"))
+                  (:translate ,ref-fun)
+                  (:policy :fast-safe)
+                  (:args (sap :scs (sap-reg)))
+                  (:arg-types system-area-pointer (:constant (signed-byte 32)))
+                  (:info offset)
+                  (:results (result :scs (,res-sc)))
+                  (:result-types ,res-type)
+                  (:generator 4 (inst ,insn result (ea offset sap))))
+                (define-vop (,set-fun)
+                  (:translate ,set-fun)
+                  (:policy :fast-safe)
+                  (:args (sap :scs (sap-reg) :to (:eval 0))
+                         (offset :scs (signed-reg) :to (:eval 0))
+                         (value :scs (,res-sc)))
+                  (:arg-types system-area-pointer signed-num ,res-type)
+                  (:results (result :scs (,res-sc)))
+                  (:result-types ,res-type)
+                  (:generator 5 (inst ,insn (ea sap offset) value) (move result value)))
+                (define-vop (,(symbolicate set-fun "-C"))
+                  (:translate ,set-fun)
+                  (:policy :fast-safe)
+                  (:args (sap :scs (sap-reg) :to (:eval 0))
+                         (value :scs (,res-sc)))
+                  (:arg-types system-area-pointer (:constant (signed-byte 32)) ,res-type)
+                  (:info offset)
+                  (:results (result :scs (,res-sc)))
+                  (:result-types ,res-type)
+                  (:generator 4 (inst ,insn (ea offset sap) value) (move result value))))))
+  (def-system-ref-and-set sap-ref-single single-reg single-float movss)
+  (def-system-ref-and-set sap-ref-double double-reg double-float movsd))
 
 ;;; noise to convert normal lisp data objects into SAPs
 
