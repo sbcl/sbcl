sbcl/sbcl

commit de997bd37fc380edf69e386a1d79e35ba4c83f7f

Print lisp_backtrace with --disable-ldb.
Similar to what --disable-debugger is doing.

src/runtime/interr.c
@@ -36,10 +36,16 @@
 #include "gc.h"
 
 /* the way that we shut down the system on a fatal error */
+void lisp_backtrace(int frames);
 
 static void
 default_lossage_handler(void)
 {
+    static int backtrace_invoked = 0;
+    if (!backtrace_invoked) {
+        backtrace_invoked = 1;
+        lisp_backtrace(100);
+    }
     exit(1);
 }
 static void (*lossage_handler)(void) = default_lossage_handler;
@@ -48,7 +54,6 @@ static void (*lossage_handler)(void) = default_lossage_handler;
 static void
 configurable_lossage_handler()
 {
-    void lisp_backtrace(int frames);
 
     if (dyndebug_config.dyndebug_backtrace_when_lost) {
         fprintf(stderr, "lose: backtrace follows as requested\n");

commit 6786b7da5f645e5621eee9a6ab2d0b09ec402090

Restore general usefulness of lose()
Revision de997bd37f caused lose() to segfault whenever it was called prior
to bringing up the lisp runtime, because lisp_backtrace assumes that
arch_os_get_current_thread() returns a valid thread struct.
As such, the low-level monitor became nearly useless for figuring out problems
in a corefile or anything else that didn't pertain to a lisp thread.

src/runtime/interr.c
@@ -44,7 +44,10 @@ default_lossage_handler(void)
     static int backtrace_invoked = 0;
     if (!backtrace_invoked) {
         backtrace_invoked = 1;
-        lisp_backtrace(100);
+        // This may not be exactly the right condition for determining
+        // whether it might be possible to backtrace, but at least it prevents
+        // lose() from itself losing early in startup.
+        if (arch_os_get_current_thread()) lisp_backtrace(100);
     }
     exit(1);
 }

src/runtime/os-common.c
@@ -10,7 +10,6 @@
  */
 # define _GNU_SOURCE /* needed for RTLD_DEFAULT from dlfcn.h */
 #include <stdio.h>
-#include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 
@@ -237,12 +236,8 @@ void load_core_bytes(int fd, int offset, os_vm_address_t addr, os_vm_size_t len)
         fail = 1;
     }
 #endif
-    if (fail) {
-        // Don't lose() - it would attempt to perform a lisp backtrace and crash.
-        fprintf(stderr, "load_core_bytes(%d,%x,%lx,%x) failed\n",
-                fd, offset, (long)addr, (int)len);
-        exit(1);
-    }
+    if (fail)
+        lose("load_core_bytes(%d,%x,%lx,%x) failed", fd, offset, (long)addr, (int)len);
 }
 
 boolean