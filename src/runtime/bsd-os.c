8797c9efed331f85f5092ef1354c54711dc07ec2
Stas Boukarev <stassats@gmail.com>
2019-10-28 13:53:07 +0300

    Remove mentions of NetBSD 2.0 from README.
    And don't check for it when starting up, it's 15 years old.

--- src/runtime/bsd-os.c
+++ src/runtime/bsd-os.c
@@ -328,21 +328,6 @@ os_install_interrupt_handlers(void)
 static void netbsd_init()
 {
     struct rlimit rl;
-    int mib[2], osrev;
-    size_t len;
-
-    /* Are we running on a sufficiently functional kernel? */
-    mib[0] = CTL_KERN;
-    mib[1] = KERN_OSREV;
-
-    len = sizeof(osrev);
-    sysctl(mib, 2, &osrev, &len, NULL, 0);
-
-    /* If we're older than 2.0... */
-    if (osrev < 200000000) {
-        fprintf(stderr, "osrev = %d (needed at least 200000000).\n", osrev);
-        lose("NetBSD kernel too old to run sbcl.");
-    }
 
     /* NetBSD counts mmap()ed space against the process's data size limit,
      * so yank it up. This might be a nasty thing to do? */
