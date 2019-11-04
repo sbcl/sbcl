1085561766916e061e62c8d84f19fcc700d16df5
Stas Boukarev <stassats@gmail.com>
2019-11-04 13:11:13 +0300

    Fix a typo and add an IGNORABLE.

--- src/compiler/generic/objdef.lisp
+++ src/compiler/generic/objdef.lisp
@@ -588,9 +588,10 @@ during backtrace.
   ;; Possibly not the best place for this definition, but other accessors
   ;; for primitive objects are here too.
   (defun code-jump-table-words (code)
-    (declare (code-component code))
-    #-(or x6 x86-64) 0
-    #+(or x6 x86-64)
+    (declare (code-component code)
+             (ignorable code))
+    #-(or x86 x86-64) 0
+    #+(or x86 x86-64)
     (with-pinned-objects (code)
       (truly-the (unsigned-byte 16) (sap-ref-word (code-instructions code) 0))))
 
