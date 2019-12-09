9bcda82e5c848a07222d609e21c2b592989c47a3
Stas Boukarev <stassats@gmail.com>
2019-12-09 15:59:17 +0300

    Remove a check for old NetBSD.
    Four years has passed, enough time to get everyone to update.

--- tools-for-build/os-provides-dladdr-test.c
+++ tools-for-build/os-provides-dladdr-test.c
@@ -7,16 +7,8 @@
 #include <stdlib.h>
 #include <dlfcn.h>
 
-#ifdef __NetBSD__
-#include <sys/param.h>
-#endif
-
 int main ()
 {
-#if defined(__NetBSD_Version__) && __NetBSD_Version__ < 700000001
-  /* dladdr(3) is broken on some NetBSD versions before 7.0 */
-  return 1;
-#else
    void * handle = dlopen((void*)0, RTLD_GLOBAL | RTLD_NOW);
    void * addr = dlsym(handle, "printf");
    Dl_info * info = (Dl_info*) malloc(sizeof(Dl_info));
@@ -25,5 +17,4 @@ int main ()
    } else {
        return 104;
    }
-#endif
 }
