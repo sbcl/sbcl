beabb4430ff682594a9f9fefa1d67416b6308e9c
Charles Zhang <charleszhang99@yahoo.com>
2020-01-19 20:33:21 -0800

    Delete ansify.lisp.
    It's been 25 years. Looks like we don't need this anymore, so let's
    hold everyone to a higher standard now.

--- src/cold/ansify.lisp
+++
@@ -1,17 +0,0 @@
-;;;; patches to work around implementation idiosyncrasies in our
-;;;; cross-compilation host
-
-;;;; This software is part of the SBCL system. See the README file for
-;;;; more information.
-;;;;
-;;;; This software is derived from the CMU CL system, which was
-;;;; written at Carnegie Mellon University and released into the
-;;;; public domain. The software is in the public domain and is
-;;;; provided with absolutely no warranty. See the COPYING and CREDITS
-;;;; files for more information.
-
-;;;; CMU CL issues
-#+cmu
-(progn
-  (setq *compile-print* nil) ; too much noise, can't see the actual warnings
-)
