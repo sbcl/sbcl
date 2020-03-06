8499524f24f06592fae7dea4a64bef59340faa7e
Douglas Katzman <dougk@google.com>
2020-03-06 15:15:47 -0500

    Enable heap relocation test on arm and arm64

--- tests/relocation.test.sh
+++ tests/relocation.test.sh
@@ -2,12 +2,10 @@
 
 . ./subr.sh
 
-# TODO: get this to run for all platforms that have a relocatable heap
-# (I think that's everything that we care about these days?)
-
-# Without sb-dynamic-core we can't build a new C runtime as it messes up
-# all of the addresses of C symbols that were wired in.
-run_sbcl --eval '(exit :code (or #+(and (or x86 x86-64) linux sb-dynamic-core) 0 1))'
+# The relocation test binary can only be built on linux.
+# FIXME: This test _should_ work on any architecture, but it doesn't,
+#        so there must have been a regression in the heap relocator.
+run_sbcl --eval '(exit :code (or #+(and (or arm arm64 x86 x86-64) linux) 0 1))'
 if [ $? -eq 1 ]
 then
     # shell tests don't have a way of exiting as "not applicable"
@@ -15,22 +13,21 @@ then
 fi
 
 set -e
-cd heap-reloc
-. ./build-test-sbcl
+(cd ../src/runtime ; make heap-reloc-test)
 
 # Exercise all the lines of 'fakemap' by starting up N times in a row.
 # KLUDGE: assume N = 6
 # FIXME: don't assume that N = 6
-export SBCL_FAKE_MMAP_INSTRUCTION_FILE=`pwd`/fakemap
+test_sbcl=../src/runtime/heap-reloc-test
+export SBCL_FAKE_MMAP_INSTRUCTION_FILE=`pwd`/heap-reloc/fakemap
 i=1
 while [ $i -le 6 ]
 do
   echo Trial $i
   i=`expr $i + 1`
-  ./test-sbcl --lose-on-corruption --disable-ldb --noinform --core ../../output/sbcl.core \
+  $test_sbcl --lose-on-corruption --disable-ldb --noinform --core ../output/sbcl.core \
               --no-sysinit --no-userinit --noprint --disable-debugger \
               --eval '(gc :full t)' --quit
 done
-rm ./test-sbcl
 
 exit $EXIT_TEST_WIN
