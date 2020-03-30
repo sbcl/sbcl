bbbb5177b88b7f2f1ed5d7612e2e7d25b5fe81cb
Stas Boukarev <stassats@gmail.com>
2020-03-30 13:40:02 +0300

    Remove src/runtime/heap-reloc-test after testing.

--- tests/relocation.test.sh
+++ tests/relocation.test.sh
@@ -12,13 +12,17 @@ then
     exit $EXIT_TEST_WIN
 fi
 
+test_sbcl=../src/runtime/heap-reloc-test
+
+rm -f $test_sbcl
+
 set -e
 (cd ../src/runtime ; make heap-reloc-test)
 
 # Exercise all the lines of 'fakemap' by starting up N times in a row.
 # KLUDGE: assume N = 6
 # FIXME: don't assume that N = 6
-test_sbcl=../src/runtime/heap-reloc-test
+
 export SBCL_FAKE_MMAP_INSTRUCTION_FILE=`pwd`/heap-reloc/fakemap
 i=1
 while [ $i -le 6 ]
@@ -29,5 +33,7 @@ do
               --no-sysinit --no-userinit --noprint --disable-debugger \
               --eval '(gc :full t)' --quit
 done
+
+rm -f $test_sbcl
 
 exit $EXIT_TEST_WIN
