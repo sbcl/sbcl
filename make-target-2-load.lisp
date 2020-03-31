89a819f56fc055e1b7179cdaa067660132663423
Stas Boukarev <stassats@gmail.com>
2020-03-31 04:32:19 +0300

    Add 18

--- make-target-2-load.lisp
+++ make-target-2-load.lisp
@@ -427,7 +427,7 @@ Please check that all strings which were not recognizable to the compiler
 
 (setq sb-c:*compile-to-memory-space* :auto)
 (when (find-package "SB-INTERPRETER") (setq sb-ext:*evaluator-mode* :interpret))
-#+x68-64 (sb-ext:fold-identical-code :aggressive t :preserve-docstrings t)
+#+x86-64 (sb-ext:fold-identical-code :aggressive t :preserve-docstrings t)
 
 ;; See comments in 'readtable.lisp'
 (setf (readtable-base-char-preference *readtable*) :symbols)
