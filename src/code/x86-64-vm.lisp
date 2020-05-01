2ad58341844494ca14b2b32c12e336f31704e8cb
Douglas Katzman <dougk@google.com>
2020-05-01 11:13:39 -0400

    Fix style-warning

--- src/code/x86-64-vm.lisp
+++ src/code/x86-64-vm.lisp
@@ -388,6 +388,7 @@
   (declare (ignorable code fixups))
   (unless (immobile-space-obj-p code)
     (return-from statically-link-code-obj code))
+  #+immobile-code
   (let* ((fdefns-start (+ code-constants-offset
                           (* code-slots-per-simple-fun (code-n-entries code))))
          (fdefns-count (code-n-named-calls code))
@@ -400,7 +401,7 @@
     ;;   that function is in immobile space and needs no trampoline.
     ;; * whether the replacement creates ambiguitity - if #'F and #'G are the same
     ;;   function, then substituting that function in for the fdefn of F and G
-    ;;   requires storing locations at which to replacement was done
+    ;;   requires storing locations at which replacement was done
     (dotimes (i fdefns-count)
       (let* ((fdefn (code-header-ref code (+ fdefns-start i)))
              (fun (when (fdefn-p fdefn) (fdefn-fun fdefn))))
