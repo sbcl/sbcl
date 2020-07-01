e706632cf3ef7b75023dfa0f5cd8ae479d454d1e
Stas Boukarev <stassats@gmail.com>
2020-07-01 03:03:20 +0300

    Disable a test on -sb-thread

--- tests/avltree.pure.lisp
+++ tests/avltree.pure.lisp
@@ -1,4 +1,7 @@
 (use-package "SB-INT")
+
+#-sb-thread (sb-ext:exit :code 104) ;; some of the symbols below disappear
+
 (import 'sb-thread::(avlnode-key avlnode-data avlnode-left avlnode-right
                      avl-insert avl-delete avl-find
                      avl-balance-factor avl-count))
