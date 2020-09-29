d0243a9f9961f0afdc09b555821b88edb2488be9
Stas Boukarev <stassats@gmail.com>
2020-09-30 02:46:31 +0300

    Fix CPUID checking.
    cpu-has- is an index, not a mask. Have popcnt and no avx => bad time.

--- src/compiler/x86-64/parms.lisp
+++ src/compiler/x86-64/parms.lisp
@@ -178,8 +178,8 @@
 (defvar *binding-stack-pointer*)
 
 ;;; Bit indices into *CPU-FEATURE-BITS*
-(defconstant cpu-has-ymm-registers   1)
-(defconstant cpu-has-popcnt          2)
+(defconstant cpu-has-ymm-registers   0)
+(defconstant cpu-has-popcnt          1)
 
 (defconstant-eqx +static-symbols+
  `#(,@+common-static-symbols+
