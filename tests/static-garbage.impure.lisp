93926771670a1467917f8a382172154e4e7776bd
Douglas Katzman <dougk@google.com>
2020-11-18 01:59:25 -0500

    Make test more specific in its intent
    Don't care how many threads and semaphores there are,
    just that none should exist in the on-disk core.

--- tests/static-garbage.impure.lisp
+++ tests/static-garbage.impure.lisp
@@ -2,7 +2,8 @@
 ;;; at all, but that's not simple to do from lisp.
 ;;; Perform this by itself to remain unaffected by anything else.
 (with-test (:name :pseudostatic-garbage :skipped-on (:not :sb-thread))
-  (let ((list1 (sb-vm::list-allocated-objects :all :test #'sb-thread::thread-p))
-        (list2 (sb-vm::list-allocated-objects :all :test #'sb-thread::semaphore-p)))
-    (assert (not (cdr list1)) nil "~a" list1)
-    (assert (not (cdr list2)))))
+  (flet ((check-not-static (list)
+           (dolist (x list)
+             (assert (< (sb-kernel:generation-of x) sb-vm:+pseudo-static-generation+)))))
+    (check-not-static (sb-vm:list-allocated-objects :all :test #'sb-thread::thread-p))
+    (check-not-static (sb-vm:list-allocated-objects :all :test #'sb-thread::semaphore-p))))
