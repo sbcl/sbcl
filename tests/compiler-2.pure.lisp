sbcl/sbcl

commit a3ea77a7940cc11ded69b8169da5c4d7a6fafb18

Fix a couple warnings under clisp.
Rearrange LOOP to put variable clause first, and use (DECLARE (TYPE ...
rather than (DECLARE (...

src/compiler/ltn.lisp
@@ -349,8 +349,8 @@
                       (primitive-types))
               (let ((n-values (nth-value 1 (values-types
                                             (lvar-derived-type arg)))))
-                (loop repeat n-values
-                      for (prim-type . lvar-type) = (pop types)
+                (loop for (prim-type . lvar-type) = (pop types)
+                      repeat n-values
                       do
                       (primitive-types (or prim-type
                                            *backend-t-primitive-type*))

src/compiler/srctran.lisp
@@ -414,7 +414,7 @@
 ;;; that negative zeros are strictly less than positive zeros.
 (macrolet ((def (name op)
              `(defun ,name (x y)
-                (declare (real x y))
+                (declare (type real x y))
                 (if (and (floatp x) (floatp y) (zerop x) (zerop y))
                     (,op (float-sign x) (float-sign y))
                     (,op x y)))))

commit 6c1aee4bd0457994b52d7b5f0183b02997bed7c0

Fix a LOOP in ltn-analyze-mv-bind.
The warnings in Clisp prompted it to get broken in a3ea77a by
rearranging the statements.

Fixes lp#1906563

src/compiler/ltn.lisp
@@ -376,13 +376,13 @@
                       (primitive-types))
               (let ((n-values (nth-value 1 (values-types
                                             (lvar-derived-type arg)))))
-                (loop for (prim-type . lvar-type) = (pop types)
-                      repeat n-values
+                (loop repeat n-values
                       do
-                      (primitive-types (or prim-type
-                                           *backend-t-primitive-type*))
-                      (lvar-types (or lvar-type
-                                      *universal-type*)))
+                      (destructuring-bind (&optional prim-type . lvar-type) (pop types)
+                        (primitive-types (or prim-type
+                                             *backend-t-primitive-type*))
+                        (lvar-types (or lvar-type
+                                        *universal-type*))))
                 (annotate-fixed-values-lvar
                  arg
                  (primitive-types) (lvar-types))))))))

tests/compiler-2.pure.lisp
@@ -3014,3 +3014,11 @@
                (fixnum x y))
       (the fixnum (+ x y)))
    :allow-notes nil))
+
+(with-test (:name :ltn-analyze-mv-bind)
+  (checked-compile-and-assert
+   ()
+   `(lambda ()
+      (multiple-value-call #'list
+        10 (apply #'values '(44 33d0))))
+   (() '(10 44  33d0) :test #'equal)))