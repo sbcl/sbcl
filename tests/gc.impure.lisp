0ce24b2f4bbb0d645657b8e3c6346e9d905abb72
Stas Boukarev <stassats@gmail.com>
2021-02-04 01:44:27 +0300

    Don't test traceroot on darwin-arm64.
    It can't encode 64-bit non-dynamic space pointers.

--- tests/gc.impure.lisp
+++ tests/gc.impure.lisp
@@ -362,7 +362,8 @@
 
 (defvar *foo*)
 #+gencgc
-(with-test (:name (sb-ext:search-roots :simple-fun))
+(with-test (:name (sb-ext:search-roots :simple-fun)
+            :broken-on (and :darwin :arm64))
   ;; Tracing a path to a simple fun wasn't working at some point
   ;; because of failure to employ fun_code_header in the right place.
   (setq *foo* (compile nil '(lambda () 42)))
