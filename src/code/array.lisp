sbcl/sbcl

commit e8a6b8d1a921cacd3150301056d3eaef0881d9bc

Sprinkle explicit-check over some list functions.

src/code/list.lisp
@@ -155,6 +155,7 @@
 (defun tree-equal (x y &key (test nil testp) (test-not nil notp))
   #!+sb-doc
   "Return T if X and Y are isomorphic trees with identical leaves."
+  (declare (explicit-check))
   (cond (notp
          (when testp
            (error ":TEST and :TEST-NOT were both supplied."))
@@ -189,6 +190,7 @@
 (defun nth (n list)
   #!+sb-doc
   "Return the nth object in a list where the car is the zero-th element."
+  (declare (explicit-check))
   (car (nthcdr n list)))
 
 (defun first (list)
@@ -368,6 +370,7 @@
 (defun make-list (size &key initial-element)
   #!+sb-doc
   "Constructs a list with size elements each set to value"
+  (declare (explicit-check))
   (%make-list size initial-element))
 ;;; This entry point is to be preferred, irrespective of
 ;;; whether or not the backend has vops for %MAKE-LIST.
@@ -848,6 +851,7 @@
   #!+sb-doc
   "Return the tail of LIST beginning with first element satisfying EQLity,
    :TEST, or :TEST-NOT with the given ITEM."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   (let ((key (and key (%coerce-callable-to-fun key)))
@@ -869,6 +873,7 @@
 (defun member-if (test list &key key)
   #!+sb-doc
   "Return tail of LIST beginning with first element satisfying TEST."
+  (declare (explicit-check))
   (let ((test (%coerce-callable-to-fun test))
         (key (and key (%coerce-callable-to-fun key))))
     (if key
@@ -878,6 +883,7 @@
 (defun member-if-not (test list &key key)
   #!+sb-doc
   "Return tail of LIST beginning with first element not satisfying TEST."
+  (declare (explicit-check))
   (let ((test (%coerce-callable-to-fun test))
         (key (and key (%coerce-callable-to-fun key))))
     (if key
@@ -896,6 +902,7 @@
 (defun adjoin (item list &key key (test #'eql testp) (test-not nil notp))
   #!+sb-doc
   "Add ITEM to LIST unless it is already a member"
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   (let ((key (and key (%coerce-callable-to-fun key)))
@@ -966,6 +973,7 @@
 (defun union (list1 list2 &key key (test nil testp) (test-not nil notp))
   #!+sb-doc
   "Return the union of LIST1 and LIST2."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   ;; We have two possibilities here: for shortish lists we pick up the
@@ -1005,6 +1013,7 @@
 (defun nunion (list1 list2 &key key (test nil testp) (test-not nil notp))
   #!+sb-doc
   "Destructively return the union of LIST1 and LIST2."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   ;; We have two possibilities here: for shortish lists we pick up the
@@ -1051,6 +1060,7 @@
                      &key key (test nil testp) (test-not nil notp))
   #!+sb-doc
   "Return the intersection of LIST1 and LIST2."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   (when (and list1 list2)
@@ -1065,6 +1075,7 @@
                       &key key (test nil testp) (test-not nil notp))
   #!+sb-doc
   "Destructively return the intersection of LIST1 and LIST2."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   (when (and list1 list2)
@@ -1081,6 +1092,7 @@
                        &key key (test nil testp) (test-not nil notp))
   #!+sb-doc
   "Return the elements of LIST1 which are not in LIST2."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   (if list2
@@ -1096,6 +1108,7 @@
                         &key key (test nil testp) (test-not nil notp))
   #!+sb-doc
   "Destructively return the elements of LIST1 which are not in LIST2."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   (if list2
@@ -1113,6 +1126,7 @@
                          &key key (test nil testp) (test-not nil notp))
   #!+sb-doc
   "Return new list of elements appearing exactly once in LIST1 and LIST2."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   (let ((result nil))
@@ -1131,6 +1145,7 @@
   #!+sb-doc
   "Destructively return a list with elements which appear but once in LIST1
    and LIST2."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   (let ((key (and key (%coerce-callable-to-fun key)))
@@ -1189,6 +1204,7 @@
 (defun subsetp (list1 list2 &key key (test #'eql testp) (test-not nil notp))
   #!+sb-doc
   "Return T if every element in LIST1 is also in LIST2."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   (with-member-test (member-test)
@@ -1218,6 +1234,7 @@
   #!+sb-doc
   "Return the cons in ALIST whose car is equal (by a given test or EQL) to
    the ITEM."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   (let ((key (and key (%coerce-callable-to-fun key)))
@@ -1240,6 +1257,7 @@
   #!+sb-doc
   "Return the first cons in ALIST whose CAR satisfies PREDICATE. If
    KEY is supplied, apply it to the CAR of each cons before testing."
+  (declare (explicit-check))
   (let ((predicate (%coerce-callable-to-fun predicate))
         (key (and key (%coerce-callable-to-fun key))))
     (if key
@@ -1250,6 +1268,7 @@
   #!+sb-doc
   "Return the first cons in ALIST whose CAR does not satisfy PREDICATE.
   If KEY is supplied, apply it to the CAR of each cons before testing."
+  (declare (explicit-check))
   (let ((predicate (%coerce-callable-to-fun predicate))
         (key (and key (%coerce-callable-to-fun key))))
     (if key
@@ -1257,10 +1276,10 @@
         (%assoc-if-not predicate alist))))
 
 (defun rassoc (item alist &key key (test nil testp) (test-not nil notp))
-  (declare (list alist))
   #!+sb-doc
   "Return the cons in ALIST whose CDR is equal (by a given test or EQL) to
    the ITEM."
+  (declare (explicit-check))
   (when (and testp notp)
     (error ":TEST and :TEST-NOT were both supplied."))
   (let ((key (and key (%coerce-callable-to-fun key)))
@@ -1283,6 +1302,7 @@
   #!+sb-doc
   "Return the first cons in ALIST whose CDR satisfies PREDICATE. If KEY
   is supplied, apply it to the CDR of each cons before testing."
+  (declare (explicit-check))
   (let ((predicate (%coerce-callable-to-fun predicate))
         (key (and key (%coerce-callable-to-fun key))))
     (if key
@@ -1293,6 +1313,7 @@
   #!+sb-doc
   "Return the first cons in ALIST whose CDR does not satisfy PREDICATE.
   If KEY is supplied, apply it to the CDR of each cons before testing."
+  (declare (explicit-check))
   (let ((predicate (%coerce-callable-to-fun predicate))
         (key (and key (%coerce-callable-to-fun key))))
     (if key
@@ -1342,33 +1363,39 @@
 (defun mapc (function list &rest more-lists)
   #!+sb-doc
   "Apply FUNCTION to successive elements of lists. Return the second argument."
+  (declare (explicit-check))
   (map1 function (cons list more-lists) nil t))
 
 (defun mapcar (function list &rest more-lists)
   #!+sb-doc
   "Apply FUNCTION to successive elements of LIST. Return list of FUNCTION
    return values."
+  (declare (explicit-check))
   (map1 function (cons list more-lists) :list t))
 
 (defun mapcan (function list &rest more-lists)
   #!+sb-doc
   "Apply FUNCTION to successive elements of LIST. Return NCONC of FUNCTION
    results."
+  (declare (explicit-check))
   (map1 function (cons list more-lists) :nconc t))
 
 (defun mapl (function list &rest more-lists)
   #!+sb-doc
   "Apply FUNCTION to successive CDRs of list. Return NIL."
+  (declare (explicit-check))
   (map1 function (cons list more-lists) nil nil))
 
 (defun maplist (function list &rest more-lists)
   #!+sb-doc
   "Apply FUNCTION to successive CDRs of list. Return list of results."
+  (declare (explicit-check))
   (map1 function (cons list more-lists) :list nil))
 
 (defun mapcon (function list &rest more-lists)
   #!+sb-doc
   "Apply FUNCTION to successive CDRs of lists. Return NCONC of results."
+  (declare (explicit-check))
   (map1 function (cons list more-lists) :nconc nil))
 
 ;;;; Specialized versions

commit 3203ba3ac5f2c3d59c25825c90fb6e8d637e8af8

Forbid template failure in VECTORISH-LTN-ANNOTATE
* Remove ALLOCATE-VECTOR stub
  I can't see how we'd ever get there. Also I didn't see how
  allocate-vector-with-widetag could have been working, ever.

* MAKE-LIST regressed in rev e8a6b8d1 due to adding EXPLICIT-CHECK
  and losing the argument type, but it was wrong on 32-bit machines
  for another reason as well.

Known problem: 'seq.pure' gets the ltn-annotate failure, so I've allowed it

src/code/array.lisp
@@ -369,9 +369,13 @@
     ;; Both it and type BIT have N-BITS-SHIFT = 0, so the determination
     ;; of true size can't be left up to VECTOR-LENGTH-IN-WORDS.
     (allocate-vector widetag length
-                     (if (/= widetag simple-array-nil-widetag)
-                         (vector-length-in-words full-length n-bits-shift)
-                         0))))
+                     ;; VECTOR-LENGTH-IN-WORDS potentially returns a machine-word-sized
+                     ;; integer, so it doesn't match the primitive type restriction of
+                     ;; POSITIVE-FIXNUM for the last argument of the vector alloc vops.
+                     (the fixnum
+                          (if (/= widetag simple-array-nil-widetag)
+                              (vector-length-in-words full-length n-bits-shift)
+                              0)))))
 
 (declaim (ftype (sfunction (array) (integer 128 255)) array-underlying-widetag))
 (defun array-underlying-widetag (array)
@@ -1878,16 +1882,13 @@ function to be removed without further warning."
            (array-dimensions array)))
   array)
 
-;;; Why is this needed for ARM and not x86 ???
-(defun allocate-vector (type length words)
-  (allocate-vector type length words))
-
 ;;; Horrible kludge for the "static-vectors" system
 ;;; which uses an internal symbol in SB-IMPL.
 (import '%vector-widetag-and-n-bits-shift 'sb-impl)
 
 (defun make-weak-vector (length &key (initial-contents nil contents-p)
                                      (initial-element nil element-p))
+  (declare (index length))
   (when (and element-p contents-p)
     (error "Can't specify both :INITIAL-ELEMENT and :INITIAL-CONTENTS"))
   ;; Explicitly compute a widetag with the weakness bit ORed in.
@@ -1897,13 +1898,12 @@ function to be removed without further warning."
     ;; almost makes me want to cry, but not quite enough for me to improve it.
     (if contents-p
         (let ((contents-length (length initial-contents)))
-          (if (= (truly-the index length) contents-length)
+          (if (= length contents-length)
               (replace (truly-the simple-vector (allocate-vector type length length))
                        initial-contents)
               (error "~S has ~D elements, vector length is ~D."
                      :initial-contents contents-length length)))
-        (fill (truly-the simple-vector
-                         (allocate-vector type (truly-the index length) length))
+        (fill (truly-the simple-vector (allocate-vector type length length))
               ;; 0 is the usual default, but NIL makes more sense for weak vectors
               ;; as it is the value assigned to broken hearts.
               (if element-p initial-element nil)))))

src/code/list.lisp
@@ -381,7 +381,12 @@
 
 (defun make-list (size &key initial-element)
   "Construct and return a list with SIZE elements each set to INITIAL-ELEMENT."
-  (declare (explicit-check))
+  ;; Can't EXPLICIT-CHECK here, otherwise SIZE gets primitive type T
+  ;; which does not match the restriction on the allocator vop.
+  ;; Despite that %MAKE-LIST says it's the "preferred entry point",
+  ;; it's faster *not* to call it from MAKE-LIST because this way
+  ;; grabs all the memory at once.
+  ;; I wish I could remember why %MAKE-LIST claims preference though.
   (%make-list size initial-element))
 ;;; This entry point is to be preferred, irrespective of
 ;;; whether or not the backend has vops for %MAKE-LIST.

src/compiler/array-tran.lisp
@@ -683,7 +683,8 @@
           ((> n-bits sb-vm:n-word-bits) ; e.g. double-float on 32-bit
            (let ((n-words-per-element
                   (the fixnum (/ n-bits sb-vm:n-word-bits)))) ; i.e., not RATIO
-             `(* ,padded-length-form ,n-words-per-element)))
+             #+64-bit `(* ,padded-length-form ,n-words-per-element)
+             #-64-bit `(the fixnum (* ,padded-length-form ,n-words-per-element))))
           (t
            ;; This would have to change if we ever implement Unicode strings
            ;; using 3 bytes per char (as has been suggested by xof) which makes

src/compiler/fndb.lisp
@@ -904,14 +904,13 @@
 (defknown list* (t &rest t) t (movable flushable))
 ;;; The length constraint on MAKE-LIST is such that:
 ;;; - not every byte of addressable memory can be used up.
-;;; - the number of bytes to allocate should not be a bignum,
-;;    nor can its native representation be larger than 'sword_t'
-;;; So it's half of most-positive-word divided by the cons size,
-;;; which is roughly twice as small as array-dimension-limit on 32-bit machines,
-;;; or 8x smaller on 64-bit machines.
+;;; - the number of bytes to allocate should be a fixnum
+;;; - type-checking can use use efficient bit-masking approach
+;;;   to combine the fixnum + range test into one instruction
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defconstant make-list-limit
-    (floor (/ sb-ext:most-positive-word 2) (* 2 sb-vm:n-word-bytes))))
+    #+64-bit (floor (/ sb-ext:most-positive-word 2) (* 2 sb-vm:n-word-bytes))
+    #-64-bit (floor (ash sb-xc:most-positive-fixnum -1) (* 2 sb-vm:n-word-bytes))))
 (defknown make-list ((integer 0 (#.make-list-limit)) &key (:initial-element t)) list
   (movable flushable))
 (defknown %make-list ((integer 0 (#.make-list-limit)) t) list (movable flushable))

src/compiler/generic/early-vm.lisp
@@ -80,6 +80,13 @@
 (defconstant array-rank-limit 256
   "the exclusive upper bound on the rank of an array")
 
+;;; FIXME: these limits are wrong at the most basic level according to the spec,
+;;; because if there are different limits for different element types, then
+;;; this constant should be the _smallest_ of the limits for any element type.
+;;; The largest element size is (COMPLEX DOUBLE-FLOAT) taking 16 bytes per element.
+;;; If you had an array of this size on 32-bit machines, it would consume about
+;;; 8GB of payload. So not only is it larger than a fixnum - a problem for the
+;;; allocator vops - it exceeds the address space.
 #-array-ubsan ; usual way
 (progn
 ;;; - 2 to leave space for the array header

src/compiler/generic/vm-ir2tran.lisp
@@ -368,11 +368,13 @@
         (setf (lvar-info arg)
               (make-ir2-lvar (primitive-type (lvar-type arg)))))
       (unless (is-ok-template-use template call (ltn-policy-safe-p ltn-policy))
-        (ltn-default-call call)
-        (return-from vectorish-ltn-annotate-helper (values)))
+        (cond ((lvar-fun-is (combination-fun call) '(%make-list))
+               (ltn-default-call call)
+               (return-from vectorish-ltn-annotate-helper (values)))
+              (t
+               (bug "vectorish-ltn-annotate-helper must not fail"))))
       (setf (basic-combination-info call) template)
       (setf (node-tail-p call) nil)
-
       (dolist (arg args)
         (annotate-1-value-lvar arg))))
 

src/compiler/ltn.lisp
@@ -584,6 +584,26 @@
         (unless (operand-restriction-ok type (lvar-ptype arg)
                                         :lvar arg)
           (return nil))))))
+(defun diagnose-template-args (template call)
+  ;; Scan all except the MORE args. If you've managed to create not-ok MORE args,
+  ;; you're probably smart enough to figure it out on your own.
+  (let ((args (basic-combination-args call))
+        (types (template-arg-types template)))
+    (unless (= (length args) (length types))
+      (return-from diagnose-template-args "bad length"))
+    (do ((args args (cdr args))
+         (i 0 (1+ i))
+         (any-fail nil)
+         (types types (cdr types)))
+        ((null types) any-fail)
+      (let* ((arg (car args))
+             (type (car types))
+             (ok (operand-restriction-ok type (lvar-ptype arg) :lvar arg)))
+        (let ((*print-pretty* nil))
+          (format t "arg~d: is ~s need ~s, ~a~%" i
+                  (primitive-type-name (lvar-ptype arg))
+                  type (if ok "OK" "FAIL")))
+        (unless ok (setq any-fail :fail))))))
 
 ;;; Check that TEMPLATE can be used with the specifed RESULT-TYPE.
 ;;; Result type checking is pretty different from argument type

src/compiler/srctran.lisp
@@ -162,8 +162,11 @@
       ;; Something fishy here- If THE is removed, OPERAND-RESTRICTION-OK
       ;; returns NIL because type inference on MAKE-LIST never happens.
       ;; But the fndb entry for %MAKE-LIST is right, so I'm slightly bewildered.
-      `(%make-list (the (integer 0 (,(1- array-dimension-limit))) ,length)
-                   ,(second rest))
+      ;; OK, much more than "slightly" now.
+      ;; Casting to (integer 0 ,make-list-limit) fails in the ltn-annotate helper.
+      #+64-bit `(%make-list (the (integer 0 (,(1- array-dimension-limit))) ,length)
+                            ,(second rest))
+      #-64-bit `(%make-list (the (integer 0 (,make-list-limit)) ,length) ,(second rest))
       (values nil t))) ; give up
 
 (deftransform %make-list ((length item) ((constant-arg (eql 0)) t)) nil)

src/compiler/x86/alloc.lisp
@@ -247,9 +247,7 @@
          (length :scs (any-reg immediate))
          (words :scs (any-reg immediate)))
   (:results (result :scs (descriptor-reg) :from :load))
-  (:arg-types positive-fixnum
-              positive-fixnum
-              positive-fixnum)
+  (:arg-types positive-fixnum positive-fixnum positive-fixnum)
   (:policy :fast-safe)
   (:node-var node)
   (:generator 100

tests/selfbuild-output.pure.lisp
@@ -1,0 +1,4 @@
+
+(with-test (:name :make-list-%make-list-not-called
+                  :fails-on (:not x86-64))
+  (assert (not (ctu:find-named-callees #'make-list))))