5ad6f40786e2cacbbe667ea155273fe5cd01cf98
Douglas Katzman <dougk@google.com>
2021-06-14 11:27:06 -0400

    Fix build-order bug regarding ALPHANUMERICP et. al.
    ALPHANUMERICP is declaimed inline, but used in 'stream.lisp' before defined.
    I can't see how this slips through, because detection of failed inlining
    mostly works. In fact, putting 'stream' later in build-order gets:
    "caught WARNING:
       Proclaiming SB-SYS:READ-N-BYTES to be INLINE, but 2 calls to it were ..."
    so how is it that ALPHANUMERICP was not warning?
    
    Anyway I found this when attempting to recompile 'stream' using 'slam.sh'
    which caused a genesis error because inlining does happen then,
    but +CHARACTER-MISC-DATABASE+ has no load-time-value yet.

--- tests/selfbuild-output.pure.lisp
+++ tests/selfbuild-output.pure.lisp
@@ -2,3 +2,12 @@
 (with-test (:name :make-list-%make-list-not-called
                   :fails-on (:not :x86-64))
   (assert (not (ctu:find-named-callees #'make-list))))
+
+;;; Make sure that we see the literal value of
+;;; SB-UNICODE::+CHARACTER-MISC-DATABASE+
+(with-test (:name :alphanumericp-is-inlined
+                  :skipped-on (:not :sb-unicode))
+  (let ((code (sb-kernel:fun-code-header #'sb-impl::case-frob-capitalize-out)))
+    (loop for i from sb-vm:code-constants-offset below (sb-kernel:code-header-words code)
+          thereis (eq (sb-kernel:code-header-ref code i)
+                      sb-unicode::+character-misc-database+))))
