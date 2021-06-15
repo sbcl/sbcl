sbcl/sbcl

commit bd1a1a9e1e3dfdf132ba9f0492a8d83e6f74e1b1

Change ENSURE-DIRECTORIES-EXIST a little.
Came for the problematic *D-P-D* rebinding, stayed for the inefficient
truename resolution. (E-D-E could be a lot better still, but only if
we can trust mkdir's errno convention on all platforms.)

src/code/filesys.lisp
@@ -266,11 +266,18 @@
                   :newest)))))
 
 
-;;;; Grabbing the kind of file when we have a namestring.
-(defun native-file-kind (namestring)
+;;;; Grabbing the kind of file when we have a native-namestring.
+(defun native-file-kind (namestring &optional resolve-symlinks)
+  #+win32 (declare (ignore resolve-symlinks))
   (multiple-value-bind (existsp errno ino mode)
       #-win32
-      (sb-unix:unix-lstat namestring)
+      ;; Note that when resolve-symlinks is true, we'll return NIL if
+      ;; there are circular and dangling symlinks anywhere in the
+      ;; path. That's different than what our TRUENAME does; this is
+      ;; intended as an efficient internal routine.
+      (if resolve-symlinks
+          (sb-unix:unix-stat namestring)
+          (sb-unix:unix-lstat namestring))
       #+win32
       (sb-unix:unix-stat namestring)
     (declare (ignore errno ino))
@@ -1240,6 +1247,15 @@ Experimental: interface subject to change."
        (null (pathname-name pathname))
        (null (pathname-type pathname))))
 
+;; FIXME: repeatedly poking at the file system ought to be
+;; unnecessary, and walking the directory hierarchy from the top down
+;; is probably a waste of effort in most cases. If we can trust mkdir
+;; returning ENOENT and EEXIST always and only where it's supposed to,
+;; we could get this down to two syscalls in the optimal success case
+;; case, one in the pessimal failure case, and no worse than this in a
+;; hypothetical average. (It looks like we inherited this approach
+;; from CMUCL; maybe some ancient Unix's mkdir errno values weren't
+;; reliable?)
 (defun ensure-directories-exist (pathspec &key verbose (mode #o777))
   "Test whether the directories containing the specified file
   actually exist, and attempt to create them if they do not.
@@ -1249,37 +1265,37 @@ Experimental: interface subject to change."
         (created-p nil))
     (when (wild-pathname-p pathname)
       (sb-kernel::%file-error pathspec "bad place for a wild pathname"))
-    (let* ((dir (pathname-directory pathname))
-           (*default-pathname-defaults*
-             (make-pathname :directory dir :device (pathname-device pathname)))
-          (dev (pathname-device pathname)))
+    (let* ((host (pathname-host pathname))
+           (dir (pathname-directory pathname))
+           (dev (pathname-device pathname)))
       (loop for i from (case dev (:unc 3) (otherwise 2))
               upto (length dir)
             do
             (let* ((newpath (make-pathname
-                             :host (pathname-host pathname)
+                             :host host
                              :device dev
                              :directory (subseq dir 0 i)))
-                   (probed (probe-file newpath)))
-              (unless (directory-pathname-p probed)
-                (let ((namestring (coerce (native-namestring newpath)
-                                          'string)))
-                  (when verbose
-                    (format *standard-output*
-                            "~&creating directory: ~A~%"
-                            namestring))
-                  (sb-unix:unix-mkdir namestring mode)
-                  (unless (directory-pathname-p (probe-file newpath))
+                   (namestring (coerce (native-namestring newpath :as-file t)
+                                       'string))
+                   (kind (native-file-kind namestring t)))
+              (unless (eq :directory kind)
+                (when verbose
+                  (format *standard-output*
+                          "~&creating directory: ~A~%"
+                          namestring))
+                (sb-unix:unix-mkdir namestring mode)
+                (let ((newkind (native-file-kind namestring t)))
+                  (unless (eq :directory newkind)
                     (restart-case
                         (sb-kernel::%file-error
                          pathspec
                          "Can't create directory ~A~:[~;,~%a file with ~
                           the same name already exists.~]"
                          namestring
-                         (and probed (not (directory-pathname-p probed))))
+                         (and kind (not (eq :directory newkind))))
                       (retry ()
                         :report "Retry directory creation."
                         (ensure-directories-exist
-                         pathspec :verbose verbose :mode mode))))
-                  (setf created-p t)))))
+                         pathspec :verbose verbose :mode mode)))))
+                (setf created-p t))))
       (values pathspec created-p))))

commit 002fea56ce4b83bb19483d50ca501c19131f9db5

Fix E-D-E with long filenames on Win32. (Regression in bd1a1a9e.)

src/code/filesys.lisp
@@ -269,8 +269,9 @@
 ;;;; Grabbing the kind of file when we have a native-namestring.
 (defun native-file-kind (namestring &optional resolve-symlinks)
   #+win32 (declare (ignore resolve-symlinks))
+  #+win32 (nth-value 1 (sb-win32::native-probe-file-name namestring))
+  #-win32
   (multiple-value-bind (existsp errno ino mode)
-      #-win32
       ;; Note that when resolve-symlinks is true, we'll return NIL if
       ;; there are circular and dangling symlinks anywhere in the
       ;; path. That's different than what our TRUENAME does; this is
@@ -278,15 +279,12 @@
       (if resolve-symlinks
           (sb-unix:unix-stat namestring)
           (sb-unix:unix-lstat namestring))
-      #+win32
-      (sb-unix:unix-stat namestring)
     (declare (ignore errno ino))
     (when existsp
       (let ((ifmt (logand mode sb-unix:s-ifmt)))
        (case ifmt
          (#.sb-unix:s-ifreg :file)
          (#.sb-unix:s-ifdir :directory)
-         #-win32
          (#.sb-unix:s-iflnk :symlink)
          (t :special))))))
 