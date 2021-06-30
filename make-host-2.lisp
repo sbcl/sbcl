8568071eea48a1495d701d6ab863f23aa6c22764
Stas Boukarev <stassats@gmail.com>
2021-06-30 19:03:10 +0300

    Finish renaming a function.

--- make-host-2.lisp
+++ make-host-2.lisp
@@ -29,7 +29,7 @@
     ;; Enforce absence of unexpected forward-references to warm loaded code.
     ;; Looking into a hidden detail of this compiler seems fair game.
     (when (and sb-c::*undefined-warnings*
-               (featurep '(:or :x86 :x86-64 :arm64))) ; until all the rest are clean
+               (target-featurep '(:or :x86 :x86-64 :arm64))) ; until all the rest are clean
       (setf fail t)
       (dolist (warning sb-c::*undefined-warnings*)
         (case (sb-c::undefined-warning-kind warning)
@@ -38,7 +38,7 @@
           (:function (setf functions t))))))
   ;; Exit the compilation unit so that the summary is printed. Then complain.
   ;; win32 is not clean
-  (when (and fail (not (featurep :win32)))
+  (when (and fail (not (target-featurep :win32)))
     (cerror "Proceed anyway"
             "Undefined ~:[~;variables~] ~:[~;types~]~
              ~:[~;functions (incomplete SB-COLD::*UNDEFINED-FUN-ALLOWLIST*?)~]"
