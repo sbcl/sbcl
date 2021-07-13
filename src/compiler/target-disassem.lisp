sbcl/sbcl

commit 95003ec162f5bab1e42371f298e6a67f7757bcb3

Remove ugly ad-hoc reinvention of ADJUST-ARRAY.
It's slower due to type unparsing/re-parsing.

src/compiler/target-disassem.lisp
@@ -1241,20 +1241,6 @@
                                 debug-var-num))
                          ))))))))
 
-;;; Return a new vector which has the same contents as the old one
-;;; VEC, plus new cells (for a total size of NEW-LEN). The additional
-;;; elements are initialized to INITIAL-ELEMENT.
-(defun grow-vector (vec new-len &optional initial-element)
-  (declare (type vector vec)
-           (type fixnum new-len))
-  (let ((new
-         (make-sequence `(vector ,(array-element-type vec) ,new-len)
-                        new-len
-                        :initial-element initial-element)))
-    (dotimes (i (length vec))
-      (setf (aref new i) (aref vec i)))
-    new))
-
 ;;; Return a STORAGE-INFO struction describing the object-to-source
 ;;; variable mappings from DEBUG-FUN.
 (defun storage-info-for-debug-fun (debug-fun)
@@ -1289,13 +1275,9 @@
                           (length (length locations))
                           (offset (sb!c:sc-offset-offset sc-offset)))
                      (when (>= offset length)
-                       (setf locations
-                             (grow-vector locations
-                                          (max (* 2 length)
-                                               (1+ offset))
-                                          nil)
-                             (location-group-locations group)
-                             locations))
+                       (setf locations (adjust-array locations
+                                                     (max (* 2 length) (1+ offset)))
+                             (location-group-locations group) locations))
                      (let ((already-there (aref locations offset)))
                        (cond ((null already-there)
                               (setf (aref locations offset) debug-var-offset))

commit 001abbae2ce44ab7ffe804829a9a18aab745f272

Fix logic that got broken 4 years ago in rev 95003ec1
GROW-VECTOR took an initial-element, and it used to be NIL in this case.
Regression coverage of the disassembler acting on code compiled in high
debug settings is obviously deficient, but I guess nobody cares.

src/compiler/target-disassem.lisp
@@ -1722,7 +1722,8 @@
                           (offset (sb-c:sc+offset-offset sc+offset)))
                      (when (>= offset length)
                        (setf locations (adjust-array locations
-                                                     (max (* 2 length) (1+ offset)))
+                                                     (max (* 2 length) (1+ offset))
+                                                     :initial-element nil)
                              (location-group-locations group) locations))
                      (let ((already-there (aref locations offset)))
                        (cond ((null already-there)