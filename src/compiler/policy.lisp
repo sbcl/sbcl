cfeaee861c3be8d23cba3665eaef37d4bd134ddc
Charles Zhang <charleszhang99@yahoo.com>
2021-08-17 15:55:03 +0200

    Fix arm32 build.
    COPY-STRUCT doesn't work early there for some reason.

--- src/compiler/policy.lisp
+++ src/compiler/policy.lisp
@@ -223,15 +223,16 @@ See also :POLICY option in WITH-COMPILATION-UNIT."
 ;;; OPTIMIZE forms have messed with it.
 #-sb-xc-host
 (defun !policy-cold-init-or-resanify ()
-  (macrolet ((init (policy-symbol) ; Act like MAKE-LOAD-FORM essentially
-               (let ((policy (symbol-value policy-symbol)))
-                 `(setq ,policy-symbol
+  (macrolet ((reflect-host-value (target-policy &optional (host-policy target-policy))
+               ;; Act like MAKE-LOAD-FORM essentially
+               (let ((policy (symbol-value host-policy)))
+                 `(setq ,target-policy
                         (make-policy ,(policy-primary-qualities policy)
                                      ,(policy-presence-bits policy)
                                      ,(policy-dependent-qualities policy))))))
-    (init **baseline-policy**)
-    (init **zero-typecheck-policy**)
-    (setq *policy* (copy-policy **baseline-policy**))))
+    (reflect-host-value **baseline-policy**)
+    (reflect-host-value **zero-typecheck-policy**)
+    (reflect-host-value *policy* **baseline-policy**)))
 
 #+sb-xc-host
 (defun init-xc-policy (&optional baseline-qualities)
