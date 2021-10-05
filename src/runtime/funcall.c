e724645f023f9c32311ecc3ac91c3457fec42a4e
Stas Boukarev <stassats@gmail.com>
2021-10-05 17:45:12 +0300

    Restore funcall.c for ppc.
    It uses the stack vector differently from arm64.
    
    Reported Bruce O'Neel.

--- src/runtime/funcall.c
+++ src/runtime/funcall.c
@@ -17,6 +17,8 @@
 
 #include "sbcl.h"
 #include "runtime.h"
+#include "globals.h"
+#include "interrupt.h"
 
 extern lispobj call_into_lisp(lispobj fun, lispobj *args, int nargs)
 
@@ -24,6 +26,17 @@ extern lispobj call_into_lisp(lispobj fun, lispobj *args, int nargs)
     __attribute__((sysv_abi))
 #endif
     ;
+
+#if defined(LISP_FEATURE_C_STACK_IS_CONTROL_STACK) || defined(LISP_FEATURE_ARM64) \
+    || defined(LISP_FEATURE_ARM)
+/* These functions are an interface to the Lisp call-in facility.
+ * Since this is C we can know nothing about the calling environment.
+ * The control stack might be the C stack if called from the monitor
+ * or the Lisp stack if called as a result of an interrupt or maybe
+ * even a separate stack. The args are most likely on that stack but
+ * could be in registers depending on what the compiler likes. So we
+ * copy the args into a portable vector and let the assembly language
+ * call-in function figure it out. */
 
 lispobj
 funcall0(lispobj function)
@@ -60,3 +73,57 @@ funcall3(lispobj function, lispobj arg0, lispobj arg1, lispobj arg2)
     return call_into_lisp(function, args, 3);
 }
 
+#else
+
+lispobj
+funcall0(lispobj function)
+{
+    lispobj **stack_pointer
+        = &access_control_stack_pointer(get_sb_vm_thread());
+    lispobj *args = *stack_pointer;
+
+    return call_into_lisp(function, args, 0);
+}
+
+lispobj
+funcall1(lispobj function, lispobj arg0)
+{
+    lispobj **stack_pointer
+        = &access_control_stack_pointer(get_sb_vm_thread());
+    lispobj *args = *stack_pointer;
+
+    *stack_pointer += 1;
+    args[0] = arg0;
+
+    return call_into_lisp(function, args, 1);
+}
+
+lispobj
+funcall2(lispobj function, lispobj arg0, lispobj arg1)
+{
+    lispobj **stack_pointer
+        = &access_control_stack_pointer(get_sb_vm_thread());
+    lispobj *args = *stack_pointer;
+
+    *stack_pointer += 2;
+    args[0] = arg0;
+    args[1] = arg1;
+
+    return call_into_lisp(function, args, 2);
+}
+
+lispobj
+funcall3(lispobj function, lispobj arg0, lispobj arg1, lispobj arg2)
+{
+    lispobj **stack_pointer
+        = &access_control_stack_pointer(get_sb_vm_thread());
+    lispobj *args = *stack_pointer;
+
+    *stack_pointer += 3;
+    args[0] = arg0;
+    args[1] = arg1;
+    args[2] = arg2;
+
+    return call_into_lisp(function, args, 3);
+}
+#endif
