4d3ddccc67d8538461208bb2c9fc4ee9c29ea633
Eric Timmons <etimmons@mit.edu>
2021-12-09 17:00:55 -0500

    Allow (and sb-linkable-runtime (or arm arm64 freebsd))

--- src/cold/shared.lisp
+++ src/cold/shared.lisp
@@ -347,9 +347,9 @@
          ("(not (or elf mach-o win32))"
           "No execute object file format feature defined")
          ("(and cons-profiling (not sb-thread))" ":CONS-PROFILING requires :SB-THREAD")
-         ("(and sb-linkable-runtime (not (or x86 x86-64)))"
+         ("(and sb-linkable-runtime (not (or arm arm64 x86 x86-64)))"
           ":SB-LINKABLE-RUNTIME not supported on selected architecture")
-         ("(and sb-linkable-runtime (not (or darwin linux win32)))"
+         ("(and sb-linkable-runtime (not (or darwin freebsd linux win32)))"
           ":SB-LINKABLE-RUNTIME not supported on selected operating system")
          ("(and sb-eval sb-fasteval)"
           ;; It sorta kinda works to have both, but there should be no need,
