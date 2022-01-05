sbcl/sbcl

commit e4ae12c2431cda22b78991a66ba3b2b2144dc903

sparc: Always use si_addr to get PC of sigill
It's not necessarily the same as *os_context_pc_addr() at least on the gcc
farm machine that runs SunOS. I have no idea why that would be.

src/runtime/sparc-arch.c
@@ -246,10 +246,8 @@ arch_handle_single_step_trap(os_context_t *context, int trap)
 }
 
 #ifdef LISP_FEATURE_GENCGC
-void
-arch_handle_allocation_trap(os_context_t *context)
+static void handle_allocation_trap(os_context_t *context, unsigned int *pc)
 {
-    unsigned int* pc;
     unsigned int or_inst;
     int rs1;
     int size;
@@ -259,7 +257,8 @@ arch_handle_allocation_trap(os_context_t *context)
     if (foreign_function_call_active)
       lose("Allocation trap inside foreign code.");
 
-    pc = (unsigned int*) *os_context_pc_addr(context);
+    struct thread* thread = arch_os_get_current_thread();
+
     or_inst = pc[-1];
 
     /*
@@ -267,8 +266,8 @@ arch_handle_allocation_trap(os_context_t *context)
      * instruction!
      */
     if (!(((or_inst >> 30) == 2) && (((or_inst >> 19) & 0x1f) == 2)))
-        lose("Allocation trap not preceded by an OR instruction: 0x%08x",
-             or_inst);
+        lose("Allocation trap @ %p not preceded by an OR instruction: 0x%08x",
+             pc, or_inst);
 
     /*
      * An OR instruction.  RS1 is the register we want to allocate to.
@@ -291,7 +290,7 @@ arch_handle_allocation_trap(os_context_t *context)
      */
     lispobj* memory;
     {
-        struct interrupt_data *data = &thread_interrupt_data(arch_os_get_current_thread());
+        struct interrupt_data *data = &thread_interrupt_data(thread);
         data->allocation_trap_context = context;
         extern lispobj *alloc(sword_t), *alloc_list(sword_t);
         memory = (rd & 1) ? alloc_list(size) : alloc(size);
@@ -313,9 +312,13 @@ static void sigill_handler(int signal, siginfo_t *siginfo,
         unsigned int inst;
         unsigned int* pc = (unsigned int*) siginfo->si_addr;
 
+        if (!gc_managed_heap_space_p((lispobj)pc))
+          lose("Illegal instruction not in lisp: %p [%x]\n", pc, *pc);
+
         inst = *pc;
         trap = inst & 0xff;
-        handle_trap(context,trap);
+        if (trap == trap_Allocation) handle_allocation_trap(context, pc);
+        else handle_trap(context,trap);
     }
     else if ((siginfo->si_code) == ILL_ILLTRP) {
         if (pseudo_atomic_trap_p(context)) {

src/runtime/interrupt.c
@@ -2167,11 +2167,6 @@ handle_trap(os_context_t *context, int trap)
         arch_skip_instruction(context);
         break;
 #endif
-#if defined(LISP_FEATURE_SPARC) && defined(LISP_FEATURE_GENCGC)
-    case trap_Allocation:
-        arch_handle_allocation_trap(context);
-        break;
-#endif
 #if defined(LISP_FEATURE_C_STACK_IS_CONTROL_STACK) && !defined(LISP_FEATURE_WIN32)
     case trap_MemoryFaultEmulation:
         handle_memory_fault_emulation_trap(context);

commit 9eda138f01d578971c05acb3a832a7d807e9f5f9

cheneygc: Nominally unbreak Sparc and 32-bit ARM
Both failed to build for numerous reasons having to do with
undefined variables, missing headers, and "unused variable" warnings.

ARM now crashes immediately in cold-init.
Sparc was broken ever since e4ae12c2 and now seems to work.

src/compiler/arm/cell.lisp
@@ -291,10 +291,14 @@
          (index :scs (any-reg))
          (value :scs (any-reg descriptor-reg)))
   (:arg-types * tagged-num *)
-  (:temporary (:scs (non-descriptor-reg)) temp card)
-  (:temporary (:scs (interior-reg)) lip)
-  (:temporary (:sc non-descriptor-reg) pa-flag)
+  (:temporary (:scs (non-descriptor-reg)) temp #+gencgc card)
+  #+gencgc (:temporary (:scs (interior-reg)) lip)
+  #+gencgc (:temporary (:sc non-descriptor-reg) pa-flag)
   (:generator 10
+    #+cheneygc
+    (progn  (inst sub temp index other-pointer-lowtag)
+            (inst str value (@ object temp)))
+    #+gencgc
     (let ((mask-fixup-label (gen-label))
           (table-fixup-label (gen-label)))
       (inst load-from-label temp lip mask-fixup-label)

src/compiler/arm/macros.lisp
@@ -218,6 +218,7 @@
   (inst b back-label))
 
 (defun allocation (type size lowtag result-tn &key flag-tn stack-allocate-p)
+  #+cheneygc (declare (ignore type))
   ;; Normal allocation to the heap.
   (cond (stack-allocate-p
          (load-csp result-tn)

src/compiler/arm/parms.lisp
@@ -93,6 +93,7 @@
 ;;; Link these as data entries so that we store only the address of the
 ;;; handwritten assembly code in the linkage able, and not a trampoline
 ;;; to the trampoline. The ALLOCATION macro just wants an address.
+#+gencgc
 (setq *linkage-space-predefined-entries* '(("alloc_tramp" t)
                                            ("list_alloc_tramp" t)))
 

src/compiler/sparc/cell.lisp
@@ -290,8 +290,13 @@
          (index :scs (any-reg))
          (value :scs (any-reg descriptor-reg)))
   (:arg-types * tagged-num *)
-  (:temporary (:scs (non-descriptor-reg)) temp card)
+  (:temporary (:scs (non-descriptor-reg)) temp #+gencgc card)
   (:generator 10
+    #+cheneygc
+    (progn (inst add temp index (- other-pointer-lowtag))
+           (inst st value object temp))
+    #+gencgc
+    (progn
     ;; Load the card mask
     (inst li temp (make-fixup "gc_card_table_mask" :foreign-dataref)) ; linkage entry
     (inst ld temp temp) ; address of gc_card_table_mask
@@ -313,7 +318,7 @@
         (inst or temp temp #x40)
         (inst stb temp object byte))
       (inst add temp index (- other-pointer-lowtag))
-      (inst st value object temp))))
+      (inst st value object temp)))))
 
 ;;;; raw instance slot accessors
 

src/runtime/cheneygc.c
@@ -17,6 +17,7 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <signal.h>
+#include <stdlib.h>
 #include "sbcl.h"
 #include "runtime.h"
 #include "os.h"
@@ -93,6 +94,7 @@ lispobj  copy_possibly_large_object(lispobj object, sword_t nwords, int page_typ
  */
 char gc_allocate_dirty = 0;
 
+#if 0
 static void verify_range(int purified, lispobj *base, lispobj *end);
 void show_spaces()
 {
@@ -112,6 +114,7 @@ void verify_heap(int purified)
     if (!purified)
     verify_range(0, current_dynamic_space, dynamic_space_free_pointer);
 }
+#endif
 
 /* Note: The generic GC interface we're implementing passes us a
  * last_generation argument. That's meaningless for us, since we're
@@ -505,6 +508,7 @@ sword_t scav_code_header(lispobj *where, lispobj header)
     return code_total_nwords(code);
 }
 
+#if 0
 static boolean in_stack_range_p(lispobj ptr)
 {
     struct thread* th = all_threads;
@@ -603,3 +607,4 @@ void dump_space_to_file(lispobj* where, lispobj* limit, char* pathname)
     fprintf(f, "--\n");
     fclose(f);
 }
+#endif

src/runtime/sparc-arch.c
@@ -319,8 +319,11 @@ static void sigill_handler(int signal, siginfo_t *siginfo,
 
         inst = *pc;
         trap = inst & 0xff;
+#ifdef LISP_FEATURE_GENCGC
         if (trap == trap_Allocation) handle_allocation_trap(context, pc);
-        else handle_trap(context,trap);
+        else
+#endif
+            handle_trap(context,trap);
     }
     else if ((siginfo->si_code) == ILL_ILLTRP) {
         if (pseudo_atomic_trap_p(context)) {