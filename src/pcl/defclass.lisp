afb26af7530fee8ebbd72cec5c87a29374ea1784
Charles Zhang <charleszhang99@yahoo.com>
2022-01-18 04:38:38 -0800

    Delete early-class.lisp.
    Nothing there is actually needed early. Put the stuff into plain defclass.

--- src/pcl/defclass.lisp
+++ src/pcl/defclass.lisp
@@ -21,6 +21,72 @@
 ;;;; warranty about the software, its performance or its conformity to any
 ;;;; specification.
 
+(in-package "SB-KERNEL")
+
+(defun call-with-defining-class (kind name thunk)
+  (declare (ignorable kind name))
+  (with-single-package-locked-error
+      (:symbol name "defining ~S as a ~(~A~)" kind)
+    (funcall thunk)))
+
+(defun preinform-compiler-about-class-type (name forthcoming-info)
+  ;; Unless the type system already has an actual type attached to
+  ;; NAME (in which case (1) writing a placeholder value over that
+  ;; actual type as a compile-time side-effect would probably be a bad
+  ;; idea and (2) anyway we don't need to modify it in order to make
+  ;; NAME be recognized as a valid type name)
+  (when (and forthcoming-info (not (info :type :kind name)))
+    ;; Tell the compiler to expect a class with the given NAME, by
+    ;; writing a kind of minimal placeholder type information. This
+    ;; placeholder will be overwritten later when the class is
+    ;; defined.
+    (setf (info :type :kind name) :forthcoming-defclass-type)))
+
+(symbol-macrolet
+    ((reader-function-type (specifier-type '(function (t) t)))
+     (writer-function-type (specifier-type '(function (t t) t))))
+  (flet ((proclaim-ftype-for-name (kind name type)
+           (ecase kind
+             (condition
+              (proclaim `(ftype ,(type-specifier type) ,name)))
+             (class
+              (when (eq (info :function :where-from name) :assumed)
+                (sb-c:proclaim-ftype name type nil :defined))))))
+
+    (defun preinform-compiler-about-accessors (kind readers writers)
+      (flet ((inform (names type)
+               (mapc (lambda (name) (proclaim-ftype-for-name kind name type))
+                     names)))
+        (inform readers reader-function-type)
+        (inform writers writer-function-type)))
+
+    (defun preinform-compiler-about-slot-functions (kind slots)
+      (flet ((inform (slots key type)
+               (mapc (lambda (slot)
+                       (let ((name (funcall key slot)))
+                         (proclaim-ftype-for-name kind name type)))
+                     slots)))
+        (inform slots #'sb-pcl::slot-reader-name reader-function-type)
+        (inform slots #'sb-pcl::slot-boundp-name reader-function-type)
+        (inform slots #'sb-pcl::slot-writer-name writer-function-type)))))
+
+(defun %%compiler-defclass (name readers writers slots)
+  ;; ANSI says (Macro DEFCLASS, section 7.7) that DEFCLASS, if it
+  ;; "appears as a top level form, the compiler must make the class
+  ;; name be recognized as a valid type name in subsequent
+  ;; declarations (as for deftype) and be recognized as a valid class
+  ;; name for defmethod parameter specializers and for use as the
+  ;; :metaclass option of a subsequent defclass."
+  (preinform-compiler-about-class-type name t)
+  (preinform-compiler-about-accessors 'class readers writers)
+  (preinform-compiler-about-slot-functions 'class slots))
+
+(defun %compiler-defclass (name readers writers slots)
+  (call-with-defining-class
+   'class name
+   (lambda ()
+     (%%compiler-defclass name readers writers slots))))
+
 (in-package "SB-PCL")
 
 ;;;; DEFCLASS macro and close personal friends
