sbcl/sbcl

commit 26bd73ecbd7af2473ff97bfbdc7eae9e39f54ba4

1.0.13.37: skip compilation of null and non-symbol toplevel atoms
 * Not a performance consideration, but rather part of tidying up code
   paths that lead DEBUG-NAME to be called with NIL as the second
   argument (in IR1-TOPLEVEL the form becomes part of the
   TOP-LEVEL-FORM debug-name.)

src/compiler/main.lisp
@@ -870,19 +870,21 @@
 ;;; *TOPLEVEL-LAMBDAS* instead.
 (defun convert-and-maybe-compile (form path)
   (declare (list path))
-  (if (fopcompilable-p form)
-      (let ((*fopcompile-label-counter* 0))
-        (fopcompile form path nil))
-      (let* ((*top-level-form-noted* (note-top-level-form form t))
-             (*lexenv* (make-lexenv
-                        :policy *policy*
-                        :handled-conditions *handled-conditions*
-                        :disabled-package-locks *disabled-package-locks*))
-             (tll (ir1-toplevel form path nil)))
-        (if (eq *block-compile* t)
-            (push tll *toplevel-lambdas*)
-            (compile-toplevel (list tll) nil))
-        nil)))
+  (let ((*top-level-form-noted* (note-top-level-form form t)))
+    ;; Don't bother to compile simple objects that just sit there.
+    (when (and form (or (symbolp form) (consp form)))
+      (if (fopcompilable-p form)
+         (let ((*fopcompile-label-counter* 0))
+           (fopcompile form path nil))
+         (let ((*lexenv* (make-lexenv
+                          :policy *policy*
+                          :handled-conditions *handled-conditions*
+                          :disabled-package-locks *disabled-package-locks*))
+               (tll (ir1-toplevel form path nil)))
+           (if (eq *block-compile* t)
+               (push tll *toplevel-lambdas*)
+               (compile-toplevel (list tll) nil))
+           nil)))))
 
 ;;; Macroexpand FORM in the current environment with an error handler.
 ;;; We only expand one level, so that we retain all the intervening

version.lisp-expr
@@ -17,4 +17,4 @@
 ;;; checkins which aren't released. (And occasionally for internal
 ;;; versions, especially for internal versions off the main CVS
 ;;; branch, it gets hairier, e.g. "0.pre7.14.flaky4.13".)
-"1.0.13.36"
+"1.0.13.37"

commit 86a8696040fcf2c3e02bced434b8b4c550106e08

Fix 14 year old typo messing up lexenv computations.
26bd73ecbd7af2473ff97bfbdc7eae9e39f54ba4 accidentally changed a LET*
to a LET. This made LEXENVs get computed wrong (specifically anything
with *handled-conditions* and *policy*, as the LET binding for the
LEXENV essentially became effectless), so that

(declaim (optimize debug)
         (muffle-conditions compiler-note))

(defun foo ()
  (declare (speed 3))
  (+ x y))

improperly signalled a condition.

This bug was hidden on debug = 1 because the fopcompiler, which is not
enabled on high debug, does hide the compiler note. (I believe because
the fopcompiler ignores lexenvs completely, and it must have mutated
NULL-LEXENV directly, thereby accidentally giving the next few tlfs
the right value for *HANDLED-CONDITIONS* via LEXENV inheritance,
instead of actually using *HANDLED-CONDITIONS*). I wonder how many
other bugs were lurking because of this oversight, and how many
workarounds were introduced in the meantime.

src/compiler/main.lisp
@@ -1055,11 +1055,11 @@ necessary, since type inference may take arbitrarily long to converge.")
              #-sb-xc-host (fopcompilable-p form expand))
         (let ((*fopcompile-label-counter* 0))
           (fopcompile form path nil expand))
-        (let ((*lexenv* (make-lexenv
-                         :policy *policy*
-                         :handled-conditions *handled-conditions*
-                         :disabled-package-locks *disabled-package-locks*))
-              (tll (ir1-toplevel form path nil)))
+        (let* ((*lexenv* (make-lexenv
+                          :policy *policy*
+                          :handled-conditions *handled-conditions*
+                          :disabled-package-locks *disabled-package-locks*))
+               (tll (ir1-toplevel form path nil)))
           (if (eq (block-compile *compilation*) t)
               (push tll (toplevel-lambdas *compilation*))
               (compile-toplevel (list tll) nil))

src/runtime/monitor.c
@@ -139,7 +139,7 @@ void save_gc_crashdump(char *pathname,
     total_npages = VARYOBJ_SPACE_SIZE / IMMOBILE_CARD_BYTES;
     int n_bitmap_elts = ALIGN_UP(total_npages, 32) / 32;
     write(fd, varyobj_page_touched_bits, n_bitmap_elts * sizeof (int));
-    write(fd, varyobj_pages, total_npages * sizeof (int));  
+    write(fd, varyobj_pages, total_npages * sizeof (int));
 #endif
     struct crash_thread_preamble thread_preamble;
     for_each_thread(th) {

tests/compiler.test.sh
@@ -265,6 +265,15 @@ EOF
 fail_on_condition_during_compile sb-ext:compiler-note $tmpfilename
 
 cat > $tmpfilename <<EOF
+    (declaim (optimize debug)
+             (muffle-conditions compiler-note))
+    (defun foo (x y)
+      (declare (optimize speed))
+      (+ x y))
+EOF
+fail_on_condition_during_compile sb-ext:compiler-note $tmpfilename
+
+cat > $tmpfilename <<EOF
     (declaim (muffle-conditions compiler-note))
     (defun foo (x y)
       (declare (unmuffle-conditions compiler-note))