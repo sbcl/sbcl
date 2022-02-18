99a64531a1714a7be12b1f2baf165215f91533ff
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2022-02-18 13:55:11 +0100

    tests: Skip (:hash-cache :interrupt) on all safepoint builds
    With safepoints, the test hangs at least on Darwin and Linux.

--- tests/hash-cache.pure.lisp
+++ tests/hash-cache.pure.lisp
@@ -1,7 +1,7 @@
 ;;; FIXME: Since timeouts do not work on Windows this would loop
 ;;; forever.
 (with-test (:name (:hash-cache :interrupt)
-            :skipped-on (or :win32 (:and :darwin :sb-safepoint)))
+            :skipped-on (or :win32 :sb-safepoint))
   (let* ((type1 (random-type 500))
          (type2 (random-type 500))
          (wanted (subtypep type1 type2)))
@@ -10,7 +10,7 @@
         (sb-ext:schedule-timer (sb-ext:make-timer
                                 (lambda ()
                                   (assert (eq wanted (subtypep type1 type2)))
-                                    (return-from foo)))
+                                  (return-from foo)))
                                0.05)
         (loop
            (assert (eq wanted (subtypep type1 type2))))))))
