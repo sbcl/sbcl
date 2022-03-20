sbcl/sbcl

commit 6720e95f7e627cda68426c2d491d21d09426c891

substitute-single-use-lvar: only check ARG to be single valued.
If it goes to a CAST.

src/compiler/ir1opt.lisp
@@ -2116,10 +2116,6 @@
              ;; we should not change lifetime of unknown values lvars
              (cast
               (and (type-single-value-p (lvar-derived-type arg))
-                   (multiple-value-bind (pdest pprev)
-                       (principal-lvar-end lvar)
-                     (declare (ignore pdest))
-                     (lvar-single-value-p pprev))
                    ;; CASTs can disappear, don't substitute if
                    ;; DEST-LVAR has other uses
                    (block nil

commit 5b0e47d9e9bc5532b9d68ab1e10f2481362ba255

Fix substitute-single-use-lvar around casts and multiple values.
Caused by the recent 6720e95. Check that the lvar is used immediately.
A better solution for handling dx/unknown values intertwining is
needed, otherwise it disables optimizations even when there's no dx
values around.

Fixes lp#1965653

src/compiler/ir1opt.lisp
@@ -2116,6 +2116,12 @@
              ;; we should not change lifetime of unknown values lvars
              (cast
               (and (type-single-value-p (lvar-derived-type arg))
+                   (or
+                    (let-var-immediately-used-p ref var arg)
+                    (multiple-value-bind (pdest pprev)
+                        (principal-lvar-end lvar)
+                      (declare (ignore pdest))
+                      (lvar-single-value-p pprev)))
                    ;; CASTs can disappear, don't substitute if
                    ;; DEST-LVAR has other uses
                    (block nil

tests/compiler-2.pure.lisp
@@ -3636,3 +3636,13 @@
       (x #.#'list))))~%"))
     (with-scratch-file (fasl "fasl")
       (compile-file lisp :output-file fasl))))
+
+(with-test (:name :substitute-single-use-lvar-mv-cast)
+  (checked-compile-and-assert
+      ()
+      `(lambda ()
+         (let ((r (random 10))
+               (x (list 1)))
+           (declare (special x)
+                    (dynamic-extent x))
+           (throw 'c (the (integer 0 10) r))))))