476b23cc45edac042d9e321fb616a18043021d16
Christophe Rhodes <csr21@cantab.net>
2022-04-10 11:18:17 +0100

    Define FUNCTION-LAMBDA-EXPRESSION earlier
    Now that we store a lambda expression in the LEXENV, as opposed to
    computing one on demand, conversion of interpreter environments must
    generate such a lambda when converting their environments to a
    compiler LEXENV; the easiest way for them to do that is to call
    FUNCTION-LAMBDA-EXPRESSION themselves.  This means that it needs to be
    defined early enough to support uses of MACROLET in warm init.

--- src/code/describe.lisp
+++ src/code/describe.lisp
@@ -19,46 +19,6 @@
         name
         class)))
 
-;;;; the ANSI interface to function names (and to other stuff too)
-;;; Note: this function gets called by the compiler (as of 1.0.17.x,
-;;; in MAYBE-INLINE-SYNTACTIC-CLOSURE), and so although ANSI says
-;;; we're allowed to return NIL here freely, it seems plausible that
-;;; small changes to the circumstances under which this function
-;;; returns non-NIL might have subtle consequences on the compiler.
-;;; So it might be desirable to have the compiler not rely on this
-;;; function, eventually.
-(defun function-lambda-expression (fun)
-  "Return (VALUES DEFINING-LAMBDA-EXPRESSION CLOSURE-P NAME), where
-  DEFINING-LAMBDA-EXPRESSION is NIL if unknown, or a suitable argument
-  to COMPILE otherwise, CLOSURE-P is non-NIL if the function's definition
-  might have been enclosed in some non-null lexical environment, and
-  NAME is some name (for debugging only) or NIL if there is no name."
-  (declare (type function fun))
-  (etypecase fun
-    (interpreted-function
-     #+sb-eval
-     (let ((name (sb-eval:interpreted-function-name fun))
-           (lambda-list (sb-eval:interpreted-function-lambda-list fun))
-           (declarations (sb-eval:interpreted-function-declarations fun))
-           (body (sb-eval:interpreted-function-body fun)))
-       (values `(lambda ,lambda-list
-                  ,@(when declarations `((declare ,@declarations)))
-                  ,@body)
-               t name))
-     #+sb-fasteval
-     (sb-interpreter:fun-lambda-expression fun))
-    (function
-     (let ((name (%fun-name fun))
-           (fun (%fun-fun fun)))
-       (acond ((%simple-fun-lexpr fun)
-               (values it t name))
-              ((legal-fun-name-p name)
-               (let ((exp (fun-name-inline-expansion name)))
-                 ;; Isn't this (NOT EXP) wrong if it's a syntactic closure?
-                 (values exp (not exp) name)))
-              (t
-               (values nil t name)))))))
-
 ;;; Prints X on a single line, limiting output length by *PRINT-RIGHT-MARGIN*
 ;;; -- good for printing object parts, etc.
 (defun prin1-to-line (x &key (columns 1) (reserve 0))
