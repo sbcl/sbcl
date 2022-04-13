ad35be781b477fe27eda6e0452376b63e557e1dc
Charles Zhang <charleszhang99@yahoo.com>
2022-04-13 02:06:47 -0700

    Remove some #-sb-xc-host.
    Just declare the values declaration.

--- src/code/cross-early.lisp
+++ src/code/cross-early.lisp
@@ -36,6 +36,8 @@
 
 (declaim (declaration explicit-check always-bound))
 
+(declaim (declaration values))
+
 (defgeneric sb-xc:make-load-form (obj &optional env))
 
 ;;; Restore normalcy of MOD and RATIONAL as type specifiers.
