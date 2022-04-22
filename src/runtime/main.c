5b60c19e4a9c258a3b46604c3092e9736144e92e
Douglas Katzman <dougk@google.com>
2022-04-21 20:15:45 -0400

    Always allow gdb attachment if #+(and linux sb-devel)
    regardless of /proc/sys/kernel/yama/ptrace_scope

--- src/runtime/main.c
+++ src/runtime/main.c
@@ -1,7 +1,13 @@
 #include <interr.h>
+#if defined LISP_FEATURE_SB_DEVEL && defined LISP_FEATURE_LINUX
+#include <sys/prctl.h>
+#endif
 
 int main(int argc, char *argv[], char *envp[])
 {
+#if defined LISP_FEATURE_SB_DEVEL && defined LISP_FEATURE_LINUX
+    prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY);
+#endif
     extern int initialize_lisp(int argc, char *argv[], char *envp[]);
     initialize_lisp(argc, argv, envp);
     lose("unexpected return from initial thread in main()");
