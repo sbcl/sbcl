796aad2ab7c2ab3e2f3f5ee42fd81a7bf5c5af27
Douglas Katzman <dougk@google.com>
2022-05-17 22:16:06 -0400

    Make autoclose test pass in evaluator-mode interpret

--- tests/autoclose-stream.impure.lisp
+++ tests/autoclose-stream.impure.lisp
@@ -5,18 +5,77 @@
 ;;; don't know how to check validity of a HANDLE in the win32 api
 #-unix (sb-ext:exit :code 104)
 
-(defvar *streams*
-  (let (l)
-    (dotimes (i 6 l)
-      (setq l (nconc (list (open "test-util.lisp")
-                           (open "assertoid.lisp"))
-                     l)))))
-(defvar *fds* (mapcar 'sb-impl::fd-stream-fd *streams*))
-(dolist (fd *fds*)
+(defun fd-has-finalizer-p (fd)
+  ;; Finalizer function can be:
+  ;; 1. a function - finalizer that was not created with :DONT-SAVE.
+  ;; 2. a single list of a function - a finalizer that was created with :DONT-SAVE
+  ;; 3. a vector - more than one finalizer, each being form 1 or form 2.
+  (flet ((checkit (thing)
+           (when (and (sb-kernel:closurep thing)
+                      (eql (sb-kernel:%closure-index-ref thing 0) fd))
+             (let ((underlying (sb-kernel:%closure-fun thing)))
+               (when (and (sb-kernel:simple-fun-p underlying)
+                          (equal (sb-kernel:%simple-fun-name underlying)
+                                 '(lambda () :in sb-sys:make-fd-stream)))
+                 (return-from fd-has-finalizer-p t))))))
+    (let ((v sb-impl::**finalizer-store**))
+      (loop for i from 3 below (length v)
+            do
+         (let ((entry (aref v i)))
+           (typecase entry
+             (list (checkit (car entry)))
+             (function (checkit entry))
+             (vector
+              (sb-int:dovector (entry entry)
+                (checkit (if (listp entry) (car entry) entry))))))))))
+
+(defvar *fds*)
+(defun threadfun ()
+  (let (streams)
+    (dotimes (i 6)
+      (setq streams (nconc (list (open "test-util.lisp")
+                                 (open "assertoid.lisp"))
+                           streams)))
+    (dolist (stream streams)
+      (assert (fd-has-finalizer-p (sb-impl::fd-stream-fd stream))))
+    (setq *fds*
+          (mapcar (lambda (x)
+                    (cons (sb-impl::fd-stream-fd x) (make-weak-pointer x)))
+                  streams))
+    (dolist (fd *fds*)
+      (let ((errno (nth-value 1 (sb-unix:unix-lseek (car fd) 0 sb-unix:l_incr))))
+        (assert (zerop errno))))))
+
+(defun assert-invalid-file-descriptor (fd)
   (let ((errno (nth-value 1 (sb-unix:unix-lseek fd 0 sb-unix:l_incr))))
-    (assert (zerop errno))))
-(setq *streams* nil)
-(sb-sys:scrub-control-stack)
+    (assert (= errno sb-unix:ebadf))))
+
+;;; A separate thread should always leave this thread with
+;;; a clean stack. But the interpreter might retain garbage.
+;;; So if we think garbage might be retained, don't require
+;;; that finalization happened, but do check that if the weak-pointer
+;;; to a stream got smashed, then the FD is invalid.
+#+(or :interpreter (not :sb-thread))
+(progn (threadfun)
+       ;; This FMAKUNBOUND definitely seems to be needed
+       ;; for sb-fasteval.
+       (fmakunbound 'threadfun)
+       (sb-sys:scrub-control-stack)
+       (gc)
+       ;; I have no idea how this write-to-string is helping,
+       ;; but without it the test becomes unreliable.
+       (write-to-string *fds*)
+       (dolist (entry *fds*)
+         (let ((fd (car entry))
+               (wp (cdr entry)))
+           (unless (weak-pointer-value wp)
+             (assert-invalid-file-descriptor fd))))
+       (sb-ext:exit :code 104))
+
+(compile 'threadfun)
+
+(let ((thread (sb-thread:make-thread #'threadfun)))
+  (sb-thread:join-thread thread))
 (gc)
 ;;; The file descriptors should cease to be valid.
 ;;; Obviously it wouldn't do any good to hold on to the lisp streams
@@ -26,7 +85,8 @@
 (with-test (:name :finalizer-closes-fdstream)
   (let ((countbad 0))
     (dolist (fd *fds*)
-      (let ((errno (nth-value 1 (sb-unix:unix-lseek fd 0 sb-unix:l_incr))))
+      (let ((errno (nth-value 1 (sb-unix:unix-lseek (car fd)
+                                                    0 sb-unix:l_incr))))
         (unless (= errno 0)
           (assert (= errno sb-unix:ebadf))
           (incf countbad))))
