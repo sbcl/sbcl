0dd7ed7875d1fd444b39071ea4dbb3777db154b5
Stas Boukarev <stassats@gmail.com>
2022-05-24 20:13:53 +0300

    Fix a test for -sb-unicode.

--- tests/seq.pure.lisp
+++ tests/seq.pure.lisp
@@ -672,9 +672,12 @@
     (check (find x y :test #'=) (or number null))
     (check (find x y :key #'car :test #'=) list)
     (check (find x (the vector y) :key #'car) list)
-    ;; (check (find-if #'evenp y) (or integer null))
+    #-sbcl
+    (check (find-if #'evenp y) (or integer null))
     (check (find-if #'evenp (the list y) :key #'car) list)
-    (check (find x (the simple-base-string y)) (or base-char null))))
+    (check (find x (the (simple-array character (*)) y)) (or character null))
+    #-sbcl
+    (check (find x (the simple-string y)) (or character null))))
 
 (with-test (:name :position-type-derive)
   (macrolet
