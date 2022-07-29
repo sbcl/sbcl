sbcl/sbcl

commit a0329ac59e6cb44d98eeafad6ffe8f20e8a7f918

Shake out some more symbols

make-target-2-load.lisp
@@ -419,14 +419,21 @@ Please check that all strings which were not recognizable to the compiler
            (let ((s (string symbol))) (and (search "THREAD-" s) (search "-SLOT" s)))
            (search "-OFFSET" (string symbol))
            (search "-TN" (string symbol))))
+      (#.(find-package "SB-ALIEN")
+       (or (eq accessibility :external) (eq symbol 'sb-alien::alien-callback-p)))
       ((#.(find-package "SB-C")
         #.(find-package "SB-ASSEM")
         #.(find-package "SB-DISASSEM")
         #.(find-package "SB-IMPL")
+        #.(find-package "SB-LOOP")
+        #.(find-package "SB-FORMAT")
         #.(find-package "SB-UNIX")
         #.(find-package "SB-PCL")
         #.(find-package "SB-MOP")
         #.(find-package "SB-PRETTY")
+        #.(find-package "SB-REGALLOC")
+        #.(find-package "SB-SYS")
+        #.(find-package "SB-UNICODE")
         #.(find-package "SB-BROTHERTREE")
         #.(find-package "SB-KERNEL"))
        ;; Assume all and only external symbols must be retained

commit 52418c9a04021a85e4dfd38a765dd34626be1606

Retain some internal symbols of SB-LOOP for CLSQL
This fixes a regression from commit a0329ac59e, after which CLSQL
couldn't be built anymore because it attempts to use certain internal
symbols from SB-LOOP.

make-target-2-load.lisp
@@ -425,7 +425,6 @@ Please check that all strings which were not recognizable to the compiler
         #.(find-package "SB-ASSEM")
         #.(find-package "SB-DISASSEM")
         #.(find-package "SB-IMPL")
-        #.(find-package "SB-LOOP")
         #.(find-package "SB-FORMAT")
         #.(find-package "SB-UNIX")
         #.(find-package "SB-PCL")
@@ -438,6 +437,11 @@ Please check that all strings which were not recognizable to the compiler
         #.(find-package "SB-KERNEL"))
        ;; Assume all and only external symbols must be retained
        (eq accessibility :external))
+      (#.(find-package "SB-LOOP")
+       (or (eq accessibility :external)
+           ;; Retain some internals to keep CLSQL working.
+           (member symbol '(sb-loop::*loop-epilogue*
+                            sb-loop::add-loop-path))))
       (#.(find-package "SB-THREAD")
        (or (eq accessibility :external)
            ;; for some reason a recent change caused the tree-shaker to drop MAKE-SPINLOCK