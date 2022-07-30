46877b84314c8afd445da257e0fa8755600cc677
Douglas Katzman <dougk@google.com>
2022-07-30 16:05:53 -0400

    Fix crashing test

--- tests/hide-packages.test.sh
+++ tests/hide-packages.test.sh
@@ -11,6 +11,27 @@ CLOBBER_INTERNAL_FEATURES="(handler-bind ((simple-error #'continue))
   (alien-funcall (extern-alien \"move_rospace_to_dynamic\" (function void int)) 1)
   (sb-kernel:set-symbol-global-value (eval ''sb-impl:+internal-features+) nil))"
 
+### Apparently the finalizer thread must be stopped before running these tests or else
+### it can crash due to the highly unorthodox use of move_rospace_to_dynamic().
+### Somehow the finalizer thread dereferenced data from the R/O space
+### after that space was reduced to 0 size. Amazingly the finalizer hadn't gotten out
+### of its new_thread_trampoline nor assigned anything to 'current_thread'.
+### parallel-exec was able to elicit about 3 failures in 20 prior to the fix.
+###
+### Backtrace from gdb:
+### ...
+### #8  0x000055d729cf8d0a in call_lossage_handler () at interr.c:116
+### #9  0x000055d729cf9127 in corruption_warning_and_maybe_lose (
+###     fmt=fmt@entry=0x55d729d2a660 "Received signal %d @ %lx in non-lisp tid %d, resignaling to a lisp thread.") at interr.c:202
+### #10 0x000055d729cf97bc in resignal_to_lisp_thread (signal=signal@entry=11,
+###     context=context@entry=0x7f9a3e216880) at interrupt.c:218
+### #11 0x000055d729cf9843 in low_level_handle_now_handler (signal=11, info=0x7f9a3e2169b0,
+###     void_context=0x7f9a3e216880) at interrupt.c:1927
+### #12 <signal handler called>
+### #13 new_thread_trampoline (arg=<optimized out>) at src/runtime/runtime.h:160
+### #14 0x00007f9a3ed20d80 in start_thread (arg=0x7f9a3e217640) at pthread_create.c:481
+### #15 0x00007f9a3eaf676f in clone () at ../sysdeps/unix/sysv/linux/x86_64/clone.S:95
+
 ### Test 1: assert that no package name string has a reference from any
 ### heap object aside from the package itself.
 ### Because SAVE-LISP-AND-DIE coalesces strings, this test provides confidence
@@ -21,6 +42,7 @@ run_sbcl <<EOF
 #+cheneygc (exit) ; not much can be done for cheney to make this pass
 #+sb-devel (exit) ; doesn't pass either
 
+#+sb-thread (sb-impl::finalizer-thread-stop)
 (defvar *weak-ptrs* nil)
 (progn
   ;; Remove features that are also names of packages.
@@ -66,6 +88,7 @@ run_sbcl <<EOF
 
 ;;; Does not pass with interpreter
 (setq sb-ext:*evaluator-mode* :compile)
+#+sb-thread (sb-impl::finalizer-thread-stop)
 
 ;;; Avoid accidental enlivenment of strings - the forms in this file must
 ;;; obscure the spellings of package name strings or else they can end up
