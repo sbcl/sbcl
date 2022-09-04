5206ce54830a992a864f963334d5e653d7d8c198
Douglas Katzman <dougk@google.com>
2022-09-04 13:26:58 -0400

    Workaround extremely suboptimal alien-funcall

--- tests/sb-md5.impure.lisp
+++ tests/sb-md5.impure.lisp
@@ -1,3 +1,16 @@
 (require :sb-md5)
+
+(let ((stream (make-string-output-stream)))
+  (with-package-iterator (iter "SB-MD5" :internal :external)
+    (loop
+     (multiple-value-bind (flag symbol) (iter)
+       (unless flag (return))
+       (when (and (fboundp symbol) (not (macro-function symbol)))
+         (let ((code (sb-kernel:fun-code-header (fdefinition symbol))))
+           (disassemble code :stream stream)
+           (let ((text (get-output-stream-string stream)))
+             (when (search "ALIEN-FUNCALL" text)
+               (error "Compiler bug on ~S" code)))))))))
+
 (setq run-tests::*allowed-inputs* :any) ; makes random pathnames without aid of WITH-SCRATCH-FILE
 (load "../contrib/sb-md5/md5-tests.lisp")
