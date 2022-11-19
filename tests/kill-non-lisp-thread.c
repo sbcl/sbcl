b1b8b40c73c8413cccbc2bfba25c9cd952929d61
Stas Boukarev <stassats@gmail.com>
2022-11-19 23:24:00 +0300

    kill-non-lisp-thread: don't sleep on the C side as well.
    And catch pthread_kill errors.

--- tests/kill-non-lisp-thread.c
+++ tests/kill-non-lisp-thread.c
@@ -3,20 +3,34 @@
 #include <stdlib.h>  // for exit()
 #include <pthread.h>
 #include <signal.h>
+#include <semaphore.h>
+
+sem_t sem, sem2;
 
 void
 wait_a_bit(void)
 {
-    sleep(5);
+    sem_post(&sem);
+    sem_wait(&sem2);
 }
 
 void
 kill_non_lisp_thread(void)
 {
     pthread_t kid;
+    sem_init(&sem, 0, 0); 
+    sem_init(&sem2, 0, 0); 
     if (pthread_create(&kid, 0, (void *(*)(void *))wait_a_bit, 0) < 0) {
         perror("pthread_create");
         exit(1);
     }
-    pthread_kill(kid, SIGURG);
+
+    sem_wait(&sem);
+
+    if (pthread_kill(kid, SIGURG)) {
+        perror("pthread_kill");
+        sem_post(&sem2);
+        exit(1);
+    }
+    sem_post(&sem2);
 }
