b73b52a6cc3e6034a413579a63faf012fcf2b1c0
Charles Zhang <charleszhang99@yahoo.com>
2022-12-28 19:56:52 +0100

    Remove sb-graph.
    It's bitrotted already, and from the dot files produced in the example
    directory it seems like there's quite a long way to go before it
    produces anything readable. The function IR1-TO-DOT produces dot files
    based directly on the normal trace file output that people use to
    understand IR1, so it seems better just to remove sb-graph, which also
    takes extra time to compile.

--- src/compiler/early-c.lisp
+++ src/compiler/early-c.lisp
@@ -81,7 +81,7 @@
 ;;; Bind this to a stream to capture various internal debugging output.
 (defvar *compiler-trace-output* nil)
 ;;; These are the default, but the list can also include
-;;; :pre-ir2-optimize, :symbolic-asm, and :sb-graph.
+;;; :pre-ir2-optimize, :symbolic-asm.
 (defvar *compile-trace-targets* '(:ir1 :ir2 :vop :symbolic-asm :disassemble))
 (defvar *current-path*)
 (defvar *current-component*)
