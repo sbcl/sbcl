5107bdf03b69d18f5315d72749daed9eaf6ef904
Douglas Katzman <dougk@google.com>
2023-01-08 16:15:22 -0500

    Convert socket-bind tests to WITH-TEST

--- contrib/sb-bsd-sockets/tests.lisp
+++ contrib/sb-bsd-sockets/tests.lisp
@@ -126,11 +126,14 @@
   t)
 
 #+ipv4-support
-(deftest inet-socket-bind
+(test-util:with-test (:name :inet-socket-bind)
   (let* ((tcp (get-protocol-by-name "tcp"))
          (address (make-inet-address "127.0.0.1"))
          (s1 (make-instance 'inet-socket :type :stream :protocol tcp))
-         (s2 (make-instance 'inet-socket :type :stream :protocol tcp)))
+         (s2 (make-instance 'inet-socket :type :stream :protocol tcp))
+         (failure)
+         (got-addrinuse))
+    (format t "~&::: INFO: made sockets~%")
     (unwind-protect
          ;; Given the functions we've got so far, if you can think of a
          ;; better way to make sure the bind succeeded than trying it
@@ -139,19 +142,25 @@
            (socket-bind s1 address 0)
            (handler-case
                (let ((port (nth-value 1 (socket-name s1))))
-                 (socket-bind s2 address port)
+                 (socket-bind s2 address port) ; should fail
                  nil)
-             (address-in-use-error () t)))
+             (address-in-use-error () (setq got-addrinuse t))
+             (condition (c) (setq failure c))))
       (socket-close s1)
-      (socket-close s2)))
-  t)
+      (socket-close s2))
+    (cond (failure (error "BIND failed with ~A" failure))
+          ((not got-addrinuse) (error "Expected ADDRESS-IN-USE err")))))
 
-(deftest inet6-socket-bind
+(test-util:with-test (:name :inet6-socket-bind)
+  (let ((notsupp)
+        (failure)
+        (got-addrinuse))
   (handler-case
       (let* ((tcp (get-protocol-by-name "tcp"))
              (address (make-inet6-address "::1"))
              (s1 (make-instance 'inet6-socket :type :stream :protocol tcp))
              (s2 (make-instance 'inet6-socket :type :stream :protocol tcp)))
+        (format t "~&::: INFO: made sockets~%")
         (unwind-protect
              ;; Given the functions we've got so far, if you can think of a
              ;; better way to make sure the bind succeeded than trying it
@@ -166,13 +175,17 @@
                  (declare (ignore x))
                  (handler-case
                      (let ((port (nth-value 1 (socket-name s1))))
-                       (socket-bind s2 address port)
+                       (socket-bind s2 address port) ; should fail
                        nil)
-                   (address-in-use-error () t))))
+                   (address-in-use-error () (setq got-addrinuse t))
+                   (condition (c) (setq failure c)))))
           (socket-close s1)
           (socket-close s2)))
-    ((or address-family-not-supported protocol-not-supported-error) () t))
-  t)
+    ((or address-family-not-supported protocol-not-supported-error) ()
+      (setq notsupp t)))
+  (cond (notsupp (format t "~&INFO: not supported~%"))
+        (failure (error "BIND failed with ~A" failure))
+        ((not got-addrinuse) (error "Expected ADDRESS-IN-USE err")))))
 
 #+ipv4-support
 (deftest simple-sockopt-test
