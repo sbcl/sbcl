949864cc2cf71635c979be1f8d3334e129ad1cc1
Charles Zhang <charleszhang99@yahoo.com>
2023-05-04 12:14:08 +0200

    Add another case to THREAD-HEADER-SLOTS.
    This is really, really, really bad. This value should derive from the
    list of slots, not some random magic.
    
    But I don't want to tackle this. I think we should just make slots at
    positive offsets everywhere to keep things sane. Is the x86-64 byte
    displacement optimization really *that* important?

--- src/compiler/generic/parms.lisp
+++ src/compiler/generic/parms.lisp
@@ -349,12 +349,14 @@
   #-sb-thread 1024) ; crazy value
 
 ;;; Thread slots accessed at negative indices relative to struct thread.
+;;; FIXME: this is extremely unmaintainable.
 (defconstant thread-header-slots
   ;; This seems to need to be an even number.
   ;; I'm not sure what the constraint on that stems from.
   #+(and x86-64 sb-safepoint) 14 ; the safepoint trap page is at word index -15
   #+(and x86-64 (not sb-safepoint)) 16
-  #-x86-64 0)
+  #+(and (not x86-64) immobile-space) 16
+  #+(and (not x86-64) (not immobile-space)) 0)
 
 #+gencgc
 (progn
