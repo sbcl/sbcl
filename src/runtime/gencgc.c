f03ab492369f2907f91e32bbe2326c02e4836500
Charles Zhang <charleszhang99@yahoo.com>
2023-05-08 20:23:40 +0200

    maybe_pin_code: Keep on-stack immobile code live if needed.
    I am not completely sure where this is actually necessary, but it
    doesn't hurt to make sure.

--- src/runtime/gencgc.c
+++ src/runtime/gencgc.c
@@ -3641,7 +3641,14 @@ dynamic_space_code_from_pc(char *pc)
 static void __attribute__((unused)) maybe_pin_code(lispobj addr) {
     page_index_t page = find_page_index((char*)addr);
 
-    if (page < 0) return;
+    if (page < 0) {
+        // FIXME: Is this actually necessary? The intention is to keep
+        // the on-stack code live, not to pin it, as it's already
+        // immobile.
+        if (immobile_space_p(addr))
+            immobile_space_preserve_pointer((void*)component_ptr_from_pc((char*)addr));
+        return;
+    }
     if (immune_set_memberp(page)) return;
 
     struct code* code = (struct code*)dynamic_space_code_from_pc((char *)addr);
