f53191603fbcf8b8c006a92c01def095eba9c3ce
Charles Zhang <charleszhang99@yahoo.com>
2023-09-07 19:59:32 +0200

    Change expansion of DX-FLET slightly.
    TRULY-DYNAMIC-EXTENT means nothing for closures (fixed-size).

--- src/code/primordial-extensions.lisp
+++ src/code/primordial-extensions.lisp
@@ -16,8 +16,7 @@
 ;;; to CALL-WITH-FOO less ugly.
 (defmacro dx-flet (functions &body forms)
   `(flet ,functions
-     (declare (#+sb-xc-host dynamic-extent #-sb-xc-host truly-dynamic-extent
-               ,@(mapcar (lambda (func) `#',(car func)) functions)))
+     (declare (dynamic-extent ,@(mapcar (lambda (func) `#',(car func)) functions)))
      ,@forms))
 
 ;;; Another similar one.
