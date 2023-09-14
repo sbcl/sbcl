9dcf16fac8ffe6dea7007ddc1bbb765b0a2166c4
Douglas Katzman <dougk@google.com>
2023-09-14 10:21:28 -0400

    Fix a test that fails if verify_gens is enabled

--- tests/save10.test.sh
+++ tests/save10.test.sh
@@ -6,9 +6,17 @@ use_test_subdirectory
 tmpcore=$TEST_FILESTEM.core
 set -e
 
+# This test asserts that preparing read-only space prior to savign a core file
+# does not attempt to rewrite pointers that appear to point to unknown memory.
+# So the test attempts to fabricate such pointers. However it's a bad idea
+# to store into the car of a cons the value
+#  (sb-kernel:%make-lisp-obj sb-vm:instance-pointer-lowtag)
+# as that cons is indistinguishable from a forwarded cons in 32-bit builds,
+# where INSTANCE_POINTER_LOWTAG is 1 and forwarding_pointer_p(x) is '*x == 1'
 run_sbcl <<EOF
   (defvar *x* (list (sb-kernel:%make-lisp-obj sb-vm:fun-pointer-lowtag)
-                    (sb-kernel:%make-lisp-obj sb-vm:instance-pointer-lowtag)))
+                    (sb-kernel:%make-lisp-obj
+                     (logior #x100 sb-vm:instance-pointer-lowtag))))
   (save-lisp-and-die "$tmpcore")
 EOF
 
