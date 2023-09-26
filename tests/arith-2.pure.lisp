f2f6450e730be960e94942d01b74fb4d004b1774
Stas Boukarev <stassats@gmail.com>
2023-09-26 11:03:52 +0300

    overflow-transform-unknown-x: give up if the result always overflows.
    Fixes lp#2037348

--- tests/arith-2.pure.lisp
+++ tests/arith-2.pure.lisp
@@ -208,3 +208,16 @@
                (sb-bignum:%bignum-set-length res 2)))))
     ((-9.223372036854776d18) nil)
     ((9.223372036854776d18) t)))
+
+(with-test (:name :overflow-t-transform)
+  (checked-compile-and-assert
+      (:allow-warnings t)
+      `(lambda (v)
+         (let ((i 0))
+           (flet ((f (i)
+                    (the fixnum i)
+                    (svref v (+ i 26387449082611642302))))
+             (f i)
+             (incf i)
+             (f i)
+             (incf i))))))
