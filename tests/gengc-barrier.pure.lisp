684802b8924fce5ee27848ce258f67a14c1cdcc7
Stas Boukarev <stassats@gmail.com>
2024-04-30 14:51:36 +0300

    Add a test for a9d69aba7.

---
+++ tests/gengc-barrier.pure.lisp
@@ -0,0 +1,26 @@
+;;;; This software is part of the SBCL system. See the README file for
+;;;; more information.
+;;;;
+;;;; While most of SBCL is derived from the CMU CL system, the test
+;;;; files (like this one) were written from scratch after the fork
+;;;; from CMU CL.
+;;;;
+;;;; This software is in the public domain and is provided with
+;;;; absolutely no warranty. See the COPYING and CREDITS files for
+;;;; more information.
+
+#-soft-card-marks
+(invoke-restart 'run-tests::skip-file)
+
+(defun assert-barriers (potential actual fun &rest compile-args)
+  (let* ((old-potential sb-vm::*store-barriers-potentially-emitted*)
+         (old-actual sb-vm::*store-barriers-emitted*))
+    (apply #'checked-compile fun compile-args)
+    (assert (= potential (- sb-vm::*store-barriers-potentially-emitted* old-potential)))
+    (assert (= actual (- sb-vm::*store-barriers-emitted* old-actual)))))
+
+(with-test (:name :rplaca-union-types)
+  (assert-barriers 1 0 
+                   `(lambda (x y)
+                      (when (typep y '(or fixnum null))
+                        (rplaca x y)))))
