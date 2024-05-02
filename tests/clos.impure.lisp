1f82fdf5970a98bd5266531ca0419e8f2956f825
Douglas Katzman <dougk@google.com>
2024-05-03 01:21:43 +0200

    Remove 2 lines of output from test

--- tests/clos.impure.lisp
+++ tests/clos.impure.lisp
@@ -1374,7 +1374,7 @@
   (:method-combination progn))
 (defmethod gf-with-keys-and-invalid-qualifier progn ((x integer) &key b)
   (declare (ignore b))
-  (print (1+ x)))
+  (print (1+ x) (make-broadcast-stream))) ; don't show noise
 ;; I'm guessing this is PROGN but spelled wrong purposely, right?
 (defmethod gf-with-keys-and-invalid-qualifier prong ((x fixnum) &key c) (print c))
 
