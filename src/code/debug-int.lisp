2658b1b7499b8f2cdaafdf30d6740a7596ca0c34
Stas Boukarev <stassats@gmail.com>
2024-05-03 15:25:04 +0300

    Make *uncompacted-fun-maps* synchronized.

--- src/code/debug-int.lisp
+++ src/code/debug-int.lisp
@@ -447,7 +447,7 @@
 ;;; This maps SB!C::COMPILED-DEBUG-FUNs to COMPILED-DEBUG-FUNs, so we
 ;;; can get at cached stuff and not duplicate COMPILED-DEBUG-FUN
 ;;; structures.
-(defvar *compiled-debug-funs* (make-hash-table :test 'eq :weakness :key))
+(defglobal *compiled-debug-funs* (make-hash-table :test 'eq :weakness :key :synchronized t))
 
 ;;; Make a COMPILED-DEBUG-FUN for a SB!C::COMPILER-DEBUG-FUN and its
 ;;; component. This maps the latter to the former in
@@ -2097,7 +2097,7 @@ register."
 
 ;;; a map from packed DEBUG-INFO function maps to unpacked
 ;;; versions thereof
-(defvar *uncompacted-fun-maps* (make-hash-table :test 'eq :weakness :key))
+(defglobal *uncompacted-fun-maps* (make-hash-table :test 'eq :weakness :key :synchronized t))
 
 ;;; Return a FUN-MAP for a given COMPILED-DEBUG-INFO object. If the
 ;;; info is packed, and has not been parsed, then parse it.
