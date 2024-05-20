f961c087e6f27d7cb37235dfe3d333c05ace1e13
Christophe Rhodes <csr21@cantab.net>
2024-05-21 00:26:30 +0100

    Fix the alien struct test to also work in interpret mode
    Make sure to attempt to call the newly-defined alien function, so that
    the alien routines get triggered in the interpreter.

--- tests/alien-struct-by-value.impure.lisp
+++ tests/alien-struct-by-value.impure.lisp
@@ -28,8 +28,9 @@
                                     "alien-struct-by-value.c")))
   (load-shared-object (truename "alien-struct-by-value.so")))
 
-(defmacro assert-unimplemented (def)
-  `(assert-error (eval ',def)))
+(defmacro assert-unimplemented ((&whole def dar name ret &optional (arg nil argp)))
+  `(assert-error (eval '(progn ,def ,(if argp `(with-alien ((x ,(second arg))) (,name (addr x))) '(name))))))
+
 ;;; Tiny struct, alignment 8
 (define-alien-type nil (struct tiny-align-8 (m0 (integer 64))))
 (with-test (:name :struct-by-value-tiny-align-8-args)
