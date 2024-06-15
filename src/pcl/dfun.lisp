6e2af617b5634fd4081ee4d8fa684fcb264cecda
Stas Boukarev <stassats@gmail.com>
2024-06-15 20:45:14 +0300

    Reverse the logic in break-vicious-metacircle.
    Look for subclasses instead of superclasses.

--- src/pcl/dfun.lisp
+++ src/pcl/dfun.lisp
@@ -1113,12 +1113,17 @@ Except see also BREAK-VICIOUS-METACIRCLE.  -- CSR, 2003-05-28
               (index (standard-slot-value/eslotd slotd 'location))
               (type (gf-info-simple-accessor-type arg-info)))
           (when (and method
-                     (memq class
-                           (standard-slot-value/class
-                            (ecase accessor-type
-                              ((reader) (car classes))
-                              ((writer) (cadr classes)))
-                            '%class-precedence-list)))
+                     (let ((method-class (ecase accessor-type
+                                           ((reader) (car classes))
+                                           ((writer) (cadr classes)))))
+                       (or (eq method-class class)
+                           ;; SUBTYPEP doesn't work because it calls the CLASS-WRAPPER GF.
+                           (block nil
+                             (sb-kernel::do-subclassoids ((subclassoid layout)
+                                                          (layout-classoid (standard-slot-value/class class 'wrapper)))
+                               (declare (ignore layout))
+                               (when (eq method-class (classoid-pcl-class subclassoid))
+                                 (return t)))))))
             (return-from break-vicious-metacircle
               (values index (list method) type index)))))))
   (error "~@<vicious metacircle:  The computation of an ~
