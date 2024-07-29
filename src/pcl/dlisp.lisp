sbcl/sbcl

commit 8c3b51059696140154f5ac1780fcc29be0a624f4

Handle SLOT-MAKUNBOUND like other slot functions
This allows removal of the coupling between dispatch functions of
SLOT-MAKUNBOUND-USING-CLASS and SLOT-BOUNDP-USING-CLASS.

Condition slots can be unbound; make the boundp and makunbound logic
explicit, and integrate it into the CLOS mechanisms.

Structure non-raw slots can also (rarely) be unbound, when a
by-order-of-arguments constructor is defined with the initialization
coming from an &AUX variable with no default.  Integrate this
knowledge into the CLOS mechanisms, avoiding handling boundness of
slots in the code fragments that CLOS uses (while preserving existing
behaviour of the accessor functions defined by DEFSTRUCT.)

Add tests of various expected behaviours of SLOT-BOUNDP and friends.
These tests are not complete, given the number of different ways CLOS
can walk and optimize code and that instances of structures,
conditions and classes can be constructed, but they hopefully exercise
some previously-untested pathways.

src/pcl/dlisp.lisp
@@ -116,6 +116,9 @@
 (defun emit-one-class-writer (class-slot-p)
   (emit-reader/writer :writer 1 class-slot-p))
 
+(defun emit-one-class-makunbound (class-slot-p)
+  (emit-reader/writer :makunbound 1 class-slot-p))
+
 (defun emit-two-class-reader (class-slot-p)
   (emit-reader/writer :reader 2 class-slot-p))
 
@@ -124,6 +127,9 @@
 
 (defun emit-two-class-writer (class-slot-p)
   (emit-reader/writer :writer 2 class-slot-p))
+
+(defun emit-two-class-makunbound (class-slot-p)
+  (emit-reader/writer :makunbound 2 class-slot-p))
 
 ;;; --------------------------------
 
@@ -136,6 +142,9 @@
 (defun emit-one-index-writers (class-slot-p)
   (emit-one-or-n-index-reader/writer :writer nil class-slot-p))
 
+(defun emit-one-index-makunbounds (class-slot-p)
+  (emit-one-or-n-index-reader/writer :makunbound nil class-slot-p))
+
 (defun emit-n-n-readers ()
   (emit-one-or-n-index-reader/writer :reader t nil))
 
@@ -144,6 +153,9 @@
 
 (defun emit-n-n-writers ()
   (emit-one-or-n-index-reader/writer :writer t nil))
+
+(defun emit-n-n-makunbounds ()
+  (emit-one-or-n-index-reader/writer :makunbound t nil))
 
 ;;; --------------------------------
 
@@ -205,7 +217,7 @@
         (closure-variables ())
         (read-form (emit-slot-read-form class-slot-p 'index 'slots)))
     (ecase reader/writer
-      ((:reader :boundp)
+      ((:reader :boundp :makunbound)
        (setq instance (dfun-arg-symbol 0)
              arglist  (list instance)))
       (:writer (setq instance (dfun-arg-symbol 1)
@@ -242,6 +254,9 @@
                 (:boundp
                  `((let ((value ,read-form))
                      (return-from access (not (unbound-marker-p value))))))
+                (:makunbound
+                 `(progn (setf ,read-form +slot-unbound+)
+                         ,instance))
                 (:writer
                  `((return-from access (setf ,read-form ,(car arglist)))))))
           (funcall miss-fn ,@arglist))))))
@@ -263,6 +278,7 @@
     (ecase reader/writer
       (:reader (emit-boundp-check read-form miss-fn arglist))
       (:boundp `(not (unbound-marker-p ,read-form)))
+      (:makunbound `(progn (setf ,read-form +slot-unbound+) ,(car arglist)))
       (:writer `(setf ,read-form ,(car arglist))))))
 
 (defmacro emit-reader/writer-macro (reader/writer 1-or-2-class class-slot-p)
@@ -278,7 +294,7 @@
                                           class-slot-p)
   (multiple-value-bind (arglist metatypes)
       (ecase reader/writer
-        ((:reader :boundp)
+        ((:reader :boundp :makunbound)
          (values (list (dfun-arg-symbol 0))
                  '(standard-instance)))
         (:writer (values (list (dfun-arg-symbol 0) (dfun-arg-symbol 1))

doc/manual/beyond-ansi.texinfo
@@ -11,6 +11,7 @@ it still has quite a few.  @xref{Contributed Modules}.
 * Package-Local Nicknames::
 * Package Variance::
 * Garbage Collection::
+* Slot Access::
 * Metaobject Protocol::
 * Extensible Sequences::
 * Support For Unix::
@@ -279,6 +280,59 @@ The @code{:print nil} argument is a bit different:
 There is no output on @code{cl:*standard-output*} and the return value
 is a single path for the target object @t{"my string"}. As before, the
 path shows the symbol and the three cons cells.
+
+@node Slot Access
+@comment  node-name,  next,  previous,  up
+@section Slot Access
+
+@findex @cl{slot-value}
+@findex @setf{@cl{slot-value}}
+@findex @cl{slot-boundp}
+@findex @cl{slot-makunbound}
+The slot access functions @code{slot-value}, @code{(setf slot-value)},
+@code{slot-boundp} and @code{slot-makunbound} are defined to function
+as expected on conditions (of metaclass @code{condition-class}) and,
+with some limitations, on structures (of metaclass
+@code{structure-class}).
+
+For structures:
+
+@itemize
+@item
+@findex @cl{defstruct}
+The name of a slot for the purposes of the slot access functions is
+the symbol used as the slot-name in the slot-description in the
+@code{defstruct} form;
+
+@item
+@findex @cl{slot-unbound}
+@findex @cl{slot-value}
+@findex @cl{slot-boundp}
+@code{slot-value} and @code{slot-boundp} function as expected,
+including (for @code{slot-value}) calling and respecting the return
+value of @code{slot-unbound} if the slot is unbound;
+
+@item
+@findex @setf{@cl{slot-value}}
+@code{(setf slot-value)} functions as expected, including performing
+type checks to verify that the new value is of an appropriate type for
+the slot;
+
+@item
+@findex @cl{slot-makunbound}
+@code{slot-makunbound} makes the slot be unbound only when the slot
+corresponds to an @code{&aux} argument with no default in a
+by-order-of-arguments (BOA) constructor.  In all other cases calling
+@code{slot-makunbound} on a structure signals an error.
+
+@item
+@findex @cl{slot-missing}
+If any of the slot access functions is called with a structure
+instance which does not have a slot of the given name,
+@code{slot-missing} is called and the return value of the effective
+method, if any, is respected.
+
+@end itemize
 
 @node Metaobject Protocol
 @comment  node-name,  next,  previous,  up

src/code/class.lisp
@@ -41,6 +41,7 @@
                   *the-class-global-reader-method*
                   *the-class-global-writer-method*
                   *the-class-global-boundp-method*
+                  *the-class-global-makunbound-method*
                   *the-class-standard-generic-function*
                   *the-class-standard-direct-slot-definition*
                   *the-class-standard-effective-slot-definition*

src/code/defstruct.lisp
@@ -266,14 +266,17 @@
                                 ,(slot-access-transform :setf '(instance value) key))))
                         (sb-c:xdefun ,accessor-name :accessor ,source-form (instance)
                                      ,(slot-access-transform :read '(instance) key))))))
-      ;; Return fragements of code that CLOS can use.
-      ;; We don't return the toplevel DEFUNs because those generally
-      ;; perform an unneeded type-check unless in safety 0.
-      ;; These lambdas don't need to check the instance
-      ;; because it was already subject to type-based dispatch.
-      ;; FIXME: it seems like all these fragments should be packed into a single codebob
-      ;; which will have less overhead than separate blobs.
-      ;; Afaict, the only way to do that is to return one lambda that returns all the lambdas.
+      ;; Return fragements of code that CLOS can use.  We don't return
+      ;; the toplevel DEFUNs because those generally perform an
+      ;; unneeded type-check unless in safety 0.  These CLOS-related
+      ;; lambdas don't need to check the type of the instance because
+      ;; it was already subject to type-based dispatch, and must let
+      ;; unbound markers through for the CLOS machinery to handle.
+      ;;
+      ;; FIXME: it seems like all these fragments should be packed
+      ;; into a single codebob which will have less overhead than
+      ;; separate blobs.  Afaict, the only way to do that is to return
+      ;; one lambda that returns all the lambdas.
       (collect ((result))
         (dolist (dsd (dd-slots dd) (result))
           (binding* ((key (cons dd dsd))
@@ -283,25 +286,24 @@
                      ;; accessor is the global defun
                      (accessor (dsd-accessor-name dsd)))
             (declare (dynamic-extent key))
-            (result (if (dsd-read-only dsd)
+            (result (if (or (dsd-read-only dsd) (not (dsd-always-boundp dsd)))
                         `(named-lambda (setf ,name) (#1=#:v #2=#:x)
                            ,@(if (eql (dsd-type dsd) 't)
                                  ;; no typecheck
                                  `((,writer (truly-the ,(dd-name dd) #2#) ,(dsd-index dsd) #1#) #1#)
                                  `(,(slot-access-transform :setf `(#1# (truly-the ,(dd-name dd) #2#))
-                                                           key :function))))
+                                                           key :function t))))
                         `'(setf ,accessor))
-                    (if nil ; TODO: policy-based selection perhaps?
+                    (if (not (dsd-always-boundp dsd))
                         `(named-lambda ,name (#2#)
-                           ,(if (and (dsd-always-boundp dsd) (dsd-safe-p dsd))
-                                ;; Most slots are always-boundp (don't have a BOA constructor
-                                ;; that omits slots) and safe-p (type-safe for reading),
+                           ,(if (dsd-safe-p dsd)
+                                ;; Most slots are safe-p (type-safe for reading),
                                 ;; so we can be concise rather than use SLOT-ACCESS-TRANSFORM
                                 ;; plus a rebinding of X with TRULY-THE.
                                 `(,reader (truly-the ,(dd-name dd) #2#) ,(dsd-index dsd))
                                 ;; Don't check X, but do check the the fetched value.
                                 (slot-access-transform :read `((truly-the ,(dd-name dd) #2#))
-                                                       key)))
+                                                       key :function t)))
                         `',accessor)))))))
 
 ;;; shared logic for host macroexpansion for SB-XC:DEFSTRUCT and
@@ -1242,7 +1244,10 @@ unless :NAMED is also specified.")))
 ;;; versus
 ;;;    (setf (mystruct-myslot s) newval) ; :MACRO
 ;;; Return NIL on failure.
-(defun slot-access-transform (operation args slot-key &optional (fun-or-macro :macro))
+(defun slot-access-transform (operation args slot-key
+                              &optional
+                                (fun-or-macro :macro)
+                                (external-unbound-handling nil))
   (binding* ((dd (car slot-key))
              (dsd (cdr slot-key))
              ((reader writer) (dsd-reader dsd (neq (dd-type dd) 'structure)))
@@ -1253,45 +1258,58 @@ unless :NAMED is also specified.")))
        (when (singleton-p args)
          (let* ((instance-form `(the ,(dd-name dd) ,(car args)))
                 (place `(,reader ,instance-form ,index)))
-            ;; There are 4 cases of {safe,unsafe} x {always-boundp,possibly-unbound}
-            ;; If unsafe - which implies TYPE-SPEC other than type T - then we must
-            ;; check the type on each read. Assuming that type-checks reject
-            ;; the unbound-marker, then we needn't separately check for it.
-            (cond ((not (dsd-safe-p dsd))
-                   `(the ,type-spec ,place))
-                  (t
-                   (unless (dsd-always-boundp dsd)
-                     (setf place
-                           `(the* ((not (satisfies sb-vm::unbound-marker-p))
-                                   :context (:struct-read ,(dd-name dd) . ,(dsd-name dsd)))
-                                  ,place)))
-                   (if (eq type-spec t) place
-                       `(the* (,type-spec :derive-type-only t) ,place)))))))
+           ;; There are 4 cases of {safe,unsafe} x {always-boundp,possibly-unbound}
+           ;; If unsafe - which implies TYPE-SPEC other than type T - then we must
+           ;; check the type on each read. Assuming that type-checks reject
+           ;; the unbound-marker, then we needn't separately check for it, but if
+           ;; we're generating code fragments for CLOS (which does its own
+           ;; EXTERNAL-UNBOUND-HANDLING) we need to let it through explicitly.
+           (cond ((not (dsd-safe-p dsd))
+                  (when (and (not (dsd-always-boundp dsd)) external-unbound-handling)
+                    (setq type-spec `(or ,type-spec (satisfies sb-vm::unbound-marker-p))))
+                  `(the ,type-spec ,place))
+                 (t
+                  ;; unless the slot is always bound, or unbound handling is external,
+                  ;; check here for unbound marker.
+                  (unless (or (dsd-always-boundp dsd) external-unbound-handling)
+                    (setf place
+                          `(the* ((not (satisfies sb-vm::unbound-marker-p))
+                                  :context (:struct-read ,(dd-name dd) . ,(dsd-name dsd)))
+                                 ,place)))
+                  (cond
+                    ((eq type-spec t) place)
+                    (t
+                     (when (and (not (dsd-always-boundp dsd)) external-unbound-handling)
+                       (setq type-spec `(or ,type-spec (satisfies sb-vm::unbound-marker-p))))
+                     `(the* (,type-spec :derive-type-only t) ,place))))))))
       (:setf
-        ;; The primitive object slot setting vops take newval last, which matches
-        ;; the order in which a use of SETF has them, but because the vops
-        ;; do not return anything, we have to bind both arguments.
-        (when (and (listp args) (singleton-p (cdr args)))
-          (multiple-value-bind (newval-form instance-form)
-              (ecase fun-or-macro
-                (:function (values (first args) (second args)))
-                (:macro (values (second args) (first args))))
-            (if (eq fun-or-macro :function)
-                ;; This used only for source-transforming (funcall #'(setf myslot) ...).
-                ;; (SETF x) writer functions have been defined as source-transforms instead of
-                ;; inline functions, which improved the semantics around clobbering defstruct
-                ;; writers with random DEFUNs either deliberately or accidentally.
-                ;; Since users can't define source-transforms (not portably anyway),
-                ;; we can easily discern which functions were system-generated.
-                `(let ((#2=#:val
-                        #4=,(if (eq type-spec t)
-                                newval-form
-                                `(the* (,type-spec :context (:struct ,(dd-name dd) . ,(dsd-name dsd)))
-                                       ,newval-form)))
-                       (#1=#:instance #3=(the ,(dd-name dd) ,instance-form)))
-                   (,writer #1# ,index #2#)
-                   #2#)
-                `(let ((#1# #3#) (#2# #4#)) (,writer #1# ,index #2#) #2#))))))))
+       ;; The primitive object slot setting vops take newval last, which matches
+       ;; the order in which a use of SETF has them, but because the vops
+       ;; do not return anything, we have to bind both arguments.
+       (when (and (listp args) (singleton-p (cdr args)))
+         (multiple-value-bind (newval-form instance-form)
+             (ecase fun-or-macro
+               (:function (values (first args) (second args)))
+               (:macro (values (second args) (first args))))
+           (if (eq fun-or-macro :function)
+               ;; This used only for source-transforming (funcall #'(setf myslot) ...).
+               ;; (SETF x) writer functions have been defined as source-transforms instead of
+               ;; inline functions, which improved the semantics around clobbering defstruct
+               ;; writers with random DEFUNs either deliberately or accidentally.
+               ;; Since users can't define source-transforms (not portably anyway),
+               ;; we can easily discern which functions were system-generated.
+               `(let ((#2=#:val
+                        #4=,(cond
+                              ((eq type-spec t) newval-form)
+                              (t
+                               (unless (or (dsd-always-boundp dsd) (not external-unbound-handling))
+                                 (setq type-spec `(or ,type-spec (satisfies sb-vm::unbound-marker-p))))
+                               `(the* (,type-spec :context (:struct ,(dd-name dd) . ,(dsd-name dsd)))
+                                      ,newval-form))))
+                      (#1=#:instance #3=(the ,(dd-name dd) ,instance-form)))
+                  (,writer #1# ,index #2#)
+                  #2#)
+               `(let ((#1# #3#) (#2# #4#)) (,writer #1# ,index #2#) #2#))))))))
 
 ;;; Apply TRANSFORM - a special indicator stored in :SOURCE-TRANSFORM
 ;;; for a DEFSTRUCT copier, accessor, or predicate - to SEXPR.
@@ -1315,12 +1333,6 @@ unless :NAMED is also specified.")))
                  (slot-access-transform (if (consp name) :setf :read)
                                         (cdr sexpr) transform :function)))))
     (values result (not result))))
-
-;;; Return a LAMBDA form which can be used to set a slot
-(defun slot-setter-lambda-form (dd dsd)
-  `(lambda (newval instance)
-     (declare (optimize (debug 0)))
-     ,(slot-access-transform :setf '(instance newval) (cons dd dsd))))
 
 ;;; Blow away all the compiler info for the structure CLASS. Iterate
 ;;; over this type, clearing the compiler structure type info, and

src/code/target-error.lisp
@@ -306,9 +306,9 @@ with that condition (or with no condition) will be returned."
 
 ;;;; slots of CONDITION objects
 
-(defun find-slot-default (class slot)
-  (multiple-value-bind (value found) (find-slot-default-initarg class slot)
-    ;; When CLASS or a superclass has a default initarg for SLOT, use
+(defun find-slot-default (condition classoid slot &optional boundp)
+  (multiple-value-bind (value found) (find-slot-default-initarg classoid slot)
+    ;; When CLASSOID or a superclass has a default initarg for SLOT, use
     ;; that.
     (cond (found
            value)
@@ -317,15 +317,20 @@ with that condition (or with no condition) will be returned."
            (let ((initfun (condition-slot-initfunction slot)))
              (aver (functionp initfun))
              (funcall initfun)))
+          ;; if we're computing SLOT-BOUNDP, return an unbound marker
+          (boundp sb-pcl:+slot-unbound+)
+          ;; if we're computing SLOT-VALUE, call SLOT-UNBOUND
           (t
-           (error "Unbound condition slot: ~S" (condition-slot-name slot))))))
+           (let ((class (classoid-pcl-class classoid))
+                 (name (condition-slot-name slot)))
+             (values (slot-unbound class condition name)))))))
 
-(defun find-slot-default-initarg (class slot)
+(defun find-slot-default-initarg (classoid slot)
   (let ((initargs (condition-slot-initargs slot))
-        (cpl (condition-classoid-cpl class)))
-    (dolist (class cpl)
+        (cpl (condition-classoid-cpl classoid)))
+    (dolist (classoid cpl)
       (let ((direct-default-initargs
-              (condition-classoid-direct-default-initargs class)))
+              (condition-classoid-direct-default-initargs classoid)))
         (dolist (initarg initargs)
           (let ((initfunction (third (assoc initarg direct-default-initargs))))
             (when initfunction
@@ -351,27 +356,66 @@ with that condition (or with no condition) will be returned."
       (return (setf (car (condition-slot-cell cslot)) new-value)))))
 
 (defun condition-slot-value (condition name)
-  (let ((val (getf (condition-assigned-slots condition) name sb-pcl:+slot-unbound+)))
-    (if (unbound-marker-p val)
-        (let ((class (wrapper-classoid (%instance-wrapper condition))))
-          (dolist (cslot
-                   (condition-classoid-class-slots class)
-                   (let ((instance-length (%instance-length condition))
-                         (slot (or (find-condition-class-slot class name)
-                                   (error "missing slot ~S of ~S" name condition))))
-                     (setf (getf (condition-assigned-slots condition) name)
-                           (do ((i (+ sb-vm:instance-data-start 1) (+ i 2)))
-                               ((>= i instance-length) (find-slot-default class slot))
-                               (when (member (%instance-ref condition i)
-                                             (condition-slot-initargs slot))
-                                 (return (%instance-ref condition (1+ i))))))))
-            (when (eq (condition-slot-name cslot) name)
-              (let ((value (car (condition-slot-cell cslot))))
-                (if (unbound-marker-p value)
-                    (error "Unbound condition slot: ~S" (condition-slot-name cslot))
-                    (return value))))))
-        val)))
+  (let* ((sentinel (load-time-value (cons nil nil)))
+         (val (getf (condition-assigned-slots condition) name sentinel)))
+    (cond
+      ((unbound-marker-p val)
+       (let* ((classoid (wrapper-classoid (%instance-wrapper condition)))
+              (class (classoid-pcl-class classoid)))
+         (values (slot-unbound class condition name))))
+      ((eql val sentinel)
+       (let ((classoid (wrapper-classoid (%instance-wrapper condition))))
+         (dolist (cslot
+                  (condition-classoid-class-slots classoid)
+                  (let ((instance-length (%instance-length condition))
+                        (slot (or (find-condition-class-slot classoid name)
+                                  (return-from condition-slot-value
+                                    (let ((class (classoid-pcl-class classoid)))
+                                      (values (slot-missing class condition name 'slot-value)))))))
+                    (setf (getf (condition-assigned-slots condition) name)
+                          (do ((i (+ sb-vm:instance-data-start 1) (+ i 2)))
+                              ((>= i instance-length)
+                               (find-slot-default condition classoid slot))
+                            (when (member (%instance-ref condition i)
+                                          (condition-slot-initargs slot))
+                              (return (%instance-ref condition (1+ i))))))))
+           (when (eq (condition-slot-name cslot) name)
+             (let ((value (car (condition-slot-cell cslot))))
+               (if (unbound-marker-p value)
+                   (let ((class (classoid-pcl-class classoid)))
+                     (return (values (slot-unbound class condition name))))
+                   (return value)))))))
+      (t val))))
 
+(defun condition-slot-boundp (condition name)
+  (let* ((sentinel (load-time-value (cons nil nil)))
+         (val (getf (condition-assigned-slots condition) name sentinel)))
+    (cond
+      ((unbound-marker-p val) nil)
+      ((eql val sentinel)
+       (let ((classoid (wrapper-classoid (%instance-wrapper condition))))
+         (dolist (cslot
+                  (condition-classoid-class-slots classoid)
+                  (let ((instance-length (%instance-length condition))
+                        (slot (or (find-condition-class-slot classoid name)
+                                  (return-from condition-slot-boundp
+                                    (not (not (slot-missing (classoid-pcl-class classoid)
+                                                            condition name 'slot-boundp)))))))
+                    (let ((val (do ((i (+ sb-vm:instance-data-start 1) (+ i 2)))
+                                   ((>= i instance-length)
+                                    (find-slot-default condition classoid slot t))
+                                 (when (member (%instance-ref condition i)
+                                               (condition-slot-initargs slot))
+                                   (return (%instance-ref condition (1+ i)))))))
+                      (setf (getf (condition-assigned-slots condition) name) val)
+                      (not (unbound-marker-p val)))))
+           (when (eq (condition-slot-name cslot) name)
+             (let ((value (car (condition-slot-cell cslot))))
+               (return (not (unbound-marker-p value))))))))
+      (t t))))
+
+(defun condition-slot-makunbound (condition name)
+  (set-condition-slot-value condition sb-pcl:+slot-unbound+ name))
 
 ;;;; MAKE-CONDITION
 
@@ -398,9 +442,9 @@ with that condition (or with no condition) will be returned."
   ;; we could say that it's a supported extension.
   (let ((classoid (named-let lookup ((designator designator))
                     (typecase designator
-                     (symbol (find-classoid designator nil))
-                     (class (lookup (class-name designator)))
-                     (t designator)))))
+                      (symbol (find-classoid designator nil))
+                      (class (lookup (class-name designator)))
+                      (t designator)))))
     (unless (condition-classoid-p classoid)
       (error 'simple-type-error
              :datum designator
@@ -422,15 +466,15 @@ with that condition (or with no condition) will be returned."
                     (<= (get-lisp-obj-address sb-vm:*control-stack-start*) addr)
                     (< addr (get-lisp-obj-address sb-vm:*control-stack-end*))))))
       (let* ((any-dx
-              (loop for arg-index from 1 below (length initargs) by 2
-                    thereis (stackp (fast-&rest-nth arg-index initargs))))
+               (loop for arg-index from 1 below (length initargs) by 2
+                       thereis (stackp (fast-&rest-nth arg-index initargs))))
              (layout (classoid-wrapper classoid))
              (extra (if (and any-dx (type-err-p layout)) 2 0)) ; space for secret initarg
              (instance (%new-instance layout
-                                        (+ sb-vm:instance-data-start
-                                          1 ; ASSIGNED-SLOTS
-                                          (length initargs)
-                                          extra)))
+                                      (+ sb-vm:instance-data-start
+                                         1 ; ASSIGNED-SLOTS
+                                         (length initargs)
+                                         extra)))
              (data-index (1+ sb-vm:instance-data-start))
              (arg-index 0)
              (have-type-error-datum)
@@ -511,7 +555,7 @@ with that condition (or with no condition) will be returned."
                 (return nil)))
         (setf (getf (condition-assigned-slots condition)
                     (condition-slot-name hslot))
-              (find-slot-default classoid hslot))))
+              (find-slot-default condition classoid hslot))))
 
     condition))
 

src/cold/exports.lisp
@@ -2488,7 +2488,7 @@ is a good idea, but see SB-SYS re. blurring of boundaries.")
            "NOTE-NAME-DEFINED"
            "%CODE-CODE-SIZE" "%CODE-TEXT-SIZE"
            "%CODE-SERIALNO"
-           "DD-SLOTS" "DD-HAS-RAW-SLOT-P" "DD-INCLUDE" "SLOT-SETTER-LAMBDA-FORM"
+           "DD-SLOTS" "DD-HAS-RAW-SLOT-P" "DD-INCLUDE"
            "SLOT-ACCESS-TRANSFORM"
            "%IMAGPART" "%CODE-DEBUG-INFO"
            "WRAPPER-CLASSOID" "WRAPPER-INVALID"
@@ -2583,6 +2583,8 @@ is a good idea, but see SB-SYS re. blurring of boundaries.")
 
            "CONDITION-SLOT-VALUE"
            "SET-CONDITION-SLOT-VALUE"
+           "CONDITION-SLOT-BOUNDP"
+           "CONDITION-SLOT-MAKUNBOUND"
 
            "CONDITION-SLOT-ALLOCATION"
            "CONDITION-SLOT-DOCUMENTATION"

src/cold/undefined-fun-allowlist.lisp-expr
@@ -38,6 +38,7 @@
   sb-mop:slot-definition-allocation
   sb-mop:slot-definition-name
   sb-pcl::slot-accessor-std-p
+  slot-missing
   slot-unbound
   sb-pcl::specializer-applicable-using-type-p
   sb-pcl::specializer-class-or-nil

src/compiler/fndb.lisp
@@ -2175,7 +2175,7 @@
   (movable foldable flushable))
 
 ;; FIXME: should T be be (OR INSTANCE FUNCALLABLE-INSTANCE) etc?
-(defknown slot-value (t symbol) t (any))
+(defknown (slot-value slot-makunbound) (t symbol) t (any))
 (defknown (slot-boundp slot-exists-p) (t symbol) boolean)
 (defknown sb-pcl::set-slot-value (t symbol t) t (any))
 

commit 0428885e8e13a7b4a2ec46927b3e5af909a5ef7f

pcl: Fix missing pair of parentheses around :MAKUNBOUND code.
This incorrect syntax was introduced in
8c3b51059696140154f5ac1780fcc29be0a624f4. I suppose this was never
exercised because the compiler must have always elided the unbound
symbol value check around PROGN. I found this by forcing pcl-compile
to go through the interpreter.

src/pcl/dlisp.lisp
@@ -256,8 +256,8 @@
                  `((let ((value ,read-form))
                      (return-from access (not (unbound-marker-p value))))))
                 (:makunbound
-                 `(progn (setf ,read-form +slot-unbound+)
-                         ,instance))
+                 `((progn (setf ,read-form +slot-unbound+)
+                          ,instance)))
                 (:writer
                  `((return-from access (setf ,read-form ,(car arglist)))))))
           (funcall miss-fn ,@arglist))))))