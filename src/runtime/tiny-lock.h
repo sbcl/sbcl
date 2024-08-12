f003a78df660a526e389213dbd1c44d6e516838f
Douglas Katzman <dougk@google.com>
2024-08-12 17:46:01 -0400

    Use futexes for tiny-lock if #+sb-futex
    And fix bug if defined NDEBUG with USE_PTHREAD_LOCK

--- src/runtime/tiny-lock.h
+++ src/runtime/tiny-lock.h
@@ -4,19 +4,49 @@
 #include <pthread.h>
 typedef pthread_mutex_t lock_t;
 
-static void acquire_lock(lock_t *l) { gc_assert(!pthread_mutex_lock(l)); }
-static void release_lock(lock_t *l) { gc_assert(!pthread_mutex_unlock(l)); }
+// gc_assert drops out if NDEBUG is defined
+static void acquire_lock(lock_t *l) {
+    __attribute__(unused)) int r=pthread_mutex_lock(l);
+    gc_assert(!r);
+}
+static void release_lock(lock_t *l) {
+    __attribute__((unused) int r=pthread_mutex_unlock(l);
+    gc_assert(!r);
+}
 
 #define LOCK_INITIALIZER PTHREAD_MUTEX_INITIALIZER
 #else
 /* A lock that has the sole benefit of not being the pthreads lock.
  * And the sole downside of not being the pthreads lock. */
-#include <sched.h>
 #include <stdatomic.h>
 
 struct lock { _Atomic(int) grabbed; };
 typedef struct lock lock_t;
 
+#ifdef LISP_FEATURE_SB_FUTEX
+// implement 'class mutex2' from "Futexes Are Tricky"
+extern int futex_wait(int*,int,long,unsigned long), futex_wake(int*, int);
+static inline int cmpxchg(lock_t *l, int old, int new) {
+  atomic_compare_exchange_weak(&l->grabbed, &old, new);
+  return old;
+}
+static void acquire_lock(lock_t *l) {
+  int c;
+  if ((c = cmpxchg(l, 0, 1)) != 0) {
+    do {
+      if (c == 2 || cmpxchg(l, 1, 2) != 0)
+        futex_wait((int*)&l->grabbed, 2, -1, 0);
+    } while ((c = cmpxchg(l, 0, 2) != 0) != 0);
+  }
+}
+static void release_lock(lock_t* l) {
+  if (atomic_fetch_sub(&l->grabbed, 1) != 1) {
+    l->grabbed = 0;
+    futex_wake((int*)&l->grabbed, 1);
+  }
+}
+#else
+#include <sched.h>
 static void acquire_lock(lock_t *l) {
   int expected = 0, cycles = 0;
   /* atomic_compare_exchange_strong kindly clobbers expected for us,
@@ -29,5 +59,7 @@ static void acquire_lock(lock_t *l) {
 static void release_lock(lock_t *l) {
   atomic_store(&l->grabbed, 0);
 }
+#endif
+
 #define LOCK_INITIALIZER { 0 }
 #endif
