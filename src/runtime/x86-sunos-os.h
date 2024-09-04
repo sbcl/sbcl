f4819a308215c7a4b12a015154d932b8f3b76ed5
Douglas Katzman <dougk@google.com>
2024-09-04 15:08:49 -0400

    Fix typo from rev 3bf3c33f9e
    I take it to mean that nobody has built x86-sunos in > 2 years

--- src/runtime/x86-sunos-os.h
+++ src/runtime/x86-sunos-os.h
@@ -6,6 +6,6 @@ typedef int os_context_register_t;
 
 #include "arch-os-generic.inc"
 
-#define OS_CONTEXT_PC(contex) context->uc_mcontext.gregs[14] /* REG_EIP */
+#define OS_CONTEXT_PC(context) context->uc_mcontext.gregs[14] /* REG_EIP */
 
 #endif /* _X86_SOLARIS_OS_H */
