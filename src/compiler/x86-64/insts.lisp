d49a183d03fd4cef84d804295bc8456ae5135f69
Douglas Katzman <dougk@google.com>
2024-09-05 17:02:21 -0400

    Revise fixup-code-object into 2 orthogonal CASE expressions
    instead of picking off a few cases then a few more and a few more.

--- src/compiler/x86-64/insts.lisp
+++ src/compiler/x86-64/insts.lisp
@@ -3366,50 +3366,47 @@
 (symbol-macrolet
     (#-sb-xc-host (sb-vm::lisp-linkage-space-addr
                    (sb-alien:extern-alien "linkage_space" sb-alien:unsigned)))
-(defun fixup-code-object (code offset value kind flavor)
+(defun fixup-code-object (code offset value kind flavor
+                          &aux (sap (code-instructions code)))
   (declare (type index offset))
-  (cond ((eq flavor :linkage-cell)
-         (setq value (let ((index (ash value word-shift)))
-                       (ecase kind
-                         #+immobile-space (:rel32 (+ sb-vm::lisp-linkage-space-addr index))
-                         (:abs32 index)))))
-        ((and (eq flavor :assembly-routine) (eq kind :*abs32))
-         (setq value (sb-vm::asm-routine-indirect-address value))))
-  (let ((sap (code-instructions code)))
-    (case flavor
-      (:card-table-index-mask ; the VALUE is nbits, so convert it to an AND mask
-       (setf (sap-ref-32 sap offset) (1- (ash 1 value))))
-      (:layout-id ; layout IDs are signed quantities on x86-64
-       (setf (signed-sap-ref-32 sap offset) value))
-      (:alien-data-linkage-index
-       (setf (sap-ref-32 sap offset) (* value alien-linkage-table-entry-size)))
-      (:alien-code-linkage-index
-       (let ((addend (sap-ref-32 sap offset)))
-         (setf (sap-ref-32 sap offset) (+ (* value alien-linkage-table-entry-size) addend))))
-      (t
-       ;; All x86-64 fixup locations contain an implicit addend at the location
-       ;; to be fixed up. The addend is always zero for certain <KIND,FLAVOR> pairs,
-       ;; but we don't need to assert that.
-       (incf value (if (eq kind :absolute)
-                       (signed-sap-ref-64 sap offset)
-                       (signed-sap-ref-32 sap offset)))
-       (ecase kind
-         ((:abs32 :*abs32) ; 32 unsigned bits
-          (setf (sap-ref-32 sap offset) value))
-         (:rel32
-          ;; Replace word with the difference between VALUE and current pc.
-          ;; JMP/CALL are relative to the next instruction,
-          ;; so add 4 bytes for the size of the displacement itself.
-          ;; Relative fixups don't exist with movable code,
-          ;; so in the #-immobile-code case, there's nothing to assert.
-          #+(and immobile-code (not sb-xc-host))
-          (unless (immobile-space-obj-p code)
-            (error "Can't compute fixup relative to movable object ~S" code))
-          (setf (signed-sap-ref-32 sap offset) (- value (+ (sap-int sap) offset 4))))
-         (:absolute
-          ;; These are used for jump tables and are not recorded in %code-fixups.
-          ;; GC knows to adjust the values if code is moved.
-          (setf (sap-ref-64 sap offset) value))))))
+  ;; Preprocess the value based on FLAVOR and the implicit addend at the
+  ;; fixup location.  The addend will be zero for most <KIND,FLAVOR> pairs.
+  (setq value
+        (+ (case flavor
+             (:card-table-index-mask ; the VALUE is nbits, so convert it to a mask
+              (aver (zerop (sap-ref-32 sap offset))) ; enforce zero addend
+              (1- (ash 1 value)))
+             (:linkage-cell
+              (let ((index (ash value word-shift)))
+                (ecase kind
+                  #+immobile-space (:rel32 (+ sb-vm::lisp-linkage-space-addr index))
+                  (:abs32 index))))
+             (:assembly-routine
+              (if (eq kind :*abs32) (sb-vm::asm-routine-indirect-address value) value))
+             ((:alien-code-linkage-index :alien-data-linkage-index)
+              (* value alien-linkage-table-entry-size))
+             (:layout-id ; layout IDs are signed quantities on x86-64
+              (setf (signed-sap-ref-32 sap offset) value)
+              (return-from fixup-code-object))
+             (t value))
+           (if (eq kind :absolute)
+               (signed-sap-ref-64 sap offset)
+               (signed-sap-ref-32 sap offset))))
+  (ecase kind
+    ((:abs32 :*abs32) ; 32 unsigned bits
+     (setf (sap-ref-32 sap offset) value))
+    (:rel32
+     ;; Replace word with the difference between VALUE and current pc.
+     ;; JMP/CALL are relative to the next instruction,
+     ;; so add 4 bytes for the size of the displacement itself.
+     ;; Relative fixups don't exist with movable code,
+     ;; so in the #-immobile-code case, there's nothing to assert.
+     #+(and immobile-code (not sb-xc-host))
+     (unless (immobile-space-obj-p code)
+       (error "Can't compute fixup relative to movable object ~S" code))
+     (setf (signed-sap-ref-32 sap offset) (- value (+ (sap-int sap) offset 4))))
+    (:absolute ; 64-bit jump table target address
+     (setf (sap-ref-64 sap offset) value)))
   nil))
 
 ;;; There are 3 data streams in the FIXUPS slot:
