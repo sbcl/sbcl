9549f0015ad3963b91a2b05d8b5bf8341e074c14
Stas Boukarev <stassats@gmail.com>
2024-11-13 03:57:02 +0300

    coreparse: do some things on empty spaces anyway.

--- src/runtime/coreparse.c
+++ src/runtime/coreparse.c
@@ -903,25 +903,25 @@ process_directory(int count, struct ndir_entry *entry,
                 madvise((void *)addr, len, MADV_MERGEABLE);
             }
 #endif
-
-            lispobj *free_pointer = (lispobj *) addr + entry->nwords;
-            switch (id) {
-            default:
-                // text free ptr is already nonzero if Lisp code in executable
-                if (!*spaces[id].pfree_pointer)
-                    *spaces[id].pfree_pointer = free_pointer;
-                break;
-            case DYNAMIC_CORE_SPACE_ID:
-                next_free_page = ALIGN_UP(entry->nwords<<WORD_SHIFT, GENCGC_PAGE_BYTES)
-                    / GENCGC_PAGE_BYTES;
-                anon_dynamic_space_start = (os_vm_address_t)(addr + len);
-            }
-#ifdef DEBUG_COREPARSE
-            printf("space %d @ %10lx pg=%4d+%4d nwords=%9ld checksum=%lx\n",
-                   (int)id, addr, (int)entry->data_page, (int)entry->page_count,
-                   entry->nwords, corespace_checksum((void*)addr, entry->nwords));
-#endif
         }
+        lispobj *free_pointer = (lispobj *) addr + entry->nwords;
+        switch (id) {
+        default:
+            // text free ptr is already nonzero if Lisp code in executable
+            if (!*spaces[id].pfree_pointer)
+                *spaces[id].pfree_pointer = free_pointer;
+            break;
+        case DYNAMIC_CORE_SPACE_ID:
+            next_free_page = ALIGN_UP(entry->nwords<<WORD_SHIFT, GENCGC_PAGE_BYTES)
+                / GENCGC_PAGE_BYTES;
+            anon_dynamic_space_start = (os_vm_address_t)(addr + len);
+        }
+#ifdef DEBUG_COREPARSE
+        printf("space %d @ %10lx pg=%4d+%4d nwords=%9ld checksum=%lx\n",
+               (int)id, addr, (int)entry->data_page, (int)entry->page_count,
+               entry->nwords, corespace_checksum((void*)addr, entry->nwords));
+#endif
+
     }
 
 #ifdef LISP_FEATURE_LINKAGE_SPACE
