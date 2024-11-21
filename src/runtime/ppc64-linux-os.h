863df52494542a7a3cd3f48a232f4b73c5a65b38
Stas Boukarev <stassats@gmail.com>
2024-11-21 18:22:37 +0300

    Include ppc-linux-os.h in ppc64-linux-os.h
    They are the same.

--- src/runtime/ppc64-linux-os.h
+++ src/runtime/ppc64-linux-os.h
@@ -1,13 +1,1 @@
-#ifndef _PPC64_LINUX_OS_H
-#define _PPC64_LINUX_OS_H
-
-typedef ucontext_t os_context_t;
-typedef unsigned long os_context_register_t;
-
-unsigned long os_context_fp_control(os_context_t *context);
-#define RESTORE_FP_CONTROL_FROM_CONTEXT
-void os_restore_fp_control(os_context_t *context);
-
-#define OS_CONTEXT_PC(context) context->uc_mcontext.regs->nip
-
-#endif /* _PPC64_LINUX_OS_H */
+#include <ppc-linux-os.h>
