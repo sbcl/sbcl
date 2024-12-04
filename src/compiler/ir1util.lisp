5e9b462c93bec6c0e69d7e11b18c221353e99926
Stas Boukarev <stassats@gmail.com>
2024-12-04 07:20:18 +0300

    erase-lvar-type: clear lvar-type-annotations.
    Might be fixing lp#2090967

--- src/compiler/ir1util.lisp
+++ src/compiler/ir1util.lisp
@@ -308,6 +308,9 @@
 (defun erase-lvar-type (lvar)
   (when lvar
     (setf (lvar-%derived-type lvar) nil)
+    (loop for annotation in (lvar-annotations lvar)
+          when (lvar-type-annotation-p annotation)
+          do (setf (lvar-annotation-fired annotation) t))
     (let ((dest (lvar-dest lvar)))
       (cond ((cast-p dest)
              (derive-node-type dest *wild-type* :from-scratch t)
