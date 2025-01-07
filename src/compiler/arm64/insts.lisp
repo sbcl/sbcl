66ae824df410b74112a302f8bfe61ce535cd376a
Stas Boukarev <stassats@gmail.com>
2025-01-07 17:53:54 +0300

    Fix arm64 build.

--- src/compiler/arm64/insts.lisp
+++ src/compiler/arm64/insts.lisp
@@ -1612,21 +1612,6 @@
   (rn :field (byte 5 5) :type 'x-reg-sp)
   (rt :field (byte 5 0) :type 'reg))
 
-#-sb-xc-host
-(defun print-stlxr/ldaxr-mnemonic (dchunk inst stream dstate)
-  (declare (ignore dstate))
-  ;; SIZE can't be constrained in the :PRINTER spec because there are two
-  ;; different values that should print the same. Also, we're printing the wrong
-  ;; size for 4 byte because 32-BIT-REGISTER-P tests bit index 31, but it's index 30
-  ;; that distinguishes those two cases. The 2-bit field would probably need to
-  ;; become two 1-bit fields to correct the disassembly.
-  (let ((suffix (case (ldb (byte 2 30) dchunk)
-                  (#b00 "B")
-                  (#b01 "H")
-                  (t ""))))
-    (when stream
-      (format stream "~A~A" (sb-disassem::inst-print-name inst) suffix))))
-
 (defmacro def-store-exclusive (name o0 o1 o2 rs
                                &optional (size '(logior #b10 (reg-size rt))))
   `(define-instruction ,name (segment ,@(and rs '(rs)) rt rn)
