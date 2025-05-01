5ee22d26621f4fab0788c6f0ea9f8f7e0979eb5f
Stas Boukarev <stassats@gmail.com>
2025-05-01 07:01:58 +0300

    Fix adjacent gc barriers elimination.
    impart_mark_stickiness skipped pointers into gen 0, which is fine if
    both the value and the object are allocated before the card mark is
    stored. Eliminating a barrier may insert an allocation sequence
    between two stores, which may cause the value and the object to have
    different generations.
    The extra cost of looking at more card marks should not be significant
    and reducing code size and performance of consequent stores is
    valuable.

--- tests/gc-cardmark.impure.lisp
+++ tests/gc-cardmark.impure.lisp
@@ -1,4 +1,19 @@
 
+(defun adjacent-barriers ()
+  (let ((o (list 1)))
+    (setf (car o) (list 100))
+    (setf (car o) (make-array 200 :element-type 'fixnum))
+    (setf (cdr o) (list 100 2))))
+
+(with-test (:name :adjacent-barrier-elimination-sticky-marks
+            :skipped-on (or :gc-stress
+                            (:not :soft-card-marks)))
+  (setf (extern-alien "pre_verify_gen_0" int) 1)
+  (loop repeat 1000000
+        do (adjacent-barriers))
+  (setf (extern-alien "pre_verify_gen_0" int) 0))
+
+
 ;;; The layout of FOO must not get promoted from gen0 to gen1.
 ;;; Due to random variation, there might be a gc-with-promotion cycle
 ;;; just after the defstruct. So prevent that.
