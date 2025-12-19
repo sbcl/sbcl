9306e9968d69fe97fef51982f390638e30ed744b
Stas Boukarev <stassats@gmail.com>
2025-12-19 12:23:41 +0300

    Fix minus-zero-ignored-p for -

--- tests/arith-2.pure.lisp
+++ tests/arith-2.pure.lisp
@@ -1491,7 +1491,13 @@
         ()
         `(lambda (a b)
            (- (* a (- b))))
-      ((4.0 0) -0.0))))
+      ((4.0 0) -0.0))
+    (checked-compile-and-assert
+        ()
+        `(lambda (a b)
+           (declare (integer b))
+           (- (- (+ a 5)) b))
+      ((-5.0 0) -0.0))))
 
 (with-test (:name :remove-negate.2)
   (flet ((test (form count)
