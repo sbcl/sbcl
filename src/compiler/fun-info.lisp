6b540627fa878e028e77848aa99d41c1bd6837da
Stas Boukarev <stassats@gmail.com>
2026-01-30 01:18:02 +0300

    Unify the transforms for (/ real complex) and (/ complex complex)

--- src/compiler/fun-info.lisp
+++ src/compiler/fun-info.lisp
@@ -189,7 +189,8 @@
   ;; An integer bitmap for positional arguments.
   ;; Sign-extended into a negative integer for &rest arguments.
   (read-only-args nil :type (or null sb-xc:fixnum))
-  (rewrite-full-call nil :type (or function null)))
+  (rewrite-full-call nil :type (or function null))
+  (flushable nil :type (or function null)))
 
 (defprinter (fun-info)
   (attributes :test (not (zerop attributes))
