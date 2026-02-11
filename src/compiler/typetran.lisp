3402aa922784d85a0ffe41b4797c01080940f537
Stas Boukarev <stassats@gmail.com>
2026-02-11 04:38:46 +0300

    (typep (or (mod 5) (unsigned-byte 65)) '(unsigned-byte 64)) => fixnump

--- src/compiler/typetran.lisp
+++ src/compiler/typetran.lisp
@@ -238,6 +238,24 @@
                             (not (eq new-predicate #+64-bit 'unsigned-byte-64-p
                                                    #-64-bit 'unsigned-byte-32-p)))
                    `(,new-predicate object))))
+              ((let ((new-predicate
+                       ;; (typep (the (or (mod 5) (unsigned-byte 65)) x) '(unsigned-byte 64))
+                       ;; => (fixnump x)
+                       (and (csubtypep intersect (specifier-type 'real))
+                            (macrolet ((up (&rest types)
+                                         `(cond ,@(loop for (type exclude predicate) on types by #'cdddr
+                                                        collect
+                                                        `((and ,@(and exclude
+                                                                      `((neq current-predicate ',exclude)))
+                                                                  (eq intersect
+                                                                      (type-intersection otype (specifier-type ',type))))
+                                                          ',predicate)))))
+                              (up fixnum nil fixnump
+                                  sb-vm:signed-word integerp #+64-bit signed-byte-64-p #-64-bit signed-byte-32-p
+                                  word integerp #+64-bit unsigned-byte-64-p #-64-bit unsigned-byte-32-p)))))
+                 (when (and new-predicate
+                            (neq new-predicate current-predicate))
+                   `(,new-predicate object))))
               ;; (typep (the float x) 'double-float) =>
               ;; (not (typep x 'single-float))
               ((let* ((diff (type-difference otype type))
