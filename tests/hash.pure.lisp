sbcl/sbcl

commit c9c117bd3c817f1450251d594778ac1b938ae4a6

Fix 32 bit adaptive weak hash tables

src/code/target-hash-table.lisp
@@ -2461,7 +2461,7 @@ nnnn 1_    any       linear scan (don't try to read when rehash already in progr
                 (address-sensitive-p
                  (and address-sensitive-p
                       (not (logtest (hash-table-flags hash-table) hash-table-userfun-flag))))
-                (hash (clip-hash (the fixnum hash0))))
+                (hash (clip-hash (the maybe-truncated-hash hash0))))
        (dx-flet ((body ()
                    (binding* (((probed-value probed-key physical-index predecessor)
                                (findhash-weak key hash-table hash address-sensitive-p))

commit 22d1336757150d882297f8cf0b78c0f52cfa5800

Fix weak hash tables with user-defined hash functions
Specifically, allow user-defined hash functions to return positive
fixnums as documented on 64bit platforms. Broken since c9c117bd3c81.

Fixes lp#2141482, reported by Patrick Poitras.

src/code/target-hash-table.lisp
@@ -2461,7 +2461,11 @@ nnnn 1_    any       linear scan (don't try to read when rehash already in progr
                 (address-sensitive-p
                  (and address-sensitive-p
                       (not (logtest (hash-table-flags hash-table) hash-table-userfun-flag))))
-                (hash (clip-hash (the maybe-truncated-hash hash0))))
+                (hash (clip-hash hash0)))
+       ;; On 32bit platforms, the largest valid MAYBE-TRUNCATED-HASH
+       ;; is higher than MOST-POSITIVE-FIXNUM. On 64bit platforms,
+       ;; it's the other way around.
+       (declare (type (or fixnum maybe-truncated-hash) hash0))
        (dx-flet ((body ()
                    (binding* (((probed-value probed-key physical-index predecessor)
                                (findhash-weak key hash-table hash address-sensitive-p))

tests/hash.pure.lisp
@@ -784,3 +784,19 @@
 (with-test (:name (:adaptive-equal-hash :eql-hash-not-truncated))
   (assert (not (truncated-hash-p (sb-impl::perhaps-truncated-equal-hash
                                   (ash 1 +truncated-hash-bit+) 0)))))
+
+(with-test (:name (:user-defined-hash :range))
+  (flet ((mpf (key)
+           (declare (ignore key))
+           ;; According to documentation hash functions must return
+           ;; (AND FIXNUM (INTEGER 0)).
+           most-positive-fixnum))
+    (dolist (test '(eq eql equal equalp))
+      (dolist (weakness '(nil :value :key :key-and-value :key-or-value))
+        (dolist (synchronized '(nil t))
+          (let ((h (make-hash-table :test test
+                                    :weakness weakness
+                                    :hash-function #'mpf)))
+            (dotimes (i 100)
+              (setf (gethash i h) i)
+              (gethash i h))))))))