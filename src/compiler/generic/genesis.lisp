0f6128e4a6faf774cc10ee1fe9da9bffdd29857f
Douglas Katzman <dougk@google.com>
2026-08-22 13:51:00 -0400

    Augment genesis/number-types.h, don't make more headers

--- src/compiler/generic/genesis.lisp
+++ src/compiler/generic/genesis.lisp
@@ -4594,7 +4594,7 @@ static char *event_printf_format[] = {~{~%  ~S~^,~}~%};~%#endif~2%"
           (mapcar #'get-primitive-obj
                   '(bignum ratio single-float double-float
                     complex complex-single-float complex-double-float
-                    simd-pack simd-pack-256 simd-pack-512))))
+                    simd-pack simd-pack-256 simd-pack-512 simd-pack-512-mask))))
 
 (defun write-c-headers (c-header-dir-name)
   (macrolet ((out-to (name &body body) ; write boilerplate and inclusion guard
