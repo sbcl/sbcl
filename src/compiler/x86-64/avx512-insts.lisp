e9ca324a702944c6dde266f9c4eec16246cdbdfb
Aaron Estrada <bin.echo@gmail.com>
2026-09-04 17:46:07 -0600

    x86-64: fix EVEX vmovsd/vmovss emitters, GPR vector move length, and 64-byte inline constant alignment
    - avx512-insts.lisp: pass explicit W=1 for vmovsd (64-bit float) and
      W=0 for vmovss to prevent #UD on EVEX scalar moves. Fix 3-operand
      emitter ordering (ModRM r/m in thing, first source in vvvv).
    - avx2-insts.lisp: force :ll 0 for GPR-to-vector moves (vmovd/vmovq)
      promoted to EVEX to prevent invalid 512-bit vector length prefixes.
    - insts.lisp: cap emit-inline-constant alignment to sb-assem::max-alignment
      so 64-byte :zword constants respect code object alignment bounds.

--- src/compiler/x86-64/avx512-insts.lisp
+++ src/compiler/x86-64/avx512-insts.lisp
@@ -67,43 +67,45 @@
   (def vmovdqu32 #xf3 #x6f #x7f 0)
   (def vmovdqu64 #xf3 #x6f #x7f 1))
 
-(macrolet ((def (name prefix)
+(macrolet ((def (name prefix w)
              `(define-instruction ,name (segment dst src &optional src2)
                 ,@(avx2-inst-printer-list 'ymm-ymm/mem-dir prefix #b0001000)
                 (:emitter
                  (cond ((ea-p src)
                         (if (zmm-register-p dst)
-                            (emit-avx512-inst segment src dst ,prefix #x10)
-                            (emit-avx2-inst segment src dst ,prefix #x10 :l 0)))
+                            (emit-avx512-inst segment src dst ,prefix #x10 :w ,w)
+                            (emit-avx2-inst segment src dst ,prefix #x10 :l 0 :w ,w)))
 
                        ((and (ea-p dst) (zmm-register-p src))
-                        (emit-avx512-inst segment dst src ,prefix #x11))
+                        (emit-avx512-inst segment dst src ,prefix #x11 :w ,w))
 
                        ((and (integerp src) src2 (register-p src2))
                         (if (or (zmm-register-p dst) (zmm-register-p src2))
-                            (emit-avx512-inst segment src2 dst ,prefix #x10)
-                            (emit-avx2-inst segment src2 dst ,prefix #x10 :l 0)))
+                            (emit-avx512-inst segment src2 dst ,prefix #x10 :w ,w)
+                            (emit-avx2-inst segment src2 dst ,prefix #x10 :l 0 :w ,w)))
 
                        ((and src2 (or (zmm-register-p dst)
                                       (zmm-register-p src)
                                       (zmm-register-p src2)))
-                        (emit-avx512-inst segment src dst ,prefix #x10 :vvvv src2))
+                        (emit-avx512-inst segment src2 dst ,prefix #x10 :vvvv src :w ,w))
 
                        ((or (zmm-register-p dst)
                             (zmm-register-p src))
-                        (emit-avx512-inst segment src dst ,prefix #x10))
+                        (emit-avx512-inst segment src dst ,prefix #x10 :vvvv dst :w ,w))
 
                        ((and src src2 dst (xmm-register-p dst))
-                        (emit-avx2-inst segment src dst ,prefix #x10 :vvvv src2 :l 0))
+                        (emit-avx2-inst segment src2 dst ,prefix #x10 :vvvv src :l 0 :w ,w))
 
                        ((xmm-register-p dst)
-                        (emit-avx2-inst segment src dst ,prefix #x10 :l 0))
+                        (if (register-p src)
+                            (emit-avx2-inst segment src dst ,prefix #x10 :vvvv dst :l 0 :w ,w)
+                            (emit-avx2-inst segment src dst ,prefix #x10 :l 0 :w ,w)))
 
                        (t
                         (aver (xmm-register-p src))
-                        (emit-avx2-inst segment dst src ,prefix #x11 :l 0)))))))
-  (def vmovsd #xf2)
-  (def vmovss #xf3))
+                        (emit-avx2-inst segment dst src ,prefix #x11 :l 0 :w ,w)))))))
+  (def vmovsd #xf2 1)
+  (def vmovss #xf3 0))
 
 ;;; Ternary logic
 (macrolet ((def (name w)
