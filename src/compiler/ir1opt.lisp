a95d3f5c508a39e76142d096a44040f848dd22f0
Stas Boukarev <stassats@gmail.com>
2026-09-08 20:12:52 +0300

    Fix block-compilation of the build

--- src/compiler/ir1opt.lisp
+++ src/compiler/ir1opt.lisp
@@ -2350,7 +2350,7 @@
                       converged-type-of-combination
                       maybe-infer-iteration-var-type
                       ir1-optimize-mv-combination
-                      substitute-single-use-lvar))
+                      substitute-single-use-lvar iteration-step-values))
 
 ;;; Propagate TYPE to LEAF and its REFS, marking things changed.
 ;;;
