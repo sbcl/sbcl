8ad2741b5e3159b3910e62fc020d9e02352d3a18
Douglas Katzman <dougk@google.com>
2026-09-14 11:33:27 -0400

    Probably de-flake the clock-gettime test
    Even after pthread_join returns there is a race with the kernel thread ceasing
    to exist, and for a correct test result, it needs not to. Rather than putting
    in a sleep delay, just pre-check whether to attempt the test.

--- tests/clockget.pure.lisp
+++ tests/clockget.pure.lisp
@@ -1,3 +1,14 @@
+#+linux
+(defun os-tid-existsp (tid)
+  ;; "Note: glibc provides no wrapper for tkill(), necessitating the use of syscall(2)."
+  ;; but also: "[[deprecated]] int syscall(SYS_tkill, pid_t tid, int sig);"
+  ;; so it might have a stub.
+  (if (sb-sys:find-foreign-symbol-address "tgkill")
+      ;; the usual trick to find out whether a pid/tid exists- deliver signal 0
+      (zerop (alien-funcall (extern-alien "tgkill" (function int int int int))
+                            (sb-unix:unix-getpid) tid 0))
+      :maybe))
+
 #+linux ; the CLOCK- symbols may not exist
 (with-test (:name :clock-gettime)
   (sb-unix:clock-gettime sb-unix:clock-monotonic-raw)
@@ -18,17 +29,30 @@
               (t2 (time-in-microsec custom-clockid))
               (t3 (time-in-microsec sb-unix:clock-thread-cputime-id)))
           (assert (<= t1 t2 t3)))))
-    (let* ((s (sb-thread:make-semaphore))
+    (let* ((alive (sb-thread:make-semaphore))
+           (stop (sb-thread:make-semaphore))
            (thread (sb-thread:make-thread
-                    (lambda () (sb-thread:wait-on-semaphore s) :hooray)))
+                    (lambda ()
+                      (sb-thread:signal-semaphore alive)
+                      (sb-thread:wait-on-semaphore stop)
+                      :hooray)))
            (result (alien-funcall pthread-getcpuclockid
                                   (sb-thread::thread-os-thread thread)
-                                  (addr custom-clockid))))
+                                  (addr custom-clockid)))
+           (tid))
       (assert (zerop result)) ; got a clockid
-      (sb-thread:signal-semaphore s) ; now we can let the thread go away
+      (sb-thread:wait-on-semaphore alive)
+      (setq tid (sb-thread::thread-os-tid thread))
+      (assert (not (zerop tid)))
+      (assert (os-tid-existsp tid))
+      (sb-thread:signal-semaphore stop) ; now we can let the thread go away
       (sb-thread:join-thread thread)
       ;; The Lisp thread is done, but the OS thread and pthread are semi-alive.
       (sb-thread::%dispose-thread-structs) ; Force resource freeing (pthread_join)
-      (multiple-value-bind (sec nsec) (sb-unix:clock-gettime custom-clockid)
-        ;; and now we should safely get NIL and NIL for the sec + nsec
-        (assert (and (null sec) (null nsec)))))))
+      ;; can't do the negative test unless the TID is actually not valid
+      (if (os-tid-existsp tid)
+          (warn "Can't test for clock-gettime returnin NIL")
+          (multiple-value-bind (sec nsec) (sb-unix:clock-gettime custom-clockid)
+            (format t "::: (ran nonexistent TID check)~%")
+            ;; and now we should safely get NIL and NIL for the sec + nsec
+            (assert (and (null sec) (null nsec))))))))
